25a24d75360f76f2348cba44f9f965974d58e91d
Patrick Lacson <placson@users.sourceforge.net>
2001-11-19 18:17:24 +0000

    Modified to work with CYGWIN users as well as regular Unix users

--- squirrel-sql/app/cmd/squirrel-sql.sh
+++ squirrel-sql/app/cmd/squirrel-sql.sh
@@ -1,2 +1,8 @@
 #! /bin/sh
-$JAVA_HOME/bin/java -cp lib/squirrel-sql.jar:lib\fw.jar:lib/nanoxml-2.1.jar:$CLASSPATH net.sourceforge.squirrel_sql.client.Main
+# $Id: squirrel-sql.sh,v 1.2 2001-11-19 18:17:24 placson Exp $
+TMP_CP=lib/squirrel-sql.jar:lib/fw.jar:lib/nanoxml-2.1.jar:$CLASSPATH 
+if [ `uname -s` = "CYGWIN_NT-5.0" ]; then
+    $JAVA_HOME/bin/java -cp `cygpath -w -p $TMP_CP` net.sourceforge.squirrel_sql.client.Main
+else
+  $JAVA_HOME/bin/java -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main
+fi
