169b573c445bc14230ebf15a89c36028a6152dda
Patrick Lacson <placson@users.sourceforge.net>
2001-11-20 09:34:42 +0000

    Working!

--- squirrel/src/resources/izpack/izpack.sh
+++ squirrel/src/resources/izpack/izpack.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
 # Installation script for izPack
-# Version: $Id: izpack.sh,v 1.1 2001-11-20 08:35:28 placson Exp $
+# Version: $Id: izpack.sh,v 1.2 2001-11-20 09:34:42 placson Exp $
 # --*-shellscript-*--
 
-cd e:/downloads/izpack/installed/bin
-
 LCL_SQL=d:/cygwin/home/Administrator/src/squirrel
-LCL_DIST=d:/cygwin/home/Administrator/src/squirrel
+LCL_DIST=$LCL_SQL
+COMPILER_LIB=$LCL_SQL/thirdparty/izpack/lib/compiler.jar
 
-$JAVA_HOME/bin/java -jar "../lib/compiler.jar" $LCL_SQL/src/resources/izpack/izpack-complete.xml -b $LCL_SQL -k standard-kunststoff -o $LCL_DIST/squirrel-sql-dist.jar
+rm -fr $LCL_DIST/output
+rm -fr $LCL_DIST/output/dist
+rm -fr $LCL_DIST/output/dist/lib
+rm -fr $LCL_DIST/output/dist/doc
+rm -fr $LCL_DIST/output/dist/src
 
-#d:\apps\jdk13\bin\java -jar "../lib/compiler.jar" %LCL_SQL%\build\izpack-basic.xml -b %LCL_SQL% -k standard-kunststoff -o %LCL_DIST%\squirrel-sql-1.1alpha1\squirrel-sql-1.1alpha1-install-basic.jar
+mkdir $LCL_DIST/output
+mkdir $LCL_DIST/output/dist
+mkdir $LCL_DIST/output/dist/lib
+mkdir $LCL_DIST/output/dist/doc
+mkdir $LCL_DIST/output/dist/src
+
+$JAVA_HOME/bin/java -jar $COMPILER_LIB $LCL_SQL/src/resources/izpack/izpack-complete-buildmagic.xml -b $LCL_SQL -k standard-kunststoff -o $LCL_DIST/squirrel-sql-dist.jar
+
 
