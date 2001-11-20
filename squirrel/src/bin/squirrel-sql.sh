5d5771eb66f0e3ac7792a4b2fb597f67f530c5c5
Patrick Lacson <placson@users.sourceforge.net>
2001-11-20 02:11:10 +0000

    Initial checkin of squirrel sources

---
+++ squirrel/src/bin/squirrel-sql.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+# $Id: squirrel-sql.sh,v 1.1 2001-11-20 02:11:09 placson Exp $
+TMP_CP=lib/squirrel-sql.jar:lib/fw.jar:lib/nanoxml-2.1.jar:$CLASSPATH 
+if [ `uname -s` = "CYGWIN_NT-5.0" ]; then
+    $JAVA_HOME/bin/java -cp `cygpath -w -p $TMP_CP` net.sourceforge.squirrel_sql.client.Main
+else
+  $JAVA_HOME/bin/java -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main
+fi
