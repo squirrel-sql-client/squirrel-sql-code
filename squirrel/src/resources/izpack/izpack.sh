e38b34b54626a9aa61b7cf85250d033fc9004f64
Patrick Lacson <placson@users.sourceforge.net>
2001-11-20 09:39:37 +0000

    really working

--- squirrel/src/resources/izpack/izpack.sh
+++ squirrel/src/resources/izpack/izpack.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # Installation script for izPack
-# Version: $Id: izpack.sh,v 1.2 2001-11-20 09:34:42 placson Exp $
+# Version: $Id: izpack.sh,v 1.3 2001-11-20 09:39:37 placson Exp $
 # --*-shellscript-*--
 
-LCL_SQL=d:/cygwin/home/Administrator/src/squirrel
+
+LCL_SQL=../../..
 LCL_DIST=$LCL_SQL
+IZPACK_BIN=$LCL_SQL/thirdparty/izpack/bin
 COMPILER_LIB=$LCL_SQL/thirdparty/izpack/lib/compiler.jar
 
 rm -fr $LCL_DIST/output
@@ -19,6 +21,8 @@ mkdir $LCL_DIST/output/dist
 mkdir $LCL_DIST/output/dist/lib
 mkdir $LCL_DIST/output/dist/doc
 mkdir $LCL_DIST/output/dist/src
+
+cd $IZPACK_BIN
 
 $JAVA_HOME/bin/java -jar $COMPILER_LIB $LCL_SQL/src/resources/izpack/izpack-complete-buildmagic.xml -b $LCL_SQL -k standard-kunststoff -o $LCL_DIST/squirrel-sql-dist.jar
 
