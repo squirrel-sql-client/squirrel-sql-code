fae11f965935b7589b132bafa235eed3d7227166
Colin Bell <colbell@users.sourceforge.net>
2001-12-05 02:08:57 +0000

    Treat toolwindows the same as other internal frames.
    When adding an internal frame allow specifiying whether menu item should be added to window menu. Allow caller to specify the action to be added to the Window menu.

--- squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/gui/BaseMDIParentFrame.java
+++ squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/gui/BaseMDIParentFrame.java
@@ -20,6 +20,8 @@ package net.sourceforge.squirrel_sql.fw.gui;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.util.HashMap;
+
+import javax.swing.Action;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
@@ -32,97 +34,143 @@ import net.sourceforge.squirrel_sql.fw.gui.action.SelectInternalFrameAction;
 
 public class BaseMDIParentFrame extends JFrame {
 
-    private JDesktopPane _desktop;
-    private IInternalFramePositioner _internalFramePositioner;
-    private HashMap _children = new HashMap();
+	private JDesktopPane _desktop;
+	private IInternalFramePositioner _internalFramePositioner;
+	private HashMap _children = new HashMap();
 
-    private MyInternalFrameListener _childListener = new MyInternalFrameListener();
+	private MyInternalFrameListener _childListener = new MyInternalFrameListener();
 
+	protected BaseMDIParentFrame(JDesktopPane desktop) {
+		this(null, desktop);
+	}
 
-    protected BaseMDIParentFrame(JDesktopPane desktop) {
-        this(null, desktop);
-    }
+	protected BaseMDIParentFrame(String title, JDesktopPane desktop) {
+		super(title);
+		if (desktop == null) {
+			throw new IllegalArgumentException("null JDesktopPane passed");
+		}
+		_desktop = desktop;
+		//setContentPane(desktop);
+	}
 
-    protected BaseMDIParentFrame(String title, JDesktopPane desktop) {
-        super(title);
-        if (desktop == null) {
-            throw new IllegalArgumentException("null JDesktopPane passed");
-        }
-        _desktop = desktop;
-        //setContentPane(desktop);
-    }
+	/**
+	 * Add the passed internal frame to this MDI frame.
+	 * Calls <TT>addInternalFrame(child, createMenuItem, null)</TT>.
+	 * 
+	 * @param	child			The internal frame to be added.
+	 * @param	createMenuItem	If <TT>true</TT> add an item to the MDI
+	 * 							Window menu to select the passed internal frame.
+	 * 
+	 * @throws	IllegalArgumentException if null <TT>JInternalFrame</TT> passed.
+	 */
+	public void addInternalFrame(JInternalFrame child, boolean createMenuItem)
+			throws IllegalArgumentException {
+		addInternalFrame(child, createMenuItem, null);
+	}
 
-    public void addInternalFrame(JInternalFrame child) {
-        child.setTitle(createTitleForChild(child));
-        _desktop.add(child);
-        if (!GUIUtils.isToolWindow(child)) {
-            positionNewInternalFrame(child);
-            final JMenu menu = getWindowsMenu();
-            JMenuItem menuItem = null;
-            if (menu != null) {
-                menuItem = menu.add(new SelectInternalFrameAction(child));
-            }
-            _children.put(child.getTitle(), new ChildInfo(child, menuItem));
-            child.addInternalFrameListener(_childListener);
-        }
-    }
+	/**
+	 * Add the passed internal frame to this MDI frame.
+	 * 
+	 * @param	child			The internal frame to be added.
+	 * @param	createMenuItem	If <TT>true</TT> add an item to the MDI
+	 * 							Window menu to select the passed internal frame.
+	 * @param	action			Action to be added to this MDI frames Window menu to
+	 *							select this internal frame. If <TT>null</TT> and
+	 *							<TT>createMenuItem</TT> is <TT>true</TT> then an
+	 * 							instance of <TT>SelectInternalFrameAction</TT>
+	 * 							will be used.
+	 * 
+	 * @throws	IllegalArgumentException if null <TT>JInternalFrame</TT> passed.
+	 */
+	public void addInternalFrame(JInternalFrame child, boolean createMenuItem, Action action)
+			throws IllegalArgumentException {
+		if (child == null) {
+			throw new IllegalArgumentException("Null JInternalFrame added");
+		}
+		child.setTitle(createTitleForChild(child));
+		_desktop.add(child);
+		if (!GUIUtils.isToolWindow(child)) {
+//			positionNewInternalFrame(child);
+//			final JMenu menu = getWindowsMenu();
+//			JMenuItem menuItem = null;
+//			if (menu != null) {
+//				menuItem = menu.add(new SelectInternalFrameAction(child));
+//			}
+//			_children.put(child.getTitle(), new ChildInfo(child, menuItem));
+//			child.addInternalFrameListener(_childListener);
+		}
+		positionNewInternalFrame(child);
 
-    public void internalFrameClosed(JInternalFrame child) {
-        if (!GUIUtils.isToolWindow(child)) {
-            child.removeInternalFrameListener(_childListener);
-            ChildInfo ci = (ChildInfo)_children.remove(child.getTitle());
-            if (ci != null && ci._menuItem != null) {
-                final JMenu menu = getWindowsMenu();
-                if (menu != null) {
-                    menu.remove(ci._menuItem);
-                }
-            }
-        }
-    }
+		JMenuItem menuItem = null;
+		if (createMenuItem) {
+			final JMenu menu = getWindowsMenu();
+			if (menu != null) {
+				if (action == null) {
+					action = new SelectInternalFrameAction(child);
+				}
+				menuItem = menu.add(action);
+			}
+		}
+		_children.put(child.getTitle(), new ChildInfo(child, menuItem));
+		child.addInternalFrameListener(_childListener);
+	}
 
-    public JDesktopPane getDesktopPane() {
-        return _desktop;
-    }
+	public void internalFrameClosed(JInternalFrame child) {
+//		if (!GUIUtils.isToolWindow(child)) {
+			child.removeInternalFrameListener(_childListener);
+			ChildInfo ci = (ChildInfo)_children.remove(child.getTitle());
+			if (ci != null && ci._menuItem != null) {
+				final JMenu menu = getWindowsMenu();
+				if (menu != null) {
+					menu.remove(ci._menuItem);
+				}
+			}
+//		}
+	}
 
-    public JMenu getWindowsMenu() {
-        return null;
-    }
+	public JDesktopPane getDesktopPane() {
+		return _desktop;
+	}
 
-    protected void positionNewInternalFrame(JInternalFrame child) {
-        getInternalFramePositioner().positionInternalFrame(child);
-    }
+	public JMenu getWindowsMenu() {
+		return null;
+	}
 
-    protected IInternalFramePositioner getInternalFramePositioner() {
-        if (_internalFramePositioner == null) {
-             _internalFramePositioner = new CascadeInternalFramePositioner();
-        }
-        return _internalFramePositioner;
-    }
+	protected void positionNewInternalFrame(JInternalFrame child) {
+		getInternalFramePositioner().positionInternalFrame(child);
+	}
 
-    private String createTitleForChild(JInternalFrame child) {
-        String title = child.getTitle();
-        String origTitle = title;
-        int index = 0;
-        while (_children.get(title) != null) {
-            title = origTitle + "(" + ++index + ")";
-        }
-        return title;
-    }
+	protected IInternalFramePositioner getInternalFramePositioner() {
+		if (_internalFramePositioner == null) {
+			 _internalFramePositioner = new CascadeInternalFramePositioner();
+		}
+		return _internalFramePositioner;
+	}
 
-    private class MyInternalFrameListener extends InternalFrameAdapter {
-        public void internalFrameClosed(InternalFrameEvent evt) {
-            BaseMDIParentFrame.this.internalFrameClosed((JInternalFrame)evt.getSource());
-        }
-    }
+	private String createTitleForChild(JInternalFrame child) {
+		String title = child.getTitle();
+		String origTitle = title;
+		int index = 0;
+		while (_children.get(title) != null) {
+			title = origTitle + "(" + ++index + ")";
+		}
+		return title;
+	}
 
-    private static class ChildInfo {
-        private JInternalFrame _child;
-        private JMenuItem _menuItem;
+	private class MyInternalFrameListener extends InternalFrameAdapter {
+		public void internalFrameClosed(InternalFrameEvent evt) {
+			BaseMDIParentFrame.this.internalFrameClosed((JInternalFrame)evt.getSource());
+		}
+	}
 
-        ChildInfo(JInternalFrame child, JMenuItem menuItem) {
-            super();
-            _child = child;
-            _menuItem = menuItem;
-        }
-    }
+	private static class ChildInfo {
+		private JInternalFrame _child;
+		private JMenuItem _menuItem;
+
+		ChildInfo(JInternalFrame child, JMenuItem menuItem) {
+			super();
+			_child = child;
+			_menuItem = menuItem;
+		}
+	}
 }
