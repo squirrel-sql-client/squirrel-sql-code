f5908389d2dfab5b91ca5effdca33266a98d3e64
Colin Bell <colbell@users.sourceforge.net>
2001-12-06 03:32:08 +0000

    Cleaned up

--- squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/sql/DatabaseObjectInfo.java
+++ squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/sql/DatabaseObjectInfo.java
@@ -19,147 +19,112 @@ package net.sourceforge.squirrel_sql.fw.sql;
  */
 
 public class DatabaseObjectInfo implements IDatabaseObjectInfo {
-    /** Catalog name. Can be <CODE>null</CODE> */
-    private final String _catalog;
+	/** Catalog name. Can be <CODE>null</CODE> */
+	private final String _catalog;
 
-    /** Schema name. Can be <CODE>null</CODE> */
-    private final String _schema;
+	/** Schema name. Can be <CODE>null</CODE> */
+	private final String _schema;
 
-    /** Simple object name. */
-    private final String _simpleName;
+	/** Simple object name. */
+	private final String _simpleName;
 
-    private String _qualifiedName;
+	/** Fully qualified name for this object. */
+	private final String _qualifiedName;
 
-    /** Catalogue separator. */
-    //private static String s_catSep;
+	public DatabaseObjectInfo(String catalog, String schema,
+								String simpleName, SQLConnection conn) {
+		super();
+		if (simpleName == null) {
+			throw new IllegalArgumentException("Null simpleName passed");
+		}
+		if (conn == null) {
+			throw new IllegalArgumentException("Null SQLConnection passed");
+		}
 
-    /** Identifier quote string. */
-    //private static String s_identifierQuoteString;
+		_catalog = catalog;
+		_schema = schema;
+		_simpleName = simpleName;
+		_qualifiedName = generateQualifiedName(conn);
+	}
 
-    //private static boolean s_staticInitialized = false;
+	public String getCatalogName() {
+		return _catalog;
+	}
 
-    public DatabaseObjectInfo(String catalog, String schema,
-                                String simpleName, SQLConnection conn) {
-        super();
-        if (simpleName == null) {
-            throw new IllegalArgumentException("Null simpleName passed");
-        }
-        if (conn == null) {
-            throw new IllegalArgumentException("Null SQLConnection passed");
-        }
+	public String getSchemaName() {
+		return _schema;
+	}
 
-//      staticInitialization(conn);
+	public String getSimpleName() {
+		return _simpleName;
+	}
 
-        _catalog = catalog;
-        _schema = schema;
-        _simpleName = simpleName;
-        _qualifiedName = generateQualifiedName(conn);
-    }
 
-    public String getCatalogName() {
-        return _catalog;
-    }
+	public String getQualifiedName() {
+		return _qualifiedName;
+	}
 
-    public String getSchemaName() {
-        return _schema;
-    }
+	protected String generateQualifiedName(SQLConnection conn) {
+		String catSep = null;
+		String identifierQuoteString = null;
+		boolean supportsSchemasInDataManipulation = false;
+		boolean supportsCatalogsInDataManipulation = false;
+		try {
+			supportsSchemasInDataManipulation = conn.supportsSchemasInDataManipulation();
+		} catch (BaseSQLException ex) {
+		}
+		try {
+			supportsCatalogsInDataManipulation = conn.supportsCatalogsInDataManipulation();
+		} catch (BaseSQLException ex) {
+		}
+		try {
+			if (supportsCatalogsInDataManipulation) {
+				catSep = conn.getCatalogSeparator();
+			}
+		} catch (BaseSQLException ex) {
+		}
+		try {
+			identifierQuoteString = conn.getIdentifierQuoteString();
+			if (identifierQuoteString != null && identifierQuoteString.equals(" ")) {
+				identifierQuoteString = null;
+			}
+		} catch (BaseSQLException ex) {
+		}
 
-    public String getSimpleName() {
-        return _simpleName;
-    }
+		StringBuffer buf = new StringBuffer();
+		if (catSep != null && catSep.length() > 0 && _catalog != null
+				&& _catalog.length() > 0) {
+			if (identifierQuoteString != null) {
+				buf.append(identifierQuoteString);
+			}
+			buf.append(_catalog);
+			if (identifierQuoteString != null) {
+				buf.append(identifierQuoteString);
+			}
+			buf.append(catSep);
+		}
 
-    public String getQualifiedName() {
-        return _qualifiedName;
-    }
+		if (supportsSchemasInDataManipulation && _schema != null
+				&& _schema.length() > 0) {
+			if (identifierQuoteString != null) {
+				buf.append(identifierQuoteString);
+			}
+			buf.append(_schema);
+			if (identifierQuoteString != null) {
+				buf.append(identifierQuoteString);
+			}
+			buf.append(".");
+		}
 
-    protected String generateQualifiedName(SQLConnection conn) {
-        String catSep = null;
-        String identifierQuoteString = null;
-        boolean supportsSchemasInDataManipulation = false;
-        boolean supportsCatalogsInDataManipulation = false;
-        try {
-            supportsSchemasInDataManipulation = conn.supportsSchemasInDataManipulation();
-        } catch (BaseSQLException ex) {
-        }
-        try {
-            supportsCatalogsInDataManipulation = conn.supportsCatalogsInDataManipulation();
-        } catch (BaseSQLException ex) {
-        }
-        try {
-            if (supportsCatalogsInDataManipulation) {
-                catSep = conn.getCatalogSeparator();
-            }
-        } catch (BaseSQLException ex) {
-        }
-        try {
-            identifierQuoteString = conn.getIdentifierQuoteString();
-            if (identifierQuoteString != null && identifierQuoteString.equals(" ")) {
-                identifierQuoteString = null;
-            }
-        } catch (BaseSQLException ex) {
-        }
-
-        StringBuffer buf = new StringBuffer();
-        if (catSep != null && catSep.length() > 0 && _catalog != null
-                && _catalog.length() > 0) {
-            if (identifierQuoteString != null) {
-                buf.append(identifierQuoteString);
-            }
-            buf.append(_catalog);
-            if (identifierQuoteString != null) {
-                buf.append(identifierQuoteString);
-            }
-            buf.append(catSep);
-        }
-
-        //try {
-            if (supportsSchemasInDataManipulation && _schema != null
-                    && _schema.length() > 0) {
-                if (identifierQuoteString != null) {
-                    buf.append(identifierQuoteString);
-                }
-                buf.append(_schema);
-                if (identifierQuoteString != null) {
-                    buf.append(identifierQuoteString);
-                }
-                buf.append("."); //?? Is it always a full stop?
-            }
-        //} catch (BaseSQLException ex) {
-            //com.bigfoot.colbell.fw.util.Debug.println(ex.toString());
-        //}
-
-        if (identifierQuoteString != null /*&& !identifierQuoteString.equals(" ")*/) {
-            buf.append(identifierQuoteString);
-        }
-        buf.append(_simpleName);
-        if (identifierQuoteString != null) {
-            buf.append(identifierQuoteString);
-        }
-        return buf.toString();
-    }
-/*  private static void staticInitialization(SQLConnection conn) {
-        synchronized (DatabaseObjectInfo.class) {
-            if (!s_staticInitialized) {
-                try {
-                    if (conn.supportsCatalogsInDataManipulation()) {
-                        s_catSep = conn.getCatalogSeparator();
-                    }
-                } catch (BaseSQLException ex) {
-                }
-                try {
-                    s_identifierQuoteString = conn.getIdentifierQuoteString();
-
-                } catch (BaseSQLException ex) {
-                }
-                try {
-                    s_supportsSchemasInDataManipulation = conn.supportsSchemasInDataManipulation();
-
-                } catch (BaseSQLException ex) {
-                }
-                s_staticInitialized = true;
-            }
-        }
-    }
-*/
+		if (identifierQuoteString != null) {
+			buf.append(identifierQuoteString);
+		}
+		buf.append(_simpleName);
+		if (identifierQuoteString != null) {
+			buf.append(identifierQuoteString);
+		}
+		return buf.toString();
+	}
 }
+
 
