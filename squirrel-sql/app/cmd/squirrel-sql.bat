4ef0ed3adebacf05877f4d4215e2d2b1f90d60c7
Colin Bell <colbell@users.sourceforge.net>
2001-12-23 21:45:56 +0000

    Allow for log4j in the classpath

--- squirrel-sql/app/cmd/squirrel-sql.bat
+++ squirrel-sql/app/cmd/squirrel-sql.bat
@@ -1,5 +1,5 @@
 @rem Run with a command window.
-"%JAVA_HOME%\bin\java" -cp lib\squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;%CLASSPATH% net.sourceforge.squirrel_sql.client.Main
+"%JAVA_HOME%\bin\java" -cp lib\squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;lib\log4j.jar;%CLASSPATH% net.sourceforge.squirrel_sql.client.Main
 
 @rem Run with no command window. However this may not work will all versions of Windows.
 @rem start "%JAVA_HOME%\bin\javaw" -cp lib\squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;%CLASSPATH% net.sourceforge.squirrel_sql.client.Main
--- squirrel-sql/app/cmd/squirrel-sql.sh
+++ squirrel-sql/app/cmd/squirrel-sql.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-# $Id: squirrel-sql.sh,v 1.2 2001-11-19 18:17:24 placson Exp $
-TMP_CP=lib/squirrel-sql.jar:lib/fw.jar:lib/nanoxml-2.1.jar:$CLASSPATH 
+# $Id: squirrel-sql.sh,v 1.3 2001-12-23 21:45:56 colbell Exp $
+TMP_CP=lib/squirrel-sql.jar:lib/fw.jar:lib/nanoxml-2.1.jar:lib/log4j.jar:$CLASSPATH 
 if [ `uname -s` = "CYGWIN_NT-5.0" ]; then
     $JAVA_HOME/bin/java -cp `cygpath -w -p $TMP_CP` net.sourceforge.squirrel_sql.client.Main
 else
