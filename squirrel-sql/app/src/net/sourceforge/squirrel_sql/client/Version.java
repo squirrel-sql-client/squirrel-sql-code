41f7c6ebeca702d1d0d3337579bfcd9536d0fe83
Colin Bell <colbell@users.sourceforge.net>
2002-01-17 10:47:37 +0000

    Version number update

--- squirrel-sql/app/src/net/sourceforge/squirrel_sql/client/Version.java
+++ squirrel-sql/app/src/net/sourceforge/squirrel_sql/client/Version.java
@@ -1,6 +1,6 @@
 package net.sourceforge.squirrel_sql.client;
 /*
- * Copyright (C) 2001 Colin Bell
+ * Copyright (C) 2001-2002 Colin Bell
  * colbell@users.sourceforge.net
  *
  * This program is free software; you can redistribute it and/or
@@ -24,34 +24,34 @@ package net.sourceforge.squirrel_sql.client;
  * @author  <A HREF="mailto:colbell@users.sourceforge.net">Colin Bell</A>
  */
 public class Version {
-    private static final String APP_NAME = "SQuirreL SQL Client";
-    private static final int MAJOR_VERSION = 1;
-    private static final int MINOR_VERSION = 1;
-    private static final String TYPE = "alpha";
+	private static final String APP_NAME = "SQuirreL SQL Client";
+	private static final int MAJOR_VERSION = 1;
+	private static final int MINOR_VERSION = 1;
+	private static final String TYPE = "alpha";
 //  private static final String TYPE = "beta";
 //  private static final String TYPE = "final";
-    //private static final int RELEASE = 7;
-    private static final int RELEASE = 4;
+	//private static final int RELEASE = 7;
+	private static final int RELEASE = 5;
 
-    private static final String COPYRIGHT = "Copyright (c) 2001 - 2002 Colin Bell";
+	private static final String COPYRIGHT = "Copyright (c) 2001 - 2002 Colin Bell";
 
-    private static final String WEB_SITE = "http://squirrel-sql.sourceforge.net/";
+	private static final String WEB_SITE = "http://squirrel-sql.sourceforge.net/";
 
-    public static String getVersion() {
-        StringBuffer buf = new StringBuffer();
-        buf.append(APP_NAME).append(" Version ").append(MAJOR_VERSION)
-            .append('.').append(MINOR_VERSION).append(TYPE);
-        if (RELEASE != 0) {
-            buf.append(RELEASE);
-        }
-        return buf.toString();
-    }
+	public static String getVersion() {
+		StringBuffer buf = new StringBuffer();
+		buf.append(APP_NAME).append(" Version ").append(MAJOR_VERSION)
+			.append('.').append(MINOR_VERSION).append(TYPE);
+		if (RELEASE != 0) {
+			buf.append(RELEASE);
+		}
+		return buf.toString();
+	}
 
-    public static String getCopyrightStatement() {
-        return COPYRIGHT;
-    }
+	public static String getCopyrightStatement() {
+		return COPYRIGHT;
+	}
 
-    public static String getWebSite() {
-        return WEB_SITE;
-    }
+	public static String getWebSite() {
+		return WEB_SITE;
+	}
 }
