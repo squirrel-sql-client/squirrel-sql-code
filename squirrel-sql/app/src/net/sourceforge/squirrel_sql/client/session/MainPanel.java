ecd7ad4f4a96e13cc2c2025c9aafc86c211cf968
Colin Bell <colbell@users.sourceforge.net>
2002-01-26 11:44:33 +0000

    no message

---
+++ squirrel-sql/app/src/net/sourceforge/squirrel_sql/client/session/MainPanel.java
@@ -0,0 +1,190 @@
+package net.sourceforge.squirrel_sql.client.session;
+/*
+ * Copyright (C) 2001 Colin Bell
+ * colbell@users.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import net.sourceforge.squirrel_sql.client.action.ActionCollection;
+import net.sourceforge.squirrel_sql.client.session.action.CommitAction;
+import net.sourceforge.squirrel_sql.client.session.action.ExecuteSqlAction;
+import net.sourceforge.squirrel_sql.client.session.action.RefreshTreeAction;
+import net.sourceforge.squirrel_sql.client.session.action.RollbackAction;
+import net.sourceforge.squirrel_sql.client.session.action.ShowNativeSQLAction;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.IMainPanelTab;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.ObjectsPanel;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.ObjectsTab;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.SQLPanel;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.SQLTab;
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
+
+public class MainPanel extends JTabbedPane {
+	/**
+	 * IDs of tabs.
+	 */
+	public interface ITabIndexes {
+		int OBJECT_TREE_TAB = 0;
+		int SQL_TAB = 1;
+	}
+
+	/** Logger for this class. */
+	private static ILogger s_log = LoggerController.createLogger(MainPanel.class);
+
+	/** Current session. */
+	private ISession _session;
+	
+	/**
+	 * Collection of <TT>IMainPanelTab</TT> objects displayed in
+	 * this tabbed panel.
+	 */
+	private List _tabs = new ArrayList();
+
+	/** Listens to changes in session properties. */
+	private MyPropertiesListener _propsListener;
+
+	private ObjectsTab _objectsTab;
+	private SQLTab _sqlTab;
+
+	/**
+	 * ctor specifying the current session.
+	 * 
+	 * @param	session		Current session.
+	 * 
+	 * @throws	IllegalArgumentException	If a <TT>null</TT>
+											<TT>ISession</TT> passed.
+	 */
+	public MainPanel(ISession session) {
+		super();
+		if (session == null) {
+			throw new IllegalArgumentException("ISession == null");
+		}
+
+		_session = session;
+
+		createUserInterface();
+		propertiesHaveChanged(null);
+		// Refresh the currently selected tab.
+		((IMainPanelTab)_tabs.get(getSelectedIndex())).select();
+	}
+
+	/**
+	 * Add a tab to this panel. If a tab with this title already exists it is
+	 * removed from the tabbed pane and the passed tab inserted in its
+	 * place. New tabs are inserted at the end.
+	 *
+	 * @param	tab	 The tab to be added.
+	 *
+	 * @throws	IllegalArgumentException
+	 *			Thrown if a <TT>null</TT> <TT>ITablePanelTab</TT> passed.
+	 */
+	public void addMainPanelTab(IMainPanelTab tab) {
+		if (tab == null) {
+			throw new IllegalArgumentException("Null IMainPanelTab passed");
+		}
+		tab.setSession(_session);
+		final String title = tab.getTitle();
+		int idx = indexOfTab(title);
+		if (idx != -1) {
+			removeTabAt(idx);
+			_tabs.set(idx, tab);
+		} else {
+			idx = getTabCount();
+			_tabs.add(tab);
+		}
+		insertTab(title, null, tab.getComponent(), tab.getHint(), idx);
+	}
+
+	void updateState() {
+		ActionCollection actions = _session.getApplication().getActionCollection();
+		if (getSelectedIndex() == ITabIndexes.SQL_TAB) {
+			actions.get(ExecuteSqlAction.class).setEnabled(true);
+			actions.get(ShowNativeSQLAction.class).setEnabled(true);
+			boolean isAutoCommit = _session.getProperties().getAutoCommit();
+			actions.get(CommitAction.class).setEnabled(!isAutoCommit);
+			actions.get(RollbackAction.class).setEnabled(!isAutoCommit);
+			actions.get(RefreshTreeAction.class).setEnabled(false);
+		} else {
+			actions.get(ExecuteSqlAction.class).setEnabled(false);
+			actions.get(ShowNativeSQLAction.class).setEnabled(false);
+			actions.get(CommitAction.class).setEnabled(false);
+			actions.get(RollbackAction.class).setEnabled(false);
+			actions.get(RefreshTreeAction.class).setEnabled(true);
+		}
+	}
+
+	void sessionClosing(ISession session) {
+		for (Iterator it = _tabs.iterator(); it.hasNext();) {
+			((IMainPanelTab)it.next()).sessionClosing(session);
+		}
+	}
+
+	private void propertiesHaveChanged(String propName) {
+//		if (propName == null ||
+//				propName.equals(SessionProperties.IPropertyNames.META_DATA_OUTPUT_CLASS_NAME)) {
+//			addDatabasePanelTab(new MetaDataTab());
+//		}
+//		if (propName == null ||
+//				propName.equals(SessionProperties.IPropertyNames.DATA_TYPES_OUTPUT_CLASS_NAME)) {
+//			addDatabasePanelTab(new DataTypesTab());
+//		}
+	}
+
+	private void createUserInterface() {
+		_objectsTab = new ObjectsTab(_session);
+		_sqlTab = new SQLTab(_session);
+
+		addMainPanelTab(_objectsTab);
+		addMainPanelTab(_sqlTab);
+
+		_propsListener = new MyPropertiesListener();
+		_session.getProperties().addPropertyChangeListener(_propsListener);
+
+		addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent evt) {
+				updateState();
+				int idx = getSelectedIndex();
+				if (idx != -1) {
+					((IMainPanelTab)_tabs.get(getSelectedIndex())).select();
+				}
+			}
+		});
+	}
+
+	private class MyPropertiesListener implements PropertyChangeListener {
+		public void propertyChange(PropertyChangeEvent evt) {
+			MainPanel.this.propertiesHaveChanged(evt.getPropertyName());
+		}
+	}
+
+	public ObjectsPanel getObjectsPanel() {
+		return _objectsTab.getObjectsPanel();
+	}
+
+	public SQLPanel getSQLPanel() {
+		return _sqlTab.getSQLPanel();
+	}
+}
+
---
+++ squirrel-sql/app/src/net/sourceforge/squirrel_sql/client/session/mainpanel/ResultFrame.java
@@ -0,0 +1,106 @@
+package net.sourceforge.squirrel_sql.client.session.mainpanel;
+/*
+ * Copyright (C) 2001-2002 Johan Compagner
+ * jcompagner@j-com.nl
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
+
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.SessionSheet;
+import net.sourceforge.squirrel_sql.client.session.action.ReturnResultTabAction;
+
+/**
+ * Torn off frame that contains SQL results.
+ *
+ * @author  <A HREF="mailto:jcompagner@j-com.nl">Johan Compagner</A>
+ * Copyright (C) 2001-2002
+ * 
+ */
+public class ResultFrame extends JInternalFrame {
+	/** Logger for this class. */
+	private static ILogger s_log = LoggerController.createLogger(ResultFrame.class);
+
+	/** Current session. */
+	private ISession _session;
+
+	/** SQL Results. */
+	private ResultTab _tab;
+
+	/**
+	 * Ctor.
+	 * 
+	 * @param	session		Current session.
+	 * @param	tab			SQL results tab.
+	 * 
+	 * @throws	IllegalArgumentException
+	 * 			If a <TT>null</TT> <TT>ISession</TT> or
+	 *			<TT>ResultTab</TT> passed.
+	 */
+	public ResultFrame(ISession session, ResultTab tab)
+			throws IllegalArgumentException {
+		super(getFrameTitle(session, tab), true, true, true, true);
+		_session = session;
+		_tab = tab;
+
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+		final Container cont = getContentPane();
+		cont.setLayout(new BorderLayout());
+		JButton rtnBtn = new JButton(new ReturnResultTabAction(session.getApplication(), session, this));
+		cont.add(rtnBtn, BorderLayout.NORTH);
+		cont.add(tab.getOutputComponent(), BorderLayout.CENTER);
+	}
+
+	/**
+	 * Close this window.
+	 */
+	public void dispose() {
+		if (_tab != null) {
+			_tab.closeTab();
+			_tab = null;
+		}
+		super.dispose();
+	}
+
+	public void returnToTabbedPane() {
+		s_log.debug("ResultFrame.returnToTabbedPane()");
+		getContentPane().remove(_tab.getOutputComponent());
+		_tab.returnToTabbedPane();
+		_tab = null;
+		dispose();
+	}
+
+	private static String getFrameTitle(ISession session, ResultTab tab)
+			throws IllegalArgumentException {
+		if (tab == null) {
+			throw new IllegalArgumentException("Null ResultTab passed");
+		}
+		if (session == null) {
+			throw new IllegalArgumentException("Null ISession passed");
+		}
+
+		SessionSheet sheet = session.getSessionSheet();
+		return sheet.getTitle() + " - " + tab.getViewableSqlString();
+	}
+}
