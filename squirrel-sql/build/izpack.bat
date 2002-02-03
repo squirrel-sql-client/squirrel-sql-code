9df86980fe4f49456e5518d17a0c26076c52d7a7
Colin Bell <colbell@users.sourceforge.net>
2002-02-03 11:37:55 +0000

    Latest changes

--- squirrel-sql/build/izpack.bat
+++ squirrel-sql/build/izpack.bat
@@ -1,6 +1,6 @@
 pushd \apps\izpack\bin
 set LCL_SQL=\src\squirrel-sql\squirrel-sql
 set LCL_DIST=\src\squirrel-sql\squirrel-sql-dist
-d:\apps\jdk13\bin\java -jar "../lib/compiler.jar" %LCL_SQL%\build\izpack-complete.xml -b %LCL_SQL% -k standard-kunststoff -o %LCL_DIST%\squirrel-sql-1.1alpha4\squirrel-sql-1.1alpha4-install-complete.jar
-d:\apps\jdk13\bin\java -jar "../lib/compiler.jar" %LCL_SQL%\build\izpack-basic.xml -b %LCL_SQL% -k standard-kunststoff -o %LCL_DIST%\squirrel-sql-1.1alpha4\squirrel-sql-1.1alpha4-install-basic.jar
+@rem d:\apps\jdk13\bin\java -jar "../lib/compiler.jar" %LCL_SQL%\build\izpack-complete.xml -b %LCL_SQL% -k standard-kunststoff -o %LCL_DIST%\squirrel-sql-1.1beta1\squirrel-sql-1.1beta1-install-complete.jar
+d:\apps\jdk13\bin\java -jar "../lib/compiler.jar" %LCL_SQL%\build\izpack-basic.xml -b %LCL_SQL% -k standard-kunststoff -o %LCL_DIST%\squirrel-sql-1.1beta1\squirrel-sql-1.1beta1-install.jar
 popd
