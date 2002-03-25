2b1ea9bba57c5f349a1b378cfcc48960dcaa76bd
(no author) <gerdhwagner@t-online.de>
2002-03-25 11:02:15 +0000

    This commit was manufactured by cvs2svn to create tag 'laf0_11'.

--- squirrel-sql/app/cmd/squirrel-sql.bat
+++
@@ -1,9 +0,0 @@
-@rem -classic is required with JDK1.3 on Windows 2000. If it isn't used then
-@rem plugins look in the wrong directory for files.
-
-@rem Run with a command window.
-"%JAVA_HOME%\bin\java" -classic -cp lib\squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;lib\log4j.jar;%CLASSPATH% net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
-
-@rem Run with no command window. However this may not work with all versions of Windows.
-@rem start "%JAVA_HOME%\bin\javaw" -classic -cp lib\squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;%CLASSPATH% net.sourceforge.squirrel_sql.client.Main
-
--- squirrel-sql/app/cmd/squirrel-sql.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# $Id: squirrel-sql.sh,v 1.4 2001-12-31 06:44:59 colbell Exp $
-TMP_CP=lib/squirrel-sql.jar:lib/fw.jar:lib/nanoxml-2.1.jar:lib/log4j.jar:$CLASSPATH 
-if [ `uname -s` = "CYGWIN_NT-5.0" ]; then
-    $JAVA_HOME/bin/java -cp `cygpath -w -p $TMP_CP` net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
-else
-  $JAVA_HOME/bin/java -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
-fi
