d95890c633206f4f298110d6e61d57e587b14fb9
Colin Bell <colbell@users.sourceforge.net>
2002-04-27 03:27:27 +0000

    no message

--- squirrel-sql/app/cmd/squirrel-sql.bat
+++ squirrel-sql/app/cmd/squirrel-sql.bat
@@ -2,7 +2,7 @@
 "%JAVA_HOME%\bin\java" -cp squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;lib\log4j.jar;"%CLASSPATH%" net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
 
 @rem Run with no command window. However this may not work will all versions of Windows.
-@rem start "%JAVA_HOME%\bin\javaw" -cp squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;"%CLASSPATH%" net.sourceforge.squirrel_sql.client.Main
+@rem start /B %JAVA_HOME%\bin\javaw -cp squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;"%CLASSPATH%" net.sourceforge.squirrel_sql.client.Main
 
 @rem Run the executable jar file. However the classes from the %CLASSPATH%
 @rem environment variable will not be available.
