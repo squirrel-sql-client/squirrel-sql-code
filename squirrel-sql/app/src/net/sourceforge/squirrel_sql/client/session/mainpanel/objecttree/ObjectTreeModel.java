10215f829ce6b6bc1017b757ce8c7d0c5a6b19e8
Colin Bell <colbell@users.sourceforge.net>
2002-04-27 04:31:31 +0000

    Added memory status panel to main frame status bar. New setting show/hide main status bar. Start new object tree.

---
+++ squirrel-sql/app/src/net/sourceforge/squirrel_sql/client/session/mainpanel/objecttree/ObjectTreeModel.java
@@ -0,0 +1,45 @@
+package net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree;
+/*
+ * Copyright (C) 2002 Colin Bell and Johan Compagner
+ * colbell@users.sourceforge.net
+ * jcompagner@j-com.nl
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
+/**
+ * This is the model for the object tree.
+ *
+ * @author  <A HREF="mailto:colbell@users.sourceforge.net">Colin Bell</A>
+ */
+public class ObjectTreeModel extends DefaultTreeModel {
+	/** Logger for this class. */
+	private static ILogger s_log = LoggerController.createLogger(ObjectTreeModel.class);
+
+	/**
+	 * Default ctor.
+	 */
+	public ObjectTreeModel() {
+		super(new DefaultMutableTreeNode());
+//		_treeLoadedListeners = new ArrayList();
+//		setSession(session);
+	}
+
+}
+
