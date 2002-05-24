f84850ec4702a51cec8c9f20a58e2f432cc54c8f
Colin Bell <colbell@users.sourceforge.net>
2002-05-24 12:52:34 +0000

    Use Java in %JAVAHOME% if it is defined else use the current path. Loads all jars from /lib directory

--- squirrel-sql/app/cmd/squirrel-sql.bat
+++ squirrel-sql/app/cmd/squirrel-sql.bat
@@ -1,8 +1,21 @@
-@rem Run with a command window.
-"%JAVA_HOME%\bin\java" -cp squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;lib\log4j.jar;"%CLASSPATH%" net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
+@echo off
 
-@rem Run with no command window. However this may not work will all versions of Windows.
-@rem start /B %JAVA_HOME%\bin\javaw -cp squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;"%CLASSPATH%" net.sourceforge.squirrel_sql.client.Main
+IF "%JAVA_HOME%"=="" SET LOCAL_JAVA=java
+IF NOT "%JAVA_HOME%"=="" SET LOCAL_JAVA="%JAVA_HOME%\bin\java"
+
+dir /b squirrel-sql.jar > temp.tmp
+FOR /F %%I IN (temp.tmp) DO CALL addpath.bat %%I
+
+dir /b lib\*.* > temp.tmp
+FOR /F %%I IN (temp.tmp) DO CALL addpath.bat lib\%%I
+
+SET TMP_CP=%TMP_CP%;%CLASSPATH%
+
+@rem Run with a command window.
+%LOCAL_JAVA% -cp "%TMP_CP%" net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
+
+@rem Run with no command window. However this may not work with all versions of Windows.
+@rem start /B %LOCAL_JAVA% -cp "%TMP_CP%" net.sourceforge.squirrel_sql.client.Main
 
 @rem Run the executable jar file. However the classes from the %CLASSPATH%
 @rem environment variable will not be available.
--- squirrel-sql/app/cmd/squirrel-sql.sh
+++ squirrel-sql/app/cmd/squirrel-sql.sh
@@ -1,8 +1,16 @@
 #! /bin/sh
-# $Id: squirrel-sql.sh,v 1.5 2002-04-06 12:35:34 colbell Exp $
-TMP_CP=squirrel-sql.jar:lib/fw.jar:lib/nanoxml-2.1.jar:lib/log4j.jar:$CLASSPATH 
-if [ `uname -s` = "CYGWIN_NT-5.0" ]; then
-    $JAVA_HOME/bin/java -cp `cygpath -w -p $TMP_CP` net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
+[ $JAVA_HOME ] && JAVA=$JAVA_HOME/bin/java || JAVA=java
+
+for a in squirrel-sql.jar lib/*; do
+    TMP_CP="$TMP_CP:$a"
+done
+
+if [ -n "$(echo `uname -s` | grep '^CYGWIN')" ]; then
+    TMP_CP=`cygpath -w -p $TMP_CP`
+	TMP_CP=$TMP_CP';'$CLASSPATH
 else
-  $JAVA_HOME/bin/java -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
+    TMP_CP=$TMP_CP:$CLASSPATH
 fi
+
+$JAVA -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
+
