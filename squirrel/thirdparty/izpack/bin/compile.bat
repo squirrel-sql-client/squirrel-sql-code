2dc4a6c0d5c18fc976fb387aafa6770adde14dc9
(no author) <gerdhwagner@t-online.de>
2002-05-25 07:12:54 +0000

    This commit was manufactured by cvs2svn to create tag 'doc1_1beta3'.

--- squirrel/thirdparty/izpack/bin/compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@rem =====================================
-@rem DOS Batch file to invoke the compiler
-@rem =====================================
-
-java -jar "../lib/compiler.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-@echo on
