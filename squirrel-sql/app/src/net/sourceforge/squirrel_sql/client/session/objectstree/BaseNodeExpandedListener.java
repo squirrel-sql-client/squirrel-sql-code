1cb857ab8a5f84c319e61f0b3adb47a81e1d5a10
(no author) <gerdhwagner@t-online.de>
2002-05-25 06:49:12 +0000

    This commit was manufactured by cvs2svn to create tag 'fw1_1beta3'.

--- squirrel-sql/app/src/net/sourceforge/squirrel_sql/client/session/objectstree/BaseNodeExpandedListener.java
+++
@@ -1,7 +0,0 @@
-package net.sourceforge.squirrel_sql.client.session.objectstree;
-
-public interface BaseNodeExpandedListener
-{
-	public void nodeExpanded(BaseNode node);
-}
-
--- squirrel-sql/app/src/net/sourceforge/squirrel_sql/client/session/objectstree/TreeLoadedListener.java
+++
@@ -1,7 +0,0 @@
-package net.sourceforge.squirrel_sql.client.session.objectstree;
-
-public interface TreeLoadedListener
-{
-	public void treeLoaded();
-}
-
