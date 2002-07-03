462ce7300cdefd052da70947b3e3ee49466cad5d
Colin Bell <colbell@users.sourceforge.net>
2002-07-03 11:29:06 +0000

    Fix the version where cmd window isn't shown

--- squirrel-sql/app/cmd/squirrel-sql.bat
+++ squirrel-sql/app/cmd/squirrel-sql.bat
@@ -1,7 +1,7 @@
-@echo off
+echo off
 
-IF "%JAVA_HOME%"=="" SET LOCAL_JAVA=java
-IF NOT "%JAVA_HOME%"=="" SET LOCAL_JAVA="%JAVA_HOME%\bin\java"
+IF "%JAVA_HOME%"=="" SET LOCAL_JAVA=javaw
+IF NOT "%JAVA_HOME%"=="" SET LOCAL_JAVA=%JAVA_HOME%\bin\javaw
 
 dir /b squirrel-sql.jar > temp.tmp
 FOR /F %%I IN (temp.tmp) DO CALL addpath.bat %%I
@@ -12,11 +12,12 @@ FOR /F %%I IN (temp.tmp) DO CALL addpath.bat lib\%%I
 SET TMP_CP=%TMP_CP%;%CLASSPATH%
 
 @rem Run with a command window.
-%LOCAL_JAVA% -cp "%TMP_CP%" net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
+"%LOCAL_JAVA%" -cp "%TMP_CP%" net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=log4j.properties
 
 @rem Run with no command window. However this may not work with all versions of Windows.
-@rem start /B %LOCAL_JAVA% -cp "%TMP_CP%" net.sourceforge.squirrel_sql.client.Main
+@rem start "SQuirreL SQL Client" /B "%LOCAL_JAVA%" -cp "%TMP_CP%" net.sourceforge.squirrel_sql.client.Main
 
-@rem Run the executable jar file. However the classes from the %CLASSPATH%
-@rem environment variable will not be available.
-@rem squirrel-sql.jar
+@rem Run the executable jar file with and without a cmd window. However the
+@rem classes from the %CLASSPATH% environment variable will not be available.
+@rem start "SQuirreL SQL Client" /B "%LOCAL_JAVA%" -jar squirrel-sql.jar
+@rem "%LOCAL_JAVA%" -jar squirrel-sql.jar
