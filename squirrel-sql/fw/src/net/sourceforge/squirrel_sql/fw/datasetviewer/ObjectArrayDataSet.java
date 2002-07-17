6d9708df7e9f363a0b23ed58e20962b53852bafb
Colin Bell <colbell@users.sourceforge.net>
2002-07-17 08:57:25 +0000

    Make column title a single blank instead of an empty string so that column sorting will show the sort icon.

--- squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/ObjectArrayDataSet.java
+++ squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/ObjectArrayDataSet.java
@@ -1,6 +1,6 @@
 package net.sourceforge.squirrel_sql.fw.datasetviewer;
 /*
- * Copyright (C) 2001 Colin Bell
+ * Copyright (C) 2001-2002 Colin Bell
  * colbell@users.sourceforge.net
  *
  * This library is free software; you can redistribute it and/or
@@ -19,7 +19,8 @@ package net.sourceforge.squirrel_sql.fw.datasetviewer;
  */
 import net.sourceforge.squirrel_sql.fw.util.IMessageHandler;
 
-public class ObjectArrayDataSet implements IDataSet {
+public class ObjectArrayDataSet implements IDataSet
+{
 	private Object[][] _src;
 	private DataSetDefinition _dsDef;
 
@@ -27,51 +28,64 @@ public class ObjectArrayDataSet implements IDataSet {
 	private int _curIndex = -1;
 	private int _columnCount;
 
-	public ObjectArrayDataSet(Object[][] src) throws DataSetException {
+	public ObjectArrayDataSet(Object[][] src) throws DataSetException
+	{
 		super();
-		if (src == null) {
+		if (src == null)
+		{
 			throw new IllegalArgumentException("Null Object[][] passed");
 		}
 		_src = src;
-		for (int i = 0; i < src.length; ++i) {
-			if (src[i] != null && src[i].length > _columnCount) {
+		for (int i = 0; i < src.length; ++i)
+		{
+			if (src[i] != null && src[i].length > _columnCount)
+			{
 				_columnCount = src[i].length;
 			}
 		}
 		_dsDef = new DataSetDefinition(createColumnDefinitions());
 	}
 
-	public final int getColumnCount() {
+	public final int getColumnCount()
+	{
 		return _columnCount;
 	}
-	public DataSetDefinition getDataSetDefinition() {
+
+	public DataSetDefinition getDataSetDefinition()
+	{
 		return _dsDef;
 	}
 
-	public synchronized boolean next(IMessageHandler msgHandler) {
+	public synchronized boolean next(IMessageHandler msgHandler)
+	{
 		_curRow = null;
-		if (_src.length > (_curIndex + 1)) {
+		if (_src.length > (_curIndex + 1))
+		{
 			_curRow = _src[++_curIndex];
 			return true;
 		}
 		return false;
 	}
 
-	public synchronized Object get(int columnIndex) {
-		if (_curRow != null && columnIndex < _curRow.length) {
+	public synchronized Object get(int columnIndex)
+	{
+		if (_curRow != null && columnIndex < _curRow.length)
+		{
 			return _curRow[columnIndex];
 		}
 		return null;
 	}
 
-	private ColumnDisplayDefinition[] createColumnDefinitions() {
+	private ColumnDisplayDefinition[] createColumnDefinitions()
+	{
 		final int columnCount = getColumnCount();
-		ColumnDisplayDefinition[] columnDefs = new ColumnDisplayDefinition[columnCount];
-		for (int i = 0; i < columnCount; ++i) {
-			columnDefs[i] = new ColumnDisplayDefinition(100, "");
+		ColumnDisplayDefinition[] columnDefs =
+			new ColumnDisplayDefinition[columnCount];
+		for (int i = 0; i < columnCount; ++i)
+		{
+			columnDefs[i] = new ColumnDisplayDefinition(100, " ");
 		}
 		return columnDefs;
 	}
 
 }
-
