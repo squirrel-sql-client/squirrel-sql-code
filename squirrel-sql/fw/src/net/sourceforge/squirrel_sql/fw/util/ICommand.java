bef80244ad9fa59535c55ca063788cc0ed00d9f9
Colin Bell <colbell@users.sourceforge.net>
2002-08-10 07:51:56 +0000

    Cleanup

--- squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/util/ICommand.java
+++ squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/util/ICommand.java
@@ -1,6 +1,6 @@
 package net.sourceforge.squirrel_sql.fw.util;
 /*
- * Copyright (C) 2001 Colin Bell
+ * Copyright (C) 2001-2002 Colin Bell
  * colbell@users.sourceforge.net
  *
  * This library is free software; you can redistribute it and/or
@@ -17,6 +17,7 @@ package net.sourceforge.squirrel_sql.fw.util;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-public interface ICommand {
+public interface ICommand
+{
 	void execute() throws BaseException;
 }
