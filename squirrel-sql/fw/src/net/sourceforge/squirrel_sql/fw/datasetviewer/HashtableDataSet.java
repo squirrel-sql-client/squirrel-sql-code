f19cbc6b27c542f7a0b51e96e7997d6490e3a955
Colin Bell <colbell@users.sourceforge.net>
2002-08-23 11:44:55 +0000

    *** empty log message ***

---
+++ squirrel-sql/fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/HashtableDataSet.java
@@ -0,0 +1,12 @@
+package net.sourceforge.squirrel_sql.fw.datasetviewer;
+
+/**
+ * @author colbell
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class HashtableDataSet
+{
+
+}
