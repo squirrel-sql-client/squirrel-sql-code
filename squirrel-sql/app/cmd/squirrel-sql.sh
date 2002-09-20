838fd26e37fc34ab673b7e9a5a02ba435f611f9a
Colin Bell <colbell@users.sourceforge.net>
2002-09-20 22:59:29 +0000

    Make work if Squirrel installed into a directory with spaces in the name.

--- squirrel-sql/app/cmd/squirrel-sql.sh
+++ squirrel-sql/app/cmd/squirrel-sql.sh
@@ -3,23 +3,23 @@
 
 SQUIRREL_SQL_HOME="$INSTALL_PATH"
 
-IFS=":"
-for a in $SQUIRREL_SQL_HOME/squirrel-sql.jar $SQUIRREL_SQL_HOME/lib/*; do
-        TMP_CP="$TMP_CP:$a"
+TMP_CP=$SQUIRREL_SQL_HOME/squirrel-sql.jar
+IFS=""
+for a in $SQUIRREL_SQL_HOME/lib/*; do
+	TMP_CP="$TMP_CP":"$a";
 done
 
 cygwin=false;
 case "`uname -s`" in
-    CYGWIN*) cygwin=true ;;
+	CYGWIN*) cygwin=true ;;
 esac
 
 if $cygwin ; then
-    TMP_CP=`cygpath -w -p $TMP_CP`
-    TMP_CP=$TMP_CP';'$CLASSPATH
+	TMP_CP=`cygpath -w -p $TMP_CP`
+	TMP_CP=$TMP_CP';'$CLASSPATH
 else
-    TMP_CP=$TMP_CP:$CLASSPATH
+	TMP_CP=$TMP_CP:$CLASSPATH
 fi
 
 $JAVA -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main -loggingConfigFile=$SQUIRREL_SQL_HOME/log4j.properties -squirrelHome=$SQUIRREL_SQL_HOME
-
 
