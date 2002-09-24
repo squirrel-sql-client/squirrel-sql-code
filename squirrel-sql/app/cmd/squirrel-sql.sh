67c179c02759b42807a5c6a81f3b270ebd9561cf
(no author) <gerdhwagner@t-online.de>
2002-09-24 12:22:41 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

--- squirrel-sql/app/cmd/squirrel-sql.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# $Id: squirrel-sql.sh,v 1.2 2001-11-19 18:17:24 placson Exp $
-TMP_CP=lib/squirrel-sql.jar:lib/fw.jar:lib/nanoxml-2.1.jar:$CLASSPATH 
-if [ `uname -s` = "CYGWIN_NT-5.0" ]; then
-    $JAVA_HOME/bin/java -cp `cygpath -w -p $TMP_CP` net.sourceforge.squirrel_sql.client.Main
-else
-  $JAVA_HOME/bin/java -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main
-fi
--- squirrel-sql/build/izpack.bat
+++
@@ -1,6 +0,0 @@
-pushd \apps\izpack\bin
-set LCL_SQL=\src\squirrel-sql\squirrel-sql
-set LCL_DIST=\src\squirrel-sql\squirrel-sql-dist
-d:\apps\jdk13\bin\java -jar "../lib/compiler.jar" %LCL_SQL%\build\izpack-complete.xml -b %LCL_SQL% -k standard-kunststoff -o %LCL_DIST%\squirrel-sql-1.1alpha1\squirrel-sql-1.1alpha1-install-complete.jar
-d:\apps\jdk13\bin\java -jar "../lib/compiler.jar" %LCL_SQL%\build\izpack-basic.xml -b %LCL_SQL% -k standard-kunststoff -o %LCL_DIST%\squirrel-sql-1.1alpha1\squirrel-sql-1.1alpha1-install-basic.jar
-popd
