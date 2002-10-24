a8d8425102c36f8ac067fb588afda2fd51ae1e9a
Colin Bell <colbell@users.sourceforge.net>
2002-10-24 09:33:35 +0000

    Fix to work in cygwin

--- squirrel-sql/app/cmd/squirrel-sql.sh
+++ squirrel-sql/app/cmd/squirrel-sql.sh
@@ -1,19 +1,33 @@
 #! /bin/sh
 [ $JAVA_HOME ] && JAVA=$JAVA_HOME/bin/java || JAVA=java
 
-SQUIRREL_SQL_HOME="$INSTALL_PATH"
-
-TMP_CP=$SQUIRREL_SQL_HOME/squirrel-sql.jar
-IFS=""
-for a in $SQUIRREL_SQL_HOME/lib/*; do
-	TMP_CP="$TMP_CP":"$a";
-done
-
+# Are we running within Cygwin on some version of Windows?
 cygwin=false;
 case "`uname -s`" in
 	CYGWIN*) cygwin=true ;;
 esac
 
+# Squirrel home.
+SQUIRREL_SQL_HOME="$INSTALL_PATH"
+
+# SQuirreL home in Unix format.
+if $cygwin ; then
+	UNIX_STYLE_HOME=`cygpath "$SQUIRREL_SQL_HOME"`
+else
+	UNIX_STYLE_HOME=$SQUIRREL_SQL_HOME
+fi
+
+# First entry in classpath is the Squirrel application.
+TMP_CP=$UNIX_STYLE_HOME/squirrel-sql.jar
+
+# Then add all library jars to the classpath.
+IFS=""
+for a in $UNIX_STYLE_HOME/lib/*; do
+	TMP_CP="$TMP_CP":"$a";
+done
+
+# Now add the system classpath to our classpath. If running
+# Cygwin we also need to change our classpath to Windows format.
 if $cygwin ; then
 	TMP_CP=`cygpath -w -p $TMP_CP`
 	TMP_CP=$TMP_CP';'$CLASSPATH
