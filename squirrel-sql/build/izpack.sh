a62375a5aa1d600b6fa110cfaee0e2f75273e242
(no author) <gerdhwagner@t-online.de>
2003-04-29 10:18:42 +0000

    This commit was manufactured by cvs2svn to create tag 'sqlbookmark_0_42'.

--- squirrel-sql/build/izpack.sh
+++
@@ -1,2 +0,0 @@
-~/apps/izpack/bin/compile izpack-basic.xml -b ~/src/squirrel-sql/squirrel-sql/build  -k standard-kunststoff -o /tmp/squirrel-sql-1.1final1-install.jar
-~/apps/izpack/bin/compile izpack-basic-win.xml -b ~/src/squirrel-sql/squirrel-sql/build  -k standard-kunststoff -o /tmp/win9598-squirrel-sql-1.1final1-install.jar
