f3990b27b5ad04b542a730c5708fcb2866cd8431
(no author) <gerdhwagner@t-online.de>
2003-07-09 12:42:40 +0000

    This commit was manufactured by cvs2svn to create tag 'sqsc_1_2beta1'.

--- squirrel/src/bin/squirrel-sql.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# $Id: squirrel-sql.sh,v 1.2 2001-11-20 22:15:20 placson Exp $
-TMP_CP=../lib/squirrel-0.1-alpha.jar:../lib/nanoxml-2.1.jar:$CLASSPATH 
-if [ `uname -s` = "CYGWIN_NT-5.0" ]; then
-    $JAVA_HOME/bin/java -cp `cygpath -w -p $TMP_CP` net.sourceforge.squirrel_sql.client.Main
-else
-  $JAVA_HOME/bin/java -cp $TMP_CP net.sourceforge.squirrel_sql.client.Main
-fi
