910990bfb0b05e240c4a3688bc26f26f2c5101ff
Colin Bell <colbell@users.sourceforge.net>
2003-07-09 12:42:39 +0000

    no message

--- build/izpack.bat
+++ build/izpack.bat
@@ -1,1 +1,1 @@
-\apps\izpack\bin\compile izpack-basic.xml  -b d:\src\squirrel-sql\squirrel-sql\build  -k standard-kunststoff -o test.jar
+\apps\izpack\bin\compile izpack-basic.xml  -b c:\src\sql12\sql12\build  -k standard-kunststoff -o \temp\squirrel-sql-1.2beta1-install.jar
