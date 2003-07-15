ac8fa4cbbdada8a04f194cb45834b54a0c6401cb
Colin Bell <colbell@users.sourceforge.net>
2003-07-15 11:48:32 +0000

    Update for 1.2beta2

--- sql12/build/izpack.bat
+++ sql12/build/izpack.bat
@@ -1,1 +1,1 @@
-\apps\izpack\bin\compile izpack-basic.xml  -b c:\src\sql12\sql12\build  -k standard-kunststoff -o \temp\squirrel-sql-1.2beta1-install.jar
+\apps\izpack\bin\compile izpack-basic.xml  -b c:\src\sql12\sql12\build  -k standard-kunststoff -o \temp\squirrel-sql-1.2beta2-install.jar
