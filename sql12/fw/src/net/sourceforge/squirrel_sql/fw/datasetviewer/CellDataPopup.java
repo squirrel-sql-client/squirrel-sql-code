b26f7fc3dcca725cfee0134dcc89cb24d00d503e
g. griffin <gwghome@users.sourceforge.net>
2003-09-04 23:11:27 +0000

    Changes to allow editing in popup

--- sql12/fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/CellDataPopup.java
+++ sql12/fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/CellDataPopup.java
@@ -28,6 +28,8 @@ import java.awt.Frame;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.event.ActionListener;
 
 import javax.swing.JDialog;
 import javax.swing.JInternalFrame;
@@ -38,6 +40,9 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.CellEditor;
 
 import net.sourceforge.squirrel_sql.fw.gui.action.BaseAction;
 import net.sourceforge.squirrel_sql.fw.gui.TextPopupMenu;
@@ -66,10 +71,13 @@ public class CellDataPopup
 	 */
 	static ColumnDisplayDefinition _colDef;
 	
+	
 	/**
 	 * function to create the popup display when called from JTable
 	 */
-	public static void showDialog(JTable table, ColumnDisplayDefinition colDef, MouseEvent evt)
+	public static void showDialog(JTable table,
+		ColumnDisplayDefinition colDef,
+		MouseEvent evt)
 	{
 		_colDef = colDef;
 		CellDataPopup popup = new CellDataPopup();
@@ -93,37 +101,47 @@ public class CellDataPopup
 			{
 				obj = "";
 			}
+			
+			// since user is now using popup, stop editing
+			// using the in-cell editor, if any
+			CellEditor editor = table.getCellEditor(row, col);
+			if (editor != null)
+				editor.cancelCellEditing();
+			
+			IDataSetTableControls creator =
+				((DataSetViewerTablePanel.MyJTable)table).getCreator();
 
 			Component comp = SwingUtilities.getRoot(table);
 			Component newComp = null;
 			if (comp instanceof BaseMDIParentFrame)
 			{
 				TextAreaInternalFrame taif = 
-					new TextAreaInternalFrame(table.getColumnName(col), (String)obj);
+					new TextAreaInternalFrame(table.getColumnName(col), (String)obj,
+						row, col, creator, table);
 				((BaseMDIParentFrame)comp).addInternalFrame(taif, false);
 				taif.setLayer(JLayeredPane.POPUP_LAYER);
 				taif.pack();
 				newComp = taif;
 			}
-			else
-			{
-				TextAreaDialog tad = null;
-				if (comp instanceof Dialog)
-				{
-					tad = new TextAreaDialog((Dialog)comp, table.getColumnName(col), (String)obj);
-				}
-				else if (comp instanceof Frame)
-				{
-					tad = new TextAreaDialog((Frame)comp, table.getColumnName(col), (String)obj);
-				}			
-				else
-				{
-					s_log.error("Creating TextAreaDialog for invalid parent of: " + comp.getClass().getName());
-					return;
-				}	
-				tad.pack();
-				newComp = tad;
-			}
+//??			else
+//??			{
+//??				TextAreaDialog tad = null;
+//??				if (comp instanceof Dialog)
+//??				{
+//??					tad = new TextAreaDialog((Dialog)comp, table.getColumnName(col), (String)obj);
+//??				}
+//??				else if (comp instanceof Frame)
+//??				{
+//??					tad = new TextAreaDialog((Frame)comp, table.getColumnName(col), (String)obj);
+//??				}			
+//??				else
+//??				{
+//??					s_log.error("Creating TextAreaDialog for invalid parent of: " + comp.getClass().getName());
+//??					return;
+//??				}	
+//??				tad.pack();
+//??				newComp = tad;
+//??			}
 
 			Dimension dim = newComp.getSize();
 			boolean dimChanged = false;
@@ -203,19 +221,32 @@ public class CellDataPopup
 		private final TextPopupMenu _popupMenu = new TextPopupMenu();
 		private final JTextArea _ta;
 		private MouseAdapter _lis;
+		private JInternalFrame _parentFrame = null;
+		private int _row;
+		private int _col;
+		private JTable _table;
 
-		ColumnDataPopupPanel(String cellContents)
+		ColumnDataPopupPanel(String cellContents, boolean tableIsEditable)
 		{
 			super(new BorderLayout());
-//			_ta = new JTextArea(cellContents);
-
 
 			_ta = CellComponentFactory.getJTextArea(_colDef);
 			_ta.setText(cellContents);
 
-//??_ta.setEditable(true);
-//??_ta.setBackground(Color.YELLOW);	// tell user it is editable
-			_ta.setEditable(false);
+			if (tableIsEditable && CellComponentFactory.isEditableInPopup(_colDef)) {
+				// data is editable in popup
+				_ta.setEditable(true);
+				_ta.setBackground(Color.YELLOW);	// tell user it is editable
+				
+				// Since data is editable, we need to add control panel
+				// to manage user requests for DB update, file IO, etc.
+				JPanel editingControls = createPopupEditingControls(_ta);
+				add(editingControls, BorderLayout.SOUTH);
+			}
+			else {
+				// data is not editable in popup
+				_ta.setEditable(false);
+			}
 
 			_ta.setLineWrap(true);
 			add(new JScrollPane(_ta), BorderLayout.CENTER);
@@ -223,6 +254,55 @@ public class CellDataPopup
 			_popupMenu.add(new WrapAction());
 			_popupMenu.setTextComponent(_ta);
 		}
+		
+		/**
+		 * Set up user controls for editing.
+		 */
+		private JPanel createPopupEditingControls(JTextArea _ta) {
+			JPanel panel = new JPanel(new BorderLayout());
+			
+			// create update/cancel controls using default layout
+			JPanel updateControls = new JPanel();
+			
+			// set up Update button
+			JButton updateButton = new JButton("Update DB");
+			updateButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					_table.setValueAt(ColumnDataPopupPanel.this._ta.getText(), _row, _col);
+					ColumnDataPopupPanel.this._parentFrame.setVisible(false);
+					ColumnDataPopupPanel.this._parentFrame.dispose();
+				}
+			});
+			
+			// set up Cancel button
+			JButton cancelButton = new JButton("Cancel");
+			cancelButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					ColumnDataPopupPanel.this._parentFrame.setVisible(false);
+					ColumnDataPopupPanel.this._parentFrame.dispose();
+				}
+			});
+			
+			// add buttons to button panel
+			updateControls.add(updateButton);
+			updateControls.add(cancelButton);
+			
+			// add button panel to main panel
+			panel.add(updateControls, BorderLayout.SOUTH);
+			
+			return panel;
+		}
+		
+		/*
+		 * Save various information which is needed to do Update & Cancel.
+		 */
+		 public void setUserActionInfo(JInternalFrame parent, int row, int col,
+		 	JTable table) {
+		 	_parentFrame = parent;
+		 	_row = row;
+		 	_col = col;
+		 	_table = table;
+		 }
 
 		public void addNotify()
 		{
@@ -281,26 +361,32 @@ public class CellDataPopup
 
 	class TextAreaInternalFrame extends JInternalFrame
 	{
-		public TextAreaInternalFrame(String column, String text)
+		public TextAreaInternalFrame(String column, String text, int row, int col,
+			 IDataSetTableControls creator, JTable table)
 		{
 			super("Value of column " + column, true, true, true, true);
-			setContentPane(new ColumnDataPopupPanel(text));
+			ColumnDataPopupPanel popup =
+				new ColumnDataPopupPanel(text, creator.isTableEditable());
+			popup.setUserActionInfo(this, row, col, table);
+			setContentPane(popup);
 		}
 	}
 
-	class TextAreaDialog extends JDialog
-	{
-		public TextAreaDialog(Dialog owner, String column, String text)
-		{
-			super(owner, "Value of column " + column, false);
-			setContentPane(new ColumnDataPopupPanel(text));
-		}
-
-		public TextAreaDialog(Frame owner, String column, String text)
-		{
-			super(owner, "Value of column " + column, false);
-			setContentPane(new ColumnDataPopupPanel(text));
-		}
-	}
+//??	class TextAreaDialog extends JDialog
+//??	{
+//??		public TextAreaDialog(Dialog owner, String column, String text)
+//??		{
+//??			super(owner, "Value of column " + column, false);
+//??			ColumnDataPopupPanel popup = new ColumnDataPopupPanel(text);
+//??			setContentPane(popup);
+//??		}
+//??
+//??		public TextAreaDialog(Frame owner, String column, String text)
+//??		{
+//??			super(owner, "Value of column " + column, false);
+//??			ColumnDataPopupPanel popup = new ColumnDataPopupPanel(text);
+//??			setContentPane(popup);
+//??		}
+//??	}
 
 }
