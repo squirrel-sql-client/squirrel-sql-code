435addeb95a96455e46265dbd43720a6d5d7561b
g. griffin <gwghome@users.sourceforge.net>
2003-09-15 14:42:23 +0000

    remove unneeded function that duplicates one in fw that returns name of SQL data type.

--- sql12/app/src/net/sourceforge/squirrel_sql/client/db/Utils.java
+++ sql12/app/src/net/sourceforge/squirrel_sql/client/db/Utils.java
@@ -51,39 +51,4 @@ public class Utils {
 		}
 	}
 
-	public static String getSqlTypeName(int type)
-	{
-		switch (type)
-		{
-			case Types.ARRAY: return "ARRAY";
-			case Types.BIGINT: return "BIGINT";
-			case Types.BIT: return "BIT";
-			case Types.TINYINT: return "TINYINT";
-			case Types.SMALLINT: return "SMALLINT";
-			case Types.INTEGER: return "INTEGER";
-			case Types.FLOAT: return "FLOAT";
-			case Types.REAL: return "REAL";
-			case Types.DOUBLE: return "DOUBLE";
-			case Types.NUMERIC: return "NUMERIC";
-			case Types.DECIMAL: return "DECIMAL";
-			case Types.CHAR: return "CHAR";
-			case Types.VARCHAR: return "VARCHAR";
-			case Types.LONGVARCHAR: return "LONGVARCHAR";
-			case Types.DATE: return "DATE";
-			case Types.TIME: return "TIME";
-			case Types.TIMESTAMP: return "TIMESTAMP";
-			case Types.BINARY: return "BINARY";
-			case Types.VARBINARY: return "VARBINARY";
-			case Types.LONGVARBINARY: return "LONGVARBINARY";
-			case Types.NULL: return "NULL";
-			case Types.OTHER: return "OTHER";
-			case Types.JAVA_OBJECT: return "JAVA_OBJECT";
-			case Types.DISTINCT: return "DISTINCT";
-			case Types.STRUCT: return "STRUCT";
-			case Types.BLOB: return "BLOB";
-			case Types.CLOB: return "CLOB";
-			case Types.REF: return "REF";
-			default: return "" + type + " Unknown";
-		}
-	}
 }
