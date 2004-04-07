083917a8dcd2e1ba6b08bc666103112cfbb2e06d
g. griffin <gwghome@users.sourceforge.net>
2004-04-07 02:45:04 +0000

    eanup imports

--- sql12/fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/cellcomponent/DataTypeOther.java
+++ sql12/fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/cellcomponent/DataTypeOther.java
@@ -19,16 +19,12 @@ package net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent;
  */
 import java.awt.event.*;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
-import javax.swing.DefaultCellEditor;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 import javax.swing.text.JTextComponent;
