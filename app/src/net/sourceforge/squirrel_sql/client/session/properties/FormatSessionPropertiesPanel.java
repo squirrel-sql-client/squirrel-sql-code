fa2411303a84fe73957ccc5163490fa88b18f589
g. griffin <gwghome@users.sourceforge.net>
2004-05-19 14:59:00 +0000

    remove unneeded code

--- app/src/net/sourceforge/squirrel_sql/client/session/properties/FormatSessionPropertiesPanel.java
+++ app/src/net/sourceforge/squirrel_sql/client/session/properties/FormatSessionPropertiesPanel.java
@@ -21,21 +21,11 @@ import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.JTextArea;
 
-import net.sourceforge.squirrel_sql.fw.datasetviewer.LargeResultSetObjectInfo;
-import net.sourceforge.squirrel_sql.fw.gui.IntegerField;
 import net.sourceforge.squirrel_sql.fw.gui.OkJPanel;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.CellComponentFactory;
 
@@ -43,9 +33,10 @@ import net.sourceforge.squirrel_sql.client.IApplication;
 import net.sourceforge.squirrel_sql.client.preferences.INewSessionPropertiesPanel;
 import net.sourceforge.squirrel_sql.client.session.ISession;
 /**
- * This panel allows the user to tailor formatting settings for a session.
+ * This panel allows the user to tailor DataType-specific settings for a session.
  *
  * @author <A HREF="mailto:colbell@users.sourceforge.net">Colin Bell</A>
+ * @author gwg
  */
 public class FormatSessionPropertiesPanel
 	implements INewSessionPropertiesPanel, ISessionPropertiesPanel
@@ -55,9 +46,6 @@ public class FormatSessionPropertiesPanel
 
 	/** The actual GUI panel that allows user to do the maintenance. */
 	private final FormatPropertiesPanel _myPanel;
-
-	/** Session properties object being maintained. */
-	private SessionProperties _props;
 
 
 	/**
@@ -83,19 +71,19 @@ public class FormatSessionPropertiesPanel
 
 	public void initialize(IApplication app)
 	{
-		_props = _app.getSquirrelPreferences().getSessionProperties();
-		_myPanel.loadData(_props);
+		// We need this method to satisfy one of the Interfaces we implement,
+		// but since we have moved all operations to the DataType sub-panels
+		// which initialize their own data during creation,
+		// there is nothing for us to do here
 	}
 
 	public void initialize(IApplication app, ISession session)
 		throws IllegalArgumentException
 	{
-		if (session == null)
-		{
-			throw new IllegalArgumentException("Null ISession passed");
-		}
-		_props = session.getProperties();
-		_myPanel.loadData(_props);
+		// We need this method to satisfy one of the Interfaces we implement,
+		// but since we have moved all operations to the DataType sub-panels
+		// which initialize their own data during creation,
+		// there is nothing for us to do here
 	}
 
 	public Component getPanelComponent()
@@ -115,7 +103,7 @@ public class FormatSessionPropertiesPanel
 
 	public void applyChanges()
 	{
-		_myPanel.applyChanges(_props);
+		_myPanel.applyChanges();
 	}
 
 	private static final class FormatPropertiesPanel extends JPanel
@@ -126,103 +114,26 @@ public class FormatSessionPropertiesPanel
 		 */
 		interface i18n
 		{
-			String ALL_OTHER = "Display Unknown, non-standard or un-handled Data Types as String";
-			String BLOB = "Blob";
-			String CLOB = "Clob";
 			String HINT = "Set options for specific Data Types";
-			String NBR_BYTES = "Number of bytes to read:";
-			String NBR_CHARS = "Number of chars to read:";
-			String SQL_OTHER = "Display SQL Other (Data Type 1111) as String";
 			String TITLE = "Data Type Controls";
-			String BLOB_WARNING = "Some DBMSs implement BLOB/CLOB fields as other Data Types\nsuch as BINARY and VARCHAR.\nThe following works only for Data Types 2004(BLOB) and 2005(CLOB).";
-			String OTHER_TYPE_WARNING = "The following Data Types are not the same in all DBMSs and\nmay cause exceptions if interpreted as Strings.";
 		}
 		
 		/** List of OkJPanels containing controls for specific DataType info */
 		OkJPanel[] dataTypePanels;
-
-		private JCheckBox _showBlobChk = new JCheckBox(i18n.BLOB);
-		private JCheckBox _showClobChk = new JCheckBox(i18n.CLOB);
-
-		private final ReadTypeCombo _blobTypeDrop = new ReadTypeCombo();
-		private final ReadTypeCombo _clobTypeDrop = new ReadTypeCombo();
-
-		private IntegerField _showBlobSizeField = new IntegerField(5);
-		private IntegerField _showClobSizeField = new IntegerField(5);
-
-		private JCheckBox _showSQLOtherChk = new JCheckBox(i18n.SQL_OTHER);
-
-		private JCheckBox _showAllOtherChk = new JCheckBox(i18n.ALL_OTHER);
-
-		/**
-		 * This object will update the status of the GUI controls as the user
-		 * makes changes.
-		 */
-		private final ControlMediator _controlMediator = new ControlMediator();
 
 		FormatPropertiesPanel(IApplication app)
 		{
 			super();
 			createGUI(app);
 		}
+		
 
-		void loadData(SessionProperties props)
-		{
-			LargeResultSetObjectInfo largeObjInfo = props.getLargeResultSetObjectInfo();
-
-			_showBlobChk.setSelected(largeObjInfo.getReadBlobs());
-			_showBlobSizeField.setInt(largeObjInfo.getReadBlobsSize());
-			_blobTypeDrop.setSelectedIndex(largeObjInfo.getReadCompleteBlobs()
-												? ReadTypeCombo.READ_ALL_IDX
-												: ReadTypeCombo.READ_PARTIAL_IDX);
-
-			_showClobChk.setSelected(largeObjInfo.getReadClobs());
-			_showClobSizeField.setInt(largeObjInfo.getReadClobsSize());
-			_clobTypeDrop.setSelectedIndex(largeObjInfo.getReadCompleteClobs()
-												? ReadTypeCombo.READ_ALL_IDX
-												: ReadTypeCombo.READ_PARTIAL_IDX);
-
-			_showSQLOtherChk.setSelected(largeObjInfo.getReadSQLOther());
-			_showAllOtherChk.setSelected(largeObjInfo.getReadAllOther());
-
-			updateControlStatus();
-		}
-
-		void applyChanges(SessionProperties props)
-		{
-			LargeResultSetObjectInfo largeObjInfo = props.getLargeResultSetObjectInfo();
-
-			largeObjInfo.setReadBlobs(_showBlobChk.isSelected());
-			largeObjInfo.setReadBlobsSize(_showBlobSizeField.getInt());
-			largeObjInfo.setReadCompleteBlobs(
-						_blobTypeDrop.getSelectedIndex() == ReadTypeCombo.READ_ALL_IDX);
-
-			largeObjInfo.setReadClobsSize(_showClobSizeField.getInt());
-			largeObjInfo.setReadClobs(_showClobChk.isSelected());
-			largeObjInfo.setReadCompleteClobs(
-				_clobTypeDrop.getSelectedIndex() == ReadTypeCombo.READ_ALL_IDX);
-
-			largeObjInfo.setReadSQLOther(_showSQLOtherChk.isSelected());
-			largeObjInfo.setReadAllOther(_showAllOtherChk.isSelected());
-			
+		void applyChanges()
+		{		
 			for (int i=0; i< dataTypePanels.length; i++)
 				dataTypePanels[i].ok();
 		}
 
-		private void updateControlStatus()
-		{
-			final boolean showBlobs = _showBlobChk.isSelected();
-			final boolean showPartialBlobs =
-				_blobTypeDrop.getSelectedIndex() == ReadTypeCombo.READ_PARTIAL_IDX;
-			_showBlobSizeField.setEnabled(showBlobs && showPartialBlobs);
-			_blobTypeDrop.setEnabled(showBlobs);
-
-			final boolean showClobs = _showClobChk.isSelected();
-			final boolean showPartialClobs =
-				_clobTypeDrop.getSelectedIndex() == ReadTypeCombo.READ_PARTIAL_IDX;
-			_showClobSizeField.setEnabled(showClobs && showPartialClobs);
-			_clobTypeDrop.setEnabled(showClobs);
-		}
 
 		private void createGUI(IApplication app)
 		{
@@ -239,18 +150,8 @@ public class FormatSessionPropertiesPanel
 
 		private JPanel createDataTypesPanel()
 		{
-/*****
-			_showBlobSizeField.setColumns(5);
-			_showClobSizeField.setColumns(5);
-
-			_showBlobChk.addActionListener(_controlMediator);
-			_showClobChk.addActionListener(_controlMediator);
-			_blobTypeDrop.addActionListener(_controlMediator);
-			_clobTypeDrop.addActionListener(_controlMediator);
-******/
 
 			JPanel pnl = new JPanel(new GridBagLayout());
-//??			pnl.setBorder(BorderFactory.createTitledBorder("Show Data as Strings for Columns of these Data Types"));
 			final GridBagConstraints gbc = new GridBagConstraints();
 			gbc.fill = GridBagConstraints.HORIZONTAL;
 			gbc.insets = new Insets(4, 4, 4, 4);
@@ -259,69 +160,8 @@ public class FormatSessionPropertiesPanel
 			gbc.gridx = 0;
 			gbc.gridy = 0;
 
-/*****
-			pnl.add(new JLabel(""), gbc);
-			
-			++gbc.gridy;	// leave a blank line to separate text from header
-			gbc.gridwidth = GridBagConstraints.REMAINDER;
-
-			JTextArea text1 = new JTextArea(i18n.BLOB_WARNING);
-			text1.setEditable(false);
-			pnl.add(text1, gbc);
-
-
-			gbc.gridwidth = 1;
-			++gbc.gridy;
-			gbc.gridx = 0;
-			pnl.add(_showBlobChk, gbc);
-
-			++gbc.gridx;
-			pnl.add(new RightLabel("Read"), gbc);
-
-			++gbc.gridx;
-			pnl.add(_blobTypeDrop, gbc);
-
-			++gbc.gridx;
-			pnl.add(_showBlobSizeField, gbc);
-			
-			++gbc.gridx;
-			pnl.add(new JLabel("Bytes"), gbc);
-			
-
-			++gbc.gridy;
-			gbc.gridx = 0;
-			pnl.add(_showClobChk, gbc);			
-			
-			++gbc.gridx;
-			pnl.add(new RightLabel("Read"), gbc);
-
-			++gbc.gridx;
-			pnl.add(_clobTypeDrop, gbc);
-
-			++gbc.gridx;
-			pnl.add(_showClobSizeField, gbc);
-
-			++gbc.gridx;
-			pnl.add(new JLabel("Chars"), gbc);
-
-
-			++gbc.gridy;	// leave blank line for visual separation
-			pnl.add(new JLabel(""), gbc);
-			
-			++gbc.gridy;
-			gbc.gridx = 0;
-			gbc.gridwidth = GridBagConstraints.REMAINDER;
-			JTextArea text2 = new JTextArea(i18n.OTHER_TYPE_WARNING);
-			text2.setEditable(false);
-			pnl.add(text2, gbc);
-			
-			++gbc.gridy;
-			pnl.add(_showSQLOtherChk, gbc);
-						
-			++gbc.gridy;
-			pnl.add(_showAllOtherChk, gbc);
-/************************************************************************************/
-
+			// add each of the panels created by the DataType objects for
+			// editing their own properties
 			dataTypePanels = CellComponentFactory.getControlPanels();
 			for (int i=0; i<dataTypePanels.length; i++) {
 				gbc.gridx=0;
@@ -341,34 +181,6 @@ public class FormatSessionPropertiesPanel
 			}
 		}
 
-		private static final class ReadTypeCombo extends JComboBox
-		{
-			static final int READ_ALL_IDX = 1;
-			static final int READ_PARTIAL_IDX = 0;
-
-			ReadTypeCombo()
-			{
-				addItem("only the first");
-				addItem("all");
-			}
-		}
-
-		/**
-		 * This class will update the status of the GUI controls as the user
-		 * makes changes.
-		 */
-		private final class ControlMediator implements ChangeListener,
-															ActionListener
-		{
-			public void stateChanged(ChangeEvent evt)
-			{
-				updateControlStatus();
-			}
-
-			public void actionPerformed(ActionEvent evt)
-			{
-				updateControlStatus();
-			}
-		}
 	}
+
 }
