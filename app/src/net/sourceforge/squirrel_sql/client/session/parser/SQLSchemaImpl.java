c5affa7931d800f7a5db23b147491ad579d542ba
Gerd Wagner <gerdhwagner@t-online.de>
2004-07-04 11:15:04 +0000

    Fixed bug #981490

--- app/src/net/sourceforge/squirrel_sql/client/session/parser/SQLSchemaImpl.java
+++ app/src/net/sourceforge/squirrel_sql/client/session/parser/SQLSchemaImpl.java
@@ -6,12 +6,14 @@ import net.sourceforge.squirrel_sql.client.session.parser.kernel.SQLSchema;
 
 import java.util.List;
 import java.util.Vector;
+import java.util.Hashtable;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 public class SQLSchemaImpl implements SQLSchema
 {
    private ISession _session;
+   private Hashtable _tableCache = new Hashtable();
 
    SQLSchemaImpl(ISession session)
 	{
@@ -28,7 +30,14 @@ public class SQLSchemaImpl implements SQLSchema
          {
             if(tableNames[i].equalsIgnoreCase(name))
             {
-               return new SQLSchema.Table(null, null, tableNames[i], _session.getSQLConnection().getConnection().getMetaData());
+               String key = getKey(catalog, schema, name);
+               SQLSchema.Table ret = (SQLSchema.Table) _tableCache.get(key);
+               if(null == ret)
+               {
+                  ret = new SQLSchema.Table(catalog, schema, tableNames[i], _session.getSQLConnection().getConnection().getMetaData());
+                  _tableCache.put(key, ret);
+               }
+               return ret;
             }
          }
          return null;
@@ -39,7 +48,24 @@ public class SQLSchemaImpl implements SQLSchema
       }
    }
 
-	public List getTables(String catalog, String schema, String name)
+   private String getKey(String catalog, String schema, String name)
+   {
+      if(null == catalog)
+      {
+         catalog = "null";
+      }
+      if(null == schema)
+      {
+         schema = "null";
+      }
+
+      StringBuffer ret = new StringBuffer();
+      ret.append(catalog).append(",").append(schema).append(",").append(name);
+
+      return ret.toString();
+   }
+
+   public List getTables(String catalog, String schema, String name)
 	{
       try
       {
@@ -48,7 +74,14 @@ public class SQLSchemaImpl implements SQLSchema
 
          for (int i = 0; i < tableNames.length; i++)
          {
-            ret.add(new SQLSchema.Table(null, null, tableNames[i], _session.getSQLConnection().getConnection().getMetaData()));
+            String key = getKey(catalog, schema, name);
+            SQLSchema.Table buf = (SQLSchema.Table) _tableCache.get(key);
+            if(null == buf)
+            {
+               buf = new SQLSchema.Table(catalog, schema, tableNames[i], _session.getSQLConnection().getConnection().getMetaData());
+               _tableCache.put(key, buf);
+            }
+            ret.add(buf);
          }
          return ret;
       }
