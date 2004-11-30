4ec2be5607049d90845f2e482c768eff77b14635
Gerd Wagner <gerdhwagner@t-online.de>
2004-11-30 21:39:14 +0000

    Clean up

--- plugins/syntax/src/net/sourceforge/squirrel_sql/plugins/syntax/netbeans/SQLSyntax.java
+++ plugins/syntax/src/net/sourceforge/squirrel_sql/plugins/syntax/netbeans/SQLSyntax.java
@@ -1,16 +1,3 @@
-/*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
- * Microsystems, Inc. All Rights Reserved.
- */
-
 package net.sourceforge.squirrel_sql.plugins.syntax.netbeans;
 
 import net.sourceforge.squirrel_sql.client.session.ISession;
@@ -26,11 +13,7 @@ import java.util.Hashtable;
 import java.util.Vector;
 
 /**
- * Syntax analyzes for Java source files.
- * Tokens and internal states are given below.
- *
- * @author Miloslav Metelka
- * @version 1.00
+ * Syntax analyzes for SQL source files.
  */
 
 public class SQLSyntax extends Syntax
