4b85b867ce6cf21625b86051c7e7679e981be728
Gerd Wagner <gerdhwagner@t-online.de>
2004-12-05 14:28:41 +0000

    Bug fix

--- sql12/plugins/sqlscript/src/net/sourceforge/squirrel_sql/plugins/sqlscript/table_script/CreateDataScriptOfCurrentSQLCommand.java
+++ sql12/plugins/sqlscript/src/net/sourceforge/squirrel_sql/plugins/sqlscript/table_script/CreateDataScriptOfCurrentSQLCommand.java
@@ -1,4 +1,5 @@
 package net.sourceforge.squirrel_sql.plugins.sqlscript.table_script;
+
 /*
  * Copyright (C) 2001 Johan Compagner
  * jcompagner@j-com.nl
@@ -17,6 +18,7 @@ package net.sourceforge.squirrel_sql.plugins.sqlscript.table_script;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
@@ -35,84 +37,125 @@ import net.sourceforge.squirrel_sql.client.session.ISession;
 
 public class CreateDataScriptOfCurrentSQLCommand extends CreateDataScriptCommand
 {
-	/** Current plugin. */
-	private final SQLScriptPlugin _plugin;
+   /**
+    * Current plugin.
+    */
+   private final SQLScriptPlugin _plugin;
 
-    /**
-     * Ctor specifying the current session.
-     */
-    public CreateDataScriptOfCurrentSQLCommand(ISession session, SQLScriptPlugin plugin) {
-        super(session, plugin);
-        _plugin = plugin;
-    }
+   /**
+    * Ctor specifying the current session.
+    */
+   public CreateDataScriptOfCurrentSQLCommand(ISession session, SQLScriptPlugin plugin)
+   {
+      super(session, plugin);
+      _plugin = plugin;
+   }
 
-    /**
-     * Execute this command.
-     */
-    public void execute()
-    {
-        _session.getApplication().getThreadPool().addTask(new Runnable()
-        {
-            public void run()
+   /**
+    * Execute this command.
+    */
+   public void execute()
+   {
+      _session.getApplication().getThreadPool().addTask(new Runnable()
+      {
+         public void run()
+         {
+            SQLConnection conn = _session.getSQLConnection();
+
+            //String selectSQL = _session.getSQLPanelAPI(_plugin).getSQLScriptToBeExecuted();
+            String selectSQL = FrameWorkAcessor.getSQLPanelAPI(_session, _plugin).getSQLScriptToBeExecuted();
+
+            final StringBuffer sbRows = new StringBuffer(1000);
+            try
             {
-                SQLConnection conn = _session.getSQLConnection();
+               final Statement stmt = conn.createStatement();
+               try
+               {
+                  ResultSet srcResult = stmt.executeQuery(selectSQL);
+                  ResultSetMetaData metaData = srcResult.getMetaData();
+                  String sTable = metaData.getTableName(1);
+                  if (sTable == null || sTable.equals(""))
+                  {
 
-                //String selectSQL = _session.getSQLPanelAPI(_plugin).getSQLScriptToBeExecuted();
-                String selectSQL = FrameWorkAcessor.getSQLPanelAPI(_session, _plugin).getSQLScriptToBeExecuted();
-
-                final StringBuffer sbRows = new StringBuffer(1000);
-                try
-                {
-                    final Statement stmt = conn.createStatement();
-                    try
-                    {
-                        ResultSet srcResult = stmt.executeQuery(selectSQL);
-                        ResultSetMetaData metaData = srcResult.getMetaData();
-                        String sTable = metaData.getTableName(1);
-                        if (sTable == null || sTable.equals(""))
-                        {
-                            int iFromIndex = selectSQL.toLowerCase().indexOf("from ") + 5;
-                            int iSpaceIndex = selectSQL.indexOf(" ", iFromIndex + 2);
-                            if (iSpaceIndex == -1)
-                            {
-                                iSpaceIndex = selectSQL.length()-1;
-                            }
-
-                            sTable = selectSQL.substring(iFromIndex, iSpaceIndex).trim();
-                        }
-                        genInserts(srcResult, sTable, sbRows);
-                    }
-                    finally
-                    {
-                        try
-                        {
-                            stmt.close();
-                        }
-                        catch (Exception e)
-                        {
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    _session.getMessageHandler().showErrorMessage(e);
-                }
-                SwingUtilities.invokeLater(new Runnable()
-                {
-                    public void run()
-                    {
-                        if(sbRows.length() > 0)
-                        {
-                            //_session.getSQLPanelAPI(_plugin).appendSQLScript(sbRows.toString(), true);
-                           FrameWorkAcessor.getSQLPanelAPI(_session, _plugin).appendSQLScript(sbRows.toString(), true);
-
-                            _session.selectMainTab(ISession.IMainPanelTabIndexes.SQL_TAB);
-                        }
-                        hideAbortFrame();
-                    }
-                });
+                     int iFromIndex = getTokenBeginIndex(selectSQL, "from");
+                     sTable = getNextToken(selectSQL, iFromIndex + "from".length());
+                  }
+                  genInserts(srcResult, sTable, sbRows);
+               }
+               finally
+               {
+                  try
+                  {
+                     stmt.close();
+                  }
+                  catch (Exception e)
+                  {
+                  }
+               }
             }
-        });
-        showAbortFrame();
-    }
+            catch (Exception e)
+            {
+               _session.getMessageHandler().showErrorMessage(e);
+            }
+            SwingUtilities.invokeLater(new Runnable()
+            {
+               public void run()
+               {
+                  if (sbRows.length() > 0)
+                  {
+                     //_session.getSQLPanelAPI(_plugin).appendSQLScript(sbRows.toString(), true);
+                     FrameWorkAcessor.getSQLPanelAPI(_session, _plugin).appendSQLScript(sbRows.toString(), true);
+
+                     _session.selectMainTab(ISession.IMainPanelTabIndexes.SQL_TAB);
+                  }
+                  hideAbortFrame();
+               }
+            });
+         }
+      });
+      showAbortFrame();
+   }
+
+   private String getNextToken(String selectSQL, int startPos)
+   {
+      int curPos = startPos;
+      while(curPos < selectSQL.length() && true == Character.isWhitespace(selectSQL.charAt(curPos)))
+      {
+         // Move over leading whitespaces
+         ++curPos;
+      }
+
+      int startPosTrimed = curPos;
+
+
+      while(curPos < selectSQL.length() && false == Character.isWhitespace(selectSQL.charAt(curPos)))
+      {
+         ++curPos;
+      }
+
+      return selectSQL.substring(startPosTrimed, curPos);
+   }
+
+   private int getTokenBeginIndex(String selectSQL, String token)
+   {
+      String lowerSel = selectSQL.toLowerCase();
+      String lowerToken = token.toLowerCase().trim();
+
+      int curPos = 0;
+      while(-1 != curPos)
+      {
+         curPos = lowerSel.indexOf(lowerToken);
+
+         if(
+                -1 < curPos
+             && (0 == curPos || Character.isWhitespace(lowerSel.charAt(curPos-1)))
+             && (lowerSel.length() == curPos + lowerToken.length() || Character.isWhitespace(lowerSel.charAt(curPos + lowerToken.length())))
+           )
+         {
+            return curPos;
+         }
+      }
+
+      return curPos;
+   }
 }
