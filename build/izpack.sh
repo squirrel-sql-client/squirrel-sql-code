c1f3049bd8ed3f918d4ae177f0fa1ff335465de5
Gerd Wagner <gerdhwagner@t-online.de>
2005-06-09 21:35:56 +0000

    Introduced Standard anf Base packages in installation routine. Clean up.

--- build/izpack.sh
+++ build/izpack.sh
@@ -1,3 +1,2 @@
-/usr/local/izpack/bin/compile izpack-basic.xml -b ~/src/sqsc/sql12/build  -k standard -o /tmp/squirrel-sql-1.2beta7-install.jar
+/home/gerd/programme/IzPack/bin/compile izpack-basic.xml -b ~/work/java/squirrel1_2/sql12/build  -k standard -o ~/tmp/squirrel-sql-2.0final-install.jar
 #/usr/local/izpack/bin/compile izpack-basic.xml -b ~/src/sqsc/sql12/build  -k standard-kunststoff -o /tmp/squirrel-sql-1.2beta7-install.jar
-# ~/apps/izpack/bin/compile izpack-basic-win.xml -b ~/src/sql12/sql12/build  -k standard-kunststoff -o /tmp/win9598-squirrel-sql-1.1final-install.jar
