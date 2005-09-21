64bc719c4d1b00663539111820b2e7ec613fdc07
Robert Manning <robert.m.manning@gmail.com>
2005-09-21 11:21:06 +0000

    class that determines the value to send for boolean types to the database, based on database product name.

---
+++ fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/cellcomponent/DatabaseSpecificBooleanValue.java
@@ -0,0 +1,74 @@
+package net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent;
+
+/*
+ * Copyright (C) 2005 Rob Manning, Gerd Wagner
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+public class DatabaseSpecificBooleanValue {
+   
+    private static final IBooleanValue[] _booleans =
+                                            new IBooleanValue[] {
+                                                        new SybaseBoolean(),
+                                                        new MSSQLServerBoolean()
+                                            };
+    
+    
+    
+    public static String getBooleanValue(String orig, 
+                                         String databaseProductName)
+    {
+        for (int i = 0; i < _booleans.length; i++) {
+            if(_booleans[i].productMatches(databaseProductName)) {
+                return _booleans[i].getBooleanValue(orig);
+            }
+        }
+        return orig;
+    }
+    
+    private static interface IBooleanValue {
+        public boolean productMatches(String databaseProductName);
+        public String getBooleanValue(String originalValue);
+    }    
+    
+    private static class SybaseBoolean implements IBooleanValue {
+        
+        public boolean productMatches(String databaseProductName) {
+            return "Adaptive Server Enterprise".equals(databaseProductName);
+        }
+        
+        public String getBooleanValue(String orig) {
+            String result = orig;
+            if ("false".equalsIgnoreCase(orig)) {
+                result = "0";
+            }
+            if ("true".equalsIgnoreCase(orig)) {
+                result = "1";
+            }
+            return result;
+        }
+    }
+    
+    private static class MSSQLServerBoolean extends SybaseBoolean {
+        
+        public boolean productMatches(String databaseProductName) {
+            return "Microsoft SQL Server".equals(databaseProductName);
+        }
+        
+    }
+
+}
