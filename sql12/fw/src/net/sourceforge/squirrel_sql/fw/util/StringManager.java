ce331578103e2c740e1e5dad6465099d7e99f4cc
Gerd Wagner <gerdhwagner@t-online.de>
2005-09-22 03:09:19 +0000

    The beginning of i18n

--- sql12/fw/src/net/sourceforge/squirrel_sql/fw/util/StringManager.java
+++ sql12/fw/src/net/sourceforge/squirrel_sql/fw/util/StringManager.java
@@ -21,7 +21,6 @@ import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.util.Vector;
 
 import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
 import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
@@ -36,47 +35,22 @@ public class StringManager
 	/** Logger for this class. */
 	private static ILogger s_log = LoggerController.createLogger(StringManager.class);
 
-    private static Vector _localeChangeListeners = new Vector();
-
-
-    /** Contains the localised strings. */
+	/** Contains the localised strings. */
 	private ResourceBundle _rsrcBundle;
 
-
-   public static void setDefaultLocale(Locale defLoc)
-   {
-      Locale.setDefault(defLoc);
-
-      for (int i = 0; i < _localeChangeListeners.size(); i++)
-      {
-         ((LocaleChangeListener) _localeChangeListeners.get(i)).localeChanged();
-      }
-   }
-
-
-
-   /**
+	/**
 	 * Ctor specifying the package name. Attempt to load a resource bundle
 	 * from the package directory.
 	 *
 	 * @param	packageName	Name of package
 	 * @param	classLoader	Class loader to use
 	 */
-	StringManager(final String packageName, final ClassLoader loader)
+	StringManager(String packageName, ClassLoader loader)
 	{
 		super();
-		_rsrcBundle = ResourceBundle.getBundle(packageName + ".I18NStrings", Locale.getDefault(), loader);
-
-      _localeChangeListeners.add(
-           new LocaleChangeListener()
-           {
-              public void localeChanged()
-              {
-                 _rsrcBundle = ResourceBundle.getBundle(packageName + ".I18NStrings", Locale.getDefault(), loader);
-              }
-           }
-      );
-   }
+		_rsrcBundle = ResourceBundle.getBundle(packageName + ".I18NStrings",
+						Locale.getDefault(), loader);
+	}
 
 	/**
 	 * Retrieve the localized string for the passed key. If it isn't found
@@ -175,10 +149,4 @@ public class StringManager
 			return msg + ": " + ex.toString();
 		}
 	}
-
-   private static interface LocaleChangeListener
-   {
-      void localeChanged();
-   }
-
 }
--- sql12/plugins/i18n/src/net/sourceforge/squirrel_sql/plugins/i18n/I18nPanel.java
+++ sql12/plugins/i18n/src/net/sourceforge/squirrel_sql/plugins/i18n/I18nPanel.java
@@ -13,7 +13,23 @@ public class I18nPanel extends JPanel
 
 
    JComboBox cboLocales = new JComboBox();
-   JList lstTranslations = new JList();
+   JButton btnLoad = new JButton(s_stringMgr.getString("I18n.loadTabs")); // i18n{I18n.loadTabs=Load tabs}
+
+   JTextField txtWorkingDir = new JTextField();
+   JButton btnChooseDir = new JButton("...");
+   JCheckBox chkShowSelectedPackages = new JCheckBox(s_stringMgr.getString("I18n.showSelectedPackages"));
+   // i18n{I18n.showSelectedPackages=Show translations for selected packages only}
+
+   JCheckBox chkShowUntranslated = new JCheckBox(s_stringMgr.getString("I18n.showUntranslated"));
+   // i18n{I18n.showUntranslated=Show untranslated resources only}
+
+   JTabbedPane tabTranslations = new JTabbedPane();
+
+
+   JTree treePackages = new JTree();
+   JTable tblTranslations = new JTable();
+   JTextField txtNote;
+
 
    public I18nPanel()
    {
@@ -22,15 +38,50 @@ public class I18nPanel extends JPanel
       GridBagConstraints gbc;
 
       gbc = new GridBagConstraints(0,0,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,5,5),0,0);
-      add(new JLabel(s_stringMgr.getString("I18n.locales")), gbc);
+      add(new JLabel(s_stringMgr.getString("I18n.locales")), gbc); // i18n{I18n.locales=Locales}
 
-      gbc = new GridBagConstraints(1,0,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,0,5),0,0);
+      gbc = new GridBagConstraints(1,0,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5,0,0,5),0,0);
       add(cboLocales, gbc);
 
-      gbc = new GridBagConstraints(0,1,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,0,5),0,0);
-      add(new JLabel(s_stringMgr.getString("I18n.translations")), gbc);
+      gbc = new GridBagConstraints(2,0,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,0,0,5),0,0);
+      add(btnLoad, gbc);
 
-      gbc = new GridBagConstraints(0,2,2,1,1,1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(5,5,0,5),0,0);
-      add(lstTranslations, gbc);
+
+      gbc = new GridBagConstraints(0,1,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,5,5),0,0);
+      add(new JLabel(s_stringMgr.getString("I18n.WorkingDir")), gbc); // i18n{I18n.WorkingDir=Working Directory}
+
+      gbc = new GridBagConstraints(1,1,1,1,1,0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5,0,0,5),0,0);
+      add(txtWorkingDir, gbc);
+
+      gbc = new GridBagConstraints(2,1,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,0,0,5),0,0);
+      add(btnChooseDir, gbc);
+
+
+      JPanel pnlChk = new JPanel(new GridBagLayout());
+
+      gbc = new GridBagConstraints(0,0,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,5,5),0,0);
+      pnlChk.add(chkShowSelectedPackages, gbc);
+
+      gbc = new GridBagConstraints(1,0,1,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,5,5),0,0);
+      pnlChk.add(chkShowUntranslated, gbc);
+
+      gbc = new GridBagConstraints(0,2,3,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,0,0,0),0,0);
+      add(pnlChk, gbc);
+
+
+      gbc = new GridBagConstraints(0,3,3,1,1,1, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(5,5,0,5),0,0);
+      add(tabTranslations, gbc);
+
+      tabTranslations.add(s_stringMgr.getString("I18n.packages"), treePackages); // i18n{I18n.packages=Packages}
+      tabTranslations.add(s_stringMgr.getString("I18n.translations"), tblTranslations); // i18n{I18n.translations=Translations}
+
+      txtNote = new JTextField();
+      txtNote.setText(s_stringMgr.getString("I18n.changeApplicationDefaultLocaleNote"));
+      txtNote.setEditable(false);
+      // i18n{I18n.changeApplicationDefaultLocaleNote=Example VM Parameters to change used locale: -Duser.language=en -Duser.country=US}
+      gbc = new GridBagConstraints(0,4,3,1,0,0, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(5,5,0,5),0,0);
+      add(txtNote, gbc);
+
+
    }
 }
