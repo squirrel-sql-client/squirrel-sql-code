10e32196bdc8b9dbc6689b8880e79773ec4ee3c1
Gerd Wagner <gerdhwagner@t-online.de>
2005-10-17 21:36:20 +0000

    i18n

--- sql12/plugins/graph/src/net/sourceforge/squirrel_sql/plugins/graph/GraphMainPanelTab.java
+++ sql12/plugins/graph/src/net/sourceforge/squirrel_sql/plugins/graph/GraphMainPanelTab.java
@@ -1,13 +1,20 @@
 package net.sourceforge.squirrel_sql.plugins.graph;
 
 import net.sourceforge.squirrel_sql.client.session.mainpanel.BaseMainPanelTab;
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
+
 import java.awt.*;
 
 
 public class GraphMainPanelTab extends BaseMainPanelTab
 {
-   private GraphDesktopController _desktopController;
-   private String _title = "New table graph";
+	private static final StringManager s_stringMgr =
+		StringManagerFactory.getStringManager(GraphMainPanelTab.class);
+
+	private GraphDesktopController _desktopController;
+	// i18n[graph.newGraph=New table graph]
+	private String _title = s_stringMgr.getString("graph.newGraph");
 
    public GraphMainPanelTab(GraphDesktopController desktopController)
    {
@@ -27,7 +34,8 @@ public class GraphMainPanelTab extends BaseMainPanelTab
 
    public String getHint()
    {
-      return "Right click table in object tree to add to graph";
+		// i18n[graph.rightClickTable=Right click table in object tree to add to graph]
+		return s_stringMgr.getString("graph.rightClickTable");
    }
 
    public Component getComponent()
--- sql12/plugins/graph/src/net/sourceforge/squirrel_sql/plugins/graph/SqlScriptAcessor.java
+++ sql12/plugins/graph/src/net/sourceforge/squirrel_sql/plugins/graph/SqlScriptAcessor.java
@@ -2,18 +2,24 @@ package net.sourceforge.squirrel_sql.plugins.graph;
 
 import net.sourceforge.squirrel_sql.client.session.ISession;
 import net.sourceforge.squirrel_sql.fw.sql.ITableInfo;
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 
 import javax.swing.*;
 
 
 public class SqlScriptAcessor
 {
-   public static void scriptTablesToSQLEntryArea(ISession session, ITableInfo[] tableInfos)
+	private static final StringManager s_stringMgr =
+		StringManagerFactory.getStringManager(SqlScriptAcessor.class);
+
+	public static void scriptTablesToSQLEntryArea(ISession session, ITableInfo[] tableInfos)
    {
       ScriptInterface si = (ScriptInterface) session.getApplication().getPluginManager().bindExternalPluginService("sqlscript", ScriptInterface.class);
       if (null == si)
       {
-         String msg = "Scripting is only available with the SQL Scripts Plugin.\n Get the plugin from www.squirrelsql.org. It's free.";
+			// i18n[graph.scriptPlugNeeded=Scripting is only available with the SQL Scripts Plugin.\nGet the plugin from www.squirrelsql.org. It's free.]
+			String msg = s_stringMgr.getString("graph.scriptPlugNeeded");
          JOptionPane.showMessageDialog(session.getApplication().getMainFrame(), msg);
          return;
       }
