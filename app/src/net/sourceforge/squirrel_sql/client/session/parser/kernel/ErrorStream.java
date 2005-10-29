0c1e44cb8958497985e2b6a4d9f9588308bc607f
Robert Manning <robert.m.manning@gmail.com>
2005-10-29 14:01:26 +0000

    i18n

--- app/src/net/sourceforge/squirrel_sql/client/session/parser/kernel/ErrorStream.java
+++ app/src/net/sourceforge/squirrel_sql/client/session/parser/kernel/ErrorStream.java
@@ -1,10 +1,33 @@
 package net.sourceforge.squirrel_sql.client.session.parser.kernel;
 
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
+
 public class ErrorStream {
 
 	int count;  // number of errors detected
 	public String fileName;
 
+    /** Internationalized strings for this class */
+    private static final StringManager s_stringMgr =
+        StringManagerFactory.getStringManager(ErrorStream.class);
+
+    // i18n[ErrorStream.expected=expected]
+    private static final String EXPECTED_STR = 
+        s_stringMgr.getString("ErrorStream.expected");
+    
+    // i18n[ErrorStream.notexpected=not expected]
+    private static final String NOT_EXPECTED_STR = 
+        s_stringMgr.getString("ErrorStream.notexpected");
+    
+    // i18n[ErrorStream.invalid=invalid]
+    private static final String INVALID_STR = 
+        s_stringMgr.getString("ErrorStream.invalid");
+    
+    // i18n[ErrorStream.unexpectedsymbol=this symbol not expected in]
+    private static final String UNEXPECTED_SYMBOL_STR = 
+        s_stringMgr.getString("ErrorStream.unexpectedsymbol");
+    
 	public ErrorStream() {
 		count = 0;
 	}
@@ -17,156 +40,158 @@ public class ErrorStream {
 		String s;
 		count++;
 		switch (n) {
-			case 0: {s = "EOF expected"; break;}
-			case 1: {s = "ident expected"; break;}
-			case 2: {s = "intValue expected"; break;}
-			case 3: {s = "float expected"; break;}
-			case 4: {s = "SQLString expected"; break;}
-			case 5: {s = "OpenParens expected"; break;}
-			case 6: {s = "\";\" expected"; break;}
-			case 7: {s = "\"UNION\" expected"; break;}
-			case 8: {s = "\"EXCEPT\" expected"; break;}
-			case 9: {s = "\"INTERSECT\" expected"; break;}
-			case 10: {s = "\"MINUS\" expected"; break;}
-			case 11: {s = "\"ALL\" expected"; break;}
-			case 12: {s = "\"UPDATE\" expected"; break;}
-			case 13: {s = "\"SET\" expected"; break;}
-			case 14: {s = "\"=\" expected"; break;}
-			case 15: {s = "\"INSERT\" expected"; break;}
-			case 16: {s = "\"INTO\" expected"; break;}
-			case 17: {s = "\"VALUES\" expected"; break;}
-			case 18: {s = "\"DELETE\" expected"; break;}
-			case 19: {s = "\"FROM\" expected"; break;}
-			case 20: {s = "\"SELECT\" expected"; break;}
-			case 21: {s = "\"DISTINCT\" expected"; break;}
-			case 22: {s = "\".\" expected"; break;}
-			case 23: {s = "\"AS\" expected"; break;}
-			case 24: {s = "\"JOIN\" expected"; break;}
-			case 25: {s = "\"CROSS\" expected"; break;}
-			case 26: {s = "\"NATURAL\" expected"; break;}
-			case 27: {s = "\"INNER\" expected"; break;}
-			case 28: {s = "\"FULL\" expected"; break;}
-			case 29: {s = "\"LEFT\" expected"; break;}
-			case 30: {s = "\"RIGHT\" expected"; break;}
-			case 31: {s = "\"OUTER\" expected"; break;}
-			case 32: {s = "\"ON\" expected"; break;}
-			case 33: {s = "\"USING\" expected"; break;}
-			case 34: {s = "\"WHERE\" expected"; break;}
-			case 35: {s = "\"GROUP\" expected"; break;}
-			case 36: {s = "\"BY\" expected"; break;}
-			case 37: {s = "\"HAVING\" expected"; break;}
-			case 38: {s = "\"ORDER\" expected"; break;}
-			case 39: {s = "\"*\" expected"; break;}
-			case 40: {s = "\"TIMESTAMP\" expected"; break;}
-			case 41: {s = "\"UPPER\" expected"; break;}
-			case 42: {s = "\"MONTH\" expected"; break;}
-			case 43: {s = "\"YEAR\" expected"; break;}
-			case 44: {s = "\"COUNT\" expected"; break;}
-			case 45: {s = "\"SUM\" expected"; break;}
-			case 46: {s = "\"MAX\" expected"; break;}
-			case 47: {s = "\"MIN\" expected"; break;}
-			case 48: {s = "\"AVG\" expected"; break;}
-			case 49: {s = "\"NULL\" expected"; break;}
-			case 50: {s = "\"DESC\" expected"; break;}
-			case 51: {s = "\"ASC\" expected"; break;}
-			case 52: {s = "\"-\" expected"; break;}
-			case 53: {s = "\":\" expected"; break;}
-			case 54: {s = "\"NOT\" expected"; break;}
-			case 55: {s = "\"/\" expected"; break;}
-			case 56: {s = "\"+\" expected"; break;}
-			case 57: {s = "\"AND\" expected"; break;}
-			case 58: {s = "\"OR\" expected"; break;}
-			case 59: {s = "\"LIKE\" expected"; break;}
-			case 60: {s = "\"ESCAPE\" expected"; break;}
-			case 61: {s = "\"IS\" expected"; break;}
-			case 62: {s = "\"<>\" expected"; break;}
-			case 63: {s = "\"<\" expected"; break;}
-			case 64: {s = "\"<=\" expected"; break;}
-			case 65: {s = "\">\" expected"; break;}
-			case 66: {s = "\">=\" expected"; break;}
-			case 67: {s = "\"BETWEEN\" expected"; break;}
-			case 68: {s = "\"IN\" expected"; break;}
-			case 69: {s = "\"COMMIT\" expected"; break;}
-			case 70: {s = "\"ROLLBACK\" expected"; break;}
-			case 71: {s = "\"WORK\" expected"; break;}
-			case 72: {s = "\"CHAR\" expected"; break;}
-			case 73: {s = "\"CHARACTER\" expected"; break;}
-			case 74: {s = "\"VARCHAR\" expected"; break;}
-			case 75: {s = "\"INTEGER\" expected"; break;}
-			case 76: {s = "\"INT\" expected"; break;}
-			case 77: {s = "\"SMALLINT\" expected"; break;}
-			case 78: {s = "\"NUMERIC\" expected"; break;}
-			case 79: {s = "\"DATE\" expected"; break;}
-			case 80: {s = "\"TIME\" expected"; break;}
-			case 81: {s = "\"DEFAULT\" expected"; break;}
-			case 82: {s = "\"PRIMARY\" expected"; break;}
-			case 83: {s = "\"KEY\" expected"; break;}
-			case 84: {s = "\"FOREIGN\" expected"; break;}
-			case 85: {s = "\"REFERENCES\" expected"; break;}
-			case 86: {s = "\"MATCH\" expected"; break;}
-			case 87: {s = "\"PARTIAL\" expected"; break;}
-			case 88: {s = "\"CASCADE\" expected"; break;}
-			case 89: {s = "\"NO\" expected"; break;}
-			case 90: {s = "\"ACTION\" expected"; break;}
-			case 91: {s = "\"UNIQUE\" expected"; break;}
-			case 92: {s = "\"CHECK\" expected"; break;}
-			case 93: {s = "\"CREATE\" expected"; break;}
-			case 94: {s = "\"TABLE\" expected"; break;}
-			case 95: {s = "\"RESTRICT\" expected"; break;}
-			case 96: {s = "\"DROP\" expected"; break;}
-			case 97: {s = "\"ADD\" expected"; break;}
-			case 98: {s = "\"ALTER\" expected"; break;}
-			case 99: {s = "\"CONSTRAINT\" expected"; break;}
-			case 100: {s = "\"INDEX\" expected"; break;}
-			case 101: {s = "\",\" expected"; break;}
-			case 102: {s = "\")\" expected"; break;}
-			case 103: {s = "not expected"; break;}
-			case 104: {s = "invalid DropPart"; break;}
-			case 105: {s = "invalid Alter"; break;}
-			case 106: {s = "invalid Add"; break;}
-			case 107: {s = "invalid CascadeRestrict"; break;}
-			case 108: {s = "invalid CreatePart"; break;}
-			case 109: {s = "invalid ForeignKey"; break;}
-			case 110: {s = "invalid ForeignKey"; break;}
-			case 111: {s = "invalid ForeignKey"; break;}
-			case 112: {s = "invalid ForeignKey"; break;}
-			case 113: {s = "invalid ColumnDefault"; break;}
-			case 114: {s = "invalid DataType"; break;}
-			case 115: {s = "invalid InSetExpr"; break;}
-			case 116: {s = "invalid LikeTest"; break;}
-			case 117: {s = "invalid WordOperator"; break;}
-			case 118: {s = "invalid MathOperator"; break;}
-			case 119: {s = "invalid TestExpr"; break;}
-			case 120: {s = "invalid TestExpr"; break;}
-			case 121: {s = "invalid Operator"; break;}
-			case 122: {s = "invalid Term"; break;}
-			case 123: {s = "invalid Term"; break;}
-			case 124: {s = "invalid Relation"; break;}
-			case 125: {s = "invalid OrderByField"; break;}
-			case 126: {s = "invalid Field"; break;}
-			case 127: {s = "invalid ColumnFunction"; break;}
-			case 128: {s = "invalid ColumnFunction"; break;}
-			case 129: {s = "invalid FunctionExpr"; break;}
-			case 130: {s = "invalid SelectField"; break;}
-			case 131: {s = "invalid JoinExpr"; break;}
-			case 132: {s = "invalid JoinType"; break;}
-			case 133: {s = "invalid JoinStmt"; break;}
-			case 134: {s = "this symbol not expected in OrderByClause"; break;}
-			case 135: {s = "this symbol not expected in HavingClause"; break;}
-			case 136: {s = "this symbol not expected in GroupByClause"; break;}
-			case 137: {s = "this symbol not expected in FromClause"; break;}
-			case 138: {s = "this symbol not expected in SelectClause"; break;}
-			case 139: {s = "invalid ColumnName"; break;}
-			case 140: {s = "this symbol not expected in WhereClause"; break;}
-			case 141: {s = "invalid SetOperator"; break;}
-			case 142: {s = "invalid Transaction"; break;}
-			case 143: {s = "invalid AlterTable"; break;}
-			case 144: {s = "invalid Drop"; break;}
-			case 145: {s = "invalid CreateStmt"; break;}
-			case 146: {s = "invalid InsertStmt"; break;}
-			case 147: {s = "invalid SQLStatement"; break;}
+			case 0: {s = "EOF "+EXPECTED_STR; break;}
+			case 1: {s = "ident "+EXPECTED_STR; break;}
+			case 2: {s = "intValue "+EXPECTED_STR; break;}
+			case 3: {s = "float "+EXPECTED_STR; break;}
+			case 4: {s = "SQLString "+EXPECTED_STR; break;}
+			case 5: {s = "OpenParens "+EXPECTED_STR; break;}
+			case 6: {s = "\";\" "+EXPECTED_STR; break;}
+			case 7: {s = "\"UNION\" "+EXPECTED_STR; break;}
+			case 8: {s = "\"EXCEPT\" "+EXPECTED_STR; break;}
+			case 9: {s = "\"INTERSECT\" "+EXPECTED_STR; break;}
+			case 10: {s = "\"MINUS\" "+EXPECTED_STR; break;}
+			case 11: {s = "\"ALL\" "+EXPECTED_STR; break;}
+			case 12: {s = "\"UPDATE\" "+EXPECTED_STR; break;}
+			case 13: {s = "\"SET\" "+EXPECTED_STR; break;}
+			case 14: {s = "\"=\" "+EXPECTED_STR; break;}
+			case 15: {s = "\"INSERT\" "+EXPECTED_STR; break;}
+			case 16: {s = "\"INTO\" "+EXPECTED_STR; break;}
+			case 17: {s = "\"VALUES\" "+EXPECTED_STR; break;}
+			case 18: {s = "\"DELETE\" "+EXPECTED_STR; break;}
+			case 19: {s = "\"FROM\" "+EXPECTED_STR; break;}
+			case 20: {s = "\"SELECT\" "+EXPECTED_STR; break;}
+			case 21: {s = "\"DISTINCT\" "+EXPECTED_STR; break;}
+			case 22: {s = "\".\" "+EXPECTED_STR; break;}
+			case 23: {s = "\"AS\" "+EXPECTED_STR; break;}
+			case 24: {s = "\"JOIN\" "+EXPECTED_STR; break;}
+			case 25: {s = "\"CROSS\" "+EXPECTED_STR; break;}
+			case 26: {s = "\"NATURAL\" "+EXPECTED_STR; break;}
+			case 27: {s = "\"INNER\" "+EXPECTED_STR; break;}
+			case 28: {s = "\"FULL\" "+EXPECTED_STR; break;}
+			case 29: {s = "\"LEFT\" "+EXPECTED_STR; break;}
+			case 30: {s = "\"RIGHT\" "+EXPECTED_STR; break;}
+			case 31: {s = "\"OUTER\" "+EXPECTED_STR; break;}
+			case 32: {s = "\"ON\" "+EXPECTED_STR; break;}
+			case 33: {s = "\"USING\" "+EXPECTED_STR; break;}
+			case 34: {s = "\"WHERE\" "+EXPECTED_STR; break;}
+			case 35: {s = "\"GROUP\" "+EXPECTED_STR; break;}
+			case 36: {s = "\"BY\" "+EXPECTED_STR; break;}
+			case 37: {s = "\"HAVING\" "+EXPECTED_STR; break;}
+			case 38: {s = "\"ORDER\" "+EXPECTED_STR; break;}
+			case 39: {s = "\"*\" "+EXPECTED_STR; break;}
+			case 40: {s = "\"TIMESTAMP\" "+EXPECTED_STR; break;}
+			case 41: {s = "\"UPPER\" "+EXPECTED_STR; break;}
+			case 42: {s = "\"MONTH\" "+EXPECTED_STR; break;}
+			case 43: {s = "\"YEAR\" "+EXPECTED_STR; break;}
+			case 44: {s = "\"COUNT\" "+EXPECTED_STR; break;}
+			case 45: {s = "\"SUM\" "+EXPECTED_STR; break;}
+			case 46: {s = "\"MAX\" "+EXPECTED_STR; break;}
+			case 47: {s = "\"MIN\" "+EXPECTED_STR; break;}
+			case 48: {s = "\"AVG\" "+EXPECTED_STR; break;}
+			case 49: {s = "\"NULL\" "+EXPECTED_STR; break;}
+			case 50: {s = "\"DESC\" "+EXPECTED_STR; break;}
+			case 51: {s = "\"ASC\" "+EXPECTED_STR; break;}
+			case 52: {s = "\"-\" "+EXPECTED_STR; break;}
+			case 53: {s = "\":\" "+EXPECTED_STR; break;}
+			case 54: {s = "\"NOT\" "+EXPECTED_STR; break;}
+			case 55: {s = "\"/\" "+EXPECTED_STR; break;}
+			case 56: {s = "\"+\" "+EXPECTED_STR; break;}
+			case 57: {s = "\"AND\" "+EXPECTED_STR; break;}
+			case 58: {s = "\"OR\" "+EXPECTED_STR; break;}
+			case 59: {s = "\"LIKE\" "+EXPECTED_STR; break;}
+			case 60: {s = "\"ESCAPE\" "+EXPECTED_STR; break;}
+			case 61: {s = "\"IS\" "+EXPECTED_STR; break;}
+			case 62: {s = "\"<>\" "+EXPECTED_STR; break;}
+			case 63: {s = "\"<\" "+EXPECTED_STR; break;}
+			case 64: {s = "\"<=\" "+EXPECTED_STR; break;}
+			case 65: {s = "\">\" "+EXPECTED_STR; break;}
+			case 66: {s = "\">=\" "+EXPECTED_STR; break;}
+			case 67: {s = "\"BETWEEN\" "+EXPECTED_STR; break;}
+			case 68: {s = "\"IN\" "+EXPECTED_STR; break;}
+			case 69: {s = "\"COMMIT\" "+EXPECTED_STR; break;}
+			case 70: {s = "\"ROLLBACK\" "+EXPECTED_STR; break;}
+			case 71: {s = "\"WORK\" "+EXPECTED_STR; break;}
+			case 72: {s = "\"CHAR\" "+EXPECTED_STR; break;}
+			case 73: {s = "\"CHARACTER\" "+EXPECTED_STR; break;}
+			case 74: {s = "\"VARCHAR\" "+EXPECTED_STR; break;}
+			case 75: {s = "\"INTEGER\" "+EXPECTED_STR; break;}
+			case 76: {s = "\"INT\" "+EXPECTED_STR; break;}
+			case 77: {s = "\"SMALLINT\" "+EXPECTED_STR; break;}
+			case 78: {s = "\"NUMERIC\" "+EXPECTED_STR; break;}
+			case 79: {s = "\"DATE\" "+EXPECTED_STR; break;}
+			case 80: {s = "\"TIME\" "+EXPECTED_STR; break;}
+			case 81: {s = "\"DEFAULT\" "+EXPECTED_STR; break;}
+			case 82: {s = "\"PRIMARY\" "+EXPECTED_STR; break;}
+			case 83: {s = "\"KEY\" "+EXPECTED_STR; break;}
+			case 84: {s = "\"FOREIGN\" "+EXPECTED_STR; break;}
+			case 85: {s = "\"REFERENCES\" "+EXPECTED_STR; break;}
+			case 86: {s = "\"MATCH\" "+EXPECTED_STR; break;}
+			case 87: {s = "\"PARTIAL\" "+EXPECTED_STR; break;}
+			case 88: {s = "\"CASCADE\" "+EXPECTED_STR; break;}
+			case 89: {s = "\"NO\" "+EXPECTED_STR; break;}
+			case 90: {s = "\"ACTION\" "+EXPECTED_STR; break;}
+			case 91: {s = "\"UNIQUE\" "+EXPECTED_STR; break;}
+			case 92: {s = "\"CHECK\" "+EXPECTED_STR; break;}
+			case 93: {s = "\"CREATE\" "+EXPECTED_STR; break;}
+			case 94: {s = "\"TABLE\" "+EXPECTED_STR; break;}
+			case 95: {s = "\"RESTRICT\" "+EXPECTED_STR; break;}
+			case 96: {s = "\"DROP\" "+EXPECTED_STR; break;}
+			case 97: {s = "\"ADD\" "+EXPECTED_STR; break;}
+			case 98: {s = "\"ALTER\" "+EXPECTED_STR; break;}
+			case 99: {s = "\"CONSTRAINT\" "+EXPECTED_STR; break;}
+			case 100: {s = "\"INDEX\" "+EXPECTED_STR; break;}
+			case 101: {s = "\",\" "+EXPECTED_STR; break;}
+			case 102: {s = "\")\" "+EXPECTED_STR; break;}
+			case 103: {s = NOT_EXPECTED_STR; break;}
+			case 104: {s = INVALID_STR +" DropPart"; break;}
+			case 105: {s = INVALID_STR +" Alter"; break;}
+			case 106: {s = INVALID_STR +" Add"; break;}
+			case 107: {s = INVALID_STR +" CascadeRestrict"; break;}
+			case 108: {s = INVALID_STR +" CreatePart"; break;}
+			case 109: {s = INVALID_STR +" ForeignKey"; break;}
+			case 110: {s = INVALID_STR +" ForeignKey"; break;}
+			case 111: {s = INVALID_STR +" ForeignKey"; break;}
+			case 112: {s = INVALID_STR +" ForeignKey"; break;}
+			case 113: {s = INVALID_STR +" ColumnDefault"; break;}
+			case 114: {s = INVALID_STR +" DataType"; break;}
+			case 115: {s = INVALID_STR +" InSetExpr"; break;}
+			case 116: {s = INVALID_STR +" LikeTest"; break;}
+			case 117: {s = INVALID_STR +" WordOperator"; break;}
+			case 118: {s = INVALID_STR +" MathOperator"; break;}
+			case 119: {s = INVALID_STR +" TestExpr"; break;}
+			case 120: {s = INVALID_STR +" TestExpr"; break;}
+			case 121: {s = INVALID_STR +" Operator"; break;}
+			case 122: {s = INVALID_STR +" Term"; break;}
+			case 123: {s = INVALID_STR +" Term"; break;}
+			case 124: {s = INVALID_STR +" Relation"; break;}
+			case 125: {s = INVALID_STR +" OrderByField"; break;}
+			case 126: {s = INVALID_STR +" Field"; break;}
+			case 127: {s = INVALID_STR +" ColumnFunction"; break;}
+			case 128: {s = INVALID_STR +" ColumnFunction"; break;}
+			case 129: {s = INVALID_STR +" FunctionExpr"; break;}
+			case 130: {s = INVALID_STR +" SelectField"; break;}
+			case 131: {s = INVALID_STR +" JoinExpr"; break;}
+			case 132: {s = INVALID_STR +" JoinType"; break;}
+			case 133: {s = INVALID_STR +" JoinStmt"; break;}
+			case 134: {s = UNEXPECTED_SYMBOL_STR +" OrderByClause"; break;}
+			case 135: {s = UNEXPECTED_SYMBOL_STR +" HavingClause"; break;}
+			case 136: {s = UNEXPECTED_SYMBOL_STR +" GroupByClause"; break;}
+			case 137: {s = UNEXPECTED_SYMBOL_STR +" FromClause"; break;}
+			case 138: {s = UNEXPECTED_SYMBOL_STR +" SelectClause"; break;}
+			case 139: {s = INVALID_STR +" ColumnName"; break;}
+			case 140: {s = UNEXPECTED_SYMBOL_STR +" WhereClause"; break;}
+			case 141: {s = INVALID_STR +" SetOperator"; break;}
+			case 142: {s = INVALID_STR +" Transaction"; break;}
+			case 143: {s = INVALID_STR +" AlterTable"; break;}
+			case 144: {s = INVALID_STR +" Drop"; break;}
+			case 145: {s = INVALID_STR +" CreateStmt"; break;}
+			case 146: {s = INVALID_STR +" InsertStmt"; break;}
+			case 147: {s = INVALID_STR +" SQLStatement"; break;}
 
-			default: s = "Syntax error " + n;
+            // i18n[ErrorStream.error.syntax=Syntax error {0}]
+            default: s = s_stringMgr.getString("ErrorStream.error.syntax", 
+                                               new Integer(n));
 		}
 		StoreError(n, line, col, s);
 	}
@@ -177,7 +202,11 @@ public class ErrorStream {
 		switch (n) {
 			// for example: case 0: s = "invalid character"; break;
 			// perhaps insert application specific error messages here
-			default: s = "Semantic error " + n; break;
+			default:
+                // i18n[ErrorStream.error.semantic=Semantic error {0}]
+                s = s_stringMgr.getString("ErrorStream.error.semantic", 
+                                          new Integer(n)); 
+                break;
 		}
 		StoreError(n, line, col, s);
 	}
@@ -188,9 +217,21 @@ public class ErrorStream {
 
 	protected void Summarize () {
 		switch (count) {
-			case 0 : ParserLogger.log("No errors detected"); break;
-			case 1 : ParserLogger.log("1 error detected"); break;
-			default: ParserLogger.log(count + " errors detected"); break;
+            
+			case 0 :
+                // i18n[ErrorStream.noerrors=No errors detected]
+                ParserLogger.log(s_stringMgr.getString("ErrorStream.noerrors")); 
+                break;
+			case 1 : 
+                // i18n[ErrorStream.oneerror=1 error detected]
+                ParserLogger.log(s_stringMgr.getString("ErrorStream.oneerror")); 
+                break;
+			default:
+                // i18n[ErrorStream.multipleerrors={0} errors detected]
+                String msg = s_stringMgr.getString("ErrorStream.multipleerrors",
+                                                   new Integer(count));
+                ParserLogger.log(msg); 
+                break;
 		}
 	}
 
