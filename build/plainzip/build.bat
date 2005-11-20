914f45e06bfb71b70818486dc80700d03af02eb7
Robert Manning <robert.m.manning@gmail.com>
2005-11-20 13:02:46 +0000

    initial release.  Files to automate the building of plain zip distros of squirrel.  Eventually, this should be incorporated into the build.xml.  This provides the "recipe" of how it can be done in an "automated" fashion.  Run build.bat on windows to produce the *.zip's and run build.sh on Linux to produce the *.tar.gz's.

---
+++ build/plainzip/build.bat
@@ -0,0 +1,40 @@
+set PLAIN_ZIP_DIR=c:\home\projects\squirrel-sql\sql12\build\plainzip
+
+set INSTALL_JAR=c:\tmp\squirrel-sql-2.1rc2-install.jar
+
+set VERSION=2.1rc2
+
+
+rm -f *.zip
+rm -rf tmp
+mkdir tmp
+
+java -jar %INSTALL_JAR% auto_install_base.xml
+
+copy squirrel-sql.bat "tmp\SQuirreL SQL Client\"
+
+cd tmp
+
+jar -cvf squirrel-sql-%VERSION%-base.zip "SQuirreL SQL Client"
+
+mv squirrel-sql-%VERSION%-base.zip ..
+
+cd ..
+
+rm -rf tmp
+mkdir tmp
+
+
+java -jar %INSTALL_JAR% auto_install_standard.xml
+
+copy squirrel-sql.bat "tmp\SQuirreL SQL Client\"
+
+cd tmp
+
+jar -cvf squirrel-sql-%VERSION%-standard.zip "SQuirreL SQL Client"
+
+mv squirrel-sql-%VERSION%-standard.zip ..
+
+cd ..
+
+rm -rf tmp
