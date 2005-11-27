d6798ab6cd2914debe0d7b3231594a915c7c401b
Robert Manning <robert.m.manning@gmail.com>
2005-11-27 00:32:56 +0000

    1276957: instead of holding onto the ResultSet that's given to us, read through it and store the rows in a list.

--- fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/DatabaseTypesDataSet.java
+++ fw/src/net/sourceforge/squirrel_sql/fw/datasetviewer/DatabaseTypesDataSet.java
@@ -20,6 +20,8 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.sourceforge.squirrel_sql.fw.sql.JDBCTypeMapper;
 import net.sourceforge.squirrel_sql.fw.util.IMessageHandler;
@@ -27,11 +29,11 @@ import net.sourceforge.squirrel_sql.fw.util.IMessageHandler;
 public class DatabaseTypesDataSet implements IDataSet
 {
 
-	private ResultSet _rs;
 	private int[] _columnIndices;
 	private int _columnCount;
 	private DataSetDefinition _dataSetDefinition;
-	private Object[] _row;
+    private List _allData = new ArrayList();
+    private int _currentRowIdx = -1;
 
 	public DatabaseTypesDataSet(ResultSet rs) throws DataSetException
 	{
@@ -43,7 +45,6 @@ public class DatabaseTypesDataSet implements IDataSet
 	{
 		super();
 
-		_rs = rs;
 		if (columnIndices != null && columnIndices.length == 0)
 		{
 			columnIndices = null;
@@ -54,18 +55,18 @@ public class DatabaseTypesDataSet implements IDataSet
 		{
 			try
 			{
-				ResultSetMetaData md = _rs.getMetaData();
+				ResultSetMetaData md = rs.getMetaData();
 				_columnCount = columnIndices != null ? columnIndices.length : md.getColumnCount();
 				_dataSetDefinition = new DataSetDefinition(createColumnDefinitions(md, columnIndices));
-				_row = new Object[_columnCount];
 			}
 			catch (SQLException ex)
 			{
 				throw new DataSetException(ex);
 			}
 		}
+        setResultSet(rs);
 	}
-
+    
 	public final int getColumnCount()
 	{
 		return _columnCount;
@@ -73,185 +74,157 @@ public class DatabaseTypesDataSet implements IDataSet
 
 	public DataSetDefinition getDataSetDefinition()
 	{
-		return _dataSetDefinition;
+        return _dataSetDefinition;
 	}
 
+    public synchronized boolean next(IMessageHandler msgHandler)
+        throws DataSetException
+    {
+        if (_currentRowIdx < _allData.size() - 1) {
+            _currentRowIdx++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 	/**
-	 * Point to the next data type. If there are no more data types
-	 * then close the ResultSet.
-	 *
-	 * @return	<TT>true</TT> if there is another data type.
+	 * Helper method to read a row from the specified ResultSet.
+     * 
+	 * @return an array of column values in the next row.	
 	 */
-	public synchronized boolean next(IMessageHandler msgHandler)
-		throws DataSetException
+	private Object[] getNextRow(ResultSet rs)
+		throws SQLException
 	{
-		if (_rs == null)
+        Object[] _row = new Object[_columnCount];
+		for (int i = 0; i < _columnCount; ++i)
 		{
-			return false;
-		}
-
-//TODO: Replace with ResultSetReader once we have column renderers.
-		try
-		{
-			if (!_rs.next())
+			int idx = _columnIndices != null ? _columnIndices[i] : i + 1;
+			switch (idx)
 			{
-				_rs.close();
-				return false;
-			}
+				case 2 :
+					// DATA_TYPE column of result set.
+					//int data = _rs.getInt(idx);
+                    int data = rs.getInt(idx);
+					StringBuffer buf = new StringBuffer();
+					buf.append(String.valueOf(data))
+						.append(" [")
+						.append(JDBCTypeMapper.getJdbcTypeName(data))
+						.append("]");
+					_row[i] = buf.toString();
+					break;
 
-			for (int i = 0; i < _columnCount; ++i)
-			{
-				int idx = _columnIndices != null ? _columnIndices[i] : i + 1;
-				try
-				{
-					switch (idx)
+				case 3:
+				case 14:
+				case 15:
+				case 18:
+					_row[i] = rs.getObject(idx);
+					if (_row[i] != null
+						&& !(_row[i] instanceof Integer))
 					{
+						if (_row[i] instanceof Number)
+						{
+							_row[i] = new Integer(((Number)_row[i]).intValue());
+						}
+						else
+						{
+							_row[i] = new Integer(_row[i].toString());
+						}
+					}
+					break;
 
-						case 2 :
-							// DATA_TYPE column of result set.
-							int data = _rs.getInt(idx);
-							StringBuffer buf = new StringBuffer();
-							buf.append(String.valueOf(data))
-								.append(" [")
-								.append(JDBCTypeMapper.getJdbcTypeName(data))
-								.append("]");
-							_row[i] = buf.toString();
+				case 7:
+					// NULLABLE column of result set.
+					short nullable = rs.getShort(idx);
+					switch (nullable)
+					{
+						case DatabaseMetaData.typeNoNulls :
+							_row[i] = "false";
 							break;
-
-						case 3:
-						case 14:
-						case 15:
-						case 18:
-							_row[i] = _rs.getObject(idx);
-							if (_row[i] != null
-								&& !(_row[i] instanceof Integer))
-							{
-								if (_row[i] instanceof Number)
-								{
-									_row[i] = new Integer(((Number)_row[i]).intValue());
-								}
-								else
-								{
-									_row[i] = new Integer(_row[i].toString());
-								}
-							}
+						case DatabaseMetaData.typeNullable :
+							_row[i] = "true";
 							break;
-
-						case 7:
-							// NULLABLE column of result set.
-							short nullable = _rs.getShort(idx);
-							switch (nullable)
-							{
-								case DatabaseMetaData.typeNoNulls :
-									_row[i] = "false";
-									break;
-								case DatabaseMetaData.typeNullable :
-									_row[i] = "true";
-									break;
-								case DatabaseMetaData.typeNullableUnknown :
-									_row[i] = "unknown";
-									break;
-								default :
-									_row[i] = nullable + "[error]";
-									break;
-							}
+						case DatabaseMetaData.typeNullableUnknown :
+							_row[i] = "unknown";
 							break;
-
-						case 8:
-						case 10:
-						case 11:
-						case 12:
-							// boolean columns
-//								_row[i] = _rs.getBoolean(idx) ? "true" : "false";
-							_row[i] = _rs.getObject(idx);
-							if (_row[i] != null
-								&& !(_row[i] instanceof Boolean))
-							{
-								if (_row[i] instanceof Number)
-								{
-									if (((Number) _row[i]).intValue() == 0)
-									{
-										_row[i] = Boolean.FALSE;
-									}
-									else
-									{
-										_row[i] = Boolean.TRUE;
-									}
-								}
-								else
-								{
-									_row[i] = Boolean.valueOf(_row[i].toString());
-								}
-							}
-							break;
-
-						case 9:
-							// SEARCHABLE column of result set.
-							short searchable = _rs.getShort(idx);
-							switch (searchable)
-							{
-								case DatabaseMetaData.typePredNone :
-									_row[i] = "no support";
-									break;
-								case DatabaseMetaData.typePredChar :
-									_row[i] = "only supports 'WHERE...like'";
-									break;
-								case DatabaseMetaData.typePredBasic :
-									_row[i] = "supports all except 'WHERE...LIKE'";
-									break;
-								case DatabaseMetaData.typeSearchable :
-									_row[i] = "supports all WHERE";
-									break;
-								default :
-									_row[i] = searchable + "[error]";
-									break;
-							}
-							break;
-
-						case 16:
-						case 17:
-							// ignore - unused.
-							break;
-
 						default :
-							_row[i] = _rs.getString(idx);
+							_row[i] = nullable + "[error]";
 							break;
-
 					}
-				}
-				catch (Throwable th)
-				{
-					if (msgHandler != null)
+					break;
+
+				case 8:
+				case 10:
+				case 11:
+				case 12:
+					// boolean columns
+//								_row[i] = _rs.getBoolean(idx) ? "true" : "false";
+					_row[i] = rs.getObject(idx);
+					if (_row[i] != null
+						&& !(_row[i] instanceof Boolean))
 					{
-						_row[i] = "<error>"; //i18n
-						msgHandler.showMessage(th);
+						if (_row[i] instanceof Number)
+						{
+							if (((Number) _row[i]).intValue() == 0)
+							{
+								_row[i] = Boolean.FALSE;
+							}
+							else
+							{
+								_row[i] = Boolean.TRUE;
+							}
+						}
+						else
+						{
+							_row[i] = Boolean.valueOf(_row[i].toString());
+						}
 					}
-					else
+					break;
+
+				case 9:
+					// SEARCHABLE column of result set.
+					short searchable = rs.getShort(idx);
+					switch (searchable)
 					{
-						throw new DataSetException(th);
+						case DatabaseMetaData.typePredNone :
+							_row[i] = "no support";
+							break;
+						case DatabaseMetaData.typePredChar :
+							_row[i] = "only supports 'WHERE...like'";
+							break;
+						case DatabaseMetaData.typePredBasic :
+							_row[i] = "supports all except 'WHERE...LIKE'";
+							break;
+						case DatabaseMetaData.typeSearchable :
+							_row[i] = "supports all WHERE";
+							break;
+						default :
+							_row[i] = searchable + "[error]";
+							break;
 					}
-				}
-			}
+					break;
 
-		}
-		catch (SQLException ex)
-		{
-			if (msgHandler != null)
-			{
-				msgHandler.showMessage(ex);
-			}
-			else
-			{
-				throw new DataSetException(ex);
+				case 16:
+				case 17:
+					// ignore - unused.
+					break;
+
+				default :
+					_row[i] = rs.getString(idx);
+					break;
+
 			}
 		}
-
-		return true;
+		return _row;
 	}
 
+    /**
+     * 
+     */
 	public Object get(int columnIndex)
 	{
-		return _row[columnIndex];
+        Object[] currentRow = (Object[])_allData.get(_currentRowIdx);
+		return currentRow[columnIndex];
 	}
 
 	private ColumnDisplayDefinition[] createColumnDefinitions(
@@ -272,4 +245,29 @@ public class DatabaseTypesDataSet implements IDataSet
 		}
 		return columnDefs;
 	}
+    
+    /**
+     * Reads the specified ResultSet of all of it's rows and closes it.
+     * 
+     * @param rs the ResultSet to read from.
+     * 
+     * @throws DataSetException
+     */
+    private void setResultSet(ResultSet rs) 
+        throws DataSetException
+    {
+        if (rs == null) {
+            return;
+        }
+        try {
+            while (rs.next()) {
+                Object[] row = getNextRow(rs);
+                _allData.add(row);
+            }
+        } catch (SQLException e) {
+            throw new DataSetException(e);
+        } finally {
+            try { rs.close(); } catch (SQLException e) {}
+        }
+    }
 }
