dcfcf983d2c97f20bd5af43a637e15a6fcdcf359
Robert Manning <robert.m.manning@gmail.com>
2005-12-27 15:33:22 +0000

    update for 2.1 final

--- app/src/net/sourceforge/squirrel_sql/client/Version.java
+++ app/src/net/sourceforge/squirrel_sql/client/Version.java
@@ -34,7 +34,7 @@ public class Version
 	private static final String APP_NAME = s_stringMgr.getString("Version.appname");
 	private static final int MAJOR_VERSION = 2;
 	private static final int MINOR_VERSION = 1;
-	private static final String TYPE = " rc2";
+	private static final String TYPE = " final";
 	private static final int RELEASE = 0;
 
 	private static final String COPYRIGHT = s_stringMgr.getString("Version.copyright");
