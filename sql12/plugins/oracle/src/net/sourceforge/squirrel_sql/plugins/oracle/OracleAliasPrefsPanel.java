aa04a5507c66679ca2bd9127ff72b7240f3fa355
Gerd Wagner <gerdhwagner@t-online.de>
2006-06-27 18:48:21 +0000

    Clean up

--- sql12/plugins/oracle/src/net/sourceforge/squirrel_sql/plugins/oracle/OracleAliasPrefsPanel.java
+++ sql12/plugins/oracle/src/net/sourceforge/squirrel_sql/plugins/oracle/OracleAliasPrefsPanel.java
@@ -24,8 +24,17 @@ public class OracleAliasPrefsPanel extends JPanel
 
       GridBagConstraints gbc;
 
-      gbc = new GridBagConstraints(0,0,1,1,1,0,GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5,5,30,5),0,0);
-      // i18n[OraclePrefsPanel.Description= "Interactions with Schema Alias properties (see 'Schemas' tab)\n
+      gbc = new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,15,5),0,0);
+      // i18n[OracleAliasPrefsPanel.header=Configure Oracle Schema loading properties]
+      JLabel lblHeader = new JLabel(s_stringMgr.getString("OracleAliasPrefsPanel.header"));
+      Font oldFont = lblHeader.getFont();
+      Font newFont = new Font(oldFont.getName(), Font.BOLD, oldFont.getSize());
+      lblHeader.setFont(newFont);
+      add(lblHeader, gbc);
+
+
+      gbc = new GridBagConstraints(0,1,1,1,1,0,GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5,5,30,5),0,0);
+      // i18n[OraclePrefsPanel.Description= "Interactions with Schema Alias properties (see 'Schemas' tab):\n
       //\n
       //1. 'Load all Schemas, cache none': Oracle Alias properties will take effect immediately.\n
       //\n
@@ -38,29 +47,29 @@ public class OracleAliasPrefsPanel extends JPanel
       add(new MultipleLineLabel(desc), gbc);
 
 
-      gbc = new GridBagConstraints(0,1,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0,5,5,5),0,0);
+      gbc = new GridBagConstraints(0,2,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0,5,5,5),0,0);
       // i18n[OracleAliasPrefsPanel.AccessibleButSys=Allow accessible Schemas excluding SYS]
       radLoadAccessibleSchemasExceptSYS = new JRadioButton(s_stringMgr.getString("OracleAliasPrefsPanel.AccessibleButSys"));
       add(radLoadAccessibleSchemasExceptSYS, gbc);
 
-      gbc = new GridBagConstraints(0,2,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0,5,5,5),0,0);
+      gbc = new GridBagConstraints(0,3,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0,5,5,5),0,0);
       // i18n[OracleAliasPrefsPanel.AccessibleAndSys=Allow accessible Schemas and SYS]
       radLoadAccessibleSchemasAndSYS = new JRadioButton(s_stringMgr.getString("OracleAliasPrefsPanel.AccessibleAndSys"));
       add(radLoadAccessibleSchemasAndSYS, gbc);
 
-      gbc = new GridBagConstraints(0,3,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0,5,5,5),0,0);
+      gbc = new GridBagConstraints(0,4,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0,5,5,5),0,0);
       // i18n[OracleAliasPrefsPanel.All=Allow all Schemas]
       radLoadAllSchemas= new JRadioButton(s_stringMgr.getString("OracleAliasPrefsPanel.All"));
       add(radLoadAllSchemas, gbc);
 
-      gbc = new GridBagConstraints(0,4,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(15,5,5,5),0,0);
+      gbc = new GridBagConstraints(0,5,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(15,5,5,5),0,0);
       // i18n[OracleAliasPrefsPanel.ApplyNow=Apply now]
       btnApplyNow = new JButton(s_stringMgr.getString("OracleAliasPrefsPanel.ApplyNow"));
       add(btnApplyNow, gbc);
 
 
 
-      gbc = new GridBagConstraints(0,5,1,1,0,1,GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(0,0,0,0),0,0);
+      gbc = new GridBagConstraints(0,6,1,1,0,1,GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(0,0,0,0),0,0);
       add(new JPanel(), gbc);
 
       ButtonGroup bg = new ButtonGroup();
