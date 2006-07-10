cdd93918936ef264894af275e84c7f6ccd99dfd3
Gerd Wagner <gerdhwagner@t-online.de>
2006-07-10 22:46:54 +0000

    Close Session startup time hint by escape

--- app/src/net/sourceforge/squirrel_sql/client/session/schemainfo/SessionStartupTimeHintController.java
+++ app/src/net/sourceforge/squirrel_sql/client/session/schemainfo/SessionStartupTimeHintController.java
@@ -8,6 +8,7 @@ import net.sourceforge.squirrel_sql.fw.gui.GUIUtils;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class SessionStartupTimeHintController
 {
@@ -34,6 +35,21 @@ public class SessionStartupTimeHintController
             showAliasProperties();
          }
       });
+
+      AbstractAction closeAction = new AbstractAction()
+      {
+         public void actionPerformed(ActionEvent actionEvent)
+         {
+            close();
+         }
+      };
+      KeyStroke escapeStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+      _dlg.getRootPane().getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(escapeStroke, "CloseAction");
+      _dlg.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeStroke, "CloseAction");
+      _dlg.getRootPane().getInputMap(JComponent.WHEN_FOCUSED).put(escapeStroke, "CloseAction");
+      _dlg.getRootPane().getActionMap().put("CloseAction", closeAction);
+
+
 
       _dlg.setSize(350, 180);
       GUIUtils.centerWithinParent(_dlg);
