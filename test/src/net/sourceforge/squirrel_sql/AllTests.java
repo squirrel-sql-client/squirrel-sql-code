e783944d8eaf091d43d520344a0820af4922019a
Robert Manning <robert.m.manning@gmail.com>
2006-08-16 21:11:35 +0000

    Test aggregater for all squirrel-sql TestCases.

---
+++ test/src/net/sourceforge/squirrel_sql/AllTests.java
@@ -0,0 +1,15 @@
+package net.sourceforge.squirrel_sql;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AllTests extends TestCase {
+	
+	public static Test suite() {
+		TestSuite result = new TestSuite("squirrel_sql tests");
+		result.addTest(net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.AllTests.suite());
+		result.addTest(net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.AllTests.suite());
+		return result;
+	}
+}
