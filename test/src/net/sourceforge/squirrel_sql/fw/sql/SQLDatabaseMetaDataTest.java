31d4904515c7b00c2aeab565076fe92d026f0aad
Robert Manning <robert.m.manning@gmail.com>
2006-09-02 15:35:48 +0000

    Use ApplicationManager to manage access to ApplicationArguments.

--- test/src/net/sourceforge/squirrel_sql/fw/sql/SQLDatabaseMetaDataTest.java
+++ test/src/net/sourceforge/squirrel_sql/fw/sql/SQLDatabaseMetaDataTest.java
@@ -1,26 +1,44 @@
 package net.sourceforge.squirrel_sql.fw.sql;
-
+/*
+ * Copyright (C) 2006 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 import java.sql.SQLException;
 
 import junit.framework.TestCase;
-import net.sourceforge.squirrel_sql.client.ApplicationArguments;
+import net.sourceforge.squirrel_sql.client.ApplicationManager;
 import net.sourceforge.squirrel_sql.mo.sql.MockDatabaseMetaData;
 
 import com.mockobjects.sql.MockConnection2;
 
+/**
+ * Test case for SQLDatabaseMetaData class.
+ * 
+ * @author manningr
+ */
 public class SQLDatabaseMetaDataTest extends TestCase {
 
 	SQLDatabaseMetaData iut = null;
 	MockConnection2 con = null;
 	MockDatabaseMetaData md = null;
-	private static boolean appArgsInitialized = false;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		if (!appArgsInitialized) {
-			appArgsInitialized = true;
-			ApplicationArguments.initialize(new String[0]);
-		}				
+		ApplicationManager.initApplication();
 		con = new MockConnection2();
 		md = new MockDatabaseMetaData("aCatalog", "aSchema");
 		con.setupMetaData(md);
