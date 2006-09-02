081c14aa8aaa65a9264c75acf1312779456954cb
Robert Manning <robert.m.manning@gmail.com>
2006-09-02 15:24:29 +0000

    Use ApplicationManager to manage access to ApplicationArguments.

--- test/src/net/sourceforge/squirrel_sql/fw/datasetviewer/cellcomponent/AbstractDataTypeTest.java
+++ test/src/net/sourceforge/squirrel_sql/fw/datasetviewer/cellcomponent/AbstractDataTypeTest.java
@@ -1,5 +1,22 @@
 package net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent;
-
+/*
+ * Copyright (C) 2006 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 import java.awt.Component;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -7,17 +24,19 @@ import java.awt.event.KeyListener;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import net.sourceforge.squirrel_sql.client.ApplicationArguments;
+import junit.framework.TestCase;
+import net.sourceforge.squirrel_sql.client.ApplicationManager;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.ColumnDisplayDefinition;
 
-import junit.framework.TestCase;
-
+/**
+ * Base class for various data type class JUnit tests.
+ * 
+ * @author manningr
+ */
 public abstract class AbstractDataTypeTest extends TestCase {
 
 	protected IDataTypeComponent iut = null;
-	
-	private static boolean appArgsInitialized = false;
-	
+		
 	public abstract void testTextComponents();
 
 	public AbstractDataTypeTest() {
@@ -26,10 +45,7 @@ public abstract class AbstractDataTypeTest extends TestCase {
 
 	public void setUp() throws Exception {
 		super.setUp();
-		if (!appArgsInitialized) {
-			appArgsInitialized = true;
-			ApplicationArguments.initialize(new String[0]);
-		}		
+		ApplicationManager.initApplication();
 	}
 	
 	protected ColumnDisplayDefinition getColDef() {
