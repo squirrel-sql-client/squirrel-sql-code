6758246cb683090db890bb78f8e75c1281dbbcbd
Robert Manning <robert.m.manning@gmail.com>
2006-09-16 22:03:48 +0000

    Changed version to 2.3 rc2

--- app/src/net/sourceforge/squirrel_sql/client/Version.java
+++ app/src/net/sourceforge/squirrel_sql/client/Version.java
@@ -34,7 +34,7 @@ public class Version
 	private static final int MAJOR_VERSION = 2;
 	private static final int MINOR_VERSION = 3;
     // TYPE should be " final", " rc1", " rc2", etc... Note the space prefix
-	private static final String TYPE = " rc1";
+	private static final String TYPE = " rc2";
 	private static final int RELEASE = 0;
 
 	private static final String COPYRIGHT = s_stringMgr.getString("Version.copyright");
