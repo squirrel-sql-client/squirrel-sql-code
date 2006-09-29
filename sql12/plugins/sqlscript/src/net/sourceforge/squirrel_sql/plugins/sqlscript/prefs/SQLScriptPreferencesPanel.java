e3480f129065be1892fc69c58ff4ad9e216bcc21
Robert Manning <robert.m.manning@gmail.com>
2006-09-29 02:23:14 +0000

    1520156: (Reverse Engineering Foreign Key Cascade Missing).  Added configuration for referential actions clause of FK constraints.

--- sql12/plugins/sqlscript/src/net/sourceforge/squirrel_sql/plugins/sqlscript/prefs/SQLScriptPreferencesPanel.java
+++ sql12/plugins/sqlscript/src/net/sourceforge/squirrel_sql/plugins/sqlscript/prefs/SQLScriptPreferencesPanel.java
@@ -21,8 +21,14 @@ package net.sourceforge.squirrel_sql.plugins.sqlscript.prefs;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
@@ -48,6 +54,18 @@ public class SQLScriptPreferencesPanel extends JPanel  {
     
     JCheckBox qualifyTableNamesCheckBox = null;
     
+    JCheckBox deleteReferentialActionCheckbox = null;
+    
+    JCheckBox updateReferentialActionCheckbox = null;
+    
+    JComboBox deleteActionComboBox = null;
+    
+    JComboBox updateActionComboBox = null;
+    
+    JLabel deleteActionLabel = null;
+    
+    JLabel updateActionLabel = null;
+    
     public SQLScriptPreferencesPanel(SQLScriptPreferenceBean prefs) {
         super();
         _prefs = prefs;
@@ -71,7 +89,11 @@ public class SQLScriptPreferencesPanel extends JPanel  {
         //i18n[SQLScriptPreferencesPanel.borderTitle=SQL Script Preferences]
         String borderTitle = s_stringMgr.getString("SQLScriptPreferencesPanel.borderTitle");
         result.setBorder(getTitledBorder(borderTitle));
-        addQualifyTableNamesCheckBox(result, 0, 0);        
+        addQualifyTableNamesCheckBox(result, 0, 0); 
+        addDeleteRefActionCheckBox(result, 0, 1);
+        addDeleteActionComboBox(result, 0, 2);
+        addUpdateRefActionCheckBox(result, 0, 3);
+        addUpdateActionComboBox(result, 0, 4);
         return result;
     }
     
@@ -90,6 +112,113 @@ public class SQLScriptPreferencesPanel extends JPanel  {
         qualifyTableNamesCheckBox.setToolTipText(cbToolTipText);
         panel.add(qualifyTableNamesCheckBox, c);
     }
+
+    private void addDeleteRefActionCheckBox(JPanel panel, int col, int row) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = col;
+        c.gridy = row;  
+        c.anchor = GridBagConstraints.WEST;
+        //i18n[SQLScriptPreferencesPanel.deleteRefActionCheckboxLabel=Add delete 
+        //referential action to the FK definition]
+        String cbLabelStr = 
+            s_stringMgr.getString("SQLScriptPreferencesPanel.deleteRefActionCheckboxLabel");
+        // i18n[SQLScriptPreferencesPanel.deleteRefActionToolTip=Append ON DELETE ...]
+        String cbToolTipText = 
+            s_stringMgr.getString("SQLScriptPreferencesPanel.deleteRefActionToolTip");
+        deleteReferentialActionCheckbox = new JCheckBox(cbLabelStr);
+        deleteReferentialActionCheckbox.setToolTipText(cbToolTipText);
+
+        deleteReferentialActionCheckbox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                boolean enabled = deleteReferentialActionCheckbox.isSelected();
+                deleteActionLabel.setEnabled(enabled);
+                deleteActionComboBox.setEnabled(enabled);
+            }
+        });
+
+        panel.add(deleteReferentialActionCheckbox, c);
+    }
+
+    private void addUpdateRefActionCheckBox(JPanel panel, int col, int row) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = col;
+        c.gridy = row;  
+        c.anchor = GridBagConstraints.WEST;
+        //i18n[SQLScriptPreferencesPanel.updateRefActionCheckboxLabel=Add update 
+        //referential action to the FK definition]
+        String cbLabelStr = 
+            s_stringMgr.getString("SQLScriptPreferencesPanel.updateRefActionCheckboxLabel");
+        // i18n[SQLScriptPreferencesPanel.updateRefActionToolTip=Append ON UPDATE ...]
+        String cbToolTipText = 
+            s_stringMgr.getString("SQLScriptPreferencesPanel.updateRefActionToolTip");
+        updateReferentialActionCheckbox = new JCheckBox(cbLabelStr);
+        updateReferentialActionCheckbox.setToolTipText(cbToolTipText);
+
+        updateReferentialActionCheckbox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                boolean enabled = updateReferentialActionCheckbox.isSelected();
+                updateActionLabel.setEnabled(enabled);
+                updateActionComboBox.setEnabled(enabled);
+            }
+        });
+
+        panel.add(updateReferentialActionCheckbox, c);
+    }
+    
+    private void addDeleteActionComboBox(JPanel panel, int col, int row) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = col;
+        c.gridy = row;  
+        c.insets = new Insets(5,25,0,0);
+        c.anchor = GridBagConstraints.WEST;
+        
+        JPanel subpanel = new JPanel();
+        
+        //i18n[SQLScriptPreferencesPanel.deleteActionLabel=Action to take on delete:]
+        String cbLabelStr = 
+            s_stringMgr.getString("SQLScriptPreferencesPanel.deleteActionLabel");
+        deleteActionLabel = new JLabel(cbLabelStr);
+        deleteActionLabel.setHorizontalAlignment(JLabel.LEFT);
+                
+        deleteActionComboBox = new JComboBox();
+        DefaultComboBoxModel model = 
+            new DefaultComboBoxModel(new String[] { "NO ACTION", 
+                                                    "CASCADE", 
+                                                    "SET DEFAULT",
+                                                    "SET NULL"});
+        deleteActionComboBox.setModel(model);
+        subpanel.add(deleteActionLabel);
+        subpanel.add(deleteActionComboBox);
+        panel.add(subpanel, c);
+    }
+
+    private void addUpdateActionComboBox(JPanel panel, int col, int row) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = col;
+        c.gridy = row;  
+        c.insets = new Insets(5,25,0,0);
+        c.anchor = GridBagConstraints.WEST;
+        
+        JPanel subpanel = new JPanel();
+        
+        //i18n[SQLScriptPreferencesPanel.updateActionLabel=Action to take on update:]
+        String cbLabelStr = 
+            s_stringMgr.getString("SQLScriptPreferencesPanel.updateActionLabel");
+        updateActionLabel = new JLabel(cbLabelStr);
+        updateActionLabel.setHorizontalAlignment(JLabel.LEFT);
+                
+        updateActionComboBox = new JComboBox();
+        DefaultComboBoxModel model = 
+            new DefaultComboBoxModel(new String[] { "NO ACTION", 
+                                                    "CASCADE", 
+                                                    "SET DEFAULT",
+                                                    "SET NULL"});
+        updateActionComboBox.setModel(model);
+        subpanel.add(updateActionLabel);
+        subpanel.add(updateActionComboBox);
+        panel.add(subpanel, c);
+    }
+
     
     
     private Border getTitledBorder(String title) {
@@ -101,10 +230,23 @@ public class SQLScriptPreferencesPanel extends JPanel  {
     
     private void loadData() {
         qualifyTableNamesCheckBox.setSelected(_prefs.isQualifyTableNames());
+        deleteReferentialActionCheckbox.setSelected(_prefs.isDeleteRefAction());
+        deleteActionComboBox.setEnabled(deleteReferentialActionCheckbox.isSelected());
+        deleteActionComboBox.setSelectedIndex(_prefs.getDeleteAction());
+        updateReferentialActionCheckbox.setSelected(_prefs.isUpdateRefAction());
+        updateActionComboBox.setEnabled(updateReferentialActionCheckbox.isSelected());
+        updateActionComboBox.setSelectedIndex(_prefs.getUpdateAction());
+        
     }
     
     private void save() {
         _prefs.setQualifyTableNames(qualifyTableNamesCheckBox.isSelected());
+        _prefs.setDeleteRefAction(deleteReferentialActionCheckbox.isSelected());
+        _prefs.setUpdateRefAction(updateReferentialActionCheckbox.isSelected());
+        int action = deleteActionComboBox.getSelectedIndex();
+        _prefs.setDeleteAction(action);
+        action = updateActionComboBox.getSelectedIndex();
+        _prefs.setUpdateAction(action);
         SQLScriptPreferencesManager.savePrefs();
     }
 
