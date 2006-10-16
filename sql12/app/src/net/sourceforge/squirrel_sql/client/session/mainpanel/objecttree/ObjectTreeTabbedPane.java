573a36f8576816dd2c91836887a1dd6485b91b14
Robert Manning <robert.m.manning@gmail.com>
2006-10-16 00:40:13 +0000

    Fix an ArrayIndexOutOfBounds exception (probably still a timing issue).  Also, debug log when we have to sync-up the _tabs list and the tablPnl.

--- sql12/app/src/net/sourceforge/squirrel_sql/client/session/mainpanel/objecttree/ObjectTreeTabbedPane.java
+++ sql12/app/src/net/sourceforge/squirrel_sql/client/session/mainpanel/objecttree/ObjectTreeTabbedPane.java
@@ -17,7 +17,6 @@ package net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-import java.awt.Component;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -30,6 +29,8 @@ import net.sourceforge.squirrel_sql.client.session.ISession;
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.IObjectTab;
 import net.sourceforge.squirrel_sql.fw.id.IIdentifier;
 import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
 /**
  * This is the tabbed panel displayed when a node is selected in the
  * object tree.
@@ -38,6 +39,11 @@ import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
  */
 class ObjectTreeTabbedPane
 {
+    
+    /** Logger for this class. */
+    private final static ILogger log = 
+                      LoggerController.createLogger(ObjectTreeTabbedPane.class);
+    
 	/** Keys to client properties stored in the component. */
 	interface IClientPropertiesKeys
 	{
@@ -105,11 +111,15 @@ class ObjectTreeTabbedPane
         // tab and not the generic database object info tab.  This causes the 
         // _tabs length to be 1 tab greater than the tabs that are actually in 
         // the _tabPnl.  This throws off the selection such that the tab 
-        // selected in the tab panel doesn't get rendered until the tab after
-        // it is selected.  This is a work-around for this problem until I can
-        // determine why the DatabaseObjectInfoTab never makes it into the 
-        // _tabPnl in the first place.
+        // selected in the tab panel doesn't get rendered until the tab to the
+        // right of the selected tab is selected.  This is a work-around for 
+        // this problem until I can determine why the DatabaseObjectInfoTab 
+        // never makes it into the _tabPnl in the first place.
 		if (_tabs.size() == 1 && _tabPnl.getTabCount() == 0) {
+            log.debug(
+                "addObjectPanelTab: _tabs.size() == 1, but " +
+                "_tabPnl.getTabCount() == 0 - adding first tab component to " +
+                "the tabbed page");
             IObjectTab firstTab = (IObjectTab)_tabs.get(0);
             _tabPnl.addTab(firstTab.getTitle(), 
                            null, 
@@ -128,7 +138,7 @@ class ObjectTreeTabbedPane
 		if (_tabPnl.getParent() != null)
 		{
 			int idx = _tabPnl.getSelectedIndex();
-			if (idx != -1)
+			if (idx != -1 && idx < _tabs.size())
 			{
 				IObjectTab tab = (IObjectTab)_tabs.get(idx);
 				if (tab != null)
