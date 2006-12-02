b17d28ebdca0259557d93d26f2a1eae188d507af
Robert Manning <robert.m.manning@gmail.com>
2006-12-02 11:25:46 +0000

    update for 2.3.1 build.

--- build/plainzip/build.bat
+++ build/plainzip/build.bat
@@ -1,8 +1,8 @@
 set PLAIN_ZIP_DIR=c:\home\projects\squirrel-sql\sql12\build\plainzip
 
-set INSTALL_JAR=c:\tmp\squirrel-sql-2.3final-install.jar
+set INSTALL_JAR=c:\tmp\squirrel-sql-2.3.1-install.jar
 
-set VERSION=2.3final
+set VERSION=2.3.1
 
 
 rm -f *.zip
