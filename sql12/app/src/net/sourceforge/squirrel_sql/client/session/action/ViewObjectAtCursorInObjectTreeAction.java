d10e9b882036166be6c8c5dd3dce47fe15993c0d
Gerd Wagner <gerdhwagner@t-online.de>
2006-12-11 22:20:32 +0000

    Function "View table in Object tree" is now available in Graphs.

--- sql12/app/src/net/sourceforge/squirrel_sql/client/session/action/ViewObjectAtCursorInObjectTreeAction.java
+++ sql12/app/src/net/sourceforge/squirrel_sql/client/session/action/ViewObjectAtCursorInObjectTreeAction.java
@@ -10,6 +10,7 @@ import net.sourceforge.squirrel_sql.client.action.SquirrelAction;
 import net.sourceforge.squirrel_sql.client.session.IObjectTreeAPI;
 import net.sourceforge.squirrel_sql.client.session.ISQLPanelAPI;
 import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.ObjectTreeSearch;
 import net.sourceforge.squirrel_sql.fw.util.StringManager;
 import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 
@@ -56,71 +57,9 @@ public class ViewObjectAtCursorInObjectTreeAction extends SquirrelAction
          return;
       }
 
-      ObjectCandidates candidates = getObjectCandidates();
-      if (candidates.size() == 0) {
-          return;
-      }
-      IObjectTreeAPI objectTree = _panel.getSession().getObjectTreeAPIOfActiveSessionWindow();
-
-      boolean success = false;
-      while (candidates.hasNext())
-      {
-          
-         String[] catSchemObj = candidates.next();
-         success = objectTree.selectInObjectTree(catSchemObj[0], catSchemObj[1], catSchemObj[2]);
-         if (success)
-         {
-            _panel.getSession().selectMainTab(ISession.IMainPanelTabIndexes.OBJECT_TREE_TAB);
-            break;
-         }
-
-      }
-
-      if (false == success)
-      {
-         // i18n[ViewObjectAtCursorInObjectTreeAction.error.objectnotfound=Could not locate the database object '{0}' in Object tree]
-         String msg =  
-             s_stringMgr.getString(
-                     "ViewObjectAtCursorInObjectTreeAction.error.objectnotfound", 
-                     candidates.getSearchString()); 
-         JOptionPane.showMessageDialog(_panel.getSession().getApplication().getMainFrame(), msg);
-      }
-
-   }
-
-   private ObjectCandidates getObjectCandidates()
-   {
       String stringAtCursor = getStringAtCursor();
 
-      ObjectCandidates ret = new ObjectCandidates(stringAtCursor);
-
-      String[] splits = stringAtCursor.split("\\.");
-
-
-      for (int i = splits.length-1; i >=0 ; i--)
-      {
-         String object = null;
-         String schema = null;
-         String catalog = null;
-
-         object = splits[i];
-
-         if (i+1 < splits.length)
-         {
-            schema = splits[i+1];
-         }
-
-         if (i+2 < splits.length)
-         {
-            catalog = splits[i+2];
-         }
-         if (catalog == null && schema == null && "".equals(object)) {
-             continue;
-         }
-         ret.add(catalog, schema, object);
-      }
-
-      return ret;
+      new ObjectTreeSearch().viewObjectInObjectTree(stringAtCursor, _panel.getSession());
    }
 
    private String getStringAtCursor()
@@ -156,48 +95,4 @@ public class ViewObjectAtCursorInObjectTreeAction extends SquirrelAction
          (treatDotAsStop && '.' == c);
 
    }
-
-   private static class ObjectCandidates
-   {
-      ArrayList _candidates = new ArrayList();
-
-      int _curIndex = 0;
-      private String _searchString;
-
-      public ObjectCandidates(String searchString)
-      {
-         _searchString = searchString;
-      }
-
-
-      public boolean hasNext()
-      {
-         return _curIndex < _candidates.size();
-      }
-
-      public String[] next()
-      {
-         ArrayList candidate =(ArrayList) _candidates.get(_curIndex++);
-         return (String[]) candidate.toArray(new String[candidate.size()]);
-      }
-
-      public String getSearchString()
-      {
-         return _searchString;
-      }
-
-      public void add(String catalog, String schema, String object)
-      {
-         ArrayList candidate = new ArrayList(3);
-         candidate.add(catalog);
-         candidate.add(schema);
-         candidate.add(object);
-         _candidates.add(candidate);
-      }
-
-      public int size() {
-          return _candidates.size();
-      }
-   }
-
 }
