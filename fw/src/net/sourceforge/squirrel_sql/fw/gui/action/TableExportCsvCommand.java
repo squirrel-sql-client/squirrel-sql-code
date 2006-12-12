034e1c1720e8cafa2e59bfb75938325f15cb6e54
Gerd Wagner <gerdhwagner@t-online.de>
2006-12-12 23:21:02 +0000

    MS Excel export

--- fw/src/net/sourceforge/squirrel_sql/fw/gui/action/TableExportCsvCommand.java
+++ fw/src/net/sourceforge/squirrel_sql/fw/gui/action/TableExportCsvCommand.java
@@ -14,6 +14,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import jxl.write.WriteException;
+import jxl.write.WritableWorkbook;
+import jxl.write.WritableSheet;
+import jxl.write.WritableCell;
+import jxl.Workbook;
+
 public class TableExportCsvCommand
 {
    private static final StringManager s_stringMgr =
@@ -72,19 +78,16 @@ public class TableExportCsvCommand
       try
       {
 
-
          file = ctrl.getFile();
          if(null != file.getParentFile())
          {
             file.getParentFile().mkdirs();
          }
 
-         FileWriter fw = new FileWriter(file);
-         BufferedWriter bw = new BufferedWriter(fw);
 
-         String separator = ctrl.getSeparatorChar();
          boolean includeHeaders = ctrl.includeHeaders();
          boolean complete = ctrl.exportComplete();
+
 
          int nbrSelRows = _table.getSelectedRowCount();
          if(0 == nbrSelRows || complete)
@@ -118,72 +121,161 @@ public class TableExportCsvCommand
             }
          }
 
-
-         if (includeHeaders)
+         if(TableExportCsvController.EXPORT_FORMAT_CSV == ctrl.getExportFormat())
          {
-            for (int colIdx = 0; colIdx < nbrSelCols; ++colIdx)
-            {
-               String columnName = _table.getColumnName(selCols[colIdx]);
-               bw.write(getData(separator, columnName));
-               if(nbrSelCols -1 > colIdx)
-               {
-                  bw.write(separator);
-               }
-            }
-            bw.write('\n');
+            return writeCSV(file, ctrl, includeHeaders, nbrSelCols, selCols, nbrSelRows, selRows);
+         }
+         else if(TableExportCsvController.EXPORT_FORMAT_XLS == ctrl.getExportFormat())
+         {
+            return writeXLSExport(file, includeHeaders, nbrSelCols, selCols, nbrSelRows, ctrl, selRows);
+         }
+         else
+         {
+            throw new IllegalStateException("Unknown export format " + ctrl.getExportFormat());
          }
 
-
-         for (int rowIdx = 0; rowIdx < nbrSelRows; ++rowIdx)
-         {
-            for (int colIdx = 0; colIdx < nbrSelCols; ++colIdx)
-            {
-               Object cellObj;
-               if(ctrl.useGloablPrefsFormatting() && _table.getColumnModel().getColumn(colIdx) instanceof ExtTableColumn)
-               {
-                  ExtTableColumn col = (ExtTableColumn) _table.getColumnModel().getColumn(colIdx);
-                  cellObj = _table.getValueAt(selRows[rowIdx], selCols[colIdx]);
-
-                  if(null != cellObj)
-                  {
-                     cellObj = CellComponentFactory.renderObject(cellObj, col.getColumnDisplayDefinition());
-                  }
-               }
-               else
-               {
-                  cellObj = _table.getValueAt(selRows[rowIdx], selCols[colIdx]);
-               }
-               bw.write(getData(separator, cellObj));
-
-               if(nbrSelCols -1 > colIdx)
-               {
-                  bw.write(separator);
-               }
-            }
-            bw.write('\n');
-         }
-
-         bw.flush();
-         fw.flush();
-         bw.close();
-         fw.close();
-
-         return true;
 
       }
       catch (IOException e)
       {
 
          Object[] params = new Object[]{file, e.getMessage()};
-         // i18n[TableExportCsvCommand.failedToWriteCsvFile=Failed to write file\n{0}\nError message\n{1}\nSee last log entry for details.]
+         // i18n[TableExportCsvCommand.failedToWriteFile=Failed to write file\n{0}\nError message\n{1}\nSee last log entry for details.]
          String msg = s_stringMgr.getString("TableExportCsvCommand.failedToWriteCsvFile", params);
          s_log.error(msg, e);
          JOptionPane.showMessageDialog(GUIUtils.getMainFrame(), msg);
          return false;
       }
+      catch (jxl.write.WriteException e)
+      {
+
+         Object[] params = new Object[]{file, e.getMessage()};
+         // i18n[TableExportCsvCommand.failedToWriteFile=Failed to write file\n{0}\nError message\n{1}\nSee last log entry for details.]
+         String msg = s_stringMgr.getString("TableExportCsvCommand.failedToWriteFile", params);
+         s_log.error(msg, e);
+         JOptionPane.showMessageDialog(GUIUtils.getMainFrame(), msg);
+         return false;
+      }
+
    }
 
-   private String getData(String sepChar, Object cellObj)
+
+   private boolean writeXLSExport(File file, boolean includeHeaders, int nbrSelCols, int[] selCols, int nbrSelRows, TableExportCsvController ctrl, int[] selRows)
+      throws IOException, WriteException
+   {
+      WritableWorkbook workbook = Workbook.createWorkbook(file);
+      WritableSheet sheet = workbook.createSheet("Squirrel SQL Export", 0);
+
+
+      int curRow= 0;
+      if (includeHeaders)
+      {
+         for (int colIdx = 0; colIdx < nbrSelCols; ++colIdx)
+         {
+            String columnName = _table.getColumnName(selCols[colIdx]);
+            jxl.write.Label label = new jxl.write.Label(colIdx, curRow, columnName);
+            sheet.addCell(label);
+         }
+         curRow++;
+      }
+
+
+      for (int rowIdx = 0; rowIdx < nbrSelRows; ++rowIdx)
+      {
+         for (int colIdx = 0; colIdx < nbrSelCols; ++colIdx)
+         {
+            Object cellObj;
+            if(ctrl.useGloablPrefsFormatting() && _table.getColumnModel().getColumn(colIdx) instanceof ExtTableColumn)
+            {
+               ExtTableColumn col = (ExtTableColumn) _table.getColumnModel().getColumn(colIdx);
+               cellObj = _table.getValueAt(selRows[rowIdx], selCols[colIdx]);
+
+               if(null != cellObj)
+               {
+                  cellObj = CellComponentFactory.renderObject(cellObj, col.getColumnDisplayDefinition());
+               }
+            }
+            else
+            {
+               cellObj = _table.getValueAt(selRows[rowIdx], selCols[colIdx]);
+            }
+            WritableCell xlsCell= null;
+            xlsCell = new jxl.write.Label(colIdx, curRow, getDataXLS(cellObj));
+            sheet.addCell(xlsCell);
+         }
+         curRow++;
+      }
+
+      // All sheets and cells added. Now write out the workbook
+      workbook.write();
+      workbook.close();
+
+      return true;
+   }
+
+
+   private boolean writeCSV(File file, TableExportCsvController ctrl, boolean includeHeaders, int nbrSelCols, int[] selCols, int nbrSelRows, int[] selRows)
+      throws IOException
+   {
+      FileWriter fw = new FileWriter(file);
+      BufferedWriter bw = new BufferedWriter(fw);
+
+      String separator = ctrl.getSeparatorChar();
+
+
+      if (includeHeaders)
+      {
+         for (int colIdx = 0; colIdx < nbrSelCols; ++colIdx)
+         {
+            String columnName = _table.getColumnName(selCols[colIdx]);
+            bw.write(getDataCSV(separator, columnName));
+            if(nbrSelCols -1 > colIdx)
+            {
+               bw.write(separator);
+            }
+         }
+         bw.write('\n');
+      }
+
+
+      for (int rowIdx = 0; rowIdx < nbrSelRows; ++rowIdx)
+      {
+         for (int colIdx = 0; colIdx < nbrSelCols; ++colIdx)
+         {
+            Object cellObj;
+            if(ctrl.useGloablPrefsFormatting() && _table.getColumnModel().getColumn(colIdx) instanceof ExtTableColumn)
+            {
+               ExtTableColumn col = (ExtTableColumn) _table.getColumnModel().getColumn(colIdx);
+               cellObj = _table.getValueAt(selRows[rowIdx], selCols[colIdx]);
+
+               if(null != cellObj)
+               {
+                  cellObj = CellComponentFactory.renderObject(cellObj, col.getColumnDisplayDefinition());
+               }
+            }
+            else
+            {
+               cellObj = _table.getValueAt(selRows[rowIdx], selCols[colIdx]);
+            }
+            bw.write(getDataCSV(separator, cellObj));
+
+            if(nbrSelCols -1 > colIdx)
+            {
+               bw.write(separator);
+            }
+         }
+         bw.write('\n');
+      }
+
+      bw.flush();
+      fw.flush();
+      bw.close();
+      fw.close();
+
+      return true;
+   }
+
+   private String getDataCSV(String sepChar, Object cellObj)
    {
       if (cellObj == null)
       {
@@ -201,4 +293,18 @@ public class TableExportCsvCommand
          return ret;
       }
    }
+
+   private String getDataXLS(Object cellObj)
+   {
+      if (cellObj == null)
+      {
+         return "";
+      }
+      else
+      {
+         String ret = cellObj.toString().trim();
+         return ret;
+      }
+   }
+
 }
