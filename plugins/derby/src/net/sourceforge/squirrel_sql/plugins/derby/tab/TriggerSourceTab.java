299b1447a5d8888a07b2cd33e7412c3a8ddbf91e
Robert Manning <robert.m.manning@gmail.com>
2007-01-07 11:45:13 +0000

    Transformed trigger source tab contents into valid SQL that can be used to re-create the trigger. Fixed multiple bugs that occurred when the same view/trigger name is used in multiple schemas.

--- plugins/derby/src/net/sourceforge/squirrel_sql/plugins/derby/tab/TriggerSourceTab.java
+++ plugins/derby/src/net/sourceforge/squirrel_sql/plugins/derby/tab/TriggerSourceTab.java
@@ -35,8 +35,47 @@ public class TriggerSourceTab extends BaseSourceTab
 {
 	/** SQL that retrieves the source of a stored procedure. */
 	private static String SQL =
-        "select triggerdefinition from sys.systriggers " +
-        "where triggername = ? ";
+        "select 'CREATE TRIGGER ' || t.TRIGGERNAME||' \n' " +
+        "    ||(select " +
+        "         CASE " +
+        "         WHEN t3.FIRINGTIME='B' THEN 'BEFORE' " +
+        "         WHEN t3.FIRINGTIME='A' THEN 'AFTER' " +
+        "         END " +
+        "       from SYS.SYSTRIGGERS t3 " +
+        "       where t.TRIGGERID = t3.TRIGGERID) " +
+        "    || ' ' " +
+        "    ||(select CASE " +
+        "         WHEN t2.EVENT='U' THEN 'UPDATE' " +
+        "         WHEN t2.EVENT='D' THEN 'DELETE' " +
+        "         WHEN t2.EVENT='I' THEN 'INSERT' " +
+        "         END " +
+        "       from SYS.SYSTRIGGERS t2 " +
+        "       where t.TRIGGERID = t2.TRIGGERID) " +
+        "     ||' ON ' " +
+        "     || ta.TABLENAME || ' \n'" +
+        "     ||(select " +
+        "        CASE " +
+        "          WHEN t4.REFERENCINGOLD = 0 THEN '' " +
+        "          WHEN t4.REFERENCINGOLD = 1 " +
+        "            THEN ' REFERENCING OLD AS ' || t4.OLDREFERENCINGNAME || ' \n'" +
+        "        END " +
+        "        from SYS.SYSTRIGGERS t4 " +
+        "        where t.TRIGGERID = t4.TRIGGERID) " +
+        "     ||(select " +
+        "        CASE " +
+        "          WHEN t5.REFERENCINGNEW = 0 THEN '' " +
+        "          WHEN t5.REFERENCINGNEW = 1 " +
+        "            THEN ' REFERENCING NEW AS ' || t5.NEWREFERENCINGNAME || ' \n'" +
+        "        END " +
+        "        from SYS.SYSTRIGGERS t5 " +
+        "        where t.TRIGGERID = t5.TRIGGERID) " +
+        "     ||' FOR EACH ROW MODE DB2SQL \n' " +
+        "     || t.triggerdefinition " +
+        "from SYS.SYSTRIGGERS t, SYS.SYSTABLES ta, SYS.SYSSCHEMAS s " +
+        "where t.TABLEID = ta.TABLEID " +
+        "and s.SCHEMAID = t.SCHEMAID " +
+        "and t.TRIGGERNAME = ? " +
+        "and s.SCHEMANAME = ? ";
     
 	/** Logger for this class. */
 	private final static ILogger s_log =
@@ -55,10 +94,12 @@ public class TriggerSourceTab extends BaseSourceTab
         if (s_log.isDebugEnabled()) {
             s_log.debug("Running SQL: "+SQL);
             s_log.debug("Trigger Name="+doi.getSimpleName());
+            s_log.debug("Schema Name="+doi.getSchemaName());
         }
 		SQLConnection conn = session.getSQLConnection();
 		PreparedStatement pstmt = conn.prepareStatement(SQL);
 		pstmt.setString(1, doi.getSimpleName());
+        pstmt.setString(2, doi.getSchemaName());
 		return pstmt;
 	}
 }
