74e8421d94b813c7032a94d5d1a118004f5374bc
Robert Manning <robert.m.manning@gmail.com>
2007-01-19 12:16:11 +0000

    Provided SQL for postgres indexes to display certain bits of info that are postgres-specific.

--- plugins/postgres/src/net/sourceforge/squirrel_sql/plugins/postgres/tab/IndexDetailsTab.java
+++ plugins/postgres/src/net/sourceforge/squirrel_sql/plugins/postgres/tab/IndexDetailsTab.java
@@ -49,44 +49,19 @@ public class IndexDetailsTab extends BasePreparedStatementTab
 
 	/** SQL that retrieves the data. */
 	private static final String SQL =
-        "SELECT T1.IID as index_identifier, " +
-        "       T1.DEFINER AS index_owner, " +
-        "       T1.INDNAME AS index_name, " +
-        "       T2.DEFINER AS table_owner, " +
-        "       T2.TABNAME AS table_name, " +
-        "       T3.TBSPACE AS table_space, " +
-        "       case T1.INDEXTYPE " +
-        "         when 'BLOK' then 'Block Index' " +
-        "         when 'CLUS' then 'Clustering Index' " +
-        "         when 'DIM' then 'Dimension Block Index' " +
-        "         when 'REG' then 'Regular Index' " +
-        "         when 'XPTH' then 'XML Path Index' " +
-        "         when 'XRGN' then 'XML Region Index' " +
-        "         when 'XVIL' then 'Index over XML column (Logical)' " +
-        "         when 'XVIP' then 'Index over XML column (Physical)' " +
-        "       end AS index_type, " +
-        "       case T1.UNIQUERULE " +
-        "         when 'U' then 'UNIQUE' " +
-        "         when 'D' then 'NON-UNIQUE' " +
-        "         when 'I' then 'UNIQUE (Implements PK)' " +
-        "       end AS uniqueness, " +
-        "       T1.NLEAF AS number_of_leaf_pages, " +
-        "       T1.NLEVELS AS number_of_levels, " +
-        "       T1.CREATE_TIME, " +
-        "       T1.STATS_TIME AS last_statistics_update, " +
-        "       case T1.REVERSE_SCANS " +
-        "         when 'Y' then 'Supported' " +
-        "         when 'N' then 'Not Supported' " +
-        "       end AS reverse_scans " +
-        "FROM    SYSCAT.INDEXES   AS T1, " +
-        "        SYSCAT.TABLES    AS T2, " +
-        "        SYSCAT.TABLESPACES as T3 " +
-        "WHERE  T3.TBSPACEID = T1.TBSPACEID " +
-        "and T2.TABNAME = T1.TABNAME " +
-        "and T2.TABSCHEMA = T1.TABSCHEMA " +
-        "AND     T1.TABSCHEMA = ? " +
-        "AND     T1.INDNAME = ? ";
-    
+        "select inds.schemaname as schemaname, " +
+        "       inds.relname as tablename, " +
+        "       i.indisunique as is_unique, " +
+        "       i.indisprimary as is_primary_key, " +
+        "       i.indisclustered as is_clustered, " +
+        "       inds.idx_scan as num_index_scans, " +
+        "       inds.idx_tup_read as num_index_entries_returned, " +
+        "       inds.idx_tup_fetch as num_table_rows_fetched " +
+        "from pg_catalog.pg_index i, pg_catalog.pg_stat_all_indexes inds " +
+        "where i.indexrelid = inds.indexrelid " +
+        "and inds.schemaname = ? " +
+        "and inds.indexrelname = ? ";
+        
 	public IndexDetailsTab()
 	{
 		super(i18n.TITLE, i18n.HINT, true);
