227318f93648b89a8d48cf926ca092614085f9e0
Robert Manning <robert.m.manning@gmail.com>
2007-01-20 04:24:05 +0000

    Updated trigger sql to hande triggers that fire on multiple events (INSERT or UPDATE or DELETE).  Previously, the SQL generated for source would be invalid and the details tab would be incomplete.

--- plugins/postgres/src/net/sourceforge/squirrel_sql/plugins/postgres/tab/TriggerSourceTab.java
+++ plugins/postgres/src/net/sourceforge/squirrel_sql/plugins/postgres/tab/TriggerSourceTab.java
@@ -35,12 +35,52 @@ public class TriggerSourceTab extends PostgresSourceTab
 {
 	/** SQL that retrieves the source of a stored procedure. */
 	private static String SQL =
-        "select 'create trigger ' || trigger_name || ' ' || condition_timing " +
-        "|| ' ' || event_manipulation || ' on ' || event_object_table " +
-        "|| ' for each ' || action_orientation || ' ' || action_statement " +
-        "from information_schema.triggers " +
-        "where trigger_schema = ? " +
-        "and trigger_name = ? ";
+        "select 'create trigger ' || t.trigger_name || ' ' || condition_timing " +
+        "|| ' ' || v.manip || ' on ' || event_object_table " +
+        "|| ' for each ' || action_orientation || ' ' || action_statement as trigdef " +
+        "FROM information_schema.triggers t, " +
+        "(select  trigger_schema, " +
+        "        trigger_name, " +
+        "        rtrim( " +
+        "            max(case when pos=0 then manip else '' end)|| " +
+        "            max(case when pos=1 then manip else '' end)|| " +
+        "            max(case when pos=2 then manip else '' end), ' or ' " +
+        "            ) as manip " +
+        "from ( " +
+        "    select a.trigger_schema, " +
+        "           a.trigger_name, " +
+        "           a.event_manipulation||' or ' as manip, " +
+        "           d.cnt, " +
+        "           a.rnk as pos " +
+        "    from (  select trigger_name, " +
+        "                   trigger_schema, " +
+        "                   event_manipulation, " +
+        "                   (select count(distinct is1.event_manipulation) " +
+        "                    from information_schema.triggers is1 " +
+        "                    where is2.event_manipulation < is1.event_manipulation) as rnk " +
+        "            from information_schema.triggers is2 " +
+        "          ) a, " +
+        "         (select trigger_schema, trigger_name, count(event_manipulation) as cnt " +
+        "          from ( " +
+        "                select trigger_schema, " +
+        "                       trigger_name, " +
+        "                       event_manipulation, " +
+        "                       (select count(distinct is3.event_manipulation) " +
+        "                        from information_schema.triggers is3 " +
+        "                        where is4.event_manipulation < is3.event_manipulation) as rnk " +
+        "                from information_schema.triggers is4 " +
+        "                ) y " +
+        "          group by trigger_schema, trigger_name) d " +
+        "    where d.trigger_name = a.trigger_name " +
+        "    and d.trigger_schema = a.trigger_schema " +
+        ") x " +
+        "group by trigger_schema, trigger_name " +
+        "order by 1) v " +
+        "where t.trigger_schema = v.trigger_schema " +
+        "and t.trigger_name = v.trigger_name " +
+        "and t.trigger_schema = ? " +
+        "and t.trigger_name = ? " +
+        "group by trigdef ";
     
 	/** Logger for this class. */
 	private final static ILogger s_log =
