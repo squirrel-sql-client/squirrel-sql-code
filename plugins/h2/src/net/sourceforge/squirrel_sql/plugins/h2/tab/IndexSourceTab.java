01ce4c570c63dc5121c2e07db25a60e73ac3e856
Robert Manning <robert.m.manning@gmail.com>
2007-01-22 00:59:36 +0000

    New H2 plugin

---
+++ plugins/h2/src/net/sourceforge/squirrel_sql/plugins/h2/tab/IndexSourceTab.java
@@ -0,0 +1,70 @@
+package net.sourceforge.squirrel_sql.plugins.h2.tab;
+/*
+ * Copyright (C) 2007 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
+import net.sourceforge.squirrel_sql.fw.sql.SQLConnection;
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
+
+import net.sourceforge.squirrel_sql.client.session.ISession;
+/**
+ * This class will display the source for an H2 index.
+ *
+ * @author manningr
+ */
+public class IndexSourceTab extends H2SourceTab
+{
+	/** SQL that retrieves the source of an index. */
+	private static String SQL =
+        "select " +
+        "'create '||index_type_name||' '||index_name||' ON '||table_name||'('||column_name||')' " +
+        "from INFORMATION_SCHEMA.INDEXES " +
+        "where table_schema = ? " +
+        "and index_name = ? ";
+    
+	/** Logger for this class. */
+	private final static ILogger s_log =
+		LoggerController.createLogger(ViewSourceTab.class);
+
+	public IndexSourceTab(String hint)
+	{
+		super(hint);
+	}
+
+	protected PreparedStatement createStatement() throws SQLException
+	{
+		final ISession session = getSession();
+		final IDatabaseObjectInfo doi = getDatabaseObjectInfo();
+
+		SQLConnection conn = session.getSQLConnection();
+        if (s_log.isDebugEnabled()) {
+            s_log.debug("Running SQL for index source tab: "+SQL);
+            s_log.debug("schema name: "+doi.getSchemaName());
+            s_log.debug("index name: "+doi.getSimpleName());
+        }
+		PreparedStatement pstmt = conn.prepareStatement(SQL);
+        
+        pstmt.setString(1, doi.getSchemaName());
+		pstmt.setString(2, doi.getSimpleName());
+		return pstmt;
+	}
+}
