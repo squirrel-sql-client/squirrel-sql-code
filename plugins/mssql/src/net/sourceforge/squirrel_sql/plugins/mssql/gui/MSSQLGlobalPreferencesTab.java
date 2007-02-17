a7798fdee6ae7b6d8af088ccb7c0568d0c094839
Robert Manning <robert.m.manning@gmail.com>
2007-02-17 15:09:41 +0000

    Added custom query tokenizer (and global pref tab) for MS SQL-Server

---
+++ plugins/mssql/src/net/sourceforge/squirrel_sql/plugins/mssql/gui/MSSQLGlobalPreferencesTab.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2007 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.sourceforge.squirrel_sql.plugins.mssql.gui;
+
+import java.awt.Component;
+
+import javax.swing.JScrollPane;
+
+import net.sourceforge.squirrel_sql.client.IApplication;
+import net.sourceforge.squirrel_sql.client.preferences.IGlobalPreferencesPanel;
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
+import net.sourceforge.squirrel_sql.plugins.mssql.prefs.PreferencesManager;
+
+public class MSSQLGlobalPreferencesTab implements IGlobalPreferencesPanel {
+
+    PreferencesPanel prefs = null;
+    private JScrollPane _myscrolledPanel;
+    
+    private static final StringManager s_stringMgr =
+        StringManagerFactory.getStringManager(MSSQLGlobalPreferencesTab.class);    
+    
+    public MSSQLGlobalPreferencesTab() {
+        prefs = new PreferencesPanel(PreferencesManager.getPreferences());
+        _myscrolledPanel = new JScrollPane(prefs);      
+    }
+    
+    public void initialize(IApplication app) {
+        /* Do Nothing */
+    }
+
+    public void uninitialize(IApplication app) {
+        /* Do Nothing */
+    }    
+    
+    public void applyChanges() {
+        if (prefs != null) {
+            prefs.applyChanges();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see net.sourceforge.squirrel_sql.client.util.IOptionPanel#getTitle()
+     */
+    public String getTitle() {
+        //i18n[MSSQLGlobalPreferencesTab.title=MS SQL-Server]
+        return s_stringMgr.getString("MSSQLGlobalPreferencesTab.title");
+    }
+
+    /* (non-Javadoc)
+     * @see net.sourceforge.squirrel_sql.client.util.IOptionPanel#getHint()
+     */
+    public String getHint() {
+        // i18n[MSSQLGlobalPreferencesTab.hint=Preferences for MS SQL-Server]
+        return s_stringMgr.getString("MSSQLGlobalPreferencesTab.hint"); 
+    }
+
+    public Component getPanelComponent() {
+        return _myscrolledPanel;
+    }
+
+}
