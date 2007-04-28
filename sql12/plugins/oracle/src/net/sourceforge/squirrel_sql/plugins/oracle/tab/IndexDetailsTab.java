15b2e9f074785e0065a2b2c2f1eb355ef6fd3c23
Robert Manning <robert.m.manning@gmail.com>
2007-04-28 11:50:39 +0000

    131657(Fix code that uses deprecated methods).  Removed duplicate class def.

--- sql12/plugins/oracle/src/net/sourceforge/squirrel_sql/plugins/oracle/tab/IndexDetailsTab.java
+++ sql12/plugins/oracle/src/net/sourceforge/squirrel_sql/plugins/oracle/tab/IndexDetailsTab.java
@@ -17,14 +17,11 @@ package net.sourceforge.squirrel_sql.plugins.oracle.tab;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectType;
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.BasePreparedStatementTab;
 import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
 import net.sourceforge.squirrel_sql.fw.util.StringManager;
 import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
-
-import net.sourceforge.squirrel_sql.client.session.IObjectTreeAPI;
-import net.sourceforge.squirrel_sql.client.session.ISession;
-import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.IObjectTab;
 /**
  * This class will display the details for an Oracle index.
  *
