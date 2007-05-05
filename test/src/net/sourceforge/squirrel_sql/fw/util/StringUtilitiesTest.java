fc77f37010e1008f4d429dbf4709dc91700171f0
Robert Manning <robert.m.manning@gmail.com>
2007-05-05 00:33:35 +0000

    New helper method, chop - like Perl chop.

--- test/src/net/sourceforge/squirrel_sql/fw/util/StringUtilitiesTest.java
+++ test/src/net/sourceforge/squirrel_sql/fw/util/StringUtilitiesTest.java
@@ -1,5 +1,22 @@
 package net.sourceforge.squirrel_sql.fw.util;
-
+/*
+ * Copyright (C) 2007 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 import junit.framework.TestCase;
 
 public class StringUtilitiesTest extends TestCase {
@@ -56,5 +73,13 @@ public class StringUtilitiesTest extends TestCase {
         sql = "select from_date from dealer";
         idx = StringUtilities.getTokenBeginIndex(sql, "from");
         assertEquals(17, idx);
+    }
+    
+    public void testChop() {
+        String toChop = "(1,2,3)";
+        String expAfterChop = "(1,2,3";
+        
+        String afterChop = StringUtilities.chop(toChop);
+        assertEquals(expAfterChop, afterChop);
     }
 }
