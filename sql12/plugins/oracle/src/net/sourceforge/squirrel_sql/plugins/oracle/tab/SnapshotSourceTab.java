777196e0f9e8bc9b84a17775199fe94f5bc840ee
tmuerell <tmuerell@users.sourceforge.net>
2007-05-05 08:07:15 +0000

    Added tab for viewing the source of materialized views.

---
+++ sql12/plugins/oracle/src/net/sourceforge/squirrel_sql/plugins/oracle/tab/SnapshotSourceTab.java
@@ -0,0 +1,38 @@
+package net.sourceforge.squirrel_sql.plugins.oracle.tab;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
+
+public class SnapshotSourceTab extends OracleSourceTab
+{
+	private static final StringManager s_stringMgr =
+		StringManagerFactory.getStringManager(SnapshotSourceTab.class);
+
+
+	/** SQL that retrieves the data. */
+	private static final String SQL =
+        "SELECT QUERY " +
+        "FROM SYS.ALL_SNAPSHOTS " +
+        "WHERE OWNER = ? AND TABLE_NAME = ? ";
+    
+	public SnapshotSourceTab()
+	{
+		// i18n[oracle.displaySnapshotDetails=Display materialized view details]
+		super(s_stringMgr.getString("oracle.displaySnapshotDetails"));
+	}
+
+	protected PreparedStatement createStatement() throws SQLException
+	{
+		ISession session = getSession();
+		PreparedStatement pstmt = session.getSQLConnection().prepareStatement(SQL);
+		IDatabaseObjectInfo doi = getDatabaseObjectInfo();
+		pstmt.setString(1, doi.getSchemaName());
+		pstmt.setString(2, doi.getSimpleName());
+		return pstmt;
+	}
+}
