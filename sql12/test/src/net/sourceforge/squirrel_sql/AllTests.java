134461c46cced16220a61f7ee0830ff920b3b91d
Robert Manning <robert.m.manning@gmail.com>
2007-05-14 00:38:25 +0000

    Fixed potential NPE.

--- sql12/test/src/net/sourceforge/squirrel_sql/AllTests.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/AllTests.java
@@ -25,6 +25,7 @@ public class AllTests extends TestCase {
 	
 	public static Test suite() {
 		TestSuite result = new TestSuite("squirrel_sql tests");
+        result.addTest(net.sourceforge.squirrel_sql.client.db.AllTests.suite());
         result.addTest(net.sourceforge.squirrel_sql.client.gui.db.AllTests.suite());
         result.addTest(net.sourceforge.squirrel_sql.client.session.mainpanel.AllTests.suite());
 		result.addTest(net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.AllTests.suite());
@@ -32,6 +33,7 @@ public class AllTests extends TestCase {
         result.addTest(net.sourceforge.squirrel_sql.fw.codereformat.AllTests.suite());
         result.addTest(net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.AllTests.suite());
         result.addTest(net.sourceforge.squirrel_sql.fw.dialects.AllTests.suite());
+        result.addTest(net.sourceforge.squirrel_sql.fw.id.AllTests.suite());
         result.addTest(net.sourceforge.squirrel_sql.fw.sql.AllTests.suite());
         result.addTest(net.sourceforge.squirrel_sql.fw.util.AllTests.suite());
         result.addTest(net.sourceforge.squirrel_sql.plugins.dbcopy.AllTests.suite());
