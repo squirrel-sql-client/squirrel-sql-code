c0c1030fed8e1102eb6fd702a3b4646a5e744ae5
Robert Manning <robert.m.manning@gmail.com>
2007-07-07 18:03:33 +0000

    Added support for properly tokenize stored procedure create statements.

---
+++ sql12/plugins/mysql/src/net/sourceforge/squirrel_sql/plugins/mysql/prefs/MysqlPreferenceBean.java
@@ -0,0 +1,176 @@
+package net.sourceforge.squirrel_sql.plugins.mysql.prefs;
+/*
+ * Copyright (C) 2007 Rob Manning
+ * manningr@users.sourceforge.net
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+import java.io.Serializable;
+
+import net.sourceforge.squirrel_sql.fw.preferences.IQueryTokenizerPreferenceBean;
+
+/**
+ * A bean class to store preferences for the Oracle plugin.
+ */
+public class MysqlPreferenceBean implements  Cloneable, 
+                                             Serializable,
+                                             IQueryTokenizerPreferenceBean {
+    
+	static final long serialVersionUID = 5818886723165356478L;
+
+    static final String UNSUPPORTED = "Unsupported";
+
+    /** Client Name. */
+	private String _clientName;
+
+	/** Client version. */
+	private String _clientVersion;
+    
+    private String statementSeparator = ";";
+    
+    private String procedureSeparator = "|";
+    
+    private String lineComment = "--";
+    
+    private boolean removeMultiLineComments = false;
+    
+    private boolean installCustomQueryTokenizer = true;    
+    
+	public MysqlPreferenceBean() {
+		super();
+	}
+
+	/**
+	 * Return a copy of this object.
+	 */
+	public Object clone() {
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException ex) {
+			throw new InternalError(ex.getMessage()); // Impossible.
+		}
+	}
+
+	/**
+	 * Retrieve the client to use. This is only
+	 * used if <TT>useAnonymousClient</TT> is false.
+	 *
+	 * @return	Client name.
+	 */
+	public String getClientName() {
+		return _clientName;
+	}
+
+	/**
+	 * Set the client name.
+	 *
+	 * @param	value	Client name
+	 */
+	public void setClientName(String value) {
+		_clientName = value;
+	}
+
+	/**
+	 * Retrieve the client version to use. This is only
+	 * used if <TT>useAnonymousLogon</TT> is false.
+	 *
+	 * @return	Client version.
+	 */
+	public String getClientVersion() {
+		return _clientVersion;
+	}
+
+	/**
+	 * Set the client version.
+	 *
+	 * @param	value	Client version
+	 */
+	public void setClientVersion(String value) {
+		_clientVersion = value;
+	}
+
+    /**
+     * @param statementSeparator the statementSeparator to set
+     */
+    public void setStatementSeparator(String statementSeparator) {
+        this.statementSeparator = statementSeparator;
+    }
+
+    /**
+     * @return the statementSeparator
+     */
+    public String getStatementSeparator() {
+        return statementSeparator;
+    }
+
+    /**
+     * @param procedureSeparator the procedureSeparator to set
+     */
+    public void setProcedureSeparator(String procedureSeparator) {
+        this.procedureSeparator = procedureSeparator;
+    }
+
+    /**
+     * @return the procedureSeparator
+     */
+    public String getProcedureSeparator() {
+        return procedureSeparator;
+    }
+
+    /**
+     * @param lineComment the lineComment to set
+     */
+    public void setLineComment(String lineComment) {
+        this.lineComment = lineComment;
+    }
+
+    /**
+     * @return the lineComment
+     */
+    public String getLineComment() {
+        return lineComment;
+    }
+
+    /**
+     * @param removeMultiLineComments the removeMultiLineComments to set
+     */
+    public void setRemoveMultiLineComments(boolean removeMultiLineComments) {
+        this.removeMultiLineComments = removeMultiLineComments;
+    }
+
+    /**
+     * @return the removeMultiLineComments
+     */
+    public boolean isRemoveMultiLineComments() {
+        return removeMultiLineComments;
+    }
+
+    /**
+     * @param installCustomQueryTokenizer the installCustomQueryTokenizer to set
+     */
+    public void setInstallCustomQueryTokenizer(boolean installCustomQueryTokenizer) {
+        this.installCustomQueryTokenizer = installCustomQueryTokenizer;
+    }
+
+    /**
+     * @return the installCustomQueryTokenizer
+     */
+    public boolean isInstallCustomQueryTokenizer() {
+        return installCustomQueryTokenizer;
+    }
+
+	
+}
+
