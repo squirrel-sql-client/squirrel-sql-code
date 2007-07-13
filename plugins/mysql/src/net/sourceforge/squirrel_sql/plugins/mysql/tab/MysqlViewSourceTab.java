c3db343ad9bbe83ed86d6175e6144544378eb88e
Robert Manning <robert.m.manning@gmail.com>
2007-07-13 23:10:47 +0000

    Added source tab for MySQL views.

---
+++ plugins/mysql/src/net/sourceforge/squirrel_sql/plugins/mysql/tab/MysqlViewSourceTab.java
@@ -0,0 +1,70 @@
+package net.sourceforge.squirrel_sql.plugins.mysql.tab;
+/*
+ * Copyright (C) 2007 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
+import net.sourceforge.squirrel_sql.fw.sql.ISQLConnection;
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
+
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.FormattedSourceTab;
+/**
+ * This class will display the source for an MySQL view.
+ *
+ * @author manningr
+ */
+public class MysqlViewSourceTab extends FormattedSourceTab
+{
+	/** SQL that retrieves the source of a view. */
+	private static String SQL =
+        "SELECT VIEW_DEFINITION " +
+        "FROM INFORMATION_SCHEMA.VIEWS " +
+        "WHERE TABLE_SCHEMA = ? " +
+        "AND TABLE_NAME = ? ";
+    
+	/** Logger for this class. */
+	private final static ILogger s_log =
+		LoggerController.createLogger(MysqlViewSourceTab.class);
+
+	public MysqlViewSourceTab(String hint, String stmtSep)
+	{
+		super(hint);
+        super.setupFormatter(stmtSep, null);
+        super.setCompressWhitespace(true);
+	}
+
+	protected PreparedStatement createStatement() throws SQLException
+	{
+		final ISession session = getSession();
+		final IDatabaseObjectInfo doi = getDatabaseObjectInfo();
+
+		ISQLConnection conn = session.getSQLConnection();
+        if (s_log.isDebugEnabled()) {
+            s_log.debug("Running SQL for View source tab: "+SQL);
+        }
+		PreparedStatement pstmt = conn.prepareStatement(SQL);
+        
+        pstmt.setString(1, doi.getCatalogName());
+		pstmt.setString(2, doi.getSimpleName());
+		return pstmt;
+	}
+}
