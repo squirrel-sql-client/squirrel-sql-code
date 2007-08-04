c20c35e2587878d10913ea62be4afb55e8ab1258
Robert Manning <robert.m.manning@gmail.com>
2007-08-04 13:33:19 +0000

    Implemented Hashcode and equals according to the specification.

--- fw/src/net/sourceforge/squirrel_sql/fw/dialects/IndexColInfo.java
+++ fw/src/net/sourceforge/squirrel_sql/fw/dialects/IndexColInfo.java
@@ -1,15 +1,17 @@
 package net.sourceforge.squirrel_sql.fw.dialects;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
 
 public class IndexColInfo
 {
    private String _colName;
-   private Integer _ordinalPosition = new Integer(0);
+   private Integer _ordinalPosition = Integer.valueOf(0);
 
    public static final NameComparator NAME_COMPARATOR = new NameComparator();
-   public static final OrdinalPositionComparator ORDINAL_POSITION_COMPARATOR = new OrdinalPositionComparator();
+   public static final OrdinalPositionComparator ORDINAL_POSITION_COMPARATOR = 
+       new OrdinalPositionComparator();
 
    public IndexColInfo(String colName)
    {
@@ -20,17 +22,49 @@ public class IndexColInfo
    {
 
       _colName = colName;
-      _ordinalPosition = new Integer(ordinalPosition);
+      _ordinalPosition = Integer.valueOf(ordinalPosition);
    }
 
-   public boolean equals(Object obj)
-   {
-      return ((IndexColInfo)obj)._colName.equals(_colName);
+   /**
+    * @see java.lang.Object#hashCode()
+    */
+   @Override
+   public int hashCode() {
+       final int PRIME = 31;
+       int result = 1;
+       result = PRIME * result + ((_colName == null) ? 0 : _colName.hashCode());
+       result = PRIME * result + ((_ordinalPosition == null) ? 0 : _ordinalPosition.hashCode());
+       return result;
+   }
+
+   /**
+    * @see java.lang.Object#equals(java.lang.Object)
+    */
+   @Override
+   public boolean equals(Object obj) {
+       if (this == obj)
+           return true;
+       if (obj == null)
+           return false;
+       if (getClass() != obj.getClass())
+           return false;
+       final IndexColInfo other = (IndexColInfo) obj;
+       if (_colName == null) {
+           if (other._colName != null)
+               return false;
+       } else if (!_colName.equals(other._colName))
+           return false;
+       if (_ordinalPosition == null) {
+           if (other._ordinalPosition != null)
+               return false;
+       } else if (!_ordinalPosition.equals(other._ordinalPosition))
+           return false;
+       return true;
    }
 
    public String toString()
    {
-      return _colName;
+       return _colName;
    }
 
    public int compareTo(Object obj)
@@ -38,20 +72,26 @@ public class IndexColInfo
       return _ordinalPosition.compareTo(((IndexColInfo)obj)._ordinalPosition);
    }
 
-   static class NameComparator implements Comparator
+   public static class NameComparator implements Comparator<IndexColInfo>, 
+                                                 Serializable
    {
-      public int compare(Object o1, Object o2)
-      {
-         return ((IndexColInfo)o1)._colName.compareTo(((IndexColInfo)o2)._colName);
-      }
+       private static final long serialVersionUID = 1L;
+
+       public int compare(IndexColInfo o1, IndexColInfo o2)
+       {
+           return o1._colName.compareTo(o2._colName);
+       }
    }
 
-   static class OrdinalPositionComparator implements Comparator
+   public static class OrdinalPositionComparator implements Comparator<IndexColInfo>,
+                                                            Serializable
    {
-      public int compare(Object o1, Object o2)
-      {
-         return ((IndexColInfo)o1)._ordinalPosition.compareTo(((IndexColInfo)o2)._ordinalPosition);
-      }
+       private static final long serialVersionUID = 1L;
+
+       public int compare(IndexColInfo o1, IndexColInfo o2)
+       {
+           return o1._ordinalPosition.compareTo(o2._ordinalPosition);
+       }
 
    }
 
