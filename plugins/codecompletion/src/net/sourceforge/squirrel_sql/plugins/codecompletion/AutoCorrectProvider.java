6b1bc87696e181b78b7eaad8ab49b8858a491ce0
Robert Manning <robert.m.manning@gmail.com>
2007-08-15 22:28:16 +0000

    1770559 (Miscellaneous Java 5 Generics) from patch submitted by Bron Eubanks

--- plugins/codecompletion/src/net/sourceforge/squirrel_sql/plugins/codecompletion/AutoCorrectProvider.java
+++ plugins/codecompletion/src/net/sourceforge/squirrel_sql/plugins/codecompletion/AutoCorrectProvider.java
@@ -1,10 +1,8 @@
 package net.sourceforge.squirrel_sql.plugins.codecompletion;
 
-import net.sourceforge.squirrel_sql.client.session.ISession;
-
 import java.util.Hashtable;
 
 public interface AutoCorrectProvider
 {
-   Hashtable getAutoCorrects();
+   Hashtable<String, String> getAutoCorrects();
 }
