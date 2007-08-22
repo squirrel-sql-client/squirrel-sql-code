0a124a08d4ae072e83227686269047a8dcf4756d
Gerd Wagner <gerdhwagner@t-online.de>
2007-08-22 23:47:33 +0000

    Clean up

--- plugins/hibernate/src/net/sourceforge/squirrel_sql/plugins/hibernate/mapping/DetailAttributeDataSet.java
+++ plugins/hibernate/src/net/sourceforge/squirrel_sql/plugins/hibernate/mapping/DetailAttributeDataSet.java
@@ -5,9 +5,15 @@ import net.sourceforge.squirrel_sql.fw.datasetviewer.DataSetException;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.DataSetDefinition;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.ColumnDisplayDefinition;
 import net.sourceforge.squirrel_sql.fw.util.IMessageHandler;
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 
 public class DetailAttributeDataSet implements IDataSet
 {
+   private static final StringManager s_stringMgr =
+      StringManagerFactory.getStringManager(DetailAttributeDataSet.class);
+
+
    private static final int DISPLAY_WIDTH = 20;
 
    private DetailAttribute[] _attributes;
@@ -15,11 +21,16 @@ public class DetailAttributeDataSet implements IDataSet
    private ColumnDisplayDefinition[] _columnDisplayDefinitions
          = new ColumnDisplayDefinition[]
                {
-                  new ColumnDisplayDefinition(DISPLAY_WIDTH, "Attribute name"),
-                  new ColumnDisplayDefinition(DISPLAY_WIDTH, "Attribute class name"),
-                  new ColumnDisplayDefinition(DISPLAY_WIDTH, "Identifier"),
-                  new ColumnDisplayDefinition(DISPLAY_WIDTH, "Database table name"),
-                  new ColumnDisplayDefinition(DISPLAY_WIDTH, "table columns")
+                  // i18n[HibernateAttribuiteDataSet.attrName=Attribute name]
+                  new ColumnDisplayDefinition(DISPLAY_WIDTH, s_stringMgr.getString("HibernateAttribuiteDataSet.attrName")),
+                  // i18n[HibernateAttribuiteDataSet.attrClassName=Attribute class name]
+                  new ColumnDisplayDefinition(DISPLAY_WIDTH, s_stringMgr.getString("HibernateAttribuiteDataSet.attrClassName")),
+                  // i18n[HibernateAttribuiteDataSet.identifier=Identifier]
+                  new ColumnDisplayDefinition(DISPLAY_WIDTH, s_stringMgr.getString("HibernateAttribuiteDataSet.identifier")),
+                  // i18n[HibernateAttribuiteDataSet.dbTableName=Database table name]
+                  new ColumnDisplayDefinition(DISPLAY_WIDTH, s_stringMgr.getString("HibernateAttribuiteDataSet.dbTableName")),
+                  // i18n[HibernateAttribuiteDataSet.tableCols=Table columns]
+                  new ColumnDisplayDefinition(DISPLAY_WIDTH, s_stringMgr.getString("HibernateAttribuiteDataSet.tableCols"))
                };
 
    public DetailAttributeDataSet(DetailAttribute[] attributes)
