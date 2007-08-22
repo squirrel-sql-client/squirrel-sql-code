3051bbac78cc5e215893d0696fd5e430fcc4ebb6
Gerd Wagner <gerdhwagner@t-online.de>
2007-08-22 17:22:13 +0000

    Mapped object tree finished for now

--- sql12/plugins/hibernate/src/net/sourceforge/squirrel_sql/plugins/hibernate/mapping/DetailPanel.java
+++ sql12/plugins/hibernate/src/net/sourceforge/squirrel_sql/plugins/hibernate/mapping/DetailPanel.java
@@ -1,14 +1,21 @@
 package net.sourceforge.squirrel_sql.plugins.hibernate.mapping;
 
+import net.sourceforge.squirrel_sql.fw.datasetviewer.DataSetViewerTablePanel;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class DetailPanel extends JPanel
 {
+   DataSetViewerTablePanel tblDetails;
+
    public DetailPanel()
    {
       super(new GridLayout(1,1));
 
-      add(new JLabel("Details will be shown here shortly"));
+      tblDetails = new DataSetViewerTablePanel();
+      tblDetails.init(null);
+
+      add(new JScrollPane(tblDetails.getComponent()));
    }
 }
--- sql12/plugins/hibernate/src/net/sourceforge/squirrel_sql/plugins/hibernate/mapping/MappedClassInfoTreeWrapper.java
+++ sql12/plugins/hibernate/src/net/sourceforge/squirrel_sql/plugins/hibernate/mapping/MappedClassInfoTreeWrapper.java
@@ -6,21 +6,23 @@ public class MappedClassInfoTreeWrapper extends Object
 {
    private MappedClassInfo _mappedClassInfo;
    private boolean _expanded;
+   private String _toString;
 
    public MappedClassInfoTreeWrapper(MappedClassInfo mappedClassInfo)
    {
       _mappedClassInfo = mappedClassInfo;
+      initToString();
    }
 
 
    public String toString()
    {
-      return _mappedClassInfo.getSimpleClassName();
+      return _toString;
    }
 
-   public ArrayList<MappedClassInfo> getMappedClassInfoProperies()
+   private void initToString()
    {
-      return null;  //To change body of created methods use File | Settings | File Templates.
+      _toString = _mappedClassInfo.getSimpleClassName() + " ->" + _mappedClassInfo.getTableName();
    }
 
    public boolean isExpanded()
@@ -32,5 +34,10 @@ public class MappedClassInfoTreeWrapper extends Object
    public void setExpanded(boolean expanded)
    {
       _expanded = expanded;
+   }
+
+   public MappedClassInfo getMappedClassInfo()
+   {
+      return _mappedClassInfo;
    }
 }
