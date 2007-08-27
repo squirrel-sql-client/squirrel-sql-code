81a376ac24b2191dae84d08f89228b7446e03c06
Robert Manning <robert.m.manning@gmail.com>
2007-08-27 00:43:05 +0000

    updates

--- sql12/test/scripts/db2/udf.sql
+++ sql12/test/scripts/db2/udf.sql
@@ -22,3 +22,49 @@
  CREATE FUNCTION columnUDF ( UDTforColumnUDF ) 
  RETURNS UDTforColumnUDF 
  SOURCE "SYSIBM".AVG( DECIMAL() ) 
+
+
+-- contributed by user osujfpd
+CREATE FUNCTION FNGETINDEXES( delimString VARCHAR(1024) )
+    RETURNS TABLE (ordinal INTEGER, index INTEGER)
+	LANGUAGE SQL
+	DETERMINISTIC
+	NO EXTERNAL ACTION
+	CONTAINS SQL
+
+    RETURN 
+		WITH tbl(ordinal, index) AS
+		(	VALUES (0, 0) 		-- beginning of string
+			UNION ALL
+			SELECT ordinal + 1, 
+					COALESCE(
+						NULLIF(LOCATE(',', delimString, index + 1), 0),
+						LENGTH(delimString) + 1)
+			FROM tbl			-- recursively locate all delimiters
+			WHERE ordinal <= 1024
+				AND LOCATE(',', delimString, index + 1) <> 0
+		)
+
+	SELECT ordinal, index 
+    	FROM tbl
+		
+	UNION ALL
+
+	SELECT MAX(ordinal) + 1, LENGTH(delimString) + 1
+		FROM tbl		-- end of string
+;
+
+
+CREATE FUNCTION TOTALPAY (
+        SALARY DECIMAL(9, 2),
+        BONUS DECIMAL(9, 2),
+        COMMISSION DECIMAL(9, 2))
+    RETURNS DECIMAL(9, 2)
+    LANGUAGE SQL
+    DETERMINISTIC
+    NO EXTERNAL ACTION
+    CONTAINS SQL
+
+RETURN COALESCE(SALARY, 0) + COALESCE(BONUS, 0) + COALESCE(COMMISSION, 0);
+
+
