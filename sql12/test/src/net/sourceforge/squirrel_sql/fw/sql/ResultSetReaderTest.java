2991aa8c5a239a1f0c22439dc2741fef2148cac1
Robert Manning <robert.m.manning@gmail.com>
2007-09-19 02:18:33 +0000

    Fixed tests that were failing due to MockObject's lack of API implementation.  Replaced with EasyMocks.

--- sql12/test/src/net/sourceforge/squirrel_sql/fw/sql/ResultSetReaderTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/fw/sql/ResultSetReaderTest.java
@@ -1,25 +1,54 @@
 package net.sourceforge.squirrel_sql.fw.sql;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.Calendar;
 
 import net.sourceforge.squirrel_sql.BaseSQuirreLTestCase;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.DTProperties;
-import net.sourceforge.squirrel_sql.mo.sql.MockResultSet;
 
-import com.mockobjects.sql.MockResultSetMetaData;
+import org.easymock.EasyMock;
 
 public class ResultSetReaderTest extends BaseSQuirreLTestCase {
 
     private static final String dateClassName = 
         "net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.DataTypeDate";
+
+    
+    ResultSet mockResultSet = null;
+    ResultSetMetaData mockResultSetMetaData = null;
+    
+    ResultSetReader readerUnderTest = null;
     
     protected void setUp() throws Exception {
         super.setUp();
+        mockResultSet = EasyMock.createMock(ResultSet.class);
+        mockResultSetMetaData = EasyMock.createMock(ResultSetMetaData.class);
+        expect(mockResultSet.getMetaData()).andReturn(mockResultSetMetaData);
+        expect(mockResultSetMetaData.getColumnCount()).andReturn(1).atLeastOnce();
+        expect(mockResultSet.next()).andReturn(true).once();
+        expect(mockResultSetMetaData.getColumnType(1)).andReturn(91).anyTimes();
+        expect(mockResultSetMetaData.getColumnTypeName(1)).andReturn("DATE").anyTimes();
+        expect(mockResultSet.wasNull()).andReturn(false).anyTimes();
     }
 
+    private void verifyAll() {
+        verify(mockResultSet);
+        verify(mockResultSetMetaData);        
+        
+    }
+    
+    private void replayAll() {
+        replay(mockResultSet);
+        replay(mockResultSetMetaData);        
+    }
+    
     protected void tearDown() throws Exception {
         super.tearDown();
     }
@@ -37,31 +66,26 @@ public class ResultSetReaderTest extends BaseSQuirreLTestCase {
     }
 
     private void testReadType(String type,  String readDatePropVal) throws SQLException {
-        if (readDatePropVal != null) {
-            DTProperties.put(dateClassName, "readDateAsTimestamp", readDatePropVal);
+        DTProperties.put(dateClassName, "readDateAsTimestamp", readDatePropVal);
+        
+        long now = System.currentTimeMillis();
+        if (type.equals(Date.class.getName())) {
+            expect(mockResultSet.getDate(1)).andReturn(new java.sql.Date(now));
         }
-        ResultSetReader reader = getDateResultSetReader();
-        Object[] result = reader.readRow();
-        if (result[1].getClass().getName().equals(type)) {
+
+        if (type.equals(Timestamp.class.getName())) {
+            expect(mockResultSet.getTimestamp(1)).andReturn(new java.sql.Timestamp(now));
+        }
+
+        replayAll();
+        readerUnderTest = new ResultSetReader(mockResultSet);
+        Object[] result = readerUnderTest.readRow();
+        if (result[0].getClass().getName().equals(type)) {
             // 
         } else {
-            fail("result[1] not a Date: "+result[1].getClass().getName());
-        }                
+            fail("result[0] not a Date: "+result[0].getClass().getName());
+        }               
+        verifyAll();
     }
-    
-    private ResultSetReader getDateResultSetReader() throws SQLException {
-        MockResultSet rs = new MockResultSet();
-        Date d = new Date(Calendar.getInstance().getTimeInMillis());
-        
-        rs.addRow(new Object[]{ Integer.valueOf(1), d });      
-        MockResultSetMetaData rsmd = new MockResultSetMetaData();
-        rsmd.setupAddColumnTypes(new int[] { 4, 91 });
-        rsmd.setupGetColumnCount(2);
-        rs.setMetaData(rsmd);
-        
-        ResultSetReader reader = new ResultSetReader(rs);
-        return reader;
-    }
-    
     
 }
