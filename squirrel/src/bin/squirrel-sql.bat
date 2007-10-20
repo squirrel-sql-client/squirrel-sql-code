30cfaf0d246d033ca3bfb5dfe491999f70a0f476
(no author) <gerdhwagner@t-online.de>
2007-10-20 20:39:02 +0000

    This commit was manufactured by cvs2svn to create branch 'squirrel_2_6_branch'.

--- squirrel/src/bin/squirrel-sql.bat
+++
@@ -1,6 +0,0 @@
-@rem Run with a command window.
-"%JAVA_HOME%\bin\java" -cp ..\lib\squirrel-0.1-alpha.jar;..\lib\nanoxml-2.1.jar;%CLASSPATH% net.sourceforge.squirrel_sql.client.Main
-
-@rem Run with no command window. However this may not work will all versions of Windows.
-@rem start "%JAVA_HOME%\bin\javaw" -cp lib\squirrel-sql.jar;lib\fw.jar;lib\nanoxml-2.1.jar;%CLASSPATH% net.sourceforge.squirrel_sql.client.Main
-
