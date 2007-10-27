9b7557a9684af6f0897f0a1466ff4d390bda49ce
Robert Manning <robert.m.manning@gmail.com>
2007-10-27 20:06:45 +0000

    1820020 (modified tables aren't sorted): Sorts the internal list containing the ITableInfos when it is modified.

--- sql12/test/src/net/sourceforge/squirrel_sql/client/session/schemainfo/SchemaInfoCacheTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/client/session/schemainfo/SchemaInfoCacheTest.java
@@ -18,9 +18,11 @@
  */
 package net.sourceforge.squirrel_sql.client.session.schemainfo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import net.sourceforge.squirrel_sql.BaseSQuirreLJUnit4TestCase;
@@ -35,72 +37,135 @@ import org.junit.Test;
 
 public class SchemaInfoCacheTest extends BaseSQuirreLJUnit4TestCase {
 
-    SchemaInfoCache schemaInfoCacheUnderTest = new SchemaInfoCache();
-    ISQLDatabaseMetaData mockMetaData = null;
-    Exception exceptionEncountered = null;
-    
-    @Before
-    public void setUp() throws Exception {
-        mockMetaData = TestUtil.getEasyMockH2SQLMetaData();
-        for (int i = 0; i < 20; i++) { 
-            String tableName = "table" + i;
-            ITableInfo info = new TableInfo("cat", "schema", tableName, "TABLE", null, mockMetaData);
-            schemaInfoCacheUnderTest.writeToTableCache(info);
-        }
-    }
+   SchemaInfoCache schemaInfoCacheUnderTest = new SchemaInfoCache();
 
-    @After
-    public void tearDown() throws Exception {
-        mockMetaData = null;
-    }
+   ISQLDatabaseMetaData mockMetaData = null;
 
+   Exception exceptionEncountered = null;
 
-    // This test disabled for now until we figure out a better way to do 
-    // concurrent modifications to the schemaInfoCache.
-    //@Test
-    public final void testGetITableInfosForReadOnly() {
-        
-        @SuppressWarnings("unchecked")
-        Map map = schemaInfoCacheUnderTest.getTableNamesForReadOnly();
-        @SuppressWarnings("unchecked")
-        IteratorThread thread = new IteratorThread(map.values().iterator());
-        
-        Thread t = new Thread(thread);
-        t.start();
-        sleep(500);
-        schemaInfoCacheUnderTest.clearTables(null, null, null, null);
-        sleep(500);
-        if (exceptionEncountered != null) {
-            exceptionEncountered.printStackTrace();
-            fail("Unexpected exception: "+exceptionEncountered.toString());
-            
-        }
-    }
-    
-    private class IteratorThread implements Runnable {
-        
-        private Iterator<ITableInfo> iterator = null;
-        
-        public IteratorThread(Iterator<ITableInfo> i) {
-            iterator = i;
-        }
-        /**
-         * @see java.lang.Runnable#run()
-         */
-        public void run() {
-            try {
-                while (iterator.hasNext()) {
-                    iterator.next();
-                    sleep(500);
-                }
-            } catch (Exception e) {
-                exceptionEncountered = e;
+   /** Contains test ITableInfos named table0, table1, ... table9 */
+   ITableInfo[] tableInfos = new ITableInfo[10];
+
+   String testCatalog = "cat";
+
+   String testSchema = "schema";
+
+   String testTableType = "TABLE";
+
+   @Before
+   public void setUp() throws Exception {
+      mockMetaData = TestUtil.getEasyMockH2SQLMetaData();
+      for (int i = 0; i < tableInfos.length; i++) {
+         String tableName = "table" + i;
+         tableInfos[i] = new TableInfo(testCatalog, testSchema, tableName,
+               testTableType, null, mockMetaData);
+      }
+      schemaInfoCacheUnderTest.writeToTableCache(tableInfos);
+   }
+
+   @After
+   public void tearDown() throws Exception {
+      mockMetaData = null;
+   }
+
+   /**
+    * Test that the tables are returned in the same order that they were stored
+    * in.
+    */
+   @Test
+   public void testGetITableInfosForReadOnly_order() {
+      List<ITableInfo> tis = schemaInfoCacheUnderTest
+            .getITableInfosForReadOnly();
+      int idx = 0;
+      for (ITableInfo iTableInfo : tis) {
+         String expectedTableName = tableInfos[idx++].getSimpleName();
+         String actualTableName = iTableInfo.getSimpleName();
+         assertEquals(expectedTableName, actualTableName);
+      }
+
+      /* Pick a tableInfo to remove and replace */
+      ITableInfo ti = tableInfos[5];
+      
+      /* remove table with name table10 */
+      schemaInfoCacheUnderTest.clearTables(testCatalog, testSchema, ti.getSimpleName(),
+            new String[] { testTableType });
+
+      /* Add it back */ 
+      schemaInfoCacheUnderTest.writeToTableCache(ti);
+      
+      checkSortOrder();
+   }
+
+   private void checkSortOrder() {
+      List<ITableInfo> tis = schemaInfoCacheUnderTest
+            .getITableInfosForReadOnly();
+
+      ITableInfo last = null;
+      for (ITableInfo iTableInfo : tis) {
+         if (last == null) {
+            last = iTableInfo;
+         } else {
+            String prev = last.getSimpleName();
+            String curr = iTableInfo.getSimpleName();
+            System.out.println("prev: "+prev+" curr:"+curr);
+            if (prev.compareTo(curr) > 0) {
+               fail("Table named "+prev+" appeared before "+curr+" in the sorted list");
             }
-        }
-        
-    }
+            last = iTableInfo;
+         }
+      }
+   }
+   
+   // This test disabled for now until we figure out a better way to do
+   // concurrent modifications to the schemaInfoCache.
+   // @Test
+   public final void testGetITableInfosForReadOnly() {
 
-    private void sleep(long millis) {
-        try { Thread.sleep(millis); } catch (Exception e) {}
-    }
+      @SuppressWarnings("unchecked")
+      Map map = schemaInfoCacheUnderTest.getTableNamesForReadOnly();
+      @SuppressWarnings("unchecked")
+      IteratorThread thread = new IteratorThread(map.values().iterator());
+
+      Thread t = new Thread(thread);
+      t.start();
+      sleep(500);
+      schemaInfoCacheUnderTest.clearTables(null, null, null, null);
+      sleep(500);
+      if (exceptionEncountered != null) {
+         exceptionEncountered.printStackTrace();
+         fail("Unexpected exception: " + exceptionEncountered.toString());
+
+      }
+   }
+
+   private class IteratorThread implements Runnable {
+
+      private Iterator<ITableInfo> iterator = null;
+
+      public IteratorThread(Iterator<ITableInfo> i) {
+         iterator = i;
+      }
+
+      /**
+       * @see java.lang.Runnable#run()
+       */
+      public void run() {
+         try {
+            while (iterator.hasNext()) {
+               iterator.next();
+               sleep(500);
+            }
+         } catch (Exception e) {
+            exceptionEncountered = e;
+         }
+      }
+
+   }
+
+   private void sleep(long millis) {
+      try {
+         Thread.sleep(millis);
+      } catch (Exception e) {
+      }
+   }
 }
