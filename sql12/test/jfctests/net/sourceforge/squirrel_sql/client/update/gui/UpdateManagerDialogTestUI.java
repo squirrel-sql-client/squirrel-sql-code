8b5fb43cee9d33592de52fc47f806d610481c7e5
Robert Manning <robert.m.manning@gmail.com>
2007-11-11 18:49:48 +0000

    manual test for UpdateManagerDialog.

---
+++ sql12/test/jfctests/net/sourceforge/squirrel_sql/client/update/gui/UpdateManagerDialogTestUI.java
@@ -0,0 +1,43 @@
+package net.sourceforge.squirrel_sql.client.update.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.sourceforge.squirrel_sql.client.ApplicationArguments;
+
+public class UpdateManagerDialogTestUI {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ApplicationArguments.initialize(new String[] {});
+        
+        
+        final JFrame frame = new JFrame("Test UpdateManagerDialog");
+        JButton showDialogButton = new JButton("Show UpdateManager Dialog");
+        showDialogButton.addActionListener(new ActionListener() {
+            UpdateManagerDialog dialog = null;
+            
+            public void actionPerformed(ActionEvent e) {
+                if (dialog == null) {
+                    dialog = new UpdateManagerDialog(frame, null);
+                }
+                dialog.setVisible(true);
+            }
+        });
+        JPanel panel = new JPanel();
+        panel.add(showDialogButton);
+        frame.getContentPane().add(panel);
+        frame.setSize(400,200);
+        frame.setVisible(true);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
