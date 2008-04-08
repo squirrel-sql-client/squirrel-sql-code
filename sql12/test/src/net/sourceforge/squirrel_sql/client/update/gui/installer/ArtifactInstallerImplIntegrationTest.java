f5b48a8d6b9adcde120d8d5b68ee1c921d94d8bb
Robert Manning <robert.m.manning@gmail.com>
2008-04-08 02:02:48 +0000

    Added more test coverage and an integration test that simply loads the bean.

---
+++ sql12/test/src/net/sourceforge/squirrel_sql/client/update/gui/installer/ArtifactInstallerImplIntegrationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2007 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.sourceforge.squirrel_sql.client.update.gui.installer;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({})
+@ContextConfiguration (locations={"classpath:net/sourceforge/squirrel_sql/**/*applicationContext.xml"})
+public class ArtifactInstallerImplIntegrationTest extends AbstractJUnit4SpringContextTests
+{	
+	
+	public static final String beanIdToTest = 
+		"net.sourceforge.squirrel_sql.client.update.gui.installer.ArtifactInstaller";
+	
+	@Test
+	public void testLoadBean() throws Exception {
+		super.applicationContext.getBean(beanIdToTest);
+	}
+
+	
+}
