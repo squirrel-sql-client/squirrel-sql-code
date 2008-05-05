7a1b88b06e0f49cf00d21054a06df925178efc23
Robert Manning <robert.m.manning@gmail.com>
2008-05-05 00:17:07 +0000

    Test for UpdateControllerImpl - not finished.

--- sql12/test/src/net/sourceforge/squirrel_sql/client/update/UpdateControllerImplTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/client/update/UpdateControllerImplTest.java
@@ -18,59 +18,145 @@
  */
 package net.sourceforge.squirrel_sql.client.update;
 
-import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertFalse;
 
-import java.io.FileNotFoundException;
+import java.util.Locale;
 
 import net.sourceforge.squirrel_sql.BaseSQuirreLJUnit4TestCase;
 import net.sourceforge.squirrel_sql.client.IApplication;
-import net.sourceforge.squirrel_sql.client.plugin.PluginManager;
+import net.sourceforge.squirrel_sql.client.plugin.IPluginManager;
 import net.sourceforge.squirrel_sql.client.preferences.SquirrelPreferences;
-import net.sourceforge.squirrel_sql.fw.util.UpdateSettings;
-import net.sourceforge.squirrel_sql.test.TestUtil;
+import net.sourceforge.squirrel_sql.client.update.xmlbeans.ChannelXmlBean;
+import net.sourceforge.squirrel_sql.client.update.xmlbeans.ReleaseXmlBean;
+import net.sourceforge.squirrel_sql.fw.util.IUpdateSettings;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import utils.EasyMockHelper;
+
 public class UpdateControllerImplTest extends BaseSQuirreLJUnit4TestCase {
 
-   UpdateControllerImpl underTest = null;
+   private static final String FILE_SYSTEM_UPDATE_PATH = "fileSystemUpdatePath";
+
+	private static final String LOCAL_RELEASE_FILE = "release.xml";
+
+	UpdateControllerImpl underTest = null;
+   
+   EasyMockHelper mockHelper = new EasyMockHelper();
    
    /* Mock objects */
    IApplication mockApplication = null;
    SquirrelPreferences prefs = null;
-   UpdateSettings updateSettings = null;
-   UpdateUtil util = null;
-   PluginManager pluginMgr = null;
+   IUpdateSettings mockUpdateSettings = null;
+   UpdateUtil mockUpdateUtil = null;
+   ChannelXmlBean mockInstalledChannelXmlBean = null;
+   ChannelXmlBean mockAvailableChannelXmlBean = null;
+   IPluginManager mockPluginManager = null;
+   
    
    @Before
    public void setUp() throws Exception {
-      util = new UpdateUtilImpl();
-      mockApplication = createMock(IApplication.class);
-      updateSettings = new UpdateSettings();
-      prefs = TestUtil.createClassMock(SquirrelPreferences.class);
-      expect(prefs.getUpdateSettings()).andReturn(updateSettings).anyTimes();
-      expect(mockApplication.getSquirrelPreferences()).andReturn(prefs).anyTimes();
-      TestUtil.replayClassMock(prefs);
-      pluginMgr = new PluginManager(mockApplication);
-      expect(mockApplication.getPluginManager()).andReturn(pluginMgr);
-      replay(mockApplication);
-      underTest = new UpdateControllerImpl(mockApplication);
-      underTest.setUpdateUtil(util);
+      
+      mockApplication = mockHelper.createMock(IApplication.class);
+      
+      prefs = mockHelper.createMock(SquirrelPreferences.class);
+      mockPluginManager = mockHelper.createMock(IPluginManager.class);
+      mockInstalledChannelXmlBean = setupSnapshotChannelXmlBean();
+      mockAvailableChannelXmlBean = mockHelper.createMock(ChannelXmlBean.class);
+      
    }
 
+   private IUpdateSettings setupUpdateSettings(boolean isRemoteUpdateSite)
+	{
+   	IUpdateSettings result = mockHelper.createMock(IUpdateSettings.class); 
+   	expect(result.getUpdateServerPath()).andStubReturn("pathForUpdates");
+   	expect(result.getFileSystemUpdatePath()).andStubReturn(FILE_SYSTEM_UPDATE_PATH);
+   	expect(result.isRemoteUpdateSite()).andStubReturn(isRemoteUpdateSite);
+		return result;
+	}
+
+	private void replayAll() {
+   	mockHelper.replayAll();
+   }
+   
+   private void verifyAll() {
+   	mockHelper.verifyAll();
+   }
+   
    @After
    public void tearDown() throws Exception {
+   	
    }
 
-   @Test (expected = FileNotFoundException.class)
-   public void testIsUpToDateLocalNoReleaseFile() throws Exception {
-      updateSettings.setRemoteUpdateSite(false);
-      updateSettings.setFileSystemUpdatePath(".");
-      underTest.isUpToDate();
-   }
+   @Test 
+   public void testIsUpToDateLocalSiteUpdatesAvailable() throws Exception {
+   	
+   	/* setup expectations */
+   	mockUpdateUtil =
+			setupUpdateUtilLocalUpdateSite(mockInstalledChannelXmlBean, mockAvailableChannelXmlBean);
+   	boolean isRemoteUpdateSite = false;
+   	mockUpdateSettings = setupUpdateSettings(isRemoteUpdateSite);
+   	
+   	expect(mockApplication.getPluginManager()).andReturn(mockPluginManager).anyTimes();
+   	expect(mockApplication.getSquirrelPreferences()).andReturn(prefs).anyTimes();
+   	expect(prefs.getUpdateSettings()).andReturn(mockUpdateSettings).anyTimes();
 
+   	setupSnapshotChannelXmlBean();
+   	
+   	replayAll();
+   	
+   	underTest = new UpdateControllerImpl(mockApplication);
+   	underTest.setUpdateUtil(mockUpdateUtil);
+   	boolean result = underTest.isUpToDate();
+   	assertFalse(result);
+   	verifyAll();
+   	
+   }
+   
+   @Test 
+   public void testIsIpToDateRemoteSiteUpdatesAvailable() throws Exception {
+   	/* setup expectations */
+   	mockUpdateUtil =
+			setupUpdateUtilRemoteUpdateSite(mockInstalledChannelXmlBean, mockAvailableChannelXmlBean);
+   	boolean isRemoteUpdateSite = true;
+   	mockUpdateSettings = setupUpdateSettings(isRemoteUpdateSite);
+
+   	
+   }
+   
+   private UpdateUtil setupUpdateUtilLocalUpdateSite(ChannelXmlBean installed, ChannelXmlBean available)
+		throws Exception
+	{
+		UpdateUtil mockUpdateUtil = setupUpdateUtil();
+		expect(mockUpdateUtil.getLocalReleaseFile()).andStubReturn(LOCAL_RELEASE_FILE);
+		expect(mockUpdateUtil.getLocalReleaseInfo(LOCAL_RELEASE_FILE)).andStubReturn(installed);
+		expect(mockUpdateUtil.loadUpdateFromFileSystem(FILE_SYSTEM_UPDATE_PATH)).andReturn(available);
+		return mockUpdateUtil;
+	}
+
+   private UpdateUtil setupUpdateUtilRemoteUpdateSite(ChannelXmlBean installed, ChannelXmlBean available)
+		throws Exception
+	{
+   	UpdateUtil mockUpdateUtil = setupUpdateUtil();
+   	//expect(mockUpdateUtil.)
+   	return mockUpdateUtil;
+	}
+   
+   private UpdateUtil setupUpdateUtil() {
+		UpdateUtil mockUpdateUtil = mockHelper.createMock(UpdateUtil.class);
+		mockUpdateUtil.setPluginManager(mockPluginManager);
+		return mockUpdateUtil;
+   }
+   
+   
+   private ChannelXmlBean setupSnapshotChannelXmlBean() {
+   	ChannelXmlBean mockChannelXmlBean = mockHelper.createMock(ChannelXmlBean.class);
+   	ReleaseXmlBean mockReleaseXmlBean = mockHelper.createMock(ReleaseXmlBean.class);
+   	expect(mockChannelXmlBean.getName()).andStubReturn("Snapshot");
+   	expect(mockChannelXmlBean.getCurrentRelease()).andStubReturn(mockReleaseXmlBean);
+   	return mockChannelXmlBean;
+   }
 }
