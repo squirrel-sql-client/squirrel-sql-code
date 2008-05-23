9cdce68f8095ab309762ed9ff7e6ba29b6f03dd1
Robert Manning <robert.m.manning@gmail.com>
2008-05-23 10:12:21 +0000

    1968807: Unsigned INT problem with IMPORT FILE functionality.  Merged fix from trunk.

--- sql12/plugins/dataimport/src/net/sourceforge/squirrel_sql/plugins/dataimport/importer/UnsupportedFormatException.java
+++ sql12/plugins/dataimport/src/net/sourceforge/squirrel_sql/plugins/dataimport/importer/UnsupportedFormatException.java
@@ -18,4 +18,12 @@ package net.sourceforge.squirrel_sql.plugins.dataimport.importer;
  */
 
 public class UnsupportedFormatException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public UnsupportedFormatException() {}
+	
+	public UnsupportedFormatException(Exception e) {
+		super(e);
+	}
 }
