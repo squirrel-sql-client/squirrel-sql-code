09e7011ffe24bbea3d66fbd4af3c8a865b67aca9
Robert Manning <robert.m.manning@gmail.com>
2008-06-03 00:05:44 +0000

    Added support for mapping the DISTINCT source column type for copying data from PostgreSQL.

--- plugins/dbcopy/src/net/sourceforge/squirrel_sql/plugins/dbcopy/DBCopyPlugin.java
+++ plugins/dbcopy/src/net/sourceforge/squirrel_sql/plugins/dbcopy/DBCopyPlugin.java
@@ -113,7 +113,7 @@ public class DBCopyPlugin extends DefaultSessionPlugin
      * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getVersion()
      */
     public String getVersion() {
-        return "1.13";
+        return "1.14";
     }
 
     /**
