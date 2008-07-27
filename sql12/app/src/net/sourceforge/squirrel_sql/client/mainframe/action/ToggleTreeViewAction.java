29f23ee89ec215a819d5e7bf635d89d2a62ff570
Robert Manning <robert.m.manning@gmail.com>
2008-07-27 00:30:57 +0000

    Fixed the following NPE  : java.lang.NullPointerException 	at net.sourceforge.squirrel_sql.client.mainframe.action.ToggleTreeViewAction.actionPerformed(ToggleTreeViewAction.java:37) 	at net.sourceforge.squirrel_sql.client.gui.db.AliasesListInternalFrame$UserInterfaceFactory$2.run(AliasesListInternalFrame.java:211) 	at java.awt.event.InvocationEvent.dispatch(InvocationEvent.java:209) 	at java.awt.EventQueue.dispatchEvent(EventQueue.java:597) 	at net.sourceforge.squirrel_sql.client.Application$1.dispatchEvent(Application.java:185) 	at java.awt.EventDispatchThread.pumpOneEventForFilters(EventDispatchThread.java:273) 	at java.awt.EventDispatchThread.pumpEventsForFilter(EventDispatchThread.java:183) 	at java.awt.EventDispatchThread.pumpEventsForHierarchy(EventDispatchThread.java:173) 	at java.awt.EventDispatchThread.pumpEvents(EventDispatchThread.java:168) 	at java.awt.EventDispatchThread.pumpEvents(EventDispatchThread.java:160) 	at java.awt.EventDispatchThread.run(EventDispatchThread.java:121)

--- sql12/app/src/net/sourceforge/squirrel_sql/client/mainframe/action/ToggleTreeViewAction.java
+++ sql12/app/src/net/sourceforge/squirrel_sql/client/mainframe/action/ToggleTreeViewAction.java
@@ -1,19 +1,21 @@
 package net.sourceforge.squirrel_sql.client.mainframe.action;
 
+import java.awt.event.ActionEvent;
+
 import net.sourceforge.squirrel_sql.client.IApplication;
 import net.sourceforge.squirrel_sql.client.action.SquirrelAction;
-import net.sourceforge.squirrel_sql.client.gui.db.IAliasesList;
-import net.sourceforge.squirrel_sql.client.gui.db.IToogleableAliasesList;
+import net.sourceforge.squirrel_sql.client.gui.WindowManager;
 import net.sourceforge.squirrel_sql.client.gui.db.AliasesListInternalFrame;
+import net.sourceforge.squirrel_sql.client.gui.db.IToogleableAliasesList;
 import net.sourceforge.squirrel_sql.fw.gui.IToggleAction;
 import net.sourceforge.squirrel_sql.fw.gui.ToggleComponentHolder;
-
-import java.awt.event.ActionEvent;
 
 public class ToggleTreeViewAction extends SquirrelAction implements IToggleAction
 {
 
-   private ToggleComponentHolder _toogleComponentHolder;
+	private static final long serialVersionUID = 2767325027149049773L;
+	
+	private ToggleComponentHolder _toogleComponentHolder;
    private IToogleableAliasesList _aliasesList;
 
    public ToggleTreeViewAction(IApplication app, IToogleableAliasesList aliasesList)
@@ -33,7 +35,19 @@ public class ToggleTreeViewAction extends SquirrelAction implements IToggleActio
 
    public void actionPerformed(ActionEvent evt)
    {
-      _aliasesList.setViewAsTree(_toogleComponentHolder.isSelected());   
-      getApplication().getWindowManager().getAliasesListInternalFrame().enableDisableActions();
+      _aliasesList.setViewAsTree(_toogleComponentHolder.isSelected());
+		IApplication application = getApplication();
+		if (application != null)
+		{
+			WindowManager windowManager = application.getWindowManager();
+			if (windowManager != null)
+			{
+				AliasesListInternalFrame aliasesListInternalFrame = windowManager.getAliasesListInternalFrame();
+				if (aliasesListInternalFrame != null)
+				{
+					aliasesListInternalFrame.enableDisableActions();
+				}
+			}
+		}
    }
 }
