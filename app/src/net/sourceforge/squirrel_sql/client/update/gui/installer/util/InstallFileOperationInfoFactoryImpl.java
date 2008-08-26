f78380a94b5c5090296716aee385ad580a4ad8b5
Robert Manning <robert.m.manning@gmail.com>
2008-08-26 15:11:01 +0000

    Don't allow null args to create factory method.  Add test case.

--- app/src/net/sourceforge/squirrel_sql/client/update/gui/installer/util/InstallFileOperationInfoFactoryImpl.java
+++ app/src/net/sourceforge/squirrel_sql/client/update/gui/installer/util/InstallFileOperationInfoFactoryImpl.java
@@ -18,12 +18,15 @@
  */
 package net.sourceforge.squirrel_sql.client.update.gui.installer.util;
 
+import static net.sourceforge.squirrel_sql.fw.util.Utilities.checkNull;
+
 import java.io.File;
 
 public class InstallFileOperationInfoFactoryImpl implements InstallFileOperationInfoFactory {
 
    
    public InstallFileOperationInfo create(File fileToCopy, File installDir) {
+   	checkNull("InstallFileOperationInfo", "fileToCopy",fileToCopy, "installDir", installDir);
       return new InstallFileOperationInfoImpl(fileToCopy, installDir);
    }
 }
