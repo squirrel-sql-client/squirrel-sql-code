9074c156f6ab6a748188cde17453c6dad596d3fe
Robert Manning <robert.m.manning@gmail.com>
2008-09-22 10:02:55 +0000

    Used DialogUtils to remove DialogFactory dependence on Swing components.

--- fw/src/net/sourceforge/squirrel_sql/fw/gui/IDialogUtils.java
+++ fw/src/net/sourceforge/squirrel_sql/fw/gui/IDialogUtils.java
@@ -22,31 +22,62 @@ import java.awt.Component;
 import java.awt.Frame;
 import java.io.File;
 
+import javax.swing.Icon;
 import javax.swing.JComponent;
 
 import net.sourceforge.squirrel_sql.fw.util.FileExtensionFilter;
 
 /**
- * This "describes" the interface of Dialogs which is a static method utility
- * class.  
+ * This "describes" the interface of Dialogs which is a static method utility class. Class should get an
+ * instance of this injected into them if they want to avoid direct dependence on GUI framework objects.
  * 
  * @author manningr
- *
  */
-public interface IDialogUtils {
+public interface IDialogUtils
+{
 
-    File selectFileForWriting(Frame parentFrame,
-                              FileExtensionFilter[] filters);
-    
-    File selectFileForWriting(Frame parentFrame,
-                              FileExtensionFilter[] filters, 
-                              JComponent accessory);
-    
-    void showNotYetImplemented(Component owner);
-    
-    boolean showYesNo(Component owner, String msg);
-    
-    boolean showYesNo(Component owner, String msg, String title);
-    
-    void showOk(Component owner, String msg);
+	File selectFileForWriting(Frame parentFrame, FileExtensionFilter[] filters);
+
+	File selectFileForWriting(Frame parentFrame, FileExtensionFilter[] filters, JComponent accessory);
+
+	void showNotYetImplemented(Component owner);
+
+	boolean showYesNo(Component owner, String msg);
+
+	boolean showYesNo(Component owner, String msg, String title);
+
+	void showOk(Component owner, String msg);
+
+	/**
+	 * From JavaDoc for JOptionPane.showInputDialog:
+	 * 
+	 * Prompts the user for input in a blocking dialog where the initial selection, possible selections, and
+	 * all other options can be specified. The user will able to choose from <code>selectionValues</code>,
+	 * where <code>null</code> implies the user can input whatever they wish, usually by means of a
+	 * <code>JTextField</code>. <code>initialSelectionValue</code> is the initial value to prompt the user
+	 * with. It is up to the UI to decide how best to represent the <code>selectionValues</code>, but usually a
+	 * <code>JComboBox</code>, <code>JList</code>, or <code>JTextField</code> will be used.
+	 * 
+	 * @param parentComponent
+	 *           the parent <code>Component</code> for the dialog
+	 * @param message
+	 *           the <code>Object</code> to display
+	 * @param title
+	 *           the <code>String</code> to display in the dialog title bar
+	 * @param messageType
+	 *           the type of message to be displayed: <code>ERROR_MESSAGE</code>,
+	 *           <code>INFORMATION_MESSAGE</code>, <code>WARNING_MESSAGE</code>, <code>QUESTION_MESSAGE</code>,
+	 *           or <code>PLAIN_MESSAGE</code>
+	 * @param icon
+	 *           the <code>Icon</code> image to display
+	 * @param selectionValues
+	 *           an array of <code>Object</code>s that gives the possible selections
+	 * @param initialSelectionValue
+	 *           the value used to initialize the input field
+	 * @return user's input, or <code>null</code> meaning the user canceled the input
+	 * @exception HeadlessException
+	 *               if <code>GraphicsEnvironment.isHeadless</code> returns <code>true</code>
+	 */
+	public String showInputDialog(Component parentComponent, Object message, String title, int messageType,
+		Icon icon, Object[] selectionValues, Object initialSelectionValue);
 }
