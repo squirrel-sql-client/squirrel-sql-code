8f86e8bf5e6add6f4df5b28f960b684a178cc79e
Gerd Wagner <gerdhwagner@t-online.de>
2008-09-27 18:19:57 +0000

    Object tree filtering:    Multiple (comma separated) Object tree filters can be defined. Filter conditions can be excluding oder including.    Better support for placeholders "%" and "_".

--- plugins/h2/src/net/sourceforge/squirrel_sql/plugins/h2/exp/SequenceParentExpander.java
+++ plugins/h2/src/net/sourceforge/squirrel_sql/plugins/h2/exp/SequenceParentExpander.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.schemainfo.ObjFilterMatcher;
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.INodeExpander;
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.ObjectTreeNode;
 import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectInfo;
@@ -74,7 +75,7 @@ public class SequenceParentExpander implements INodeExpander
 		final SQLDatabaseMetaData md = session.getSQLConnection().getSQLMetaData();
 		final String catalogName = parentDbinfo.getCatalogName();
 		final String schemaName = parentDbinfo.getSchemaName();
-		final String objFilter =  session.getProperties().getObjectFilter();
+      final ObjFilterMatcher filterMatcher = new ObjFilterMatcher(session.getProperties());
 
 
 		final PreparedStatement pstmt = conn.prepareStatement(SQL);
@@ -82,14 +83,18 @@ public class SequenceParentExpander implements INodeExpander
 		try
 		{
 			pstmt.setString(1, schemaName);
-			pstmt.setString(2, objFilter != null && objFilter.length() > 0 ? objFilter :"%"); 
+			pstmt.setString(2, filterMatcher.getSqlLikeMatchString());
 			rs = pstmt.executeQuery();
 				while (rs.next())
 				{
 					IDatabaseObjectInfo si = new DatabaseObjectInfo(catalogName,
 												schemaName, rs.getString(1),
 												DatabaseObjectType.SEQUENCE, md);
-					childNodes.add(new ObjectTreeNode(session, si));
+
+               if(filterMatcher.matches(si.getSimpleName()))
+               {
+                  childNodes.add(new ObjectTreeNode(session, si));
+               }
 				}
 		}
 		finally
