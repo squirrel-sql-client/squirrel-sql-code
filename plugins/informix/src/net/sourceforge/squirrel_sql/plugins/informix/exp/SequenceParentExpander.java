535335fcac5001ea55a3b2be65a1f65d91a40ad6
Robert Manning <robert.m.manning@gmail.com>
2008-09-27 21:00:52 +0000

    Organized imports on FilterMatcher.  Fixed compile issues based on Gerd's work in other plugins.

--- plugins/informix/src/net/sourceforge/squirrel_sql/plugins/informix/exp/SequenceParentExpander.java
+++ plugins/informix/src/net/sourceforge/squirrel_sql/plugins/informix/exp/SequenceParentExpander.java
@@ -26,11 +26,13 @@ import java.util.List;
 import net.sourceforge.squirrel_sql.client.session.ISession;
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.INodeExpander;
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.ObjectTreeNode;
+import net.sourceforge.squirrel_sql.client.session.schemainfo.ObjFilterMatcher;
 import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectInfo;
 import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectType;
 import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
 import net.sourceforge.squirrel_sql.fw.sql.ISQLConnection;
 import net.sourceforge.squirrel_sql.fw.sql.SQLDatabaseMetaData;
+import net.sourceforge.squirrel_sql.fw.sql.SQLUtilities;
 /**
  * This class handles the expanding of the "Sequence Group"
  * node. It will give a list of all the Sequences available in the schema.
@@ -76,32 +78,30 @@ public class SequenceParentExpander implements INodeExpander
 		final SQLDatabaseMetaData md = session.getSQLConnection().getSQLMetaData();
 		final String catalogName = parentDbinfo.getCatalogName();
 		final String schemaName = parentDbinfo.getSchemaName();
-		final String objFilter =  session.getProperties().getObjectFilter();
-
+		final ObjFilterMatcher filterMatcher = new ObjFilterMatcher(session.getProperties());
 
 		final PreparedStatement pstmt = conn.prepareStatement(SQL);
-        ResultSet rs = null;
+		ResultSet rs = null;
 		try
 		{
 			pstmt.setString(1, schemaName);
-			pstmt.setString(2, objFilter != null && objFilter.length() > 0 ? objFilter :"%"); 
+			pstmt.setString(2, filterMatcher.getSqlLikeMatchString());
 			rs = pstmt.executeQuery();
-				while (rs.next())
+			while (rs.next())
+			{
+				IDatabaseObjectInfo si =
+					new DatabaseObjectInfo(catalogName, schemaName, rs.getString(1), DatabaseObjectType.SEQUENCE,
+						md);
+				if (filterMatcher.matches(si.getSimpleName()))
 				{
-					IDatabaseObjectInfo si = new DatabaseObjectInfo(catalogName,
-												schemaName, rs.getString(1),
-												DatabaseObjectType.SEQUENCE, md);
 					childNodes.add(new ObjectTreeNode(session, si));
 				}
+			}
 		}
 		finally
 		{
-            if (rs != null) {
-                try {rs.close();} catch (SQLException e) {}
-            }
-            if (pstmt != null) {
-                try {pstmt.close();} catch (SQLException e) {}
-            }
+			SQLUtilities.closeResultSet(rs);
+			SQLUtilities.closeStatement(pstmt);
 		}
 		return childNodes;
 	}
