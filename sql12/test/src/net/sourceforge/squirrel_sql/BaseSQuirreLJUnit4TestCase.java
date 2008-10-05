26050aa470ee36dd49f575dfd7a853aa932e1530
Robert Manning <robert.m.manning@gmail.com>
2008-10-05 09:55:59 +0000

    Added mockHelper for subclasses.

--- sql12/test/src/net/sourceforge/squirrel_sql/BaseSQuirreLJUnit4TestCase.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/BaseSQuirreLJUnit4TestCase.java
@@ -6,22 +6,30 @@ import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
 
 import org.apache.log4j.Level;
 
-public class BaseSQuirreLJUnit4TestCase {
+import utils.EasyMockHelper;
 
-    public BaseSQuirreLJUnit4TestCase() {
-        ApplicationManager.initApplication();
-    }
-    
-    @SuppressWarnings("unchecked")
-    protected static void disableLogging(Class c) {
-        ILogger s_log = LoggerController.createLogger(c);
-        s_log.setLevel(Level.OFF);        
-    }
-    
-    @SuppressWarnings("unchecked")
-    protected static void debugLogging(Class c) {
-        ILogger s_log = LoggerController.createLogger(c);
-        s_log.setLevel(Level.DEBUG);        
-    }
-    
+public class BaseSQuirreLJUnit4TestCase
+{
+
+	protected EasyMockHelper mockHelper = new EasyMockHelper();
+
+	public BaseSQuirreLJUnit4TestCase()
+	{
+		ApplicationManager.initApplication();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected static void disableLogging(Class c)
+	{
+		ILogger s_log = LoggerController.createLogger(c);
+		s_log.setLevel(Level.OFF);
+	}
+
+	@SuppressWarnings("unchecked")
+	protected static void debugLogging(Class c)
+	{
+		ILogger s_log = LoggerController.createLogger(c);
+		s_log.setLevel(Level.DEBUG);
+	}
+
 }
