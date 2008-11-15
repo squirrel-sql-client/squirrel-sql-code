41f475b2f91a64f1eeb796fb7297e9edd9abaa99
Robert Manning <robert.m.manning@gmail.com>
2008-11-15 16:14:09 +0000

    Updated plugin impl to tell where the license file is.

--- plugins/dbcopy/src/net/sourceforge/squirrel_sql/plugins/dbcopy/DBCopyPlugin.java
+++ plugins/dbcopy/src/net/sourceforge/squirrel_sql/plugins/dbcopy/DBCopyPlugin.java
@@ -19,7 +19,6 @@ package net.sourceforge.squirrel_sql.plugins.dbcopy;
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
 import javax.swing.SwingUtilities;
 
 import net.sourceforge.squirrel_sql.client.IApplication;
@@ -43,277 +42,323 @@ import net.sourceforge.squirrel_sql.plugins.dbcopy.gui.DBCopyGlobalPreferencesTa
 import net.sourceforge.squirrel_sql.plugins.dbcopy.prefs.PreferencesManager;
 
 /**
- * The class that sets up the various resources required by SQuirreL to 
- * implement a plugin.  This plugin implements the ability to copy tables and 
- * various other table-related objects from one database to another.
+ * The class that sets up the various resources required by SQuirreL to implement a plugin. This plugin
+ * implements the ability to copy tables and various other table-related objects from one database to another.
  */
-public class DBCopyPlugin extends DefaultSessionPlugin 
-                          implements SessionInfoProvider {
+public class DBCopyPlugin extends DefaultSessionPlugin implements SessionInfoProvider
+{
 
-    /** Logger for this class. */
-    private final static ILogger s_log = LoggerController.createLogger(DBCopyPlugin.class);
-    
-    private PluginResources _resources;
-    
-    private ISession copySourceSession = null;
-    
-    private ISession copyDestSession = null;
-    
-    private IDatabaseObjectInfo[] selectedDatabaseObjects = null;   
-    
-    private IDatabaseObjectInfo selectedDestDatabaseObject = null;
-    
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.client.plugin.ISessionPlugin#sessionStarted(net.sourceforge.squirrel_sql.client.session.ISession)
-     */
-    public PluginSessionCallback sessionStarted(final ISession session) {
-        IObjectTreeAPI api = session.getObjectTreeAPIOfActiveSessionWindow();
-        addMenuItemsToContextMenu(api);        
-        return new DBCopyPluginSessionCallback(this);
-    }
-    
-    public void sessionEnding(final ISession session) {
-        if (session.equals(copySourceSession)) {
-            copySourceSession = null;
-            // Can't paste from a session that is no longer around.
-            setPasteMenuEnabled(false);
-        }
-    }
-    
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getInternalName()
-     */
-    public String getInternalName() {
-        return "dbcopy";
-    }
+	/** Logger for this class. */
+	private final static ILogger s_log = LoggerController.createLogger(DBCopyPlugin.class);
 
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getDescriptiveName()
-     */
-    public String getDescriptiveName() {
-        return "DBCopy Plugin";
-    }
+	private PluginResources _resources;
 
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getAuthor()
-     */
-    public String getAuthor() {
-        return "Rob Manning";
-    }
+	private ISession copySourceSession = null;
 
-    
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.client.plugin.DefaultPlugin#getContributors()
-     */
-    public String getContributors() {
-        return "Dan Dragut";
-    }
+	private ISession copyDestSession = null;
 
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getVersion()
-     */
-    public String getVersion() {
-        return "1.14";
-    }
+	private IDatabaseObjectInfo[] selectedDatabaseObjects = null;
 
-    /**
-     * Returns the name of the Help file for the plugin. This should
-     * be a text or HTML file residing in the <TT>getPluginAppSettingsFolder</TT>
-     * directory.
-     *
-     * @return  the Help file name or <TT>null</TT> if plugin doesn't have
-     * a help file.
-     */
-    public String getHelpFileName()
-    {
-       return "readme.html";
-    }    
-    
-    /**
-     * Returns the name of the change log for the plugin. This should
-     * be a text or HTML file residing in the <TT>getPluginAppSettingsFolder</TT>
-     * directory.
-     *
-     * @return  the changelog file name or <TT>null</TT> if plugin doesn't have
-     *          a change log.
-     */
-    public String getChangeLogFileName()
-    {
-        return "changes.txt";
-    }
-    
-    public void initialize() throws PluginException {
-        super.initialize();
-        //md = new MemoryDiagnostics();
-        if (s_log.isDebugEnabled()) {
-            s_log.debug("Initializing DB Copy Plugin");
-        }
-        _resources =
-            new DBCopyPluginResources(
-                "net.sourceforge.squirrel_sql.plugins.dbcopy.dbcopy",
-                this);
-        PreferencesManager.initialize(this);
-        
-        IApplication app = getApplication();
-        ActionCollection coll = app.getActionCollection();        
-        coll.add(new CopyTableAction(app, _resources, this));
-        coll.add(new PasteTableAction(app, _resources, this));
-        
-        setPasteMenuEnabled(false);
-    }
-    
-    public void unload() {
-        super.unload();
-        copySourceSession = null;
-        setPasteMenuEnabled(false);
-        PreferencesManager.unload();
-    }    
-    
-    public void setCopyMenuEnabled(boolean enabled) {
-        final ActionCollection coll = getApplication().getActionCollection();
-        CopyTableAction copyAction = 
-            (CopyTableAction)coll.get(CopyTableAction.class);
-        copyAction.setEnabled(enabled);        
-    }
+	private IDatabaseObjectInfo selectedDestDatabaseObject = null;
 
-    public void setPasteMenuEnabled(final boolean enabled) {
-        GUIUtils.processOnSwingEventThread(new Runnable() {
-            public void run() {
-                final ActionCollection coll = getApplication().getActionCollection();
-                PasteTableAction pasteAction = 
-                    (PasteTableAction)coll.get(PasteTableAction.class);
-                pasteAction.setEnabled(enabled);                
-            }
-        });
-    }
-    
-    /**
-     * @param selectedDatabaseObjects The selectedDatabaseObjects to set.
-     */
-    public void setSelectedDatabaseObjects(IDatabaseObjectInfo[] dbObjArr) {
-        if (dbObjArr != null) {
-            selectedDatabaseObjects = dbObjArr;
-            for (int i = 0; i < dbObjArr.length; i++) {
-                if (s_log.isDebugEnabled()) {
-                    s_log.debug(
-                        "setSelectedDatabaseObjects: IDatabaseObjectInfo["+
-                        i+"]="+dbObjArr[i]);
-                }
-            }
-        }
-    }
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.ISessionPlugin#sessionStarted(net.sourceforge.squirrel_sql.client.session.ISession)
+	 */
+	public PluginSessionCallback sessionStarted(final ISession session)
+	{
+		IObjectTreeAPI api = session.getObjectTreeAPIOfActiveSessionWindow();
+		addMenuItemsToContextMenu(api);
+		return new DBCopyPluginSessionCallback(this);
+	}
 
-    /**
-     * Create panel for the Global Properties dialog.
-     * 
-     * @return  properties panel.
-     */
-    public IGlobalPreferencesPanel[] getGlobalPreferencePanels() {
-        DBCopyGlobalPreferencesTab tab = new DBCopyGlobalPreferencesTab();
-        return new IGlobalPreferencesPanel[] { tab };
-    }
-    
-    /**
-     * @param coll
-     * @param api
-     */
-    protected void addMenuItemsToContextMenu(final IObjectTreeAPI api) 
-    {
-        //final IObjectTreeAPI api = Compat.getIObjectTreeAPI(session, this);
-        final ActionCollection coll = getApplication().getActionCollection();
-        
-        if (SwingUtilities.isEventDispatchThread()) {
-            addToPopup(api, coll);
-        } else {
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    addToPopup(api, coll);
-                }
-            });
-        }
-    }
-    
-    private void addToPopup(IObjectTreeAPI api, ActionCollection coll) {
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.DefaultSessionPlugin#sessionEnding(net.sourceforge.squirrel_sql.client.session.ISession)
+	 */
+	public void sessionEnding(final ISession session)
+	{
+		if (session.equals(copySourceSession))
+		{
+			copySourceSession = null;
+			// Can't paste from a session that is no longer around.
+			setPasteMenuEnabled(false);
+		}
+	}
 
-        api.addToPopup(DatabaseObjectType.TABLE_TYPE_DBO,
-                       coll.get(CopyTableAction.class));
-    	
-        api.addToPopup(DatabaseObjectType.TABLE_TYPE_DBO,
-	                   coll.get(PasteTableAction.class));
-        
-    	// Copy action object tree types
-        api.addToPopup(DatabaseObjectType.TABLE, 
-                       coll.get(CopyTableAction.class));
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getInternalName()
+	 */
+	public String getInternalName()
+	{
+		return "dbcopy";
+	}
 
-        api.addToPopup(DatabaseObjectType.TABLE,
-		           coll.get(PasteTableAction.class));        
-        
-        // Paste action object tree types
-        api.addToPopup(DatabaseObjectType.SCHEMA, 
-                       coll.get(PasteTableAction.class));
-           
-        // MySQL shows databases as "CATALOGS" not "SCHEMAS"
-        api.addToPopup(DatabaseObjectType.CATALOG, 
-                       coll.get(PasteTableAction.class));      
-        
-        api.addToPopup(DatabaseObjectType.SESSION, 
-                       coll.get(PasteTableAction.class));
-        
-    }
-        
-    private class DBCopyPluginResources extends PluginResources {
-        DBCopyPluginResources(String rsrcBundleBaseName, IPlugin plugin) {
-            super(rsrcBundleBaseName, plugin);
-        }
-    }
-    
-    // Interface SessionInfoProvider implementation
-    
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getCopySourceSession()
-     */
-    public ISession getCopySourceSession() {
-        return copySourceSession;
-    }
-    
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#setCopySourceSession(net.sourceforge.squirrel_sql.client.session.ISession)
-     */
-    public void setCopySourceSession(ISession session) {
-        if (session != null) {
-            copySourceSession = session;
-        }
-    }
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getDescriptiveName()
+	 */
+	public String getDescriptiveName()
+	{
+		return "DBCopy Plugin";
+	}
 
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getSelectedDatabaseObjects()
-     */
-    public IDatabaseObjectInfo[] getSourceSelectedDatabaseObjects() {
-        return selectedDatabaseObjects;
-    }
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getAuthor()
+	 */
+	public String getAuthor()
+	{
+		return "Rob Manning";
+	}
 
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getCopyDestSession()
-     */
-    public ISession getCopyDestSession() {
-        return copyDestSession;
-    }
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.DefaultPlugin#getContributors()
+	 */
+	public String getContributors()
+	{
+		return "Dan Dragut";
+	}
 
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#setDestCopySession(net.sourceforge.squirrel_sql.client.session.ISession)
-     */
-    public void setDestCopySession(ISession session) {
-        copyDestSession = session;
-    }
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.IPlugin#getVersion()
+	 */
+	public String getVersion()
+	{
+		return "1.14";
+	}
 
-    /* (non-Javadoc)
-     * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getDestSelectedDatabaseObject()
-     */
-    public IDatabaseObjectInfo getDestSelectedDatabaseObject() {
-        return selectedDestDatabaseObject;
-    }
-    
-    public void setDestSelectedDatabaseObject(IDatabaseObjectInfo info) {
-        selectedDestDatabaseObject = info;
-    }
+	/**
+	 * Returns the name of the Help file for the plugin. This should be a text or HTML file residing in the
+	 * <TT>getPluginAppSettingsFolder</TT> directory.
+	 * 
+	 * @return the Help file name or <TT>null</TT> if plugin doesn't have a help file.
+	 */
+	public String getHelpFileName()
+	{
+		return "readme.html";
+	}
+
+	/**
+	 * Returns the name of the change log for the plugin. This should be a text or HTML file residing in the
+	 * <TT>getPluginAppSettingsFolder</TT> directory.
+	 * 
+	 * @return the changelog file name or <TT>null</TT> if plugin doesn't have a change log.
+	 */
+	public String getChangeLogFileName()
+	{
+		return "changes.txt";
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.DefaultPlugin#getLicenceFileName()
+	 */
+	@Override
+	public String getLicenceFileName()
+	{
+		return "license.txt";
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.DefaultPlugin#initialize()
+	 */
+	public void initialize() throws PluginException
+	{
+		super.initialize();
+		// md = new MemoryDiagnostics();
+		if (s_log.isDebugEnabled())
+		{
+			s_log.debug("Initializing DB Copy Plugin");
+		}
+		_resources = new DBCopyPluginResources("net.sourceforge.squirrel_sql.plugins.dbcopy.dbcopy", this);
+		PreferencesManager.initialize(this);
+
+		IApplication app = getApplication();
+		ActionCollection coll = app.getActionCollection();
+		coll.add(new CopyTableAction(app, _resources, this));
+		coll.add(new PasteTableAction(app, _resources, this));
+
+		setPasteMenuEnabled(false);
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.plugin.DefaultPlugin#unload()
+	 */
+	public void unload()
+	{
+		super.unload();
+		copySourceSession = null;
+		setPasteMenuEnabled(false);
+		PreferencesManager.unload();
+	}
+
+	/**
+	 * @param enabled
+	 */
+	public void setCopyMenuEnabled(boolean enabled)
+	{
+		final ActionCollection coll = getApplication().getActionCollection();
+		CopyTableAction copyAction = (CopyTableAction) coll.get(CopyTableAction.class);
+		copyAction.setEnabled(enabled);
+	}
+
+	/**
+	 * @param enabled
+	 */
+	public void setPasteMenuEnabled(final boolean enabled)
+	{
+		GUIUtils.processOnSwingEventThread(new Runnable()
+		{
+			public void run()
+			{
+				final ActionCollection coll = getApplication().getActionCollection();
+				PasteTableAction pasteAction = (PasteTableAction) coll.get(PasteTableAction.class);
+				pasteAction.setEnabled(enabled);
+			}
+		});
+	}
+
+	/**
+	 * @param selectedDatabaseObjects
+	 *           The selectedDatabaseObjects to set.
+	 */
+	public void setSelectedDatabaseObjects(IDatabaseObjectInfo[] dbObjArr)
+	{
+		if (dbObjArr != null)
+		{
+			selectedDatabaseObjects = dbObjArr;
+			for (int i = 0; i < dbObjArr.length; i++)
+			{
+				if (s_log.isDebugEnabled())
+				{
+					s_log.debug("setSelectedDatabaseObjects: IDatabaseObjectInfo[" + i + "]=" + dbObjArr[i]);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Create panel for the Global Properties dialog.
+	 * 
+	 * @return properties panel.
+	 */
+	public IGlobalPreferencesPanel[] getGlobalPreferencePanels()
+	{
+		DBCopyGlobalPreferencesTab tab = new DBCopyGlobalPreferencesTab();
+		return new IGlobalPreferencesPanel[] { tab };
+	}
+
+	/**
+	 * @param coll
+	 * @param api
+	 */
+	protected void addMenuItemsToContextMenu(final IObjectTreeAPI api)
+	{
+		// final IObjectTreeAPI api = Compat.getIObjectTreeAPI(session, this);
+		final ActionCollection coll = getApplication().getActionCollection();
+
+		if (SwingUtilities.isEventDispatchThread())
+		{
+			addToPopup(api, coll);
+		}
+		else
+		{
+			SwingUtilities.invokeLater(new Runnable()
+			{
+				public void run()
+				{
+					addToPopup(api, coll);
+				}
+			});
+		}
+	}
+
+	/**
+	 * @param api
+	 * @param coll
+	 */
+	private void addToPopup(IObjectTreeAPI api, ActionCollection coll)
+	{
+
+		api.addToPopup(DatabaseObjectType.TABLE_TYPE_DBO, coll.get(CopyTableAction.class));
+
+		api.addToPopup(DatabaseObjectType.TABLE_TYPE_DBO, coll.get(PasteTableAction.class));
+
+		// Copy action object tree types
+		api.addToPopup(DatabaseObjectType.TABLE, coll.get(CopyTableAction.class));
+
+		api.addToPopup(DatabaseObjectType.TABLE, coll.get(PasteTableAction.class));
+
+		// Paste action object tree types
+		api.addToPopup(DatabaseObjectType.SCHEMA, coll.get(PasteTableAction.class));
+
+		// MySQL shows databases as "CATALOGS" not "SCHEMAS"
+		api.addToPopup(DatabaseObjectType.CATALOG, coll.get(PasteTableAction.class));
+
+		api.addToPopup(DatabaseObjectType.SESSION, coll.get(PasteTableAction.class));
+
+	}
+
+	private class DBCopyPluginResources extends PluginResources
+	{
+		DBCopyPluginResources(String rsrcBundleBaseName, IPlugin plugin)
+		{
+			super(rsrcBundleBaseName, plugin);
+		}
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getCopySourceSession()
+	 */
+	public ISession getCopySourceSession()
+	{
+		return copySourceSession;
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#setCopySourceSession(net.sourceforge.squirrel_sql.client.session.ISession)
+	 */
+	public void setCopySourceSession(ISession session)
+	{
+		if (session != null)
+		{
+			copySourceSession = session;
+		}
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getSourceSelectedDatabaseObjects()
+	 */
+	public IDatabaseObjectInfo[] getSourceSelectedDatabaseObjects()
+	{
+		return selectedDatabaseObjects;
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getCopyDestSession()
+	 */
+	public ISession getCopyDestSession()
+	{
+		return copyDestSession;
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#setDestCopySession(net.sourceforge.squirrel_sql.client.session.ISession)
+	 */
+	public void setDestCopySession(ISession session)
+	{
+		copyDestSession = session;
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#getDestSelectedDatabaseObject()
+	 */
+	public IDatabaseObjectInfo getDestSelectedDatabaseObject()
+	{
+		return selectedDestDatabaseObject;
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.plugins.dbcopy.SessionInfoProvider#setDestSelectedDatabaseObject(net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo)
+	 */
+	public void setDestSelectedDatabaseObject(IDatabaseObjectInfo info)
+	{
+		selectedDestDatabaseObject = info;
+	}
 }
