cef3b9d04f7b4af29931d0f45740d37d4e81f7e6
Robert Manning <robert.m.manning@gmail.com>
2008-11-15 17:14:02 +0000

    Added tests

---
+++ sql12/test/src/net/sourceforge/squirrel_sql/plugins/postgres/PostgresPluginTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.sourceforge.squirrel_sql.plugins.postgres;
+
+import net.sourceforge.squirrel_sql.plugins.AbstractPluginTest;
+import net.sourceforge.squirrel_sql.plugins.DatabaseProductVersionData;
+
+import org.junit.After;
+import org.junit.Before;
+
+
+public class PostgresPluginTest extends AbstractPluginTest implements DatabaseProductVersionData
+{	
+	@Before
+	public void setUp() throws Exception
+	{
+		classUnderTest = new PostgresPlugin();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		classUnderTest = null;
+	}		
+
+}
