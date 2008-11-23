34e99dfcd542d6219151e18bb6472a54ae54d064
Robert Manning <robert.m.manning@gmail.com>
2008-11-23 23:03:03 +0000

    Added test for proper serialization. Convert to JUnit4

--- sql12/test/src/net/sourceforge/squirrel_sql/client/gui/db/SQLAliasTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/client/gui/db/SQLAliasTest.java
@@ -1,4 +1,5 @@
 package net.sourceforge.squirrel_sql.client.gui.db;
+
 /*
  * Copyright (C) 2007 Rob Manning
  * manningr@users.sourceforge.net
@@ -17,42 +18,54 @@ package net.sourceforge.squirrel_sql.client.gui.db;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-import net.sourceforge.squirrel_sql.BaseSQuirreLTestCase;
+import static org.junit.Assert.assertEquals;
 import net.sourceforge.squirrel_sql.client.util.IdentifierFactory;
 import net.sourceforge.squirrel_sql.fw.id.IIdentifier;
 import net.sourceforge.squirrel_sql.fw.id.IIdentifierFactory;
+import net.sourceforge.squirrel_sql.plugins.syntax.AbstractSerializableTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import com.gargoylesoftware.base.testing.EqualsTester;
 
-public class SQLAliasTest extends BaseSQuirreLTestCase {
+public class SQLAliasTest extends AbstractSerializableTest
+{
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
+	@Before
+	public void setUp() throws Exception
+	{
+		super.serializableToTest = new SQLAlias();
+	}
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
+	@After
+	public void tearDown() throws Exception
+	{
+		super.serializableToTest = null;
+	}
 
-    @SuppressWarnings("serial")
-    public void testEqualsObject() {
-        IIdentifierFactory factory = IdentifierFactory.getInstance();
-        IIdentifier id1 = factory.createIdentifier();
-        IIdentifier id2 = factory.createIdentifier();
-        SQLAlias alias1 = new SQLAlias(id1);
-        SQLAlias alias2 = new SQLAlias(id1);
-        SQLAlias alias3 = new SQLAlias(id2);
-        SQLAlias alias4 = new SQLAlias(id1) {
-            
-        };
-        new EqualsTester(alias1, alias2, alias3, alias4);
-    }
+	@Test
+	public void testEqualsObject()
+	{
+		IIdentifierFactory factory = IdentifierFactory.getInstance();
+		IIdentifier id1 = factory.createIdentifier();
+		IIdentifier id2 = factory.createIdentifier();
+		SQLAlias alias1 = new SQLAlias(id1);
+		SQLAlias alias2 = new SQLAlias(id1);
+		SQLAlias alias3 = new SQLAlias(id2);
+		SQLAlias alias4 = new SQLAlias(id1)
+		{
+			private static final long serialVersionUID = 1L;
+		};
+		new EqualsTester(alias1, alias2, alias3, alias4);
+	}
 
-    public void testIsValid() {
-        SQLAlias uninitializedAlias = new SQLAlias();
-        assertEquals(false, uninitializedAlias.isValid());
-    }
+	@Test
+	public void testIsValid()
+	{
+		SQLAlias uninitializedAlias = new SQLAlias();
+		assertEquals(false, uninitializedAlias.isValid());
+	}
 
 }
