52c875d11f1618f11e95c974c0bf77ce8c01f758
Robert Manning <robert.m.manning@gmail.com>
2008-11-27 18:25:29 +0000

    Fixed potential datarace condition.  Also, there is no method getShowDebugLogMessages (the actual method that corresponds to this getter is not plural -> getShowDebugLogMessage.)  Changed this BeanInfo to match the class, since existing prefs.xml will have the non-plural version.

--- app/src/net/sourceforge/squirrel_sql/client/preferences/SquirrelPreferencesBeanInfo.java
+++ app/src/net/sourceforge/squirrel_sql/client/preferences/SquirrelPreferencesBeanInfo.java
@@ -26,109 +26,106 @@ import java.beans.SimpleBeanInfo;
 
 /**
  * This is the <CODE>BeanInfo</CODE> class for <CODE>SquirrelPreferences</CODE>.
- *
+ * 
  * @author <A HREF="mailto:colbell@users.sourceforge.net">Colin Bell</A>
  */
-public class SquirrelPreferencesBeanInfo extends SimpleBeanInfo
-   implements SquirrelPreferences.IPropertyNames
+public class SquirrelPreferencesBeanInfo extends SimpleBeanInfo implements SquirrelPreferences.IPropertyNames
 {
-   private static PropertyDescriptor[] s_dscrs;
 
-   @SuppressWarnings("unchecked")
-   private static Class CLS = SquirrelPreferences.class;
+	/**
+	 * See http://tinyurl.com/63no6t for discussion of the proper thread-safe way to implement
+	 * getPropertyDescriptors().
+	 * 
+	 * @see java.beans.SimpleBeanInfo#getPropertyDescriptors()
+	 */
+	@Override	
+	public PropertyDescriptor[] getPropertyDescriptors()
+	{
+		try
+		{
+			PropertyDescriptor[] result =
+				new PropertyDescriptor[] {
+						new PropertyDescriptor(SESSION_PROPERTIES, SquirrelPreferences.class,
+							"getSessionProperties", "setSessionProperties"),
+						new PropertyDescriptor(MAIN_FRAME_STATE, SquirrelPreferences.class,
+							"getMainFrameWindowState", "setMainFrameWindowState"),
+						new PropertyDescriptor(SHOW_CONTENTS_WHEN_DRAGGING, SquirrelPreferences.class,
+							"getShowContentsWhenDragging", "setShowContentsWhenDragging"),
+						new PropertyDescriptor(LOGIN_TIMEOUT, SquirrelPreferences.class, "getLoginTimeout",
+							"setLoginTimeout"),
+						new PropertyDescriptor(LARGE_SCRIPT_STMT_COUNT, SquirrelPreferences.class,
+							"getLargeScriptStmtCount", "setLargeScriptStmtCount"),
+						new PropertyDescriptor(JDBC_DEBUG_TYPE, SquirrelPreferences.class, "getJdbcDebugType",
+							"setJdbcDebugType"),
+						new PropertyDescriptor(SHOW_MAIN_STATUS_BAR, SquirrelPreferences.class,
+							"getShowMainStatusBar", "setShowMainStatusBar"),
+						new PropertyDescriptor(SHOW_MAIN_TOOL_BAR, SquirrelPreferences.class, "getShowMainToolBar",
+							"setShowMainToolBar"),
+						new PropertyDescriptor(SHOW_ALIASES_TOOL_BAR, SquirrelPreferences.class,
+							"getShowAliasesToolBar", "setShowAliasesToolBar"),
+						new PropertyDescriptor(SHOW_DRIVERS_TOOL_BAR, SquirrelPreferences.class,
+							"getShowDriversToolBar", "setShowDriversToolBar"),
+						new PropertyDescriptor(SHOW_TOOLTIPS, SquirrelPreferences.class, "getShowToolTips",
+							"setShowToolTips"),
+						new PropertyDescriptor(SCROLLABLE_TABBED_PANES, SquirrelPreferences.class,
+							"getUseScrollableTabbedPanes", "setUseScrollableTabbedPanes"),
+						new IndexedPropertyDescriptor(ACTION_KEYS, SquirrelPreferences.class, "getActionKeys",
+							"setActionKeys", "getActionKeys", "setActionKeys"),
+						new PropertyDescriptor(PROXY, SquirrelPreferences.class, "getProxySettings",
+							"setProxySettings"),
+						new PropertyDescriptor(UPDATE, SquirrelPreferences.class, "getUpdateSettings",
+							"setUpdateSettings"),
+						new PropertyDescriptor(SHOW_LOADED_DRIVERS_ONLY, SquirrelPreferences.class,
+							"getShowLoadedDriversOnly", "setShowLoadedDriversOnly"),
+						new PropertyDescriptor(MAXIMIMIZE_SESSION_SHEET_ON_OPEN, SquirrelPreferences.class,
+							"getMaximizeSessionSheetOnOpen", "setMaximizeSessionSheetOnOpen"),
+						new PropertyDescriptor(SHOW_COLOR_ICONS_IN_TOOLBAR, SquirrelPreferences.class,
+							"getShowColoriconsInToolbar", "setShowColoriconsInToolbar"),
+						new PropertyDescriptor(FIRST_RUN, SquirrelPreferences.class, "isFirstRun", "setFirstRun"),
+						new PropertyDescriptor(CONFIRM_SESSION_CLOSE, SquirrelPreferences.class,
+							"getConfirmSessionClose", "setConfirmSessionClose"),
+						new IndexedPropertyDescriptor(PLUGIN_STATUSES, SquirrelPreferences.class,
+							"getPluginStatuses", "setPluginStatuses", "getPluginStatus", "setPluginStatus"),
+						new PropertyDescriptor(NEW_SESSION_VIEW, SquirrelPreferences.class, "getNewSessionView",
+							"setNewSessionView"),
+						new PropertyDescriptor(FILE_OPEN_IN_PREVIOUS_DIR, SquirrelPreferences.class,
+							"isFileOpenInPreviousDir", "setFileOpenInPreviousDir"),
+						new PropertyDescriptor(FILE_OPEN_IN_SPECIFIED_DIR, SquirrelPreferences.class,
+							"isFileOpenInSpecifiedDir", "setFileOpenInSpecifiedDir"),
+						new PropertyDescriptor(FILE_SPECIFIED_DIR, SquirrelPreferences.class,
+							"getFileSpecifiedDir", "setFileSpecifiedDir"),
+						new PropertyDescriptor(FILE_PREVIOUS_DIR, SquirrelPreferences.class, "getFilePreviousDir",
+							"setFilePreviousDir"),
+						new PropertyDescriptor(SHOW_PLUGIN_FILES_IN_SPLASH_SCREEN, SquirrelPreferences.class,
+							"getShowPluginFilesInSplashScreen", "setShowPluginFilesInSplashScreen"),
+						new PropertyDescriptor(WARN_JRE_JDBC_MISMATCH, SquirrelPreferences.class,
+							"getWarnJreJdbcMismatch", "setWarnJreJdbcMismatch"),
+						new PropertyDescriptor(WARN_FOR_UNSAVED_FILE_EDITS, SquirrelPreferences.class,
+							"getWarnForUnsavedFileEdits", "setWarnForUnsavedFileEdits"),
+						new PropertyDescriptor(WARN_FOR_UNSAVED_BUFFER_EDITS, SquirrelPreferences.class,
+							"getWarnForUnsavedBufferEdits", "setWarnForUnsavedBufferEdits"),
+						new PropertyDescriptor(SHOW_SESSION_STARTUP_TIME_HINT, SquirrelPreferences.class,
+							"getShowSessionStartupTimeHint", "setShowSessionStartupTimeHint"),
+						new PropertyDescriptor(SHOW_DEBUG_LOG_MESSAGES, SquirrelPreferences.class,
+							"getShowDebugLogMessage", "setShowDebugLogMessages"),
+						new PropertyDescriptor(SHOW_INFO_LOG_MESSAGES, SquirrelPreferences.class,
+							"getShowInfoLogMessages", "setShowInfoLogMessages"),
+						new PropertyDescriptor(SHOW_ERROR_LOG_MESSAGES, SquirrelPreferences.class,
+							"getShowErrorLogMessages", "setShowErrorLogMessages"),
+						new PropertyDescriptor(SAVE_PREFERENCES_IMMEDIATELY, SquirrelPreferences.class,
+							"getSavePreferencesImmediately", "setSavePreferencesImmediately"),
+						new PropertyDescriptor(SELECT_ON_RIGHT_MOUSE_CLICK, SquirrelPreferences.class,
+							"getSelectOnRightMouseClick", "setSelectOnRightMouseClick"),
+						new PropertyDescriptor(SHOW_PLEASE_WAIT_DIALOG, SquirrelPreferences.class,
+							"getShowPleaseWaitDialog", "setShowPleaseWaitDialog"),
+						new PropertyDescriptor(PREFERRED_LOCALE, SquirrelPreferences.class, "getPreferredLocale",
+							"setPreferredLocale"), };
 
-   public SquirrelPreferencesBeanInfo() throws IntrospectionException
-   {
-      super();
-      if (s_dscrs == null)
-      {
-         s_dscrs = new PropertyDescriptor[]
-         {
-            new PropertyDescriptor(SESSION_PROPERTIES, CLS,
-               "getSessionProperties", "setSessionProperties"),
-            new PropertyDescriptor(MAIN_FRAME_STATE, CLS,
-               "getMainFrameWindowState", "setMainFrameWindowState"),
-            new PropertyDescriptor(SHOW_CONTENTS_WHEN_DRAGGING, CLS,
-               "getShowContentsWhenDragging", "setShowContentsWhenDragging"),
-            new PropertyDescriptor(LOGIN_TIMEOUT, CLS,
-               "getLoginTimeout", "setLoginTimeout"),
-            new PropertyDescriptor(LARGE_SCRIPT_STMT_COUNT, CLS,
-               "getLargeScriptStmtCount", "setLargeScriptStmtCount"),
-            new PropertyDescriptor(JDBC_DEBUG_TYPE, CLS,
-               "getJdbcDebugType", "setJdbcDebugType"),
-            new PropertyDescriptor(SHOW_MAIN_STATUS_BAR, CLS,
-               "getShowMainStatusBar", "setShowMainStatusBar"),
-            new PropertyDescriptor(SHOW_MAIN_TOOL_BAR, CLS,
-               "getShowMainToolBar", "setShowMainToolBar"),
-            new PropertyDescriptor(SHOW_ALIASES_TOOL_BAR, CLS,
-               "getShowAliasesToolBar", "setShowAliasesToolBar"),
-            new PropertyDescriptor(SHOW_DRIVERS_TOOL_BAR, CLS,
-               "getShowDriversToolBar", "setShowDriversToolBar"),
-            new PropertyDescriptor(SHOW_TOOLTIPS, CLS,
-               "getShowToolTips", "setShowToolTips"),
-            new PropertyDescriptor(SCROLLABLE_TABBED_PANES, CLS,
-               "getUseScrollableTabbedPanes", "setUseScrollableTabbedPanes"),
-            new IndexedPropertyDescriptor(ACTION_KEYS, CLS,
-               "getActionKeys", "setActionKeys",
-               "getActionKeys", "setActionKeys"),
-            new PropertyDescriptor(PROXY, CLS,
-               "getProxySettings", "setProxySettings"),
-            new PropertyDescriptor(UPDATE, CLS,
-               "getUpdateSettings", "setUpdateSettings"),
-            new PropertyDescriptor(SHOW_LOADED_DRIVERS_ONLY, CLS,
-               "getShowLoadedDriversOnly", "setShowLoadedDriversOnly"),
-            new PropertyDescriptor(MAXIMIMIZE_SESSION_SHEET_ON_OPEN, CLS,
-               "getMaximizeSessionSheetOnOpen", "setMaximizeSessionSheetOnOpen"),
-            new PropertyDescriptor(SHOW_COLOR_ICONS_IN_TOOLBAR, CLS,
-               "getShowColoriconsInToolbar", "setShowColoriconsInToolbar"),
-            new PropertyDescriptor(FIRST_RUN, CLS,
-               "isFirstRun", "setFirstRun"),
-            new PropertyDescriptor(CONFIRM_SESSION_CLOSE, CLS,
-               "getConfirmSessionClose", "setConfirmSessionClose"),
-            new IndexedPropertyDescriptor(PLUGIN_STATUSES, CLS,
-               "getPluginStatuses", "setPluginStatuses",
-               "getPluginStatus", "setPluginStatus"),
-            new PropertyDescriptor(NEW_SESSION_VIEW, CLS,
-               "getNewSessionView", "setNewSessionView"),
-            new PropertyDescriptor(FILE_OPEN_IN_PREVIOUS_DIR, CLS,
-               "isFileOpenInPreviousDir", "setFileOpenInPreviousDir"),
-            new PropertyDescriptor(FILE_OPEN_IN_SPECIFIED_DIR, CLS,
-               "isFileOpenInSpecifiedDir", "setFileOpenInSpecifiedDir"),
-            new PropertyDescriptor(FILE_SPECIFIED_DIR, CLS,
-               "getFileSpecifiedDir", "setFileSpecifiedDir"),
-            new PropertyDescriptor(FILE_PREVIOUS_DIR, CLS,
-               "getFilePreviousDir", "setFilePreviousDir"),
-            new PropertyDescriptor(SHOW_PLUGIN_FILES_IN_SPLASH_SCREEN, CLS,
-               "getShowPluginFilesInSplashScreen", 
-               "setShowPluginFilesInSplashScreen"),
-            new PropertyDescriptor(WARN_JRE_JDBC_MISMATCH, CLS,
-               "getWarnJreJdbcMismatch", "setWarnJreJdbcMismatch"),
-            new PropertyDescriptor(WARN_FOR_UNSAVED_FILE_EDITS, CLS,
-               "getWarnForUnsavedFileEdits", "setWarnForUnsavedFileEdits"),
-            new PropertyDescriptor(WARN_FOR_UNSAVED_BUFFER_EDITS, CLS,
-               "getWarnForUnsavedBufferEdits", "setWarnForUnsavedBufferEdits"),
-            new PropertyDescriptor(SHOW_SESSION_STARTUP_TIME_HINT, CLS,
-               "getShowSessionStartupTimeHint", "setShowSessionStartupTimeHint"),
-            new PropertyDescriptor(SHOW_DEBUG_LOG_MESSAGES, CLS,
-               "getShowDebugLogMessages", "setShowDebugLogMessages"),               
-            new PropertyDescriptor(SHOW_INFO_LOG_MESSAGES, CLS,
-               "getShowInfoLogMessages", "setShowInfoLogMessages"),
-            new PropertyDescriptor(SHOW_ERROR_LOG_MESSAGES, CLS,
-               "getShowErrorLogMessages", "setShowErrorLogMessages"),
-            new PropertyDescriptor(SAVE_PREFERENCES_IMMEDIATELY, CLS,
-               "getSavePreferencesImmediately", "setSavePreferencesImmediately"),
-            new PropertyDescriptor(SELECT_ON_RIGHT_MOUSE_CLICK, CLS,
-               "getSelectOnRightMouseClick", "setSelectOnRightMouseClick"),            
-            new PropertyDescriptor(SHOW_PLEASE_WAIT_DIALOG, CLS,
-               "getShowPleaseWaitDialog", "setShowPleaseWaitDialog"),            
-            new PropertyDescriptor(PREFERRED_LOCALE, CLS,
-                "getPreferredLocale", "setPreferredLocale"),            
-         };
-      }
-   }
-
-   public PropertyDescriptor[] getPropertyDescriptors()
-   {
-      return s_dscrs;
-   }
+			return result;
+		}
+		catch (IntrospectionException e)
+		{
+			throw new Error(e);
+		}
+	}
 }
