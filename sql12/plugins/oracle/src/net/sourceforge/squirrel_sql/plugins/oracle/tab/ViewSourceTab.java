07f6b06ccbef30f65a27e20d4adcf3f95772dbb4
Robert Manning <robert.m.manning@gmail.com>
2008-11-29 15:05:03 +0000

    Added copyright and license and fixed javadoc.

--- sql12/plugins/oracle/src/net/sourceforge/squirrel_sql/plugins/oracle/tab/ViewSourceTab.java
+++ sql12/plugins/oracle/src/net/sourceforge/squirrel_sql/plugins/oracle/tab/ViewSourceTab.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2005 Alexander Buloichik
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package net.sourceforge.squirrel_sql.plugins.oracle.tab;
 
 import java.sql.PreparedStatement;
@@ -8,6 +25,9 @@ import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
 import net.sourceforge.squirrel_sql.fw.util.StringManager;
 import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 
+/**
+ * This class will display the source for an Oracle view.
+ */
 public class ViewSourceTab extends OracleSourceTab
 {
 	private static final StringManager s_stringMgr =
@@ -15,8 +35,7 @@ public class ViewSourceTab extends OracleSourceTab
 
 
 	/**
-	 * This interface defines locale specific strings. This should be
-	 * replaced with a property file.
+	 * This interface defines locale specific strings.
 	 */
 	private interface i18n
 	{
