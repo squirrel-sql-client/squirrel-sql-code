f2b37433a8fd03f730d561131f1630f424364d05
Robert Manning <robert.m.manning@gmail.com>
2008-12-02 02:23:20 +0000

    Added tests.

--- test/src/net/sourceforge/squirrel_sql/fw/sql/SQLDriverPropertyTest.java
+++ test/src/net/sourceforge/squirrel_sql/fw/sql/SQLDriverPropertyTest.java
@@ -18,6 +18,10 @@
  */
 package net.sourceforge.squirrel_sql.fw.sql;
 
+import static org.junit.Assert.assertEquals;
+
+import java.sql.DriverPropertyInfo;
+
 import net.sourceforge.squirrel_sql.BaseSQuirreLJUnit4TestCase;
 
 import org.junit.Before;
@@ -27,6 +31,17 @@ public class SQLDriverPropertyTest extends BaseSQuirreLJUnit4TestCase
 {
 
 	SQLDriverProperty classUnderTest = null;
+	
+	// Mocks
+	
+	DriverPropertyInfo mockDriverPropertyInfo = new DriverPropertyInfo(PROP_NAME, PROP_VALUE);
+
+	// Data
+	
+	private static final String PROP_VALUE = "aPropValue";
+
+	private static final String PROP_NAME = "aPropName";
+	
 	
 	@Before
 	public void setUp() throws Exception
@@ -40,4 +55,34 @@ public class SQLDriverPropertyTest extends BaseSQuirreLJUnit4TestCase
 		classUnderTest.setDriverPropertyInfo(null);
 	}
 
+	@Test
+	public void testGetName() throws Exception
+	{
+		classUnderTest.setName("aTestString");
+		assertEquals("aTestString", classUnderTest.getName());
+	}
+
+	@Test
+	public void testGetValue() throws Exception
+	{
+		classUnderTest.setValue("aTestString");
+		assertEquals("aTestString", classUnderTest.getValue());
+	}
+
+	@Test
+	public void testIsSpecified() throws Exception
+	{
+		assertEquals(false, classUnderTest.isSpecified());
+	}
+
+	@Test
+	public void testGetDriverPropertyInfo() throws Exception
+	{
+		mockHelper.replayAll();
+		classUnderTest.setName(PROP_NAME);
+		classUnderTest.setDriverPropertyInfo(mockDriverPropertyInfo);
+		assertEquals(mockDriverPropertyInfo, classUnderTest.getDriverPropertyInfo());
+		mockHelper.verifyAll();
+	}
+	
 }
