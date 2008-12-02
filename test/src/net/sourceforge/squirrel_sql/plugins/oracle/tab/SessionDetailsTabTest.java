138e29319ffe23ca8efb71685f9fcdf004cb7da9
Robert Manning <robert.m.manning@gmail.com>
2008-12-02 11:11:53 +0000

    Reverted yesterday's "fix" for what appeared to be broken functionality in the Oracle plugin.  I wasn't looking for the detail tabs in the correct location.

--- test/src/net/sourceforge/squirrel_sql/plugins/oracle/tab/SessionDetailsTabTest.java
+++ test/src/net/sourceforge/squirrel_sql/plugins/oracle/tab/SessionDetailsTabTest.java
@@ -18,9 +18,9 @@
  */
 package net.sourceforge.squirrel_sql.plugins.oracle.tab;
 
-
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.AbstractBasePreparedStatementTabTest;
 
+import org.easymock.EasyMock;
 import org.junit.Before;
 
 public class SessionDetailsTabTest extends AbstractBasePreparedStatementTabTest
@@ -29,7 +29,17 @@ public class SessionDetailsTabTest extends AbstractBasePreparedStatementTabTest
 	@Before
 	public void setUp() throws Exception
 	{
-		classUnderTest = new SessionDetailsTab(1);
+		classUnderTest = new SessionDetailsTab();
+	}
+
+	/**
+	 * @see net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.AbstractBasePreparedStatementTabTest#setupMockDatabaseObjectInfo()
+	 */
+	@Override
+	protected void setupMockDatabaseObjectInfo()
+	{
+		super.setupMockDatabaseObjectInfo();
+		EasyMock.expect(mockDatabaseObjectInfo.getSimpleName()).andReturn("1").anyTimes();
 	}
 
 }
