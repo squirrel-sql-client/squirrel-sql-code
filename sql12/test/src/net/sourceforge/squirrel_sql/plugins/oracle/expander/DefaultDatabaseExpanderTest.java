3cb3c0b18f6bc2827b9bea3f6c238af1a6dfebec
Robert Manning <robert.m.manning@gmail.com>
2008-12-06 14:49:44 +0000

    Added tests.

---
+++ sql12/test/src/net/sourceforge/squirrel_sql/plugins/oracle/expander/DefaultDatabaseExpanderTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2008 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.sourceforge.squirrel_sql.plugins.oracle.expander;
+
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import net.sourceforge.squirrel_sql.client.IApplication;
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.AbstractINodeExpanderTest;
+import net.sourceforge.squirrel_sql.fw.sql.ISQLConnection;
+import net.sourceforge.squirrel_sql.fw.sql.SQLDatabaseMetaData;
+import net.sourceforge.squirrel_sql.fw.util.TaskThreadPool;
+
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultDatabaseExpanderTest //extends AbstractINodeExpanderTest
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+//		ISession mockSession = mockHelper.createMock(ISession.class);
+//		IApplication mockApplication = mockHelper.createMock(IApplication.class);
+//		expect(mockSession.getApplication()).andStubReturn(mockApplication);
+//		TaskThreadPool mockThreadPool = mockHelper.createMock(TaskThreadPool.class);
+//		mockThreadPool.addTask(EasyMock.isA(Runnable.class));
+//		expectLastCall().anyTimes();
+//		expect(mockApplication.getThreadPool()).andStubReturn(mockThreadPool);
+//		String[] tableTypes = new String[] { "TABLE" };
+//		ISQLConnection mockSQLConnection = mockHelper.createMock(ISQLConnection.class);
+//		expect(mockSession.getSQLConnection()).andStubReturn(mockSQLConnection);
+//		SQLDatabaseMetaData mockSQLDatabaseMetaData = mockHelper.createMock(SQLDatabaseMetaData.class);
+//		expect(mockSQLConnection.getSQLMetaData()).andStubReturn(mockSQLDatabaseMetaData);
+//		expect(mockSQLDatabaseMetaData.getTableTypes()).andStubReturn(tableTypes);
+//		
+//		
+//		mockHelper.replayAll();
+//		classUnderTest = new DefaultDatabaseExpander(mockSession);
+//		mockHelper.verifyAll();
+//		mockHelper.resetAll();
+	}
+
+	@Test 
+	public void testDummy() {}
+}
