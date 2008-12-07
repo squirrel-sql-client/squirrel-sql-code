671259bc5a99a3cb8e802721f16f0b75af2263fc
Robert Manning <robert.m.manning@gmail.com>
2008-12-07 23:56:42 +0000

    Added test.

--- test/src/net/sourceforge/squirrel_sql/plugins/refactoring/gui/MergeTableColumnTableModelTest.java
+++ test/src/net/sourceforge/squirrel_sql/plugins/refactoring/gui/MergeTableColumnTableModelTest.java
@@ -18,17 +18,18 @@
  */
 package net.sourceforge.squirrel_sql.plugins.refactoring.gui;
 
+
 import net.sourceforge.squirrel_sql.client.update.gui.AbstractTableModelTest;
 
 import org.junit.Before;
 
-public class AddUniqueConstraintColumnTableModelTest extends AbstractTableModelTest
+public class MergeTableColumnTableModelTest extends AbstractTableModelTest
 {
 
 	@Before
 	public void setUp() throws Exception
 	{
-		classUnderTest = new AddUniqueConstraintColumnTableModel();
+		classUnderTest = new MergeTableColumnTableModel();
 	}
 
 }
