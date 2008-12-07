a421ba5b93790599ce41f69a5a424c35b56d2dd4
Robert Manning <robert.m.manning@gmail.com>
2008-12-07 23:56:26 +0000

    Refactored out String TableModel so that it can be unit tested and possibly re-used.

--- plugins/refactoring/src/net/sourceforge/squirrel_sql/plugins/refactoring/gui/MergeTableDialog.java
+++ plugins/refactoring/src/net/sourceforge/squirrel_sql/plugins/refactoring/gui/MergeTableDialog.java
@@ -353,7 +353,7 @@ public class MergeTableDialog extends AbstractRefactoringTabbedDialog implements
 
 		  private JTable _columTable;
 
-        private final ColumnTableModel _columnTableModel = new ColumnTableModel();
+        private final MergeTableColumnTableModel _columnTableModel = new MergeTableColumnTableModel();
 
         private JComboBox referencingBox;
 
@@ -480,60 +480,6 @@ public class MergeTableDialog extends AbstractRefactoringTabbedDialog implements
 
         public Vector<String> getMergeColumns() {
             return _columnTableModel.getRowData();
-        }
-    }
-
-    class ColumnTableModel extends AbstractTableModel {
-   	  private static final long serialVersionUID = -1078295535237858500L;
-		  private final Vector<String> rowData = new Vector<String>();
-        private final String[] columnNames = new String[]{i18n.COLUMNS_REFERENCED_HEADER};
-
-
-        public String getColumnName(int col) {
-            return columnNames[col];
-        }
-
-
-        public int getRowCount() {
-            return rowData.size();
-        }
-
-
-        public int getColumnCount() {
-            return columnNames.length;
-        }
-
-
-        public Object getValueAt(int row, int col) {
-            return rowData.get(row);
-        }
-
-
-        public boolean isCellEditable(int row, int col) {
-            return false;
-        }
-
-
-        public void addColumn(String column) {
-            rowData.add(column);
-            fireTableDataChanged();
-        }
-
-
-        public String deleteRow(int row) {
-            String removedRow = rowData.remove(row);
-            fireTableDataChanged();
-            return removedRow;
-        }
-
-
-        public Vector<String> getRowData() {
-            return rowData;
-        }
-
-
-        public void clear() {
-            rowData.clear();
         }
     }
 
