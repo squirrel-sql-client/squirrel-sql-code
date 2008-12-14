c943a3a7dbce2735217f520cc7dfe5157cfee9be
Robert Manning <robert.m.manning@gmail.com>
2008-12-14 12:06:02 +0000

    Added tests.

--- sql12/test/src/net/sourceforge/squirrel_sql/client/update/gui/installer/event/InstallStatusListenerImplTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/client/update/gui/installer/event/InstallStatusListenerImplTest.java
@@ -18,7 +18,10 @@
  */
 package net.sourceforge.squirrel_sql.client.update.gui.installer.event;
 
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.isA;
 import net.sourceforge.squirrel_sql.BaseSQuirreLJUnit4TestCase;
+import net.sourceforge.squirrel_sql.client.update.gui.installer.ProgressDialogController;
 
 import org.junit.After;
 import org.junit.Before;
@@ -29,10 +32,12 @@ public class InstallStatusListenerImplTest extends BaseSQuirreLJUnit4TestCase
 
 	InstallStatusListenerImpl classUnderTest = null;
 	
+	ProgressDialogController mockController = mockHelper.createMock(ProgressDialogController.class);
+	
 	@Before
 	public void setUp() throws Exception
 	{
-		classUnderTest = new InstallStatusListenerImpl();
+		classUnderTest = new InstallStatusListenerImpl(mockController);
 	}
 
 	@After
@@ -42,10 +47,99 @@ public class InstallStatusListenerImplTest extends BaseSQuirreLJUnit4TestCase
 	}
 
 	@Test
-	public void testHandleInstallStatusEvent()
+	public void testHandleBackupStartedStatusEvent()
 	{
-		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.INSTALL_STARTED);
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.BACKUP_STARTED);
+		int numFilesToUpdate = 10;
+		evt.setNumFilesToUpdate(numFilesToUpdate);
+		mockController.showProgressDialog(isA(String.class), isA(String.class), eq(numFilesToUpdate));
+		
+		mockHelper.replayAll();
 		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
 	}
 
+	@Test
+	public void testHandleFileBackupStartedStatusEvent()
+	{
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.FILE_BACKUP_STARTED);
+		mockController.setMessage(isA(String.class));
+		
+		mockHelper.replayAll();
+		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
+	}
+	
+	@Test
+	public void testHandleFileBackupCompleteStatusEvent()
+	{
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.FILE_BACKUP_COMPLETE);
+		mockController.incrementProgress();
+		
+		mockHelper.replayAll();
+		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
+	}
+	
+	@Test
+	public void testHandleBackupCompleteStatusEvent()
+	{
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.BACKUP_COMPLETE);
+		mockController.hideProgressDialog();
+		
+		mockHelper.replayAll();
+		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
+	}
+	
+	@Test
+	public void testHandleInstallStartedStatusEvent()
+	{
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.INSTALL_STARTED);
+		int numFilesToUpdate = 10;
+		evt.setNumFilesToUpdate(numFilesToUpdate);
+		mockController.showProgressDialog(isA(String.class), isA(String.class), eq(numFilesToUpdate));
+		
+		mockHelper.replayAll();
+		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
+	}
+
+	@Test
+	public void testHandleFileInstallStartedStatusEvent()
+	{
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.FILE_INSTALL_STARTED);
+		mockController.setMessage(isA(String.class));
+		
+		mockHelper.replayAll();
+		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
+	}
+
+	@Test
+	public void testHandleFileInstallCompleteStatusEvent()
+	{
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.FILE_INSTALL_COMPLETE);
+		mockController.incrementProgress();
+		
+		mockHelper.replayAll();
+		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
+	}
+
+	@Test
+	public void testHandleInstallCompleteStatusEvent()
+	{
+		InstallStatusEvent evt = new InstallStatusEvent(InstallEventType.INSTALL_COMPLETE);
+		mockController.hideProgressDialog();
+		
+		mockHelper.replayAll();
+		classUnderTest.handleInstallStatusEvent(evt);
+		mockHelper.verifyAll();
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNullArgumentConstructor() {
+		classUnderTest = new InstallStatusListenerImpl(null);
+	}
 }
