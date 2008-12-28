6ffdf3042f9d0f10da819c36f6e23b1282c93a27
Gerd Wagner <gerdhwagner@t-online.de>
2008-12-28 17:32:43 +0000

    New user interface layout: Tabbed layout --> Fixed memory leak.

--- app/src/net/sourceforge/squirrel_sql/client/gui/builders/SquirrelTabbedPane.java
+++ app/src/net/sourceforge/squirrel_sql/client/gui/builders/SquirrelTabbedPane.java
@@ -20,6 +20,7 @@ package net.sourceforge.squirrel_sql.client.gui.builders;
  */
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.lang.ref.WeakReference;
 
 import javax.swing.*;
 
@@ -58,9 +59,9 @@ class SquirrelTabbedPane extends JTabbedPane
 	public void addNotify()
 	{
 		super.addNotify();
-		_prefsListener = new PropsListener();
+		_prefsListener = new PropsListener(_prefs, this);
 		_prefs.addPropertyChangeListener(_prefsListener);
-		propertiesHaveChanged(null);
+		_prefsListener.propertiesHaveChanged(null);
 	}
 
 	/**
@@ -77,22 +78,55 @@ class SquirrelTabbedPane extends JTabbedPane
 		}
 	}
 
-	private void propertiesHaveChanged(String propName)
-	{
-		if (propName == null || propName.equals(IAppPrefPropertynames.SCROLLABLE_TABBED_PANES))
-		{
-			int tabLayoutPolicy =
-				_prefs.getUseScrollableTabbedPanes() ? JTabbedPane.SCROLL_TAB_LAYOUT
-					: JTabbedPane.WRAP_TAB_LAYOUT;
-			setTabLayoutPolicy(tabLayoutPolicy);
-		}
-	}
 
-	private final class PropsListener implements PropertyChangeListener
+   /**
+    * Avoids memory leaks.
+    *
+    * Removing the global listener in removeNotify() did not prove really save.
+    * We introduced this listener class to hold a weak reference to the tabbed pane
+    * to make sure Sessions get garbage collected:
+    *
+    * If removeNotify() does not work this listener will remain in the list of the global prefs
+    * listener. It will then be the only global reference to the tabbed pane.
+    * The tabbed pane then can be garbage collected which will result in garbage collecting the
+    * complete Session. 
+    *
+    */
+	private static final class PropsListener implements PropertyChangeListener
 	{
-		public void propertyChange(PropertyChangeEvent evt)
+      private SquirrelPreferences _prefs;
+
+      private WeakReference<SquirrelTabbedPane> _refSquirrelTabbedPane;
+
+      public PropsListener(SquirrelPreferences prefs, SquirrelTabbedPane squirrelTabbedPane)
+      {
+         _prefs = prefs;
+         _refSquirrelTabbedPane = new WeakReference<SquirrelTabbedPane>(squirrelTabbedPane);
+      }
+
+      public void propertyChange(PropertyChangeEvent evt)
 		{
 			propertiesHaveChanged(evt.getPropertyName());
 		}
+
+      void propertiesHaveChanged(String propName)
+      {
+         SquirrelTabbedPane squirrelTabbedPane = _refSquirrelTabbedPane.get();
+
+         if(null == squirrelTabbedPane)
+         {
+            return;
+         }
+
+
+         if (propName == null || propName.equals(IAppPrefPropertynames.SCROLLABLE_TABBED_PANES))
+         {
+            int tabLayoutPolicy =
+               _prefs.getUseScrollableTabbedPanes() ? JTabbedPane.SCROLL_TAB_LAYOUT
+                  : JTabbedPane.WRAP_TAB_LAYOUT;
+            squirrelTabbedPane.setTabLayoutPolicy(tabLayoutPolicy);
+         }
+      }
+
 	}
 }
