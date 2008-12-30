b352edf1a0f62d92f62852241b8f2a5316784b4a
Robert Manning <robert.m.manning@gmail.com>
2008-12-30 11:34:10 +0000

    Added test

---
+++ test/src/net/sourceforge/squirrel_sql/fw/util/FileWrapperImplTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2008 Rob Manning
+ * manningr@users.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.sourceforge.squirrel_sql.fw.util;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gargoylesoftware.base.testing.EqualsTester;
+
+public class FileWrapperImplTest
+{
+
+	FileWrapperImpl classUnderTest = null;
+	
+	private final static String tmpDir = System.getProperty("java.io.tmpdir");
+	private final static String userHome = System.getProperty("user.home");
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		classUnderTest = new FileWrapperImpl(tmpDir);
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		classUnderTest = null;
+	}
+	
+	@Test
+	public void testGetAbsolutePath() {
+		assertEquals(tmpDir, classUnderTest.getAbsolutePath());
+	}
+	
+	@Test
+	public void testEqualsAndHashCode() {
+		FileWrapperImpl a = new FileWrapperImpl(tmpDir);
+		FileWrapperImpl b = new FileWrapperImpl(tmpDir);
+		FileWrapperImpl c = new FileWrapperImpl(userHome);
+		FileWrapperImpl d = new FileWrapperImpl(tmpDir) {
+			private static final long serialVersionUID = 1L;			
+		};
+		new EqualsTester(a, b, c, d);		
+	}
+
+}
