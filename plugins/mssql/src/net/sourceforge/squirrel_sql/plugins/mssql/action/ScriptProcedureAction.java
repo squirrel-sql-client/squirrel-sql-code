788403fd95111c0f1309f680775a78de006c6d17
Robert Manning <robert.m.manning@gmail.com>
2009-01-08 11:33:12 +0000

    Fixed warnings.

--- plugins/mssql/src/net/sourceforge/squirrel_sql/plugins/mssql/action/ScriptProcedureAction.java
+++ plugins/mssql/src/net/sourceforge/squirrel_sql/plugins/mssql/action/ScriptProcedureAction.java
@@ -19,22 +19,18 @@ package net.sourceforge.squirrel_sql.plugins.mssql.action;
  */
 
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 
 import net.sourceforge.squirrel_sql.client.IApplication;
 import net.sourceforge.squirrel_sql.client.action.SquirrelAction;
-import net.sourceforge.squirrel_sql.client.plugin.IPlugin;
 import net.sourceforge.squirrel_sql.client.session.IObjectTreeAPI;
 import net.sourceforge.squirrel_sql.client.session.ISession;
 import net.sourceforge.squirrel_sql.client.session.action.ISessionAction;
-import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.ObjectTreeNode;
-import net.sourceforge.squirrel_sql.fw.gui.Dialogs;
 import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
 import net.sourceforge.squirrel_sql.fw.util.Resources;
-
 import net.sourceforge.squirrel_sql.plugins.mssql.MssqlPlugin;
 
 public class ScriptProcedureAction extends SquirrelAction implements ISessionAction {
+	private static final long serialVersionUID = 1L;
 	private ISession _session;
 	private final MssqlPlugin _plugin;
 
@@ -45,7 +41,6 @@ public class ScriptProcedureAction extends SquirrelAction implements ISessionAct
 
 	public void actionPerformed(ActionEvent evt) {
 		if (_session != null) {
-			IPlugin plugin = _session.getApplication().getDummyAppPlugin();
 			IObjectTreeAPI treeAPI = _session.getSessionInternalFrame().getObjectTreeAPI();
 			final IDatabaseObjectInfo[] dbObjs = treeAPI.getSelectedDatabaseObjects();
             
