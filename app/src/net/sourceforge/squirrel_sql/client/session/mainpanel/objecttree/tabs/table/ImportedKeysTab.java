40ade0079c2bcdb7976fe13e61d4955eca756f94
Robert Manning <robert.m.manning@gmail.com>
2009-02-28 17:50:14 +0000

    Refactored duplicate code into base class.  Now these two tabs use a FilterDataSet to provide meaning of type codes for DELETE/UPDATE ACTION on imported or exported keys.

--- app/src/net/sourceforge/squirrel_sql/client/session/mainpanel/objecttree/tabs/table/ImportedKeysTab.java
+++ app/src/net/sourceforge/squirrel_sql/client/session/mainpanel/objecttree/tabs/table/ImportedKeysTab.java
@@ -1,4 +1,10 @@
 package net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.table;
+
+import net.sourceforge.squirrel_sql.fw.datasetviewer.DataSetException;
+import net.sourceforge.squirrel_sql.fw.datasetviewer.IDataSet;
+import net.sourceforge.squirrel_sql.fw.sql.ITableInfo;
+import net.sourceforge.squirrel_sql.fw.sql.SQLDatabaseMetaData;
+
 /*
  * Copyright (C) 2001-2003 Colin Bell
  * colbell@users.sourceforge.net
@@ -17,53 +23,31 @@ package net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.ta
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-import net.sourceforge.squirrel_sql.fw.datasetviewer.DataSetException;
-import net.sourceforge.squirrel_sql.fw.datasetviewer.IDataSet;
-import net.sourceforge.squirrel_sql.fw.sql.ISQLConnection;
-import net.sourceforge.squirrel_sql.fw.util.StringManager;
-import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
+
 /**
  * This tab shows the imported keys in the currently selected table.
- *
- * @author  <A HREF="mailto:colbell@users.sourceforge.net">Colin Bell</A>
+ * 
+ * @author <A HREF="mailto:colbell@users.sourceforge.net">Colin Bell</A>
  */
-public class ImportedKeysTab extends BaseTableTab
+public class ImportedKeysTab extends ForeignKeysBaseTab
 {
-	
-    /** Internationalized strings for this class. */
-    private static final StringManager s_stringMgr =
-        StringManagerFactory.getStringManager(ImportedKeysTab.class);
-	
-	/**
-	 * Return the title for the tab.
-	 *
-	 * @return	The title for the tab.
-	 */
-	public String getTitle()
+
+	@Override
+	protected String getTitleKey()
 	{
-		//i18n[ImportedKeysTab.title=Imported Keys]
-		return s_stringMgr.getString("ImportedKeysTab.title");
+		return "ImportedKeysTab.title";
 	}
 
-	/**
-	 * Return the hint for the tab.
-	 *
-	 * @return	The hint for the tab.
-	 */
-	public String getHint()
+	@Override
+	protected String getHintKey()
 	{
-		//i18n[ImportedKeysTab.hint=Show tables that the selected table makes reference to] 
-		return s_stringMgr.getString("ImportedKeysTab.hint");
+		return "ImportedKeysTab.hint";
 	}
 
-	/**
-	 * Retrieve the <TT>IDataSet</TT> to be displayed in this tab.
-	 * 
-	 * @return	the <TT>IDataSet</TT> to be displayed in this tab.
-	 */
-	protected IDataSet createDataSet() throws DataSetException
+	@Override
+	protected IDataSet getUnfilteredDataSet(SQLDatabaseMetaData md, ITableInfo tableInfo)
+		throws DataSetException
 	{
-		final ISQLConnection conn = getSession().getSQLConnection();
-        return conn.getSQLMetaData().getImportedKeysDataSet(getTableInfo());
+		return md.getImportedKeysDataSet(tableInfo);
 	}
 }
