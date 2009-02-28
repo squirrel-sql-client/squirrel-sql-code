6348be584bce73132e78ea9064ead8c706af0c9c
Robert Manning <robert.m.manning@gmail.com>
2009-02-28 03:33:53 +0000

    Use stringbuilder instead of stringbuffer.

--- app/src/net/sourceforge/squirrel_sql/client/gui/ViewLogsSheet.java
+++ app/src/net/sourceforge/squirrel_sql/client/gui/ViewLogsSheet.java
@@ -1,4 +1,5 @@
 package net.sourceforge.squirrel_sql.client.gui;
+
 /*
  * Copyright (C) 2002-2006 Colin Bell
  * colbell@users.sourceforge.net
@@ -19,13 +20,28 @@ package net.sourceforge.squirrel_sql.client.gui;
  */
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 
-import javax.swing.*;
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 import net.sourceforge.squirrel_sql.client.IApplication;
 import net.sourceforge.squirrel_sql.client.gui.desktopcontainer.DialogWidget;
@@ -41,9 +57,10 @@ import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 import net.sourceforge.squirrel_sql.fw.util.Utilities;
 import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
 import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
+
 /**
  * This sheet shows the SQuirreL log files.
- *
+ * 
  * @author <A HREF="mailto:colbell@users.sourceforge.net">Colin Bell</A>
  */
 public class ViewLogsSheet extends DialogWidget
@@ -53,17 +70,16 @@ public class ViewLogsSheet extends DialogWidget
 		StringManagerFactory.getStringManager(ViewLogsSheet.class);
 
 	/** Logger for this class. */
-	private static final ILogger s_log =
-		LoggerController.createLogger(ViewLogsSheet.class);
+	private static final ILogger s_log = LoggerController.createLogger(ViewLogsSheet.class);
 
 	/** Singleton instance of this class. */
 	private static ViewLogsSheet s_instance;
 
 	/** Application API. */
 	private final IApplication _app;
-    
-    /** Preferences */
-    private final SquirrelPreferences _prefs;
+
+	/** Preferences */
+	private final SquirrelPreferences _prefs;
 
 	/** Combo box containing all the log files. */
 	private final LogsComboBox _logDirCmb = new LogsComboBox();
@@ -74,10 +90,12 @@ public class ViewLogsSheet extends DialogWidget
 	/** Button that refreshes the log contents. */
 	private final JButton _refreshBtn = new JButton(s_stringMgr.getString("ViewLogsSheet.refresh"));
 
-    private final JCheckBox _errorChkbox = new JCheckBox("Errors");
-    private final JCheckBox _debugChkbox = new JCheckBox("Debug");
-    private final JCheckBox _infoChkbox = new JCheckBox("Info");
-    
+	private final JCheckBox _errorChkbox = new JCheckBox("Errors");
+
+	private final JCheckBox _debugChkbox = new JCheckBox("Debug");
+
+	private final JCheckBox _infoChkbox = new JCheckBox("Info");
+
 	/** Directory containing the log files. */
 	private final File _logDir;
 
@@ -86,36 +104,33 @@ public class ViewLogsSheet extends DialogWidget
 
 	/** If <TT>true</TT> log is being refreshed. */
 	private boolean _refreshing = false;
-    
+
 	/**
 	 * Ctor specifying the application API.
-	 *
-	 * @param	app		Application API.
-	 *
-	 * @throws	IllegalArgumentException
-	 *			Thrown if a <TT>null</TT> <TT>IApplication passed.
+	 * 
+	 * @param app
+	 *           Application API.
+	 * @throws IllegalArgumentException
+	 *            Thrown if a <TT>null</TT> <TT>IApplication passed.
 	 */
 	private ViewLogsSheet(IApplication app)
 	{
 		super(s_stringMgr.getString("ViewLogsSheet.title"), true, true, true, true, app);
-		if (app == null)
-		{
-			throw new IllegalArgumentException("IApplication == null");
-		}
+		if (app == null) { throw new IllegalArgumentException("IApplication == null"); }
 
 		_app = app;
-        _prefs = _app.getSquirrelPreferences();
+		_prefs = _app.getSquirrelPreferences();
 		_logDir = new ApplicationFiles().getExecutionLogFile().getParentFile();
 		createUserInterface();
 	}
 
 	/**
 	 * Show this window
-	 *
-	 * @param	app		Application API.
-	 *
-	 * @throws	IllegalArgumentException
-	 * 			Thrown if a <TT>null</TT> <TT>IApplication</TT> object passed.
+	 * 
+	 * @param app
+	 *           Application API.
+	 * @throws IllegalArgumentException
+	 *            Thrown if a <TT>null</TT> <TT>IApplication</TT> object passed.
 	 */
 	public static synchronized void showSheet(IApplication app)
 	{
@@ -139,19 +154,21 @@ public class ViewLogsSheet extends DialogWidget
 	}
 
 	/**
-	 * Nulls the singleton instance of this class in a thread-safe way. 
+	 * Nulls the singleton instance of this class in a thread-safe way.
 	 */
-	public static synchronized void disposeInstance() {
+	public static synchronized void disposeInstance()
+	{
 		s_instance = null;
 	}
-	
-   public void dispose()
+
+	@Override
+	public void dispose()
 	{
 		// Stop refresh if it is running.
 		_closing = true;
 
 		ViewLogsSheet.disposeInstance();
-		
+
 		super.dispose();
 	}
 
@@ -174,27 +191,28 @@ public class ViewLogsSheet extends DialogWidget
 		}
 	}
 
-    /**
-     * Enables the log combo box and refresh button using the Swing event 
-     * thread.
-     */
-    private void enableComponents(final boolean enabled) 
-    {
-        GUIUtils.processOnSwingEventThread(new Runnable() {
-            public void run() {
-                _refreshBtn.setEnabled(enabled);
-                _logDirCmb.setEnabled(enabled);
-            }
-        });
-    }
-    
+	/**
+	 * Enables the log combo box and refresh button using the Swing event thread.
+	 */
+	private void enableComponents(final boolean enabled)
+	{
+		GUIUtils.processOnSwingEventThread(new Runnable()
+		{
+			public void run()
+			{
+				_refreshBtn.setEnabled(enabled);
+				_logDirCmb.setEnabled(enabled);
+			}
+		});
+	}
+
 	/**
 	 * Refresh the log.
 	 */
 	private void refreshLog()
 	{
-	    enableComponents(false);
-        CursorChanger cursorChg = new CursorChanger(getAwtContainer());
+		enableComponents(false);
+		final CursorChanger cursorChg = new CursorChanger(getAwtContainer());
 		cursorChg.show();
 		try
 		{
@@ -208,12 +226,12 @@ public class ViewLogsSheet extends DialogWidget
 					}
 				});
 			}
-			catch (Exception ex)
+			catch (final Exception ex)
 			{
-                // i18n[ViewLogsSheet.error.clearlogcontents=Error clearing the log contents]
+				// i18n[ViewLogsSheet.error.clearlogcontents=Error clearing the log contents]
 				s_log.error(s_stringMgr.getString("ViewLogsSheet.error.clearlogcontents"), ex);
 			}
-			final File logFile = (File)_logDirCmb.getSelectedItem();
+			final File logFile = (File) _logDirCmb.getSelectedItem();
 			if (logFile != null)
 			{
 				try
@@ -224,13 +242,10 @@ public class ViewLogsSheet extends DialogWidget
 						try
 						{
 							String line = null;
-							StringBuffer chunk = new StringBuffer(16384);
+							StringBuilder chunk = new StringBuilder(16384);
 							while ((line = rdr.readLine()) != null)
 							{
-								if (_closing)
-								{
-									return;
-								}
+								if (_closing) { return; }
 
 								if (chunk.length() > 16000)
 								{
@@ -245,20 +260,18 @@ public class ViewLogsSheet extends DialogWidget
 											}
 										}
 									});
-									chunk = new StringBuffer(16384);
+									chunk = new StringBuilder(16384);
 								}
 								else
 								{
-                                    if (shouldAppendLineToChunk(line)) {
-                                        chunk.append(line).append('\n');
-                                    }                                    
+									if (shouldAppendLineToChunk(line))
+									{
+										chunk.append(line).append('\n');
+									}
 								}
 							}
 
-							if (_closing)
-							{
-								return;
-							}
+							if (_closing) { return; }
 
 							final String finalLine = chunk.toString();
 							SwingUtilities.invokeAndWait(new Runnable()
@@ -278,90 +291,94 @@ public class ViewLogsSheet extends DialogWidget
 						}
 					}
 				}
-				catch (Exception ex)
+				catch (final Exception ex)
 				{
-                    // i18n[ViewLogsSheet.error.processinglogfile=Error occured processing log file]
+					// i18n[ViewLogsSheet.error.processinglogfile=Error occured processing log file]
 					final String msg = s_stringMgr.getString("ViewLogsSheet.error.processinglogfile");
 					s_log.error(msg, ex);
 				}
 			}
 			else
 			{
-                // i18n[ViewLogsSheet.info.nulllogfile=Null log file name]
-				s_log.debug(s_stringMgr.getString("ViewLogsSheet.info.nulllogfile"));
+				if (s_log.isDebugEnabled()) {
+					// i18n[ViewLogsSheet.info.nulllogfile=Null log file name]
+					s_log.debug(s_stringMgr.getString("ViewLogsSheet.info.nulllogfile"));
+				}
 			}
 
-			if (_closing)
-			{
-				return;
-			}
+			if (_closing) { return; }
 
 			// Position to the start of the last line in log.
 			try
 			{
-				int pos = Math.max(0, _logContentsTxt.getText().length() - 1);
-				int line = _logContentsTxt.getLineOfOffset(pos);
+				final int pos = Math.max(0, _logContentsTxt.getText().length() - 1);
+				final int line = _logContentsTxt.getLineOfOffset(pos);
 				final int finalpos = _logContentsTxt.getLineStartOffset(line);
-                SwingUtilities.invokeAndWait(new Runnable() {
-                    public void run()
-                    {
-                        _logContentsTxt.setCaretPosition(finalpos);
-                    }
-                });
+				SwingUtilities.invokeAndWait(new Runnable()
+				{
+					public void run()
+					{
+						_logContentsTxt.setCaretPosition(finalpos);
+					}
+				});
 			}
-			catch (Exception ex)
+			catch (final Exception ex)
 			{
-                // i18n[ViewLogsSheet.error.setcaret=Error positioning caret in log text component]
+				// i18n[ViewLogsSheet.error.setcaret=Error positioning caret in log text component]
 				s_log.error(s_stringMgr.getString("ViewLogsSheet.error.setcaret"), ex);
 			}
 		}
 		finally
 		{
-            enableComponents(true);
+			enableComponents(true);
 			_refreshing = false;
 			cursorChg.restore();
 		}
 	}
 
-    /**
-       199828 [Foo Thread] message 
-     * @param line
-     * @return
-     */
-    private boolean shouldAppendLineToChunk(String line) {
-        boolean result = false;
-        if (line == null || line.length() == 0) {
-            return false;
-        }
-        if (_errorChkbox.isSelected() 
-                && _debugChkbox.isSelected() 
-                && _infoChkbox.isSelected()) 
-        {
-            return true;
-        }
-        int threadNameEndIdx = line.indexOf("]");
-        if (threadNameEndIdx > -1) {
-        	char levelChar = ' ';
-        	if (line.length() >= threadNameEndIdx + 2)
-        		levelChar = line.charAt(threadNameEndIdx + 2);
-            if (_errorChkbox.isSelected() && levelChar == 'E') {
-                result = true;
-            }
-            if (_debugChkbox.isSelected() && levelChar == 'D') {
-                result = true;
-            }
-            if (_infoChkbox.isSelected() && levelChar == 'I') {
-                result = true;
-            }
-            if (levelChar != 'E' && levelChar != 'D' && levelChar != 'I') {
-                result = true;
-            }
-        } else {
-            result = true;
-        }
-        return result;
-    }
-    
+	/**
+	 * 199828 [Foo Thread] message
+	 * 
+	 * @param line
+	 * @return
+	 */
+	private boolean shouldAppendLineToChunk(String line)
+	{
+		boolean result = false;
+		if (line == null || line.length() == 0) { return false; }
+		if (_errorChkbox.isSelected() && _debugChkbox.isSelected() && _infoChkbox.isSelected()) { return true; }
+		final int threadNameEndIdx = line.indexOf("]");
+		if (threadNameEndIdx > -1)
+		{
+			char levelChar = ' ';
+			if (line.length() >= threadNameEndIdx + 2)
+			{
+				levelChar = line.charAt(threadNameEndIdx + 2);
+			}
+			if (_errorChkbox.isSelected() && levelChar == 'E')
+			{
+				result = true;
+			}
+			if (_debugChkbox.isSelected() && levelChar == 'D')
+			{
+				result = true;
+			}
+			if (_infoChkbox.isSelected() && levelChar == 'I')
+			{
+				result = true;
+			}
+			if (levelChar != 'E' && levelChar != 'D' && levelChar != 'I')
+			{
+				result = true;
+			}
+		}
+		else
+		{
+			result = true;
+		}
+		return result;
+	}
+
 	/**
 	 * Create user interface.
 	 */
@@ -369,30 +386,30 @@ public class ViewLogsSheet extends DialogWidget
 	{
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		makeToolWindow(true);
-		Container contentPane = getContentPane();
+		final Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 		contentPane.add(createToolBar(), BorderLayout.NORTH);
 		contentPane.add(createMainPanel(), BorderLayout.CENTER);
 		contentPane.add(createButtonsPanel(), BorderLayout.SOUTH);
 		pack();
 
-
-      AbstractAction closeAction = new AbstractAction()
-      {
+		final AbstractAction closeAction = new AbstractAction()
+		{
 			private static final long serialVersionUID = 1L;
 
 			public void actionPerformed(ActionEvent actionEvent)
-         {
-            performClose();
-         }
-      };
-      KeyStroke escapeStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
-      getRootPane().getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(escapeStroke, "CloseAction");
-      getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeStroke, "CloseAction");
-      getRootPane().getInputMap(JComponent.WHEN_FOCUSED).put(escapeStroke, "CloseAction");
-      getRootPane().getActionMap().put("CloseAction", closeAction);
+			{
+				performClose();
+			}
+		};
+		final KeyStroke escapeStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+		getRootPane().getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(escapeStroke,
+			"CloseAction");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeStroke, "CloseAction");
+		getRootPane().getInputMap(JComponent.WHEN_FOCUSED).put(escapeStroke, "CloseAction");
+		getRootPane().getActionMap().put("CloseAction", closeAction);
 
-   }
+	}
 
 	private ToolBar createToolBar()
 	{
@@ -400,7 +417,7 @@ public class ViewLogsSheet extends DialogWidget
 		tb.setUseRolloverButtons(true);
 		tb.setFloatable(false);
 
-		final Object[] args = {getTitle(), _logDir.getAbsolutePath()};
+		final Object[] args = { getTitle(), _logDir.getAbsolutePath() };
 		final String lblTitle = s_stringMgr.getString("ViewLogsSheet.storedin", args);
 		final JLabel lbl = new JLabel(lblTitle);
 		lbl.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
@@ -414,11 +431,12 @@ public class ViewLogsSheet extends DialogWidget
 	 */
 	private JPanel createMainPanel()
 	{
-		//_logContentsTxt.setEditable(false);
+		// _logContentsTxt.setEditable(false);
 		final TextPopupMenu pop = new TextPopupMenu();
 		pop.setTextComponent(_logContentsTxt);
 		_logContentsTxt.addMouseListener(new MouseAdapter()
 		{
+			@Override
 			public void mousePressed(MouseEvent evt)
 			{
 				if (evt.isPopupTrigger())
@@ -426,6 +444,8 @@ public class ViewLogsSheet extends DialogWidget
 					pop.show(evt.getComponent(), evt.getX(), evt.getY());
 				}
 			}
+
+			@Override
 			public void mouseReleased(MouseEvent evt)
 			{
 				if (evt.isPopupTrigger())
@@ -435,7 +455,7 @@ public class ViewLogsSheet extends DialogWidget
 			}
 		});
 
-		File appLogFile = new ApplicationFiles().getExecutionLogFile();
+		final File appLogFile = new ApplicationFiles().getExecutionLogFile();
 		_logDirCmb.load(appLogFile.getParentFile());
 
 		if (_logDirCmb.getModel().getSize() > 0)
@@ -462,7 +482,7 @@ public class ViewLogsSheet extends DialogWidget
 	 */
 	private JPanel createButtonsPanel()
 	{
-		JPanel pnl = new JPanel();
+		final JPanel pnl = new JPanel();
 
 		pnl.add(_refreshBtn);
 		_refreshBtn.addActionListener(new ActionListener()
@@ -473,7 +493,7 @@ public class ViewLogsSheet extends DialogWidget
 			}
 		});
 
-		JButton closeBtn = new JButton(s_stringMgr.getString("ViewLogsSheet.close"));
+		final JButton closeBtn = new JButton(s_stringMgr.getString("ViewLogsSheet.close"));
 		closeBtn.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent evt)
@@ -483,29 +503,35 @@ public class ViewLogsSheet extends DialogWidget
 		});
 		pnl.add(closeBtn);
 
-        _errorChkbox.setSelected(_prefs.getShowErrorLogMessages());
-        _errorChkbox.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                _prefs.setShowErrorLogMessages(_errorChkbox.isSelected());
-            }
-        }); 
-        _infoChkbox.setSelected(_prefs.getShowInfoLogMessages());
-        _infoChkbox.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                _prefs.setShowInfoLogMessages(_infoChkbox.isSelected());
-            }
-        }); 
-        _debugChkbox.setSelected(_prefs.getShowDebugLogMessage());
-        _debugChkbox.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                _prefs.setShowDebugLogMessages(_debugChkbox.isSelected());
-            }
-        }); 
-        pnl.add(_errorChkbox);
-        pnl.add(_infoChkbox);
-        pnl.add(_debugChkbox);
-                
-		GUIUtils.setJButtonSizesTheSame(new JButton[] {closeBtn, _refreshBtn});
+		_errorChkbox.setSelected(_prefs.getShowErrorLogMessages());
+		_errorChkbox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				_prefs.setShowErrorLogMessages(_errorChkbox.isSelected());
+			}
+		});
+		_infoChkbox.setSelected(_prefs.getShowInfoLogMessages());
+		_infoChkbox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				_prefs.setShowInfoLogMessages(_infoChkbox.isSelected());
+			}
+		});
+		_debugChkbox.setSelected(_prefs.getShowDebugLogMessage());
+		_debugChkbox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				_prefs.setShowDebugLogMessages(_debugChkbox.isSelected());
+			}
+		});
+		pnl.add(_errorChkbox);
+		pnl.add(_infoChkbox);
+		pnl.add(_debugChkbox);
+
+		GUIUtils.setJButtonSizesTheSame(new JButton[] { closeBtn, _refreshBtn });
 		getRootPane().setDefaultButton(closeBtn);
 
 		return pnl;
@@ -530,15 +556,17 @@ public class ViewLogsSheet extends DialogWidget
 	private static final class LogsComboBox extends DirectoryListComboBox
 	{
 		private static final long serialVersionUID = 1L;
-		
+
 		private File _dir;
 
+		@Override
 		public void load(File dir, FilenameFilter filter)
 		{
 			_dir = dir;
 			super.load(dir, filter);
 		}
 
+		@Override
 		public void addItem(Object anObject)
 		{
 			super.addItem(new LogFile(_dir, anObject.toString()));
@@ -548,18 +576,18 @@ public class ViewLogsSheet extends DialogWidget
 	private static final class LogFile extends File
 	{
 		private static final long serialVersionUID = 1L;
-		
+
 		private final String _stringRep;
 
 		LogFile(File dir, String name)
 		{
 			super(dir, name);
-			StringBuffer buf = new StringBuffer();
-			buf.append(getName()).append(" (")
-				.append(Utilities.formatSize(length())).append(")");
+			final StringBuffer buf = new StringBuffer();
+			buf.append(getName()).append(" (").append(Utilities.formatSize(length())).append(")");
 			_stringRep = buf.toString();
 		}
 
+		@Override
 		public String toString()
 		{
 			return _stringRep;
