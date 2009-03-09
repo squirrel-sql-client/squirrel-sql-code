beb684797893fb66ae1e928193997a6d8772e3e7
Robert Manning <robert.m.manning@gmail.com>
2009-03-09 01:13:14 +0000

    preparation for 3.0.1 maintenance release.

--- sql12/app/src/net/sourceforge/squirrel_sql/client/Version.java
+++ sql12/app/src/net/sourceforge/squirrel_sql/client/Version.java
@@ -33,7 +33,7 @@ public class Version
 	private static final String APP_NAME = s_stringMgr.getString("Version.appname");
 	private static final int MAJOR_VERSION = 3;
 	private static final int MINOR_VERSION = 0;
-	private static final int RELEASE = 0;
+	private static final int RELEASE = 1;
 
 	private static final String COPYRIGHT = s_stringMgr.getString("Version.copyright");
 
