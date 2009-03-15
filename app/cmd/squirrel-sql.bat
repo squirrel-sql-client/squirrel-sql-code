2b3c2afe3d2a90405b110e3a15cd138db970ce30
Robert Manning <robert.m.manning@gmail.com>
2009-03-15 18:14:49 +0000

    2685144: Added check for minimum JVM version of 1.6.  This will work for any JVM version >= 1.2.2.  This will also inform the user which JVM was used to launch the application.

--- app/cmd/squirrel-sql.bat
+++ app/cmd/squirrel-sql.bat
@@ -10,6 +10,8 @@ set basedir=%basedir:~0,-1%
 if NOT "%removed%"=="\" goto strip
 set SQUIRREL_SQL_HOME=%basedir%
 
+"%LOCAL_JAVA%w" -cp "%SQUIRREL_SQL_HOME%\lib\versioncheck.jar" JavaVersionChecker
+if ErrorLevel 1 goto ExitForWrongJavaVersion
 
 @rem If the changelist.xml file isn't present or the downloaded update jars don't exist, skip launching the updater - these files are created by the 
 @rem software update feature inside of SQuirreL. So their absence, simply means the software update feature hasn't been accessed.
@@ -49,3 +51,5 @@ start "SQuirreL SQL Client" /B "%LOCAL_JAVA%w" -Xmx256m -cp %SQUIRREL_CP% net.so
 @rem "%LOCAL_JAVA%" -jar "%SQUIRREL_SQL_HOME%\squirrel-sql.jar" %TMP_PARMS%
 @rem start "SQuirreL SQL Client" /B "%LOCAL_JAVA%w" -jar "%SQUIRREL_SQL_HOME%\squirrel-sql.jar" %TMP_PARMS%
 
+:ExitForWrongJavaVersion
+exit
