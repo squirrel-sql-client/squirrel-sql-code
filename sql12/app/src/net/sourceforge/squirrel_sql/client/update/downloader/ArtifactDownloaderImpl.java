8a87c63ba643f6701684f4830bd78b0b87316d65
Robert Manning <robert.m.manning@gmail.com>
2009-03-16 22:00:06 +0000

    Merged from trunk (1.10).  Fixed problem with updating from local path.

--- sql12/app/src/net/sourceforge/squirrel_sql/client/update/downloader/ArtifactDownloaderImpl.java
+++ sql12/app/src/net/sourceforge/squirrel_sql/client/update/downloader/ArtifactDownloaderImpl.java
@@ -72,10 +72,9 @@ public class ArtifactDownloaderImpl implements Runnable, ArtifactDownloader
 
 	private UpdateUtil _util = null;
 
-	/** TODO: change this to Spring-injected when this class becomes a Spring bean. */
 	private PathUtils _pathUtils = new PathUtilsImpl();
 
-	private IProxySettings _proxySettings = null;	
+	private IProxySettings _proxySettings = null;
 
 	public ArtifactDownloaderImpl(List<ArtifactStatus> artifactStatus)
 	{
@@ -113,6 +112,7 @@ public class ArtifactDownloaderImpl implements Runnable, ArtifactDownloader
 				}
 				String fileToGet =
 					_pathUtils.buildPath(true, _path, _channelName, status.getType(), status.getName());
+
 				String destDir = getArtifactDownloadDestDir(status);
 
 				if (_util.isPresentInDownloadsDirectory(status))
@@ -131,21 +131,25 @@ public class ArtifactDownloaderImpl implements Runnable, ArtifactDownloader
 				{
 					int count = 0;
 					boolean success = false;
-					while (count++ <= 3 && !success) {
+					while (count++ <= 3 && !success)
+					{
 						success = attemptFileDownload(fileToGet, destDir, status);
-						if (!success) {
-							long sleepTime = (count+1) * 3000;
+						if (!success)
+						{
+							long sleepTime = (count + 1) * 3000;
 							Utilities.sleep(sleepTime);
 						}
 					}
-					if (!success) {
+					if (!success)
+					{
 						sendDownloadFailed();
 						return;
 					}
 				}
 				else
 				{
-					fileToGet = _pathUtils.buildPath(false, this._fileSystemUpdatePath, fileToGet);
+					fileToGet =
+						_pathUtils.buildPath(false, this._fileSystemUpdatePath, status.getType(), status.getName());
 					result = _util.downloadLocalUpdateFile(fileToGet, destDir);
 				}
 				if (result == false)
@@ -182,8 +186,7 @@ public class ArtifactDownloaderImpl implements Runnable, ArtifactDownloader
 	private boolean attemptFileDownload(String fileToGet, String destDir, ArtifactStatus status)
 	{
 		boolean success = true;
-		
-		
+
 		try
 		{
 			_util.downloadHttpUpdateFile(_host, _port, fileToGet, destDir, status.getSize(),
