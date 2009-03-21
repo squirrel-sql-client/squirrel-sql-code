ca2a01554840dff4b3a859a7321fd3f0f1473a85
Robert Manning <robert.m.manning@gmail.com>
2009-03-21 16:23:41 +0000

    make test script depend less upon Oracle.

--- test/sql/test_drop.sql
+++ test/sql/test_drop.sql
@@ -23,20 +23,20 @@
 
 create table i (
     i_pkcol integer primary key, 
-    i_data varchar2(100)
+    i_data varchar(100)
 );
 
 create table e ( 
     e_pkcol integer primary key, 
     i_fk integer references i(i_pkcol),
-    e_data varchar2(100)
+    e_data varchar(100)
 );
 
 create table g ( 
     g_pkcol integer primary key, 
     e_fk integer references e(e_pkcol), 
     i_fk integer references i(i_pkcol),
-    gdata varchar2(100)
+    gdata varchar(100)
 );
 
 create table c ( 
@@ -88,4 +88,35 @@ create table j (
     h_fk integer references h(h_pkcol)
 );
 
+-- Data
 
+
+INSERT INTO I (I_PKCOL,I_DATA) VALUES (0,'Some Data');
+INSERT INTO I (I_PKCOL,I_DATA) VALUES (1,'Some more data');
+
+INSERT INTO E (E_PKCOL,I_FK,E_DATA) VALUES (0,0,'Some E data');
+INSERT INTO E (E_PKCOL,I_FK,E_DATA) VALUES (1,1,'Some more E data');
+
+INSERT INTO G (G_PKCOL,E_FK,I_FK,GDATA) VALUES (0,0,0,'Some G Data');
+INSERT INTO G (G_PKCOL,E_FK,I_FK,GDATA) VALUES (1,1,1,'Some more G data');
+
+INSERT INTO C (C_PKCOL,E_FK,G_FK) VALUES (0,0,0);
+INSERT INTO C (C_PKCOL,E_FK,G_FK) VALUES (1,1,1);
+
+INSERT INTO F (F_PKCOL,C_FK,G_FK) VALUES (0,0,0);
+INSERT INTO F (F_PKCOL,C_FK,G_FK) VALUES (1,1,1);
+
+INSERT INTO A (A_PKCOL,C_FK,I_FK,F_FK) VALUES (0,0,0,0);
+INSERT INTO A (A_PKCOL,C_FK,I_FK,F_FK) VALUES (1,1,1,1);
+
+INSERT INTO D (D_PKCOL,A_FK,F_FK) VALUES (0,0,0);
+INSERT INTO D (D_PKCOL,A_FK,F_FK) VALUES (1,1,1);
+
+INSERT INTO B (B_PKCOL,D_FK,I_FK,F_FK) VALUES (0,0,0,0);
+INSERT INTO B (B_PKCOL,D_FK,I_FK,F_FK) VALUES (1,1,1,1);
+
+INSERT INTO H (H_PKCOL,A_FK,C_FK,G_FK,F_FK) VALUES (0,0,0,0,0);
+INSERT INTO H (H_PKCOL,A_FK,C_FK,G_FK,F_FK) VALUES (1,1,1,1,1);
+
+INSERT INTO J (J_PKCOL,A_FK,C_FK,G_FK,F_FK,H_FK) VALUES (0,0,0,0,0,0);
+INSERT INTO J (J_PKCOL,A_FK,C_FK,G_FK,F_FK,H_FK) VALUES (1,1,1,1,1,1);
