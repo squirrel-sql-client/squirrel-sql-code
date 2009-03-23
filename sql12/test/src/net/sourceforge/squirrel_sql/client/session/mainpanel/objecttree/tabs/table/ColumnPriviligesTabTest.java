086a4e2e4c5fbdf385091e76b61b81b0c24e5750
Robert Manning <robert.m.manning@gmail.com>
2009-03-23 02:16:10 +0000

    added tests

--- sql12/test/src/net/sourceforge/squirrel_sql/client/session/mainpanel/objecttree/tabs/table/ColumnPriviligesTabTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/client/session/mainpanel/objecttree/tabs/table/ColumnPriviligesTabTest.java
@@ -18,19 +18,28 @@
  */
 package net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.table;
 
-
+import static org.easymock.EasyMock.aryEq;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.isNull;
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.AbstractBaseDataSetTabTest;
+import net.sourceforge.squirrel_sql.fw.sql.ITableInfo;
 
+import org.easymock.EasyMock;
 import org.junit.Before;
 
 public class ColumnPriviligesTabTest extends AbstractBaseDataSetTabTest
 {
+	private int[] columnIndices = new int[] { 4, 6, 7, 5, 8 };
 
 	@Before
 	public void setUp() throws Exception
 	{
 		super.setUp();
 		classUnderTest = new ColumnPriviligesTab();
+		super.clazz = ColumnPriviligesTab.class;
+		EasyMock.expect(
+			mockSQLMetaData.getColumnPrivilegesDataSet((ITableInfo) isNull(), aryEq(columnIndices), eq(true)))
+			.andStubReturn(null);
 	}
 
 }
