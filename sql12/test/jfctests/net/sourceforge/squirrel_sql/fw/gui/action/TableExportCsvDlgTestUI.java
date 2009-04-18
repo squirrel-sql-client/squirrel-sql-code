ea19f0998c1fa16102647c2b4129c68a864c399b
Robert Manning <robert.m.manning@gmail.com>
2009-04-18 22:40:38 +0000

    Some cleanup - removed duplicate DummyPlugin implementations.

--- sql12/test/jfctests/net/sourceforge/squirrel_sql/fw/gui/action/TableExportCsvDlgTestUI.java
+++ sql12/test/jfctests/net/sourceforge/squirrel_sql/fw/gui/action/TableExportCsvDlgTestUI.java
@@ -3,10 +3,11 @@ package net.sourceforge.squirrel_sql.fw.gui.action;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import net.sourceforge.squirrel_sql.BaseSQuirreLJUnit4TestCase;
 import net.sourceforge.squirrel_sql.client.ApplicationArguments;
 import net.sourceforge.squirrel_sql.fw.gui.GUIUtils;
 
-public class TableExportCsvDlgTestUI
+public class TableExportCsvDlgTestUI extends BaseSQuirreLJUnit4TestCase
 {
 
 	/**
@@ -14,8 +15,6 @@ public class TableExportCsvDlgTestUI
 	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		ApplicationArguments.initialize(new String[] {});
 
 		TableExportCsvDlg dialog = new TableExportCsvDlg();
 		dialog.setSize(500, 500);
