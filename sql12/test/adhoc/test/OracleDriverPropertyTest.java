1b8345ef0de1158b6f13d6b078e252782e1f1da6
Robert Manning <robert.m.manning@gmail.com>
2009-07-23 01:45:38 +0000

    test various combinations of DriverProperties to see Oracle's behavior.

--- sql12/test/adhoc/test/OracleDriverPropertyTest.java
+++ sql12/test/adhoc/test/OracleDriverPropertyTest.java
@@ -31,11 +31,30 @@ public class OracleDriverPropertyTest
 	public static void main(String[] args) throws Exception
 	{
 		Driver driver = (Driver) (Class.forName("oracle.jdbc.driver.OracleDriver").newInstance());
-		DriverPropertyInfo[] infos =
-			driver.getPropertyInfo("jdbc:oracle:thin:@localhost:1521:orcl", new Properties());
+		
+		String nullUrl = null;
+		String emptyUrl = "";
+		String oracleUrl = "jdbc:oracle:thin:@localhost:1521:orcl";
+		
+		Properties nullProps = null;
+		Properties emptyProps = new Properties();
+		
+		checkForUrl(driver, nullUrl, nullProps);
+		checkForUrl(driver, emptyUrl, nullProps);
+		checkForUrl(driver, oracleUrl, nullProps);
+		
+		checkForUrl(driver, nullUrl, emptyProps);
+		checkForUrl(driver, emptyUrl, emptyProps);
+		checkForUrl(driver, oracleUrl, emptyProps);
+		
+	}
+	
+	private static void checkForUrl(Driver driver, String url, Properties props) throws Exception {
+		System.out.println("Checking driver for properties for URL: "+url+" with props="+props);
+		DriverPropertyInfo[] infos = driver.getPropertyInfo(url, props);
 		for (DriverPropertyInfo info : infos) {
 			System.out.println("info.name="+info.name);
-		}
+		}		
 	}
 
 }
