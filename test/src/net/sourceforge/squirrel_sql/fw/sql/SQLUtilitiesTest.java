2e10c91fabc3c593a4908f45847ba9945461111b
Robert Manning <robert.m.manning@gmail.com>
2009-07-25 01:16:35 +0000

    Removed dependency on mock stub.

--- test/src/net/sourceforge/squirrel_sql/fw/sql/SQLUtilitiesTest.java
+++ test/src/net/sourceforge/squirrel_sql/fw/sql/SQLUtilitiesTest.java
@@ -1,8 +1,10 @@
 package net.sourceforge.squirrel_sql.fw.sql;
 
+import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,72 +15,113 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SQLUtilitiesTest extends BaseSQuirreLJUnit4TestCase {
+public class SQLUtilitiesTest extends BaseSQuirreLJUnit4TestCase
+{
 
-    @Before
-    public void setUp() throws Exception {
+	private ITableInfo mockTable1 = mockHelper.createMock("mockTable1", ITableInfo.class);
 
-    }
+	private ITableInfo mockTable2 = mockHelper.createMock("mockTable2", ITableInfo.class);
 
-    @After
-    public void tearDown() throws Exception {
-        
-    }
+	private ITableInfo mockTable3 = mockHelper.createMock("mockTable3", ITableInfo.class);
 
-    @Test
-    public void testGetInsertionOrder() {
-        
-        ArrayList<ITableInfo> tables = new ArrayList<ITableInfo>();
-        ITableInfo ti = 
-            new MockTableInfo("mockTable","mockSchema","mockCatalog");
-        ITableInfo ti2 = 
-            new MockTableInfo("mockTable2","mockSchema2","mockCatalog2");
-        ITableInfo ti3 = 
-            new MockTableInfo("mockTable3","mockSchema3","mockCatalog3");
-        
-        tables.add(ti);
-        tables.add(ti2);
-        tables.add(ti3);
-        
-        SQLDatabaseMetaData md = new MockSQLDatabaseMetaData();
-        
-        try {
-            List<ITableInfo> result = 
-                SQLUtilities.getInsertionOrder(tables, md, new MyCallback());
-            Assert.assertEquals(tables.size(), result.size());
-        } catch (Exception e) {
-            fail("Unexpected exception: "+e.getMessage());
-        }
-    }
+	private SQLDatabaseMetaData mockSQLDatabaseMetaData =
+		mockHelper.createMock("mockSQLDatabaseMetaData", SQLDatabaseMetaData.class);
 
-    @Test 
-    public void testNullQuoteIdentifier() {
-        Assert.assertNull(SQLUtilities.quoteIdentifier(null));
-    }
-    
-    @Test
-    public void testQuoteIdentifierWithEmbeddedQuotes() {
-        String tableNameWithAnEmbeddedQuote = "foo\"bar";
-        String newTableName = 
-            SQLUtilities.quoteIdentifier(tableNameWithAnEmbeddedQuote);
-        assertEquals("foo\"\"bar", newTableName);
-        
-        tableNameWithAnEmbeddedQuote = "\"foo\"bar\"";
-        newTableName = 
-            SQLUtilities.quoteIdentifier(tableNameWithAnEmbeddedQuote);
-        assertEquals("\"foo\"\"bar\"", newTableName);
-        
-        String tableName = "MyTable";
-        String quotedTableName = SQLUtilities.quoteIdentifier(tableName);
-        assertEquals(tableName, quotedTableName);
-    }
-    
-    
-    private static class MyCallback implements ProgressCallBack {
+	@Before
+	public void setUp() throws Exception
+	{
 
-        public void currentlyLoading(String simpleName) {
-            // Do Nothing
-        }
-        
-    }
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testGetInsertionOrder() throws SQLException
+	{
+
+		ArrayList<ITableInfo> tables = new ArrayList<ITableInfo>();
+
+		expect(mockTable1.getSimpleName()).andStubReturn("mockTable1");
+		expect(mockTable1.getImportedKeys()).andStubReturn(null);
+		expect(mockTable1.getExportedKeys()).andStubReturn(null);
+		
+		expect(mockTable2.getSimpleName()).andStubReturn("mockTable2");
+		expect(mockTable2.getImportedKeys()).andStubReturn(null);
+		expect(mockTable2.getExportedKeys()).andStubReturn(null);
+
+		
+		expect(mockTable3.getSimpleName()).andStubReturn("mockTable3");
+		expect(mockTable3.getImportedKeys()).andStubReturn(null);
+		expect(mockTable3.getExportedKeys()).andStubReturn(null);
+		
+		tables.add(mockTable1);
+		tables.add(mockTable2);
+		tables.add(mockTable3);
+
+		expect(mockSQLDatabaseMetaData.getImportedKeysInfo(mockTable1)).andStubReturn(null);
+		mockTable1.setImportedKeys(null);
+		expect(mockSQLDatabaseMetaData.getExportedKeysInfo(mockTable1)).andStubReturn(null);
+		mockTable1.setExportedKeys(null);
+		
+		expect(mockSQLDatabaseMetaData.getImportedKeysInfo(mockTable2)).andStubReturn(null);
+		mockTable2.setImportedKeys(null);
+		expect(mockSQLDatabaseMetaData.getExportedKeysInfo(mockTable2)).andStubReturn(null);
+		mockTable2.setExportedKeys(null);
+
+		expect(mockSQLDatabaseMetaData.getImportedKeysInfo(mockTable3)).andStubReturn(null);
+		mockTable3.setImportedKeys(null);
+		expect(mockSQLDatabaseMetaData.getExportedKeysInfo(mockTable3)).andStubReturn(null);
+		mockTable3.setExportedKeys(null);
+		
+		mockHelper.replayAll();
+
+		try
+		{
+			List<ITableInfo> result =
+				SQLUtilities.getInsertionOrder(tables, mockSQLDatabaseMetaData, new MyCallback());
+			Assert.assertEquals(tables.size(), result.size());
+		}
+		catch (Exception e)
+		{
+			fail("Unexpected exception: " + e.getMessage());
+		}
+
+		mockHelper.verifyAll();
+	}
+
+	@Test
+	public void testNullQuoteIdentifier()
+	{
+		Assert.assertNull(SQLUtilities.quoteIdentifier(null));
+	}
+
+	@Test
+	public void testQuoteIdentifierWithEmbeddedQuotes()
+	{
+		String tableNameWithAnEmbeddedQuote = "foo\"bar";
+		String newTableName = SQLUtilities.quoteIdentifier(tableNameWithAnEmbeddedQuote);
+		assertEquals("foo\"\"bar", newTableName);
+
+		tableNameWithAnEmbeddedQuote = "\"foo\"bar\"";
+		newTableName = SQLUtilities.quoteIdentifier(tableNameWithAnEmbeddedQuote);
+		assertEquals("\"foo\"\"bar\"", newTableName);
+
+		String tableName = "MyTable";
+		String quotedTableName = SQLUtilities.quoteIdentifier(tableName);
+		assertEquals(tableName, quotedTableName);
+	}
+
+	private static class MyCallback implements ProgressCallBack
+	{
+
+		public void currentlyLoading(String simpleName)
+		{
+			// Do Nothing
+		}
+
+	}
 }
