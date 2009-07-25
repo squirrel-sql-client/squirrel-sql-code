ee9a12524396255af84e0985f6e3d8a5a762c5b1
Robert Manning <robert.m.manning@gmail.com>
2009-07-25 14:17:29 +0000

    Replaced system out with assertion.

--- sql12/test/src/net/sourceforge/squirrel_sql/fw/sql/QueryTokenizerTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/fw/sql/QueryTokenizerTest.java
@@ -1,58 +1,56 @@
-
 package net.sourceforge.squirrel_sql.fw.sql;
 
 import junit.framework.TestCase;
 
-public class QueryTokenizerTest extends TestCase implements GenericSQL {
+public class QueryTokenizerTest extends TestCase implements GenericSQL
+{
 
-    static String nullSQL = null;       
-    static String tmpFilename = null;
-    static boolean removeMultilineComment = true;
-    QueryTokenizer qt = null;
-    
-    
-    public void testHasQueryOne() {
-        qt = new QueryTokenizer(";", "--", removeMultilineComment);
-        qt.setScriptToTokenize(CREATE_STUDENT);
-        SQLUtil.checkQueryTokenizer(qt, 1);
-        
-        qt = new QueryTokenizer(";", "--", removeMultilineComment);
-        qt.setScriptToTokenize(CREATE_COURSES);
-        SQLUtil.checkQueryTokenizer(qt, 1);    
-        
-        qt = new QueryTokenizer(";", "--", removeMultilineComment);
-        qt.setScriptToTokenize(STUDENTS_NOT_TAKING_CS112);
-        SQLUtil.checkQueryTokenizer(qt, 1);            
-    }
+	static String nullSQL = null;
 
-    public void testEmbeddedComments() {
-        qt = new QueryTokenizer(";", "--", false);
-        qt.setScriptToTokenize(ANSI_SQL_92_PROCEDURE);
-        StringBuffer sql = new StringBuffer();
-        while (qt.hasQuery()) {
-            sql.append(qt.nextQuery());
-            sql.append(";");
-        }
-/*        System.out.println("length="+sql.length());
-        System.out.println("sql="+sql);
-        System.out.println("comment at "+sql.indexOf("/* remove"));
-        System.out.println("comment at "+sql.indexOf("/* add"));
-*/
-        
-        int firstCommentIndex = sql.indexOf("/* remove");
-        assertTrue("first comment not found", firstCommentIndex != -1);
-        int secondCommentIndex =  sql.indexOf("/* add");
-        assertTrue("second comment not found", secondCommentIndex != -1);
-        
-        
-        
-    }
-    
-    public void testHasQueryAll() {
-        qt = new QueryTokenizer(";", "--", removeMultilineComment);
-        qt.setScriptToTokenize(SQLUtil.getGenericSQLScript());
-        SQLUtil.checkQueryTokenizer(qt, SQLUtil.getGenericSQLCount()); 
-    }
-        
-            
+	static String tmpFilename = null;
+
+	static boolean removeMultilineComment = true;
+
+	QueryTokenizer qt = null;
+
+	public void testHasQueryOne()
+	{
+		qt = new QueryTokenizer(";", "--", removeMultilineComment);
+		qt.setScriptToTokenize(CREATE_STUDENT);
+		SQLUtil.checkQueryTokenizer(qt, 1);
+
+		qt = new QueryTokenizer(";", "--", removeMultilineComment);
+		qt.setScriptToTokenize(CREATE_COURSES);
+		SQLUtil.checkQueryTokenizer(qt, 1);
+
+		qt = new QueryTokenizer(";", "--", removeMultilineComment);
+		qt.setScriptToTokenize(STUDENTS_NOT_TAKING_CS112);
+		SQLUtil.checkQueryTokenizer(qt, 1);
+	}
+
+	public void testEmbeddedComments()
+	{
+		qt = new QueryTokenizer(";", "--", false);
+		qt.setScriptToTokenize(ANSI_SQL_92_PROCEDURE);
+		StringBuffer sql = new StringBuffer();
+		while (qt.hasQuery())
+		{
+			sql.append(qt.nextQuery());
+			sql.append(";");
+		}
+
+		int firstCommentIndex = sql.indexOf("/* remove");
+		assertTrue("first comment not found", firstCommentIndex != -1);
+		int secondCommentIndex = sql.indexOf("/* add");
+		assertTrue("second comment not found", secondCommentIndex != -1);
+
+	}
+
+	public void testHasQueryAll()
+	{
+		qt = new QueryTokenizer(";", "--", removeMultilineComment);
+		qt.setScriptToTokenize(SQLUtil.getGenericSQLScript());
+		SQLUtil.checkQueryTokenizer(qt, SQLUtil.getGenericSQLCount());
+	}
+
 }
--- sql12/test/src/net/sourceforge/squirrel_sql/fw/sql/SQLUtil.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/fw/sql/SQLUtil.java
@@ -1,4 +1,5 @@
 package net.sourceforge.squirrel_sql.fw.sql;
+
 /*
  * Copyright (C) 2007 Rob Manning
  * manningr@users.sourceforge.net
@@ -17,92 +18,100 @@ package net.sourceforge.squirrel_sql.fw.sql;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-import junit.framework.Assert;
-
 /**
  * A helper class for testing components that manipulate SQL.
  * 
  * @author mannignr
  */
-public class SQLUtil {
-    
-    private static int genericSQLCount = 0;
-    
-    public static String getGenericSQLScript() {
-        StringBuffer result = new StringBuffer();
-        result.append(GenericSQL.CREATE_STUDENT);
-        result.append("\n\n");
-        result.append(GenericSQL.CREATE_COURSES);
-        result.append("\n\n");
-        result.append(GenericSQL.CREATE_PROFESSOR);
-        result.append("\n\n");
-        result.append(GenericSQL.CREATE_TAKE);
-        result.append("\n\n");
-        result.append(GenericSQL.CREATE_TEACH);
-        result.append("\n\n");
-        result.append(GenericSQL.STUDENTS_NOT_TAKING_CS112);
-        result.append("\n\n");
-        // Don't forget to set this to the number of statements in result
-        genericSQLCount = 6;
-        return result.toString();
-    }
+public class SQLUtil
+{
 
-    public static void checkQueryTokenizer(IQueryTokenizer qt, 
-                                           int stmtCount) 
-    {
-        int count = 0;
-        while (qt.hasQuery()) {
-            count++;
-            System.out.println(" query: "+qt.nextQuery());
-        }
-        Assert.assertEquals(stmtCount, count);                
-    }    
-    
-    /**
-     * @param genericSQLCount the genericSQLCount to set
-     */
-    public static void setGenericSQLCount(int genericSQLCount) {
-        SQLUtil.genericSQLCount = genericSQLCount;
-    }
+	private static int genericSQLCount = 0;
 
-    /**
-     * @return the genericSQLCount
-     */
-    public static int getGenericSQLCount() {
-        return genericSQLCount;
-    }
-    
-    /**
-     * Creates a temporary file with the specified SQL statements in it.
-     * @param sqls
-     * @param deleteOnExit
-     * @return
-     * @throws IOException
-     */
-    public static String createSQLFile(List<String> sqls, 
-                                       boolean deleteOnExit) 
-        throws IOException 
-    {
-        File f = File.createTempFile("test", ".sql");
-        if (deleteOnExit) {
-            f.deleteOnExit();
-        }
-        PrintWriter out = new PrintWriter(new FileWriter(f));
-        for (String sql : sqls) {
-            out.println(sql);
-            out.println();            
-        }
-        out.close();
-        String tmpFilename = f.getAbsolutePath();
-        System.out.println("tmpFilename="+tmpFilename);
-                
-        return tmpFilename;
-    }
-    
+	public static String getGenericSQLScript()
+	{
+		StringBuffer result = new StringBuffer();
+		result.append(GenericSQL.CREATE_STUDENT);
+		result.append("\n\n");
+		result.append(GenericSQL.CREATE_COURSES);
+		result.append("\n\n");
+		result.append(GenericSQL.CREATE_PROFESSOR);
+		result.append("\n\n");
+		result.append(GenericSQL.CREATE_TAKE);
+		result.append("\n\n");
+		result.append(GenericSQL.CREATE_TEACH);
+		result.append("\n\n");
+		result.append(GenericSQL.STUDENTS_NOT_TAKING_CS112);
+		result.append("\n\n");
+		// Don't forget to set this to the number of statements in result
+		genericSQLCount = 6;
+		return result.toString();
+	}
+
+	public static void checkQueryTokenizer(IQueryTokenizer qt, int stmtCount)
+	{
+		int count = 0;
+		while (qt.hasQuery())
+		{
+			count++;
+			String query = qt.nextQuery();
+			assertNotNull(query);
+		}
+		assertEquals(stmtCount, count);
+	}
+
+	/**
+	 * @param genericSQLCount
+	 *           the genericSQLCount to set
+	 */
+	public static void setGenericSQLCount(int genericSQLCount)
+	{
+		SQLUtil.genericSQLCount = genericSQLCount;
+	}
+
+	/**
+	 * @return the genericSQLCount
+	 */
+	public static int getGenericSQLCount()
+	{
+		return genericSQLCount;
+	}
+
+	/**
+	 * Creates a temporary file with the specified SQL statements in it.
+	 * 
+	 * @param sqls
+	 * @param deleteOnExit
+	 * @return
+	 * @throws IOException
+	 */
+	public static String createSQLFile(List<String> sqls, boolean deleteOnExit) throws IOException
+	{
+		File f = File.createTempFile("test", ".sql");
+		if (deleteOnExit)
+		{
+			f.deleteOnExit();
+		}
+		PrintWriter out = new PrintWriter(new FileWriter(f));
+		for (String sql : sqls)
+		{
+			out.println(sql);
+			out.println();
+		}
+		out.close();
+		String tmpFilename = f.getAbsolutePath();
+		System.out.println("tmpFilename=" + tmpFilename);
+
+		return tmpFilename;
+	}
+
 }
