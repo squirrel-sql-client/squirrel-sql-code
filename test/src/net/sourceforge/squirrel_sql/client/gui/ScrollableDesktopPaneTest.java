e4d2bd179c8a4005839908c98afa06bfc6b9d11a
Robert Manning <robert.m.manning@gmail.com>
2009-07-31 01:05:05 +0000

    Removed trivial dependency on MockApplication stub class.

--- test/src/net/sourceforge/squirrel_sql/client/gui/ScrollableDesktopPaneTest.java
+++ test/src/net/sourceforge/squirrel_sql/client/gui/ScrollableDesktopPaneTest.java
@@ -20,35 +20,42 @@ package net.sourceforge.squirrel_sql.client.gui;
 
 import java.awt.Component;
 
+import net.sourceforge.squirrel_sql.BaseSQuirreLJUnit4TestCase;
+import net.sourceforge.squirrel_sql.client.IApplication;
+import net.sourceforge.squirrel_sql.client.gui.desktopcontainer.DialogWidget;
+import net.sourceforge.squirrel_sql.client.gui.desktopcontainer.ScrollableDesktopPane;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import net.sourceforge.squirrel_sql.BaseSQuirreLJUnit4TestCase;
-import net.sourceforge.squirrel_sql.client.gui.desktopcontainer.ScrollableDesktopPane;
-import net.sourceforge.squirrel_sql.client.gui.desktopcontainer.DialogWidget;
-import net.sourceforge.squirrel_sql.client.MockApplication;
 
 public class ScrollableDesktopPaneTest extends BaseSQuirreLJUnit4TestCase
 {
 
 	ScrollableDesktopPane classUnderTest = null;
 	
+	// Mocks
+	IApplication mockApplication = mockHelper.createMock("mockApplication", IApplication.class);
+	
 	@Before
 	public void setUp() throws Exception
 	{
-		classUnderTest = new ScrollableDesktopPane(new MockApplication());
+		classUnderTest = new ScrollableDesktopPane(mockApplication);
 	}
 
 	@Test
 	public void testRemoveComponent_nullarg()
 	{
+		mockHelper.replayAll();
 		classUnderTest.remove((Component)null);
+		mockHelper.verifyAll();
 	}
 
 	@Test
 	public void testAddImplComponentObjectInt_nullargs()
 	{
+		mockHelper.replayAll();
 		classUnderTest.addWidget((DialogWidget)null);
+		mockHelper.verifyAll();
 	}
 
 }
