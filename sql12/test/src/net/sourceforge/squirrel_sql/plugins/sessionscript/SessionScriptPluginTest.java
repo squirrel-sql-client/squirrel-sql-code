5104494602a2e4945a5b5534b5657fc247f06de1
Robert Manning <robert.m.manning@gmail.com>
2009-08-06 23:41:51 +0000

    Fixed test to test the right plugin.

--- sql12/test/src/net/sourceforge/squirrel_sql/plugins/sessionscript/SessionScriptPluginTest.java
+++ sql12/test/src/net/sourceforge/squirrel_sql/plugins/sessionscript/SessionScriptPluginTest.java
@@ -20,18 +20,17 @@ package net.sourceforge.squirrel_sql.plugins.sessionscript;
 
 import net.sourceforge.squirrel_sql.client.plugin.AbstractPluginTest;
 import net.sourceforge.squirrel_sql.client.plugin.DatabaseProductVersionData;
-import net.sourceforge.squirrel_sql.plugins.sqlscript.SQLScriptPlugin;
 
 import org.junit.After;
 import org.junit.Before;
 
 
-public class SQLScriptPluginTest extends AbstractPluginTest implements DatabaseProductVersionData
+public class SessionScriptPluginTest extends AbstractPluginTest implements DatabaseProductVersionData
 {	
 	@Before
 	public void setUp() throws Exception
 	{
-		classUnderTest = new SQLScriptPlugin();
+		classUnderTest = new SessionScriptPlugin();
 	}
 
 	@After
