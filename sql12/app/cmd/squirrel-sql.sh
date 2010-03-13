adc0a91ac72cdaf3c9c4f16208c7638bdc510ba3
Robert Manning <robert.m.manning@gmail.com>
2010-03-13 15:41:50 +0000

    2965617 (Fail to open session with RSyntax editor): rebuild the classpath from lib/ folder after updater runs to add any new jars to the CLASSPATH variable.

--- sql12/app/cmd/squirrel-sql.sh
+++ sql12/app/cmd/squirrel-sql.sh
@@ -1,5 +1,25 @@
 #! /bin/sh
 
+# This function sets a global variable named "CP" to a command-path separated list of jars located beneath the 
+# specified folder.  If the specified folder contains a lib directory, then jars beneath the lib folder are 
+# @ added as well as the squirrel-sql.jar file located in the directory specified as the first argument to 
+# this function. 
+buildCPFromDir()
+{
+	if [ -d "$1"/lib ]; then
+		# First entry in classpath is the Squirrel application.
+		CP="$1/squirrel-sql.jar"
+
+		# Then add all library jars to the classpath.
+		for a in "$1"/lib/*; do
+			CP="$CP":"$a"
+		done
+	else 
+		for a in "$1"/*; do
+			CP="$CP":"$a"
+		done
+	fi
+} 
 
 # IZPACK_JAVA_HOME is filtered in by the IzPack installer when this script is installed
 IZPACK_JAVA_HOME=%JAVA_HOME
@@ -56,20 +76,13 @@ if [ "$?" = "1" ]; then
 	exit
 fi
 
-# First entry in classpath is the Squirrel application.
-TMP_CP="$UNIX_STYLE_HOME/squirrel-sql.jar"
-
-# Then add all library jars to the classpath.
-for a in "$UNIX_STYLE_HOME"/lib/*; do
-	TMP_CP="$TMP_CP":"$a"
-done
+# Build a command-path separated list of installed jars from the lib folder and squirrel-sql.jar
+buildCPFromDir "$UNIX_STYLE_HOME"
+TMP_CP=$CP
 
 # Set the update app's classpath to use jars in download area first, then the installed jars
-UPDATE_CP=$TMP_CP
-for a in "$UNIX_STYLE_HOME"/update/downloads/core/*; do
-	UPDATE_CP="$a":"$UPDATE_CP"
-done
-
+buildCPFromDir "$UNIX_STYLE_HOME/update/downloads/core"
+UPDATE_CP=$CP:$TMP_CP
 
 # Now add the system classpath to the classpath. If running
 # Cygwin we also need to change the classpath to Windows format.
@@ -102,5 +115,9 @@ else
 	SCRIPT_ARGS="$1 $2 $3 $4 $5 $6 $7 $8 $9"
 fi
 
+# Now, pickup all jars once again from the installation and lib directories in case any new jars were
+# downloaded during the update process.  The variable "CP" is assigned this value.
+buildCPFromDir "$UNIX_STYLE_HOME"
+
 # Launch SQuirreL application
-$JAVACMD -Xmx256m -cp "$TMP_CP" $MACOSX_SQUIRREL_PROPS net.sourceforge.squirrel_sql.client.Main --log-config-file "$UNIX_STYLE_HOME"/log4j.properties --squirrel-home "$UNIX_STYLE_HOME" $NATIVE_LAF_PROP $SCRIPT_ARGS
+$JAVACMD -Xmx256m -cp "$CP" $MACOSX_SQUIRREL_PROPS net.sourceforge.squirrel_sql.client.Main --log-config-file "$UNIX_STYLE_HOME"/log4j.properties --squirrel-home "$UNIX_STYLE_HOME" $NATIVE_LAF_PROP $SCRIPT_ARGS
