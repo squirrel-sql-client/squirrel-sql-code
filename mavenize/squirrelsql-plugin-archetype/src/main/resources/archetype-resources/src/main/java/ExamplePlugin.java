d1d218e77982bb845863cd9a7e36ed9166abaf14
Robert Manning <robert.m.manning@gmail.com>
2010-04-17 15:02:39 +0000

    Maven archetype project for the Example plugin.  Plugin writers can use this archetype to create a new maven project to start writing their plugin as follows: mvn org.apache.maven.plugins:maven-archetype-plugin:2.0-alpha-4:generate -DarchetypeGroupId=net.sf.squirrel-sql -DarchetypeArtifactId=squirrelsql-plugin-archetype -DarchetypeVersion=3.1.0-SNAPSHOT -DgroupId=com.myco -DartifactId=CoolPlugin -Dpackage=net.sourceforge.squirrel_sql.plugins.example

---
+++ mavenize/squirrelsql-plugin-archetype/src/main/resources/archetype-resources/src/main/java/ExamplePlugin.java
@@ -0,0 +1,160 @@
+package net.sourceforge.squirrel_sql.plugins.example;
+
+import net.sourceforge.squirrel_sql.client.plugin.DefaultSessionPlugin;
+import net.sourceforge.squirrel_sql.client.plugin.PluginException;
+import net.sourceforge.squirrel_sql.client.plugin.PluginResources;
+import net.sourceforge.squirrel_sql.client.plugin.PluginSessionCallback;
+import net.sourceforge.squirrel_sql.client.plugin.PluginSessionCallbackAdaptor;
+import net.sourceforge.squirrel_sql.client.preferences.IGlobalPreferencesPanel;
+import net.sourceforge.squirrel_sql.client.session.IObjectTreeAPI;
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectType;
+
+/**
+ * The Example plugin class.
+ */
+public class ExamplePlugin extends DefaultSessionPlugin
+{
+	private PluginResources _resources;
+
+	/**
+	 * Return the internal name of this plugin.
+	 *
+	 * @return  the internal name of this plugin.
+	 */
+	public String getInternalName()
+	{
+		return "example";
+	}
+
+	/**
+	 * Return the descriptive name of this plugin.
+	 *
+	 * @return  the descriptive name of this plugin.
+	 */
+	public String getDescriptiveName()
+	{
+		return "Example Plugin";
+	}
+
+	/**
+	 * Returns the current version of this plugin.
+	 *
+	 * @return  the current version of this plugin.
+	 */
+	public String getVersion()
+	{
+		return "0.01";
+	}
+
+	/**
+	 * Returns the authors name.
+	 *
+	 * @return  the authors name.
+	 */
+	public String getAuthor()
+	{
+		return "Gerd Wagner";
+	}
+
+	/**
+	 * Returns the name of the change log for the plugin. This should
+	 * be a text or HTML file residing in the <TT>getPluginAppSettingsFolder</TT>
+	 * directory.
+	 *
+	 * @return	the changelog file name or <TT>null</TT> if plugin doesn't have
+	 * 			a change log.
+	 */
+	public String getChangeLogFileName()
+	{
+		return "changes.txt";
+	}
+
+	/**
+	 * Returns the name of the Help file for the plugin. This should
+	 * be a text or HTML file residing in the <TT>getPluginAppSettingsFolder</TT>
+	 * directory.
+	 *
+	 * @return	the Help file name or <TT>null</TT> if plugin doesn't have
+	 * 			a help file.
+	 */
+	public String getHelpFileName()
+	{
+		return "readme.txt";
+	}
+
+	/**
+	 * Returns the name of the Licence file for the plugin. This should
+	 * be a text or HTML file residing in the <TT>getPluginAppSettingsFolder</TT>
+	 * directory.
+	 *
+	 * @return	the Licence file name or <TT>null</TT> if plugin doesn't have
+	 * 			a licence file.
+	 */
+	public String getLicenceFileName()
+	{
+		return "licence.txt";
+	}
+
+	/**
+	 * @return	Comma separated list of contributors.
+	 */
+	public String getContributors()
+	{
+		return "";
+	}
+
+	/**
+	 * Create preferences panel for the Global Preferences dialog.
+	 *
+	 * @return  Preferences panel.
+	 */
+	public IGlobalPreferencesPanel[] getGlobalPreferencePanels()
+	{
+		return new IGlobalPreferencesPanel[0];
+	}
+
+	/**
+	 * Initialize this plugin.
+	 */
+	public synchronized void initialize() throws PluginException
+	{
+		_resources = new PluginResources("net.sourceforge.squirrel_sql.plugins.example.example", this);
+	}
+
+
+	/**
+	 * Called when a session started. Add commands to popup menu
+	 * in object tree.
+	 *
+	 * @param   session	 The session that is starting.
+	 *
+	 * @return An implementation of PluginSessionCallback or null to indicate
+    * the plugin does not work with this session
+	 */
+	public PluginSessionCallback sessionStarted(ISession session)
+	{
+		try
+		{
+         String driverName = session.getSQLConnection().getConnection().getMetaData().getDriverName();
+         if(false == driverName.toUpperCase().startsWith("IBM DB2 JDBC"))
+         {
+            // Plugin knows only how to script Views and Stored Procedures on DB2.
+            // So if it's not a DB2 Session we tell SQuirreL the Plugin should not be used.
+            return null;
+         }
+
+         // Add context menu items to the object tree's view and procedure nodes.
+         IObjectTreeAPI otApi = session.getSessionInternalFrame().getObjectTreeAPI();
+         otApi.addToPopup(DatabaseObjectType.VIEW, new ScriptDB2ViewAction(getApplication(), _resources, session));
+         otApi.addToPopup(DatabaseObjectType.PROCEDURE, new ScriptDB2ProcedureAction(getApplication(), _resources, session));
+
+         return new PluginSessionCallbackAdaptor(this);
+		}
+		catch(Exception e)
+		{
+         throw new RuntimeException(e);
+		}
+	}
+
+}
---
+++ mavenize/squirrelsql-plugin-archetype/src/main/resources/archetype-resources/src/main/java/ScriptDB2ViewAction.java
@@ -0,0 +1,85 @@
+package net.sourceforge.squirrel_sql.plugins.example;
+
+import net.sourceforge.squirrel_sql.client.IApplication;
+import net.sourceforge.squirrel_sql.client.action.SquirrelAction;
+import net.sourceforge.squirrel_sql.client.gui.session.SessionInternalFrame;
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
+import net.sourceforge.squirrel_sql.fw.sql.ITableInfo;
+import net.sourceforge.squirrel_sql.fw.util.Resources;
+
+import java.awt.event.ActionEvent;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ScriptDB2ViewAction extends SquirrelAction
+{
+   private static final long serialVersionUID = 1L;
+
+   private ISession _session;
+
+
+   public ScriptDB2ViewAction(IApplication app, Resources rsrc, ISession session)
+	{
+		super(app, rsrc);
+      _session = session;
+   }
+
+
+	public void actionPerformed(ActionEvent evt)
+	{
+      try
+      {
+         Statement stat = _session.getSQLConnection().createStatement();
+
+         SessionInternalFrame sessMainFrm = _session.getSessionInternalFrame();
+         IDatabaseObjectInfo[] dbObjs = sessMainFrm.getObjectTreeAPI().getSelectedDatabaseObjects();
+
+
+         StringBuffer script = new StringBuffer();
+         for (int i = 0; i < dbObjs.length; i++)
+         {
+            ITableInfo ti = (ITableInfo) dbObjs[i];
+
+            ///////////////////////////////////////////////////////////
+            // IBM DB 2 specific code to read view definitions.
+            String sql =
+               "SELECT TEXT " +
+               "FROM SYSIBM.SYSVIEWS " +
+               "WHERE NAME = '" + ti.getSimpleName() + "'";
+
+            ResultSet res = stat.executeQuery(sql);
+            res.next();
+
+            script.append(res.getString("TEXT"));
+            script.append(getStatementSeparator());
+            res.close();
+            //
+            ///////////////////////////////////////////////////////////
+         }
+
+         stat.close();
+
+         sessMainFrm.getSQLPanelAPI().appendSQLScript(script.toString());
+         sessMainFrm.getSessionPanel().selectMainTab(ISession.IMainPanelTabIndexes.SQL_TAB);
+      }
+      catch (Exception e)
+      {
+         throw new RuntimeException(e);
+      }
+   }
+
+   private String getStatementSeparator()
+   {
+      String statementSeparator = _session.getQueryTokenizer().getSQLStatementSeparator();
+
+      if (1 < statementSeparator.length())
+      {
+         statementSeparator = "\n" + statementSeparator + "\n";
+      }
+
+      return statementSeparator;
+   }
+
+
+}
