bd3cdbd09613534cb69aa4eadaca20bec582a095
Robert Manning <robert.m.manning@gmail.com>
2010-08-29 21:04:53 +0000

    Added test coverage.

--- sql12/app/src/test/java/net/sourceforge/squirrel_sql/client/ApplicationArgumentsTest.java
+++ sql12/app/src/test/java/net/sourceforge/squirrel_sql/client/ApplicationArgumentsTest.java
@@ -19,39 +19,172 @@
 package net.sourceforge.squirrel_sql.client;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ApplicationArgumentsTest {
+public class ApplicationArgumentsTest
+{
 
-    ApplicationArguments applicationArgumentsUnderTest = null;
-    
-    String[] _rawArgs = new String[] { "1", "2", "3", "4" };
-    
-    @Before
-    public void setUp() throws Exception {
-        ApplicationArguments.reset();
-        ApplicationArguments.initialize(_rawArgs);
-        applicationArgumentsUnderTest = ApplicationArguments.getInstance();
-    }
+	private static final String LOG_CONFIG_FILE_PROP = "--" + ApplicationArguments.IOptions.LOG_FILE[1];
 
-    @After
-    public void tearDown() throws Exception {
-        ApplicationArguments.reset();
-    }
+	private static final String USER_SETTINGS_DIR_PROP =
+		"--" + ApplicationArguments.IOptions.USER_SETTINGS_DIR[1];
 
-    @Test
-    public final void testGetRawArguments() {
-        String[] rawArgs = applicationArgumentsUnderTest.getRawArguments();
-        assertNotNull(rawArgs);
-        assertEquals(4, rawArgs.length);
-        assertEquals("1", rawArgs[0]);
-        assertEquals("2", rawArgs[1]);
-        assertEquals("3", rawArgs[2]);
-        assertEquals("4", rawArgs[3]);
-    }
-    
+	private static final String SQUIRREL_HOME_PROP = "--" + ApplicationArguments.IOptions.SQUIRREL_HOME[1];
+
+	private static final String NO_SPLASH_PROP = "--" + ApplicationArguments.IOptions.NO_SPLASH[1];
+
+	private static final String NO_PLUGINS = "--" + ApplicationArguments.IOptions.NO_PLUGINS[1];
+
+	private static final String SHOW_HELP = "--" + ApplicationArguments.IOptions.HELP[1];
+
+	private static final String USE_DEFAULT_METAL_THEME =
+		"--" + ApplicationArguments.IOptions.USE_DEFAULT_METAL_THEME[1];
+
+	private static final String USE_NATIVE_LAF = "--" + ApplicationArguments.IOptions.USE_NATIVE_LAF[1];
+	
+	private static final String SQUIRREL_USERHOME = "/home/squirreluser";
+
+	private static final String SQUIRREL_HOME = "/tmp";
+
+	private static final String LOG_CONFIG_FILE = SQUIRREL_HOME + "log4j.properties";
+
+	private static final String PLUGIN_LIST_PROP = "--plugin-classpath-list";
+
+	private static final String PLUGIN_LIST = "net.sourceforge.squirrel_sql.plugins.graph.GraphPlugin";
+
+	private static final String[] arguments =
+		new String[] { SQUIRREL_HOME_PROP, SQUIRREL_HOME, USER_SETTINGS_DIR_PROP, SQUIRREL_USERHOME,
+				LOG_CONFIG_FILE_PROP, LOG_CONFIG_FILE, PLUGIN_LIST_PROP, PLUGIN_LIST, NO_SPLASH_PROP, NO_PLUGINS };
+
+	@Before
+	public void setUp() throws Exception
+	{
+		assertTrue(ApplicationArguments.initialize(arguments));
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		ApplicationArguments.reset();
+	}
+
+	@Test
+	public final void testGetRawArguments() throws Exception
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		String[] rawArgs = applicationArgumentsUnderTest.getRawArguments();
+
+		assertNotNull(rawArgs);
+		assertEquals(arguments.length, rawArgs.length);
+		assertEquals(SQUIRREL_HOME_PROP, rawArgs[0]);
+		assertEquals(SQUIRREL_HOME, rawArgs[1]);
+		assertEquals(USER_SETTINGS_DIR_PROP, rawArgs[2]);
+		assertEquals(SQUIRREL_USERHOME, rawArgs[3]);
+		assertEquals(LOG_CONFIG_FILE_PROP, rawArgs[4]);
+		assertEquals(LOG_CONFIG_FILE, rawArgs[5]);
+		assertEquals(PLUGIN_LIST_PROP, rawArgs[6]);
+		assertEquals(PLUGIN_LIST, rawArgs[7]);
+	}
+
+	@Test
+	public final void testGetSquirrelHomeDirectory()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertEquals(SQUIRREL_HOME, applicationArgumentsUnderTest.getSquirrelHomeDirectory());
+	}
+
+	@Test
+	public final void testGetUserSettingsDirectoryOverride()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertEquals(SQUIRREL_USERHOME, applicationArgumentsUnderTest.getUserSettingsDirectoryOverride());
+	}
+
+	@Test
+	public final void testGetLoggingConfigFileName()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertEquals(LOG_CONFIG_FILE, applicationArgumentsUnderTest.getLoggingConfigFileName());
+	}
+
+	@Test
+	public final void testGetPluginList()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		List<String> pluginList = applicationArgumentsUnderTest.getPluginList();
+		assertEquals(1, pluginList.size());
+		String plugin = pluginList.get(0);
+		assertEquals(PLUGIN_LIST, plugin);
+	}
+
+	@Test
+	public final void testGetUserInterfaceDebugEnabled()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertFalse(applicationArgumentsUnderTest.getUserInterfaceDebugEnabled());
+	}
+
+	@Test
+	public final void testGetLoadPlugins()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertFalse(applicationArgumentsUnderTest.getLoadPlugins());
+		ApplicationArguments.reset();
+		ApplicationArguments.initialize(new String[] { SQUIRREL_HOME_PROP, SQUIRREL_HOME });
+		applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertTrue(applicationArgumentsUnderTest.getLoadPlugins());
+	}
+
+	@Test
+	public final void testGetShowSplashScreen()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertFalse(applicationArgumentsUnderTest.getShowSplashScreen());
+		ApplicationArguments.reset();
+		ApplicationArguments.initialize(new String[] { SQUIRREL_HOME_PROP, SQUIRREL_HOME });
+		applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertTrue(applicationArgumentsUnderTest.getShowSplashScreen());
+	}
+
+	@Test
+	public final void testGetShowHelp()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertFalse(applicationArgumentsUnderTest.getShowHelp());
+		ApplicationArguments.reset();
+		ApplicationArguments.initialize(new String[] { SHOW_HELP });
+		applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertTrue(applicationArgumentsUnderTest.getShowHelp());
+	}
+
+	@Test
+	public final void testUseDefaultMetalTheme()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertFalse(applicationArgumentsUnderTest.useDefaultMetalTheme());
+		ApplicationArguments.reset();
+		ApplicationArguments.initialize(new String[] { USE_DEFAULT_METAL_THEME });
+		applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertTrue(applicationArgumentsUnderTest.useDefaultMetalTheme());
+	}
+
+	@Test
+	public final void testUseNativeLAF()
+	{
+		ApplicationArguments applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertFalse(applicationArgumentsUnderTest.useNativeLAF());
+		ApplicationArguments.reset();
+		ApplicationArguments.initialize(new String[] { USE_NATIVE_LAF });
+		applicationArgumentsUnderTest = ApplicationArguments.getInstance();
+		assertTrue(applicationArgumentsUnderTest.useNativeLAF());		
+	}
+
 }
