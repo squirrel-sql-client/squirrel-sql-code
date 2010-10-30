cfb30a1935670130898c674b0a231c01d2ca5d27
Robert Manning <robert.m.manning@gmail.com>
2010-10-30 18:04:58 +0000

    Determine forward or backward path slashes not based on OS, but rather on the script name (On Windows, we fix both types of scripts now).

--- sql12/fw/src/main/java/net/sourceforge/squirrel_sql/fw/util/ScriptLineFixer.java
+++ sql12/fw/src/main/java/net/sourceforge/squirrel_sql/fw/util/ScriptLineFixer.java
@@ -20,7 +20,10 @@ package net.sourceforge.squirrel_sql.fw.util;
  */
 
 /**
- * Interface for line fixer implementations.
+ * Interface for line fixer implementations. ScriptLineFixers are called upon to fix lines in scripts that are
+ * shipped in the installer. Since scripts can be modified by the installer at install time and end users
+ * after installation is complete, they cannot simply be overwritten during a software update. Instead, each
+ * line of the script is examined and transformed as required by the current release.
  */
 public interface ScriptLineFixer
 {
@@ -28,9 +31,11 @@ public interface ScriptLineFixer
 	/**
 	 * Fixes the line specified, returning the "fixed" version
 	 * 
+	 * @param scriptFileName
+	 *           the filename of the script (may or may not include the absolute path)
 	 * @param line
 	 *           the line that needs to be fixed
 	 * @return the fixed line
 	 */
-	String fixLine(String line);
+	String fixLine(String scriptFileName, String line);
 }
