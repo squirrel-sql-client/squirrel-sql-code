c6a0608e9c4a99a93c4cb9412a06a286c14149f2
Robert Manning <robert.m.manning@gmail.com>
2010-10-30 18:04:09 +0000

    Determine forward or backward path slashes not based on OS, but rather on the script name (On Windows, we fix both types of scripts now).

--- sql12/app/src/main/java/net/sourceforge/squirrel_sql/client/update/gui/installer/SplashScreenFixer.java
+++ sql12/app/src/main/java/net/sourceforge/squirrel_sql/client/update/gui/installer/SplashScreenFixer.java
@@ -1,8 +1,5 @@
 package net.sourceforge.squirrel_sql.client.update.gui.installer;
 
-import net.sourceforge.squirrel_sql.fw.util.ScriptLineFixer;
-
-
 /*
  * Copyright (C) 2010 Rob Manning
  * manningr@users.sourceforge.net
@@ -22,6 +19,7 @@ import net.sourceforge.squirrel_sql.fw.util.ScriptLineFixer;
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+import net.sourceforge.squirrel_sql.fw.util.ScriptLineFixer;
 
 /**
  * A line fixer implementation that adds the splash screen icon to the line that launches the SQuirreL 
@@ -33,7 +31,7 @@ public class SplashScreenFixer implements ScriptLineFixer {
 	public static final String CLIENT_MAIN_CLASS = "net.sourceforge.squirrel_sql.client.Main";
 	
 	/** The splash setting */
-	public static final String SPLASH_ICON_ARGUMENT = "-splash:icons/splash.jpg";
+	public static final String SPLASH_ICON = "splash.jpg";
 	
 	/** A regex pattern version of the main class */
 	private static final String MAIN_CLASS_PATTERN = "net\\.sourceforge\\.squirrel_sql\\.client\\.Main";
@@ -45,16 +43,24 @@ public class SplashScreenFixer implements ScriptLineFixer {
 	 * @see net.sourceforge.squirrel_sql.fw.util.ScriptLineFixer#fixLine(java.lang.String)
 	 */
 	@Override
-	public String fixLine(String line) {
+	public String fixLine(String scriptFileName, String line) {
 		String result = line;
+		
+		String splashIconArgument = "-splash:" + SPLASH_ICON;
+		if (scriptFileName.toLowerCase().endsWith(".bat")) {
+			splashIconArgument = "-splash:\"%SQUIRREL_SQL_HOME%\\icons\\"+SPLASH_ICON+"\""; 
+		} else {
+			splashIconArgument = "-splash:\"$SQUIRREL_SQL_HOME/icons/"+SPLASH_ICON+"\"";
+		}
+		
 		if (line.contains(CLIENT_MAIN_CLASS)) {
-			if (!line.contains(SPLASH_ICON_ARGUMENT)) {
+			if (!line.contains(SPLASH_ICON)) {
 				String[] parts = line.split(MAIN_CLASS_PATTERN);
 				if (parts.length == 2) {
 					StringBuilder newline = new StringBuilder();
 					newline.append(parts[0]);
 					newline.append(" ");
-					newline.append(SPLASH_ICON_ARGUMENT);
+					newline.append(splashIconArgument);
 					newline.append(" ");
 					newline.append(CLIENT_MAIN_CLASS);
 					newline.append(" ");
