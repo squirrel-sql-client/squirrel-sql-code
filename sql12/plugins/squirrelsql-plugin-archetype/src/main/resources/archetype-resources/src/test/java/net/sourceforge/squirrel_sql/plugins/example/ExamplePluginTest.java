c743531cdeed0e2c90a86381c29215d5d8eec359
Robert Manning <robert.m.manning@gmail.com>
2010-12-26 22:47:52 +0000

    New tests for the example plugin in the archetype.  Synchronizing with the actual Example plugin project.  Once an integration test has been setup for this archetype, it will no longer be necessary to have the Example plugin project.

--- sql12/plugins/squirrelsql-plugin-archetype/src/main/resources/archetype-resources/src/test/java/net/sourceforge/squirrel_sql/plugins/example/ExamplePluginTest.java
+++ sql12/plugins/squirrelsql-plugin-archetype/src/main/resources/archetype-resources/src/test/java/net/sourceforge/squirrel_sql/plugins/example/ExamplePluginTest.java
@@ -1,9 +1,12 @@
 package net.sourceforge.squirrel_sql.plugins.example;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 import net.sourceforge.squirrel_sql.client.plugin.AbstractSessionPluginTest;
 
 import org.junit.Before;
-
+import org.junit.Test;
 
 /*
  * Copyright (C) 2010 Rob Manning
@@ -24,24 +27,73 @@ import org.junit.Before;
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/**
+ * This test extends AbstractSessionPluginTest which provides testing for many of the methods in the
+ * ISessionPlugin interface that are implemented by such a plugin. Run this test through a code coverage tool
+ * such as Emma and you will find that many of the simpler methods in the interface are covered.
+ */
 public class ExamplePluginTest extends AbstractSessionPluginTest
 {
 
+	/**
+	 * getDatabaseProductName returns "DB2" because that is the product name for sessions that the example
+	 * plugin is interested in. This value will be returned by the SQLDatabaseMetaData for the method called
+	 * getDatabaseProductName() that is associated with Session that is used when testing methods that on the
+	 * ISessionPlugin interface that require a Session.
+	 * 
+	 * @see net.sourceforge.squirrel_sql.client.plugin.AbstractSessionPluginTest#getDatabaseProductName()
+	 */
 	@Override
 	protected String getDatabaseProductName()
 	{
-		return "DB2";		
+		return "DB2";
 	}
 
+	/**
+	 * No version is given here, because it is not required to distinguish dialects, since there is only one
+	 * DB2 dialect at the current time.
+	 * 
+	 * @see net.sourceforge.squirrel_sql.client.plugin.AbstractSessionPluginTest#getDatabaseProductVersion()
+	 */
 	@Override
 	protected String getDatabaseProductVersion()
 	{
 		return null;
 	}
-	
+
 	@Before
 	public void setUp() throws Exception
 	{
+		super.setUp();
 		classUnderTest = new ExamplePlugin();
 	}
+
+	@Test
+	public void testSessionStarted() throws Exception
+	{
+		classUnderTest.initialize();
+		((ExamplePlugin) classUnderTest).sessionStarted(mockSession);
+	}
+
+	@Test
+	public void testSessionStartedNullApplication() throws Exception
+	{
+		try
+		{
+			when(mockSession.getApplication()).thenReturn(null);
+			classUnderTest.initialize();
+			((ExamplePlugin) classUnderTest).sessionStarted(mockSession);
+			fail("Exception to get an exception for null IApplication returned from Session.getApplication");
+		}
+		catch (Exception e)
+		{
+			// This is expected.
+		}
+	}
+	
+	@Test
+	public void testGetGlobalPreferencePanels() {
+		assertNotNull(classUnderTest.getGlobalPreferencePanels());
+	}
+
 }
