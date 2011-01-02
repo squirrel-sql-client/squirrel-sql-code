1a3f3da1e6b63d3707a3c0853ef9aa5b4c380631
Robert Manning <robert.m.manning@gmail.com>
2011-01-02 22:37:33 +0000

    Allow PluginResources implementation to be injected.

--- plugins/userscript/src/main/java/net/sourceforge/squirrel_sql/plugins/userscript/UserScriptAction.java
+++ plugins/userscript/src/main/java/net/sourceforge/squirrel_sql/plugins/userscript/UserScriptAction.java
@@ -1,15 +1,15 @@
 package net.sourceforge.squirrel_sql.plugins.userscript;
 
 
+import java.awt.event.ActionEvent;
+
 import net.sourceforge.squirrel_sql.client.IApplication;
 import net.sourceforge.squirrel_sql.client.action.SquirrelAction;
 import net.sourceforge.squirrel_sql.client.session.ISession;
 import net.sourceforge.squirrel_sql.client.session.action.ISessionAction;
-import net.sourceforge.squirrel_sql.fw.util.Resources;
+import net.sourceforge.squirrel_sql.fw.util.IResources;
 import net.sourceforge.squirrel_sql.plugins.userscript.kernel.ScriptListController;
 import net.sourceforge.squirrel_sql.plugins.userscript.kernel.UserScriptAdmin;
-
-import java.awt.event.ActionEvent;
 
 public class UserScriptAction extends SquirrelAction implements ISessionAction
 {
@@ -20,9 +20,9 @@ public class UserScriptAction extends SquirrelAction implements ISessionAction
 	/** Current plugin. */
 	protected final UserScriptPlugin _plugin;
 
-	public UserScriptAction(IApplication app, Resources rsrc, UserScriptPlugin plugin)
+	public UserScriptAction(IApplication app, IResources resources, UserScriptPlugin plugin)
 	{
-		super(app, rsrc);
+		super(app, resources);
 		_plugin = plugin;
 	}
 
