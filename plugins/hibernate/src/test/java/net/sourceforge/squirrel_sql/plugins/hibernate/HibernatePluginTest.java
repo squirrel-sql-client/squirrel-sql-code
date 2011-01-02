5efeb7ad2367beae92724c479665869cead7b70a
Robert Manning <robert.m.manning@gmail.com>
2011-01-02 22:29:00 +0000

    Modifed test to support new plugin tests.

--- plugins/hibernate/src/test/java/net/sourceforge/squirrel_sql/plugins/hibernate/HibernatePluginTest.java
+++ plugins/hibernate/src/test/java/net/sourceforge/squirrel_sql/plugins/hibernate/HibernatePluginTest.java
@@ -18,25 +18,17 @@
  */
 package net.sourceforge.squirrel_sql.plugins.hibernate;
 
-import net.sourceforge.squirrel_sql.client.plugin.AbstractPluginTest;
+import net.sourceforge.squirrel_sql.client.plugin.AbstractSessionPluginTest;
 import net.sourceforge.squirrel_sql.client.plugin.DatabaseProductVersionData;
-
-import org.junit.After;
-import org.junit.Before;
+import net.sourceforge.squirrel_sql.client.plugin.IPlugin;
 
 
-public class HibernatePluginTest extends AbstractPluginTest implements DatabaseProductVersionData
-{	
-	@Before
-	public void setUp() throws Exception
+public class HibernatePluginTest extends AbstractSessionPluginTest implements DatabaseProductVersionData
+{
+
+	@Override
+	protected IPlugin getPluginToTest() throws Exception
 	{
-		classUnderTest = new HibernatePlugin();
-	}
-
-	@After
-	public void tearDown() throws Exception
-	{
-		classUnderTest = null;
-	}		
-
+		return new HibernatePlugin();
+	}	
 }
