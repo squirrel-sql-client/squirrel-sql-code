fd0f10903b67b6c48898436efa1d7fc5e17eff80
Robert Manning <robert.m.manning@gmail.com>
2011-01-02 22:32:47 +0000

    Modifed tests to support new plugin tests.

--- plugins/refactoring/src/test/java/net/sourceforge/squirrel_sql/plugins/refactoring/RefactoringPluginTest.java
+++ plugins/refactoring/src/test/java/net/sourceforge/squirrel_sql/plugins/refactoring/RefactoringPluginTest.java
@@ -18,25 +18,18 @@
  */
 package net.sourceforge.squirrel_sql.plugins.refactoring;
 
-import net.sourceforge.squirrel_sql.client.plugin.AbstractPluginTest;
+import net.sourceforge.squirrel_sql.client.plugin.AbstractSessionPluginTest;
 import net.sourceforge.squirrel_sql.client.plugin.DatabaseProductVersionData;
-
-import org.junit.After;
-import org.junit.Before;
+import net.sourceforge.squirrel_sql.client.plugin.IPlugin;
 
 
-public class RefactoringPluginTest extends AbstractPluginTest implements DatabaseProductVersionData
-{	
-	@Before
-	public void setUp() throws Exception
+public class RefactoringPluginTest extends AbstractSessionPluginTest implements DatabaseProductVersionData
+{
+
+	@Override
+	protected IPlugin getPluginToTest() throws Exception
 	{
-		classUnderTest = new RefactoringPlugin();
-	}
-
-	@After
-	public void tearDown() throws Exception
-	{
-		classUnderTest = null;
-	}		
+		return new RefactoringPlugin();
+	}	
 
 }
