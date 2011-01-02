c5671bcf6c9a2144c77b1bff4af62caed7b636bb
Robert Manning <robert.m.manning@gmail.com>
2011-01-02 22:23:33 +0000

    Modifed tests to support new plugin tests.

--- sql12/plugins/codecompletion/src/test/java/net/sourceforge/squirrel_sql/plugins/codecompletion/CodeCompletionPluginTest.java
+++ sql12/plugins/codecompletion/src/test/java/net/sourceforge/squirrel_sql/plugins/codecompletion/CodeCompletionPluginTest.java
@@ -18,25 +18,18 @@
  */
 package net.sourceforge.squirrel_sql.plugins.codecompletion;
 
-import net.sourceforge.squirrel_sql.client.plugin.AbstractPluginTest;
+import net.sourceforge.squirrel_sql.client.plugin.AbstractSessionPluginTest;
 import net.sourceforge.squirrel_sql.client.plugin.DatabaseProductVersionData;
-
-import org.junit.After;
-import org.junit.Before;
+import net.sourceforge.squirrel_sql.client.plugin.IPlugin;
 
 
-public class CodeCompletionPluginTest extends AbstractPluginTest implements DatabaseProductVersionData
-{	
-	@Before
-	public void setUp() throws Exception
+public class CodeCompletionPluginTest extends AbstractSessionPluginTest implements DatabaseProductVersionData
+{
+
+	@Override
+	protected IPlugin getPluginToTest() throws Exception
 	{
-		classUnderTest = new CodeCompletionPlugin();
-	}
-
-	@After
-	public void tearDown() throws Exception
-	{
-		classUnderTest = null;
-	}		
+		return new CodeCompletionPlugin();
+	}	
 
 }
