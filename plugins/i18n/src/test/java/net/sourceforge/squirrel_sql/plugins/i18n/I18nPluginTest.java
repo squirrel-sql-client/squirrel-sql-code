a59e2cadca4dc3cbbf002914e1cdfdcbd0def9fd
Robert Manning <robert.m.manning@gmail.com>
2011-01-02 22:29:34 +0000

    Modifed test to support new plugin tests.

--- plugins/i18n/src/test/java/net/sourceforge/squirrel_sql/plugins/i18n/I18nPluginTest.java
+++ plugins/i18n/src/test/java/net/sourceforge/squirrel_sql/plugins/i18n/I18nPluginTest.java
@@ -18,25 +18,18 @@
  */
 package net.sourceforge.squirrel_sql.plugins.i18n;
 
-import net.sourceforge.squirrel_sql.client.plugin.AbstractPluginTest;
+import net.sourceforge.squirrel_sql.client.plugin.AbstractSessionPluginTest;
 import net.sourceforge.squirrel_sql.client.plugin.DatabaseProductVersionData;
-
-import org.junit.After;
-import org.junit.Before;
+import net.sourceforge.squirrel_sql.client.plugin.IPlugin;
 
 
-public class I18nPluginTest extends AbstractPluginTest implements DatabaseProductVersionData
-{	
-	@Before
-	public void setUp() throws Exception
+public class I18nPluginTest extends AbstractSessionPluginTest implements DatabaseProductVersionData
+{
+
+	@Override
+	protected IPlugin getPluginToTest() throws Exception
 	{
-		classUnderTest = new I18nPlugin();
-	}
-
-	@After
-	public void tearDown() throws Exception
-	{
-		classUnderTest = null;
-	}		
+		return new I18nPlugin();
+	}	
 
 }
