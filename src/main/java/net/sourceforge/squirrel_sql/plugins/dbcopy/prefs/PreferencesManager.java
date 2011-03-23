1a12a9025ca68d422a57e638bed181fec5daba9c
Robert Manning <robert.m.manning@gmail.com>
2011-03-23 00:46:58 +0000

    Replaced direct java.io.File construction with FileWrapper (FileWrapperImpl), which is a framework that allows tests to control where files are created and/or control whether or not a file is created at all, or just in memory for the life of the test.

--- src/main/java/net/sourceforge/squirrel_sql/plugins/dbcopy/prefs/PreferencesManager.java
+++ src/main/java/net/sourceforge/squirrel_sql/plugins/dbcopy/prefs/PreferencesManager.java
@@ -18,7 +18,6 @@
  */
 package net.sourceforge.squirrel_sql.plugins.dbcopy.prefs;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
@@ -27,92 +26,121 @@ import net.sourceforge.squirrel_sql.client.Version;
 import net.sourceforge.squirrel_sql.client.plugin.IPlugin;
 import net.sourceforge.squirrel_sql.client.plugin.PluginException;
 import net.sourceforge.squirrel_sql.client.plugin.PreferenceUtil;
+import net.sourceforge.squirrel_sql.fw.util.FileWrapper;
+import net.sourceforge.squirrel_sql.fw.util.FileWrapperFactory;
+import net.sourceforge.squirrel_sql.fw.util.FileWrapperFactoryImpl;
+import net.sourceforge.squirrel_sql.fw.util.Utilities;
 import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
 import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
 import net.sourceforge.squirrel_sql.fw.xml.XMLBeanReader;
 import net.sourceforge.squirrel_sql.fw.xml.XMLBeanWriter;
 
-public class PreferencesManager {
+public class PreferencesManager
+{
 
-    /** Logger for this class. */
-    private final static ILogger s_log = 
-        LoggerController.createLogger(PreferencesManager.class);
-    
-    /** Name of preferences file. */
-    private static final String USER_PREFS_FILE_NAME = "prefs.xml";    
-    
-    /** Folder to store user settings in. */
-    private static File _userSettingsFolder;
-    
-    private static DBCopyPreferenceBean _prefs = null;
-    
-    private static IPlugin plugin = null;
-    
-    public static void initialize(IPlugin thePlugin) throws PluginException {
-        plugin = thePlugin;
-        
-        // Folder to store user settings.
-        try {
-            _userSettingsFolder = plugin.getPluginUserSettingsFolder();
-        } catch (IOException ex) {
-            throw new PluginException(ex);
-        }        
-        
-        loadPrefs();
-    }
-    
-    public static DBCopyPreferenceBean getPreferences() {
-        return _prefs;
-    }
-    
-    public static void unload() {
-        savePrefs();
-    }
-    
-    /**
-     * Save preferences to disk.  Always write to the user settings folder, not
-     * the application settings folder.
-     */
-    public static void savePrefs() {
-        try {
-            XMLBeanWriter wtr = new XMLBeanWriter(_prefs);
-            wtr.save(new File(_userSettingsFolder, USER_PREFS_FILE_NAME));
-        } catch (Exception ex) {
-            s_log.error("Error occured writing to preferences file: "
-                    + USER_PREFS_FILE_NAME, ex);
-        }
-    }
+	/** Logger for this class. */
+	private final static ILogger s_log = LoggerController.createLogger(PreferencesManager.class);
 
-    /**
-     * Load from preferences file.
-     */
-    private static void loadPrefs() {
-        File prefFile = null;
-        try {
-            XMLBeanReader doc = new XMLBeanReader();
-            
-            prefFile = PreferenceUtil.getPreferenceFileToReadFrom(plugin);
-            
-            doc.load(prefFile, DBCopyPreferenceBean.class.getClassLoader());
-            
-            Iterator<Object> it = doc.iterator();
-            if (it.hasNext()) {
-                _prefs = (DBCopyPreferenceBean)it.next();
-            }
-        } catch (FileNotFoundException ignore) {
-            s_log.info(USER_PREFS_FILE_NAME + "("+prefFile.getAbsolutePath()+
-                       ") not found - will be created");
-        } catch (Exception ex) {
-            s_log.error("Error occured reading from preferences file: "
-                    + USER_PREFS_FILE_NAME, ex);
-        }
-        if (_prefs == null) {
-            _prefs = new DBCopyPreferenceBean();
-        }
+	/** Name of preferences file. */
+	private static final String USER_PREFS_FILE_NAME = "prefs.xml";
 
-        _prefs.setClientName(Version.getApplicationName() + "/" + plugin.getDescriptiveName());
-        _prefs.setClientVersion(Version.getShortVersion() + "/" + plugin.getVersion());
-    }    
-    
-    
+	/** Folder to store user settings in. */
+	private static FileWrapper _userSettingsFolder;
+
+	/** factory for creating FileWrappers which insulate the application from direct reference to File */
+	private static FileWrapperFactory fileWrapperFactory = new FileWrapperFactoryImpl();
+
+	private static DBCopyPreferenceBean _prefs = null;
+
+	private static IPlugin plugin = null;
+
+	public static void initialize(IPlugin thePlugin) throws PluginException
+	{
+		plugin = thePlugin;
+
+		// Folder to store user settings.
+		try
+		{
+			_userSettingsFolder = plugin.getPluginUserSettingsFolder();
+		}
+		catch (IOException ex)
+		{
+			throw new PluginException(ex);
+		}
+
+		loadPrefs();
+	}
+
+	public static DBCopyPreferenceBean getPreferences()
+	{
+		return _prefs;
+	}
+
+	public static void unload()
+	{
+		savePrefs();
+	}
+
+	/**
+	 * @param fileWrapperFactory the fileWrapperFactory to set
+	 */
+	public static void setFileWrapperFactory(FileWrapperFactory fileWrapperFactory)
+	{
+		Utilities.checkNull("setFileWrapperFactory", "fileWrapperFactory", fileWrapperFactory);
+		PreferencesManager.fileWrapperFactory = fileWrapperFactory;
+	}
+		
+	/**
+	 * Save preferences to disk. Always write to the user settings folder, not the application settings folder.
+	 */
+	public static void savePrefs()
+	{
+		try
+		{
+			XMLBeanWriter wtr = new XMLBeanWriter(_prefs);
+			wtr.save(fileWrapperFactory.create(_userSettingsFolder, USER_PREFS_FILE_NAME));
+		}
+		catch (Exception ex)
+		{
+			s_log.error("Error occured writing to preferences file: " + USER_PREFS_FILE_NAME, ex);
+		}
+	}
+
+	/**
+	 * Load from preferences file.
+	 */
+	private static void loadPrefs()
+	{
+		FileWrapper prefFile = null;
+		try
+		{
+			XMLBeanReader doc = new XMLBeanReader();
+
+			prefFile = PreferenceUtil.getPreferenceFileToReadFrom(plugin);
+
+			doc.load(prefFile, DBCopyPreferenceBean.class.getClassLoader());
+
+			Iterator<Object> it = doc.iterator();
+			if (it.hasNext())
+			{
+				_prefs = (DBCopyPreferenceBean) it.next();
+			}
+		}
+		catch (FileNotFoundException ignore)
+		{
+			s_log.info(USER_PREFS_FILE_NAME + "(" + prefFile.getAbsolutePath() + ") not found - will be created");
+		}
+		catch (Exception ex)
+		{
+			s_log.error("Error occured reading from preferences file: " + USER_PREFS_FILE_NAME, ex);
+		}
+		if (_prefs == null)
+		{
+			_prefs = new DBCopyPreferenceBean();
+		}
+
+		_prefs.setClientName(Version.getApplicationName() + "/" + plugin.getDescriptiveName());
+		_prefs.setClientVersion(Version.getShortVersion() + "/" + plugin.getVersion());
+	}
+
 }
