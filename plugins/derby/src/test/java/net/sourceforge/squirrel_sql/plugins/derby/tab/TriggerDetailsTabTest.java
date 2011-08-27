33140ed5ea78716be32177c6b9922231c8b8bbc5
Robert Manning <robert.m.manning@gmail.com>
2011-08-27 17:07:34 +0000

    Added unit test for TriggerDetailsTab.createStatement().

--- plugins/derby/src/test/java/net/sourceforge/squirrel_sql/plugins/derby/tab/TriggerDetailsTabTest.java
+++ plugins/derby/src/test/java/net/sourceforge/squirrel_sql/plugins/derby/tab/TriggerDetailsTabTest.java
@@ -19,17 +19,56 @@
 package net.sourceforge.squirrel_sql.plugins.derby.tab;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.sql.PreparedStatement;
+
+import net.sourceforge.squirrel_sql.client.IApplication;
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.SessionManager;
 import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.tabs.AbstractBasePreparedStatementTabTest;
+import net.sourceforge.squirrel_sql.fw.id.IIdentifier;
+import net.sourceforge.squirrel_sql.fw.sql.IDatabaseObjectInfo;
+import net.sourceforge.squirrel_sql.fw.sql.ISQLConnection;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 
 public class TriggerDetailsTabTest extends AbstractBasePreparedStatementTabTest
 {
-
+	
+	private ISession mockSession = Mockito.mock(ISession.class);
+	private IIdentifier mockSessionIdentifier = Mockito.mock(IIdentifier.class);
+	private IApplication mockApplication = Mockito.mock(IApplication.class);
+	private SessionManager mockSessionManager = Mockito.mock(SessionManager.class);
+	private ISQLConnection mockSQLConnection = Mockito.mock(ISQLConnection.class);
+	private IDatabaseObjectInfo mockDatabaseObjectInfo = Mockito.mock(IDatabaseObjectInfo.class);
+	private PreparedStatement mockPreparedStatement = Mockito.mock(PreparedStatement.class);
+	
 	@Before
 	public void setUp() throws Exception
 	{
 		classUnderTest = new TriggerDetailsTab();
+		
+		when(mockSession.getIdentifier()).thenReturn(mockSessionIdentifier);
+		when(mockSession.getApplication()).thenReturn(mockApplication);
+		when(mockSession.getSQLConnection()).thenReturn(mockSQLConnection);
+		when(mockApplication.getSessionManager()).thenReturn(mockSessionManager);
+		when(mockSessionManager.getSession(mockSessionIdentifier)).thenReturn(mockSession);
+		when(mockSQLConnection.prepareStatement(Mockito.anyString())).thenReturn(mockPreparedStatement);
+		
 	}
 
+	@Test
+	public void testCreateStatement() throws Exception 
+	{
+		TriggerDetailsTab tabToTest = new TriggerDetailsTab(); 	
+		tabToTest.setSession(mockSession);
+		tabToTest.setDatabaseObjectInfo(mockDatabaseObjectInfo);
+		PreparedStatement result = tabToTest.createStatement();
+		assertEquals(mockPreparedStatement, result);
+	}
+	
 }
