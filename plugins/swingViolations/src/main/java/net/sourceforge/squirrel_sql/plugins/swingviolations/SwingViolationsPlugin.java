63e278405bee7dd5cdccfa5334f0ecab316336be
wis775 <stefan.home@aon.at>
2012-02-08 20:31:31 +0000

    New Plugin "Swing Violations Detector". This one helps to detect violations of the swing programming model. e.g. The event dispatching thread.

---
+++ plugins/swingViolations/src/main/java/net/sourceforge/squirrel_sql/plugins/swingviolations/SwingViolationsPlugin.java
@@ -0,0 +1,140 @@
+package net.sourceforge.squirrel_sql.plugins.swingviolations;
+
+import javax.swing.RepaintManager;
+
+import net.sourceforge.squirrel_sql.client.plugin.DefaultSessionPlugin;
+import net.sourceforge.squirrel_sql.client.plugin.PluginException;
+import net.sourceforge.squirrel_sql.client.plugin.PluginResources;
+import net.sourceforge.squirrel_sql.client.plugin.PluginSessionCallback;
+import net.sourceforge.squirrel_sql.client.plugin.PluginSessionCallbackAdaptor;
+import net.sourceforge.squirrel_sql.client.preferences.IGlobalPreferencesPanel;
+import net.sourceforge.squirrel_sql.client.session.IObjectTreeAPI;
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.fw.dialects.DialectFactory;
+import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectType;
+import net.sourceforge.squirrel_sql.fw.util.IMessageHandler;
+import net.sourceforge.squirrel_sql.plugins.swingviolations.EDTViolationRepaintManager;
+
+/**
+ *
+ * </pre>
+ */
+public class SwingViolationsPlugin extends DefaultSessionPlugin
+{
+	private PluginResources _resources;
+
+	/**
+	 * Return the internal name of this plugin.
+	 * 
+	 * @return the internal name of this plugin.
+	 */
+	public String getInternalName()
+	{
+		return "swingViolations";
+	}
+
+	/**PluginResources
+	 * Return the descriptive name of this plugin.
+	 * 
+	 * @return the descriptive name of this plugin.
+	 */
+	public String getDescriptiveName()
+	{
+		return "Swing Violation Detector Plugin";
+	}
+
+	/**
+	 * Returns the current version of this plugin.
+	 * 
+	 * @return the current version of this plugin.
+	 */
+	public String getVersion()
+	{
+		return "0.01";
+	}
+
+	/**
+	 * Returns the authors name.
+	 * 
+	 * @return the authors name.
+	 */
+	public String getAuthor()
+	{
+		return "Stefan Willinger";
+	}
+
+	/**
+	 * Returns the name of the change log for the plugin. This should be a text or HTML file residing in the
+	 * <TT>getPluginAppSettingsFolder</TT> directory.
+	 * 
+	 * @return the changelog file name or <TT>null</TT> if plugin doesn't have a change log.
+	 */
+	public String getChangeLogFileName()
+	{
+		return "changes.txt";
+	}
+
+	/**
+	 * Returns the name of the Help file for the plugin. This should be a text or HTML file residing in the
+	 * <TT>getPluginAppSettingsFolder</TT> directory.
+	 * 
+	 * @return the Help file name or <TT>null</TT> if plugin doesn't have a help file.
+	 */
+	public String getHelpFileName()
+	{
+		return "readme.txt";
+	}
+
+	/**
+	 * Returns the name of the Licence file for the plugin. This should be a text or HTML file residing in the
+	 * <TT>getPluginAppSettingsFolder</TT> directory.
+	 * 
+	 * @return the Licence file name or <TT>null</TT> if plugin doesn't have a licence file.
+	 */
+	public String getLicenceFileName()
+	{
+		return "licence.txt";
+	}
+
+	/**
+	 * @return Comma separated list of contributors.
+	 */
+	public String getContributors()
+	{
+		return "";
+	}
+
+	/**
+	 * Create preferences panel for the Global Preferences dialog.
+	 * 
+	 * @return Preferences panel.
+	 */
+	public IGlobalPreferencesPanel[] getGlobalPreferencePanels()
+	{
+		return new IGlobalPreferencesPanel[0];
+	}
+
+	/**
+	 * Initialize this plugin.
+	 */
+	public synchronized void initialize() throws PluginException
+	{
+		_resources = new PluginResources("net.sourceforge.squirrel_sql.plugins.swing-violation.swing-violation", this);
+		EDTViolationRepaintManager manager = new EDTViolationRepaintManager(true, getApplication());
+		RepaintManager.setCurrentManager(manager);
+	}
+
+	/**
+	 * Called when a session started. Add commands to popup menu in object tree.
+	 * 
+	 * @param session
+	 *           The session that is starting.
+	 * @return An implementation of PluginSessionCallback or null to indicate the plugin does not work with
+	 *         this session
+	 */
+	public PluginSessionCallback sessionStarted(ISession session)
+	{
+		return null;
+	}
+
+}
