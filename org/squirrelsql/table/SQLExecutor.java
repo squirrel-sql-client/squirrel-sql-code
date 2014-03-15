a03f61cd8e6433c8ab5aec61f3e9e789d8d384a8
gerd <gerd@gerd.de>
2014-03-15 16:35:15 +0100

    Refactoring

---
+++ org/squirrelsql/table/SQLExecutor.java
@@ -0,0 +1,74 @@
+package org.squirrelsql.table;
+
+import org.squirrelsql.aliases.dbconnector.DbConnectorResult;
+import org.squirrelsql.services.Utils;
+import org.squirrelsql.session.SQLResult;
+import org.squirrelsql.session.StatementChannel;
+import org.squirrelsql.session.StatementExecutionState;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLExecutor
+{
+   public static SQLResult loadDataFromSQL(DbConnectorResult dbConnectorResult, String sql, Integer maxResults, StatementChannel statementChannel)
+   {
+      Statement stat = null;
+      ResultSet res = null;
+      try
+      {
+         stat = dbConnectorResult.getSQLConnection().getConnection().createStatement();
+
+         if (null != maxResults)
+         {
+            stat.setMaxRows(maxResults);
+         }
+
+         long executionTimeBegin;
+         long executionTimeEnd;
+         long buildingOutputTimeBegin;
+         long buildingOutputTimeEnd;
+
+         try
+         {
+            statementChannel.setStatementExecutionState(StatementExecutionState.EXECUTING);
+
+            statementChannel.setCancelCandidate(stat);
+            executionTimeBegin = System.currentTimeMillis();
+            res = stat.executeQuery(sql);
+            executionTimeEnd = System.currentTimeMillis();
+            statementChannel.setCancelCandidate(null);
+
+            if(statementChannel.isCanceled())
+            {
+               return new SQLResult(new SQLException("Statement canceled while executing"));
+            }
+
+         }
+         catch (SQLException e)
+         {
+            statementChannel.setStatementExecutionState(StatementExecutionState.ERROR);
+            return new SQLResult(e);
+         }
+         statementChannel.setStatementExecutionState(StatementExecutionState.BUILDING_OUTPUT);
+
+         buildingOutputTimeBegin = System.currentTimeMillis();
+         TableLoader tableLoader = TableLoaderFactory.loadDataFromResultSet(res, statementChannel);
+         buildingOutputTimeEnd = System.currentTimeMillis();
+         statementChannel.setStatementExecutionState(StatementExecutionState.FINSHED);
+
+         return new SQLResult(tableLoader, executionTimeEnd - executionTimeBegin, buildingOutputTimeEnd - buildingOutputTimeBegin);
+      }
+      catch (Throwable e)
+      {
+         statementChannel.setStatementExecutionState(StatementExecutionState.ERROR);
+         throw new RuntimeException(e);
+      }
+      finally
+      {
+         Utils.close(res);
+         Utils.close(stat);
+      }
+   }
+}
