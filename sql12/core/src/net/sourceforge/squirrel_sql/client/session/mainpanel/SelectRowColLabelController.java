32de2248f24e9a8a8ac6e67734c89b1aa7612c77
gerd <gerd@gerd.de>
2014-03-19 23:06:27 +0100

    FR #469 Show selected row count (like result row count)

--- sql12/core/src/net/sourceforge/squirrel_sql/client/session/mainpanel/SelectRowColLabelController.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/client/session/mainpanel/SelectRowColLabelController.java
@@ -2,12 +2,17 @@ package net.sourceforge.squirrel_sql.client.session.mainpanel;
 
 import net.sourceforge.squirrel_sql.fw.datasetviewer.IDataSetViewer;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.RowColSelectedCountListener;
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class SelectRowColLabelController extends Component
 {
+   private static final StringManager s_stringMgr = StringManagerFactory.getStringManager(SelectRowColLabelController.class);
+
+
    private JLabel _lbl;
 
    public SelectRowColLabelController()
@@ -34,6 +39,7 @@ public class SelectRowColLabelController extends Component
 
    private void onRowColSelectedCountChanged(int selectedRowCount, int selectedColumnCount)
    {
+      s_stringMgr.getString("SelectRowColLabelController.RowColSelectedCountLabel", selectedRowCount, selectedColumnCount);
       _lbl.setText("Rows: " + selectedRowCount + ", Cols: " + selectedColumnCount);
    }
 }
