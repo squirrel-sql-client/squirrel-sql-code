f7ebcbbce7f493b65b6accc4fadf209680477f49
gerd <gerd@gerd.de>
2014-03-20 20:01:06 +0100

    Removed test buttons

--- org/squirrelsql/session/SessionTabbedPaneCtrl.java
+++ org/squirrelsql/session/SessionTabbedPaneCtrl.java
@@ -11,71 +11,13 @@ import org.squirrelsql.services.MessageHandlerDestination;
 
 public class SessionTabbedPaneCtrl
 {
-   private MessageHandler _mhPanel = new MessageHandler(this.getClass(), MessageHandlerDestination.MESSAGE_PANEL);
-   private MessageHandler _mhLog = new MessageHandler(this.getClass(), MessageHandlerDestination.MESSAGE_LOG);
-
-
-
    private TabPane _tabPane = new TabPane();
-
 
    public Node getNode()
    {
-      BorderPane bp = new BorderPane();
-      bp.setCenter(_tabPane);
-
-
-      EventHandler<ActionEvent> eventHandler = new EventHandler<ActionEvent>()
-      {
-         @Override
-         public void handle(ActionEvent actionEvent)
-         {
-            onAction(actionEvent);
-         }
-      };
-
-      HBox hBox = new HBox();
-
-      Button error = new Button("Error");
-      error.setOnAction(eventHandler);
-      hBox.getChildren().add(error);
-
-      Button warning = new Button("Warning");
-      warning.setOnAction(eventHandler);
-      hBox.getChildren().add(warning);
-
-      Button message = new Button("Message");
-      message.setOnAction(eventHandler);
-      hBox.getChildren().add(message);
-
-      bp.setBottom(hBox);
-
-
-      return bp;
+      return _tabPane;
    }
 
-   private void onAction(ActionEvent actionEvent)
-   {
-      Button source = (Button) actionEvent.getSource();
-      if("Error".equals(source.getText()))
-      {
-         _mhLog.error("Test error");
-         _mhPanel.error("Test error");
-         IllegalStateException illegalStateException = new IllegalStateException("Test exception");
-         _mhPanel.error(illegalStateException);
-         throw illegalStateException;
-      }
-      else if("Warning".equals(source.getText()))
-      {
-         _mhLog.warning("Test warning");
-         _mhPanel.warning("Test warning");
-      }
-      else if("Message".equals(source.getText()))
-      {
-         _mhLog.info("Test info");
-         _mhPanel.info("Test info");
-      }
-   }
 
    public Tab addSessionTab(SessionCtrl sessionCtrl)
    {
