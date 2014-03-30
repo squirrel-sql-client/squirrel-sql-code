ac65ea948e54264ad9907b86c161d41e037fa8f3
gerd <gerd@gerd.de>
2014-03-30 13:54:22 +0200

    Bug fix

--- org/squirrelsql/aliases/AliasEditController.java
+++ org/squirrelsql/aliases/AliasEditController.java
@@ -141,7 +141,7 @@ public class AliasEditController
          Alias buf = new Alias();
          storeToAlias(buf, sqlDriver);
 
-         DBConnector dbConnector = new DBConnector(_alias, _dialog, SchemaCacheConfig.LOAD_NOTHING);
+         DBConnector dbConnector = new DBConnector(buf, _dialog, SchemaCacheConfig.LOAD_NOTHING);
 
          dbConnector.tryConnect(dbConnectorResult -> onTryConnectFinished(dbConnectorResult));
 
