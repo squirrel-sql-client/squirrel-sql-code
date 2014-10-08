e079a646c60ae690d44d654ad0dec757d9662416
Steven Zeck <steven.zeck@gmail.com>
2014-10-08 18:29:17 -0500

    Added a right click menu in the messages pane, starting with a clear option to clear messages from it.

---
+++ sqfx/core/src/org/squirrelsql/RightMouseMenuHandler.java
@@ -0,0 +1,56 @@
+package org.squirrelsql;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Control;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+
+public class RightMouseMenuHandler
+{
+   //TODO Test this class in ResultTabController in place of SQLResultRightMouseMenuHandler. If no issues use this for any control where a right click should be implemented
+   private ContextMenu _contextMenu;
+
+   public RightMouseMenuHandler(Control control)
+   {
+      createRightMouseMenu(control);
+   }
+
+   private void createRightMouseMenu(Control control)
+   {
+      _contextMenu = new ContextMenu();
+
+      _contextMenu.setAutoHide(true);
+
+      control.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>()
+      {
+         @Override
+         public void handle(MouseEvent e)
+         {
+            onShowRightMouseMenu(e, _contextMenu, control);
+         }
+      });
+   }
+
+   private void onShowRightMouseMenu(MouseEvent e, ContextMenu cm, Control control)
+   {
+      if (e.getButton() == MouseButton.SECONDARY)
+      {
+         cm.show(control, e.getScreenX(), e.getScreenY());
+      }
+      else
+      {
+         cm.hide();
+      }
+   }
+
+   public void addMenu(String menuTitle, RightMouseMenuHandlerListener rightMouseMenuHandlerListener)
+   {
+      MenuItem menuItem = new MenuItem(menuTitle);
+
+      menuItem.setOnAction(e -> rightMouseMenuHandlerListener.menuSelected());
+
+      _contextMenu.getItems().add(menuItem);
+   }
+}
