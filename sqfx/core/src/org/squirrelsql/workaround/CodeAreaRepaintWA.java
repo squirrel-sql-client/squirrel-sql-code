f35ea2fdf88f4a7ba61264d2aa8f42a591c713af
gerdwagner <gerdhwagner@t-online.de>
2014-11-29 23:48:33 +0100

    Preventing some more RichTextFx repaint problems, not all.

--- sqfx/core/src/org/squirrelsql/workaround/CodeAreaRepaintWA.java
+++ sqfx/core/src/org/squirrelsql/workaround/CodeAreaRepaintWA.java
@@ -1,5 +1,6 @@
 package org.squirrelsql.workaround;
 
+import javafx.application.Platform;
 import org.fxmisc.richtext.CodeArea;
 
 public class CodeAreaRepaintWA
@@ -7,5 +8,10 @@ public class CodeAreaRepaintWA
    public static void avoidRepaintProblemsAfterTextModification(CodeArea sqlTextArea)
    {
       sqlTextArea.requestLayout();
+      sqlTextArea.layout();
+
+      Platform.runLater(() -> sqlTextArea.requestLayout());
+      Platform.runLater(() -> sqlTextArea.layout());
+
    }
 }
