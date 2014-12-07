3b73cfb206f84bf0b4b730505894f299e191f9bf
gerdwagner <gerdhwagner@t-online.de>
2014-12-07 21:48:03 +0100

    Settings --> Begin

--- sqfx/core/src/org/squirrelsql/settings/SettingsView.java
+++ sqfx/core/src/org/squirrelsql/settings/SettingsView.java
@@ -3,11 +3,14 @@ package org.squirrelsql.settings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class SettingsView
 {
+   @FXML RadioButton radStandardProps;
+   @FXML RadioButton radUserDefinedProps;
    @FXML AnchorPane apProperties;
    @FXML TextField txtPropertiesFileLocation;
    @FXML CheckBox chkMultibleLinesInCells;
