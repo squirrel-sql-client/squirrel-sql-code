460afc4b2933e744282a02e550cb11e3aecd16e7
Neville <Neville@rowe3>
2015-02-19 21:49:54 +0000

    Fix to bug https://sourceforge.net/p/squirrel-sql/bugs/1177/ - export to excel .xls fails if there are more than 4,000 date cells in the dataset. Apache POI only allows 4,000 styles to be defined and we used to create a new style for every cell which is a date. I've modified the excel writer to cache and reuse styles.

--- sql12/core/src/net/sourceforge/squirrel_sql/fw/gui/action/exportData/DataExportExcelWriter.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/fw/gui/action/exportData/DataExportExcelWriter.java
@@ -24,240 +24,224 @@ import java.io.IOException;
 import java.sql.Types;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 import net.sourceforge.squirrel_sql.fw.gui.action.ExportFormat;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
 import net.sourceforge.squirrel_sql.fw.datasetviewer.ColumnDisplayDefinition;
 import net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.CellComponentFactory;
 import net.sourceforge.squirrel_sql.fw.gui.action.TableExportCsvController;
 import net.sourceforge.squirrel_sql.fw.sql.ProgressAbortCallback;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 /**
  * Exports {@link IExportData} to a Excel file.
- * <b>Note:</b> This class is the result of a refactoring task. The code was taken from TableExportCsvCommand.
+ * <b>Note:</b> This class is the result of a refactoring task. The code was
+ * taken from TableExportCsvCommand.
+ *
  * @author Stefan Willinger
  *
  */
 public class DataExportExcelWriter extends AbstractDataExportFileWriter {
+
     private Workbook workbook; // The streaming api export for (very) large xlsx files
-    private Sheet   sheet; // We write the data to this sheet
-    private File    file; // File where the export is written to
+    private Sheet sheet; // We write the data to this sheet
+    private File file; // File where the export is written to
     private boolean withHeader = false;
-   private ExportFormat _exportFormat;
+    private final ExportFormat _exportFormat;
+    private HashMap<String, CellStyle> formatCache = null;
 
+    /**
+     * @param file
+     * @param ctrl
+     * @param includeHeaders
+     * @param exportFormat
+     * @param progressController
+     */
+    public DataExportExcelWriter(File file, TableExportCsvController ctrl, boolean includeHeaders, ExportFormat exportFormat, ProgressAbortCallback progressController) {
+        super(file, ctrl, includeHeaders, progressController);
+        _exportFormat = exportFormat;
+    }
 
-   /**
-    * @param file
-    * @param ctrl
-    * @param includeHeaders
-    * @param exportFormat
-    * @param progressController
-    */
-	public DataExportExcelWriter(File file, TableExportCsvController ctrl, boolean includeHeaders, ExportFormat exportFormat, ProgressAbortCallback progressController) {
-		super(file, ctrl, includeHeaders, progressController);
-      _exportFormat = exportFormat;
-   }
+    private Cell getXlsCell(ColumnDisplayDefinition colDef, int colIdx, int curRow, Object cellObj) {
+        Row row = sheet.getRow(curRow);
+        if (row == null) {
+            row = sheet.createRow(curRow);
+        }
+        Cell retVal = row.createCell(colIdx);
 
-	private Cell getXlsCell(ColumnDisplayDefinition colDef, int colIdx, int curRow, Object cellObj)
-   {
-      Row row = sheet.getRow(curRow);
-      if (row == null)
-      {
-         row = sheet.createRow(curRow);
-      }
-      Cell retVal = row.createCell(colIdx);
-
-      if (null == cellObj || null == colDef)
-      {
-         retVal.setCellValue(getDataXLSAsString(cellObj));
-         return retVal;
-      }
-
-      int colType = colDef.getSqlType();
-
-      switch (colType)
-      {
-         case Types.BIT:
-         case Types.BOOLEAN:
-            if (null == cellObj)
-            {
-               //retVal.setCellValue((Boolean)null);
-            }
-            else
-            {
-               retVal.setCellValue((Boolean) cellObj);
-            }
-            break;
-         case Types.INTEGER:
-            if (null == cellObj)
-            {
-               //retVal.setCellValue((Integer)null);
-            }
-            else
-            {
-               retVal.setCellValue(((Number) cellObj).intValue());
-            }
-            break;
-         case Types.SMALLINT:
-         case Types.TINYINT:
-            if (null == cellObj)
-            {
-               //retVal.setCellValue(((Short) null));
-            }
-            else
-            {
-               retVal.setCellValue(((Number) cellObj).shortValue());
-            }
-            break;
-         case Types.NUMERIC:
-         case Types.DECIMAL:
-         case Types.FLOAT:
-         case Types.DOUBLE:
-         case Types.REAL:
-            if (null == cellObj)
-            {
-               //retVal.setCellValue((Double) null);
-            }
-            else
-            {
-               retVal.setCellValue(((Number) cellObj).doubleValue());
-            }
-            break;
-         case Types.BIGINT:
-            if (null == cellObj)
-            {
-               //retVal.setCellValue((Long)null);
-            }
-            else
-            {
-               retVal.setCellValue(Long.parseLong(cellObj.toString()));
-            }
-            break;
-         case Types.DATE:
-            makeTemporalCell(retVal, (Date) cellObj, "m/d/yy");
-            break;
-         case Types.TIMESTAMP:
-            makeTemporalCell(retVal, (Date) cellObj, "m/d/yy h:mm");
-            break;
-         case Types.TIME:
-            makeTemporalCell(retVal, (Date) cellObj, "h:mm");
-            break;
-         case Types.CHAR:
-         case Types.VARCHAR:
-         case Types.LONGVARCHAR:
-            cellObj = CellComponentFactory.renderObject(cellObj, colDef);
+        if (null == cellObj || null == colDef) {
             retVal.setCellValue(getDataXLSAsString(cellObj));
-            break;
-         default:
-            cellObj = CellComponentFactory.renderObject(cellObj, colDef);
-            retVal.setCellValue(getDataXLSAsString(cellObj));
-      }
+            return retVal;
+        }
 
-      return retVal;
-   }
+        int colType = colDef.getSqlType();
 
-   private void makeTemporalCell(Cell retVal, Date cellObj, String format)
-   {
-      CreationHelper creationHelper = workbook.getCreationHelper();
-      CellStyle cellStyle = workbook.createCellStyle();
-      cellStyle.setDataFormat(creationHelper.createDataFormat().getFormat(format));
-      retVal.setCellStyle(cellStyle);
+        switch (colType) {
+            case Types.BIT:
+            case Types.BOOLEAN:
+                retVal.setCellValue((Boolean) cellObj);
+                break;
+            case Types.INTEGER:
+                retVal.setCellValue(((Number) cellObj).intValue());
+                break;
+            case Types.SMALLINT:
+            case Types.TINYINT:
+                retVal.setCellValue(((Number) cellObj).shortValue());
+                break;
+            case Types.NUMERIC:
+            case Types.DECIMAL:
+            case Types.FLOAT:
+            case Types.DOUBLE:
+            case Types.REAL:
+                retVal.setCellValue(((Number) cellObj).doubleValue());
+                break;
+            case Types.BIGINT:
+                retVal.setCellValue(Long.parseLong(cellObj.toString()));
+                break;
+            case Types.DATE:
+                makeTemporalCell(retVal, (Date) cellObj, "m/d/yy");
+                break;
+            case Types.TIMESTAMP:
+                makeTemporalCell(retVal, (Date) cellObj, "m/d/yy h:mm");
+                break;
+            case Types.TIME:
+                makeTemporalCell(retVal, (Date) cellObj, "h:mm");
+                break;
+            case Types.CHAR:
+            case Types.VARCHAR:
+            case Types.LONGVARCHAR:
+                cellObj = CellComponentFactory.renderObject(cellObj, colDef);
+                retVal.setCellValue(getDataXLSAsString(cellObj));
+                break;
+            default:
+                cellObj = CellComponentFactory.renderObject(cellObj, colDef);
+                retVal.setCellValue(getDataXLSAsString(cellObj));
+        }
 
-      if (null != cellObj)
-      {
-         Calendar calendar = Calendar.getInstance();
-         calendar.setTime((Date) cellObj);
-         retVal.setCellValue(calendar);
-      }
-   }
+        return retVal;
+    }
 
-   private String getDataXLSAsString(Object cellObj) {
-		if (cellObj == null) {
-			return "";
-		} else {
-			return cellObj.toString().trim();
-		}
-	}
+    /*
+     * note POI which we use for the excel export has a limit of 4000 styles
+     * therefore formatCache has been introduced to re-use styles across cells.
+     * This resolves bug https://sourceforge.net/p/squirrel-sql/bugs/1177/
+     */
+    private void makeTemporalCell(Cell retVal, Date cellObj, String format) {
+        CreationHelper creationHelper = workbook.getCreationHelper();
+        CellStyle cellStyle;
+        if (formatCache == null) {
+            cellStyle = workbook.createCellStyle();
+            cellStyle.setDataFormat(creationHelper.createDataFormat().getFormat(format));
+            formatCache = new HashMap<String, CellStyle>();
+            formatCache.put(format, cellStyle);
+        } else {
+            cellStyle = formatCache.get(format);
+            if (cellStyle == null) {
+                cellStyle = workbook.createCellStyle();
+                cellStyle.setDataFormat(creationHelper.createDataFormat().getFormat(format));
+                formatCache.put(format, cellStyle);
+            }
+        }
+        retVal.setCellStyle(cellStyle);
+        if (null != cellObj) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime((Date) cellObj);
+            retVal.setCellValue(calendar);
+        }
+    }
 
-	/**
-	 * @see net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#beforeWorking()
-	 */
-	@Override
-   protected void beforeWorking(File file) throws IOException
-   {
-      if (_exportFormat == ExportFormat.EXPORT_FORMAT_XLS)
-      {
-         this.workbook = new HSSFWorkbook(); // See https://gist.github.com/madan712/3912272
-      }
-      else
-      {
-         this.workbook = new SXSSFWorkbook(100); // keep 100 rows in memory, exceeding rows will be flushed to disk
-      }
-      this.file = file;
-      this.sheet = workbook.createSheet("Squirrel SQL Export");
-   }
+    private String getDataXLSAsString(Object cellObj) {
+        if (cellObj == null) {
+            return "";
+        } else {
+            return cellObj.toString().trim();
+        }
+    }
 
-	/**
-	 * @see net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#addHeaderCell(int, java.lang.String)
-	 */
-	@Override
-	protected void addHeaderCell(int colIdx, String columnName) throws Exception {
-		this.withHeader = true;
-                Row headerRow= sheet.getRow(0);
-                if (headerRow == null)
-                {
-                    headerRow= sheet.createRow(0);
-                }
-                Cell cell= headerRow.createCell(colIdx);
-                cell.setCellValue(columnName);
-	}
+    /**
+     * @throws java.io.IOException
+     * @see
+     * net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#beforeWorking()
+     */
+    @Override
+    protected void beforeWorking(File file) throws IOException {
+        if (_exportFormat == ExportFormat.EXPORT_FORMAT_XLS) {
+            this.workbook = new HSSFWorkbook(); // See https://gist.github.com/madan712/3912272
+        } else {
+            this.workbook = new SXSSFWorkbook(100); // keep 100 rows in memory, exceeding rows will be flushed to disk
+        }
+        this.file = file;
+        this.sheet = workbook.createSheet("Squirrel SQL Export");
+    }
 
-	/**
-	 * @see net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#addCell(net.sourceforge.squirrel_sql.fw.gui.action.TableExportCsvController, int, int, net.sourceforge.squirrel_sql.fw.gui.action.exportData.IExportDataCell)
-	 */
-	@Override
-	protected void addCell(IExportDataCell cell) throws Exception{
-		Cell xlsCell;
-		if (getCtrl().useGloablPrefsFormatting()) {
-			xlsCell = getXlsCell(cell.getColumnDisplayDefinition(), cell.getColumnIndex(),
-					calculateRowIdx(cell), cell.getObject());
-		} else {
-			xlsCell = getXlsCell(null, cell.getColumnIndex(), calculateRowIdx(cell), cell.getObject());
-		}
-	}
+    /**
+     * @throws java.lang.Exception
+     * @see
+     * net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#addHeaderCell(int,
+     * java.lang.String)
+     */
+    @Override
+    protected void addHeaderCell(int colIdx, String columnName) throws Exception {
+        this.withHeader = true;
+        Row headerRow = sheet.getRow(0);
+        if (headerRow == null) {
+            headerRow = sheet.createRow(0);
+        }
+        Cell cell = headerRow.createCell(colIdx);
+        cell.setCellValue(columnName);
+    }
 
+    /**
+     * @throws java.lang.Exception
+     * @see
+     * net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#addCell(net.sourceforge.squirrel_sql.fw.gui.action.TableExportCsvController,
+     * int, int,
+     * net.sourceforge.squirrel_sql.fw.gui.action.exportData.IExportDataCell)
+     */
+    @Override
+    protected void addCell(IExportDataCell cell) throws Exception {
+        if (getCtrl().useGloablPrefsFormatting()) {
+            getXlsCell(cell.getColumnDisplayDefinition(), cell.getColumnIndex(), calculateRowIdx(cell), cell.getObject());
+        } else {
+            getXlsCell(null, cell.getColumnIndex(), calculateRowIdx(cell), cell.getObject());
+        }
+    }
 
-	/**
-	 * @param cell
-	 * @return
-	 */
-	private int calculateRowIdx(IExportDataCell cell) {
-		if(this.withHeader ){
-			return cell.getRowIndex()+1;
-		}else{
-			return cell.getRowIndex();
-		}
-	}
+    /**
+     * @param cell
+     * @return
+     */
+    private int calculateRowIdx(IExportDataCell cell) {
+        if (this.withHeader) {
+            return cell.getRowIndex() + 1;
+        } else {
+            return cell.getRowIndex();
+        }
+    }
 
-	/**
-	 * @see net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#afterWorking()
-	 */
-	@Override
-	protected void afterWorking() throws Exception{
-            FileOutputStream out = new FileOutputStream(this.file);
-            this.workbook.write(out);
-            out.close();
+    /**
+     * @throws java.lang.Exception
+     * @see
+     * net.sourceforge.squirrel_sql.fw.gui.action.exportData.AbstractDataExportFileWriter#afterWorking()
+     */
+    @Override
+    protected void afterWorking() throws Exception {
+        FileOutputStream out = new FileOutputStream(this.file);
+        this.workbook.write(out);
+        out.close();
 
-            // dispose of temporary files backing this workbook on disk
-      if (workbook instanceof SXSSFWorkbook)
-      {
-         ((SXSSFWorkbook)workbook).dispose();
-      }
-   }
+        // dispose of temporary files backing this workbook on disk
+        if (workbook instanceof SXSSFWorkbook) {
+            ((SXSSFWorkbook) workbook).dispose();
+        }
+    }
 
 }
