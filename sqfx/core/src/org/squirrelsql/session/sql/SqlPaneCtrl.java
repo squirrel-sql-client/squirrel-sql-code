7e0c7f5555b23f5d86bdfb64d662c90ca948b4a5
gerdwagner <gerdhwagner@t-online.de>
2015-03-10 22:37:46 +0100

    Bug fix: SQLs where executed twice

--- sqfx/core/src/org/squirrelsql/session/sql/SqlPaneCtrl.java
+++ sqfx/core/src/org/squirrelsql/session/sql/SqlPaneCtrl.java
@@ -111,6 +111,7 @@ public class SqlPaneCtrl
             if (actionCfg.matchesKeyEvent(keyEvent))
             {
                actionCfg.fire();
+               keyEvent.consume();
                return;
             }
          }
