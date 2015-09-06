c2632d146bae8338022e144a33959b964521c81c
gerd <gerdhwagner@t-online.de>
2015-09-06 10:38:25 +0200

    #1210 Empty Contents Tab in SQuirreL when Column-Names contain dashes (except HSQL-DB)

--- sql12/core/src/net/sourceforge/squirrel_sql/fw/dialects/DialectUtils2.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/fw/dialects/DialectUtils2.java
@@ -1,10 +1,12 @@
 package net.sourceforge.squirrel_sql.fw.dialects;
 
+import net.sourceforge.squirrel_sql.fw.util.StringUtilities;
+
 public class DialectUtils2
 {
    public static String checkColumnDoubleQuotes(DialectType dialectType, String columnName)
    {
-      if (dialectType == DialectType.HSQLDB || dialectType == DialectType.POSTGRES)
+      if (shouldQuoteColumnName(dialectType, columnName))
       {
          return "\"" + columnName + "\"";
       }
@@ -12,5 +14,36 @@ public class DialectUtils2
       {
          return columnName;
       }
+   }
+
+   private static boolean shouldQuoteColumnName(DialectType dialectType, String columnName)
+   {
+      if(dialectType == DialectType.HSQLDB || dialectType == DialectType.POSTGRES || dialectType == DialectType.H2)
+      {
+         return true;
+      }
+
+
+      if (false == StringUtilities.isEmpty(columnName))
+      {
+
+         if(false == Character.isJavaIdentifierStart(columnName.charAt(0)))
+         {
+            return true;
+         }
+
+
+         for (int i = 1; i < columnName.length(); i++)
+         {
+            char c = columnName.charAt(i);
+            if(false == Character.isJavaIdentifierPart(c) || '.' == c || '$' == c )
+            {
+               return true;
+            }
+         }
+      }
+
+
+      return false;
    }
 }
