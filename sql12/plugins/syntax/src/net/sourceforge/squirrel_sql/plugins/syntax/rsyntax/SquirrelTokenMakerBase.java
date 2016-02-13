0d005dc6573dcc12df03917ee974a0736b4d5cfd
gerdwagner <gerdhwagner@t-online.de>
2016-02-13 21:31:56 +0100

    Bug #1236 Shortcut for comment/uncomment current line (ctrl + "/") does not work   Fixed according to the suggestion in the bug #1236   Please note: The orginal comment/uncomment hot key of SQuirreL is ctrl+Numpad minus / ctrl+shift+Numpad minus.

--- sql12/plugins/syntax/src/net/sourceforge/squirrel_sql/plugins/syntax/rsyntax/SquirrelTokenMakerBase.java
+++ sql12/plugins/syntax/src/net/sourceforge/squirrel_sql/plugins/syntax/rsyntax/SquirrelTokenMakerBase.java
@@ -315,14 +315,14 @@ public class SquirrelTokenMakerBase extends AbstractJFlexTokenMaker {
 	}
 
 
-	/**
+   /**
 	 * Returns the text to place at the beginning and end of a
 	 * line to "comment" it in a this programming language.
 	 *
 	 * @return The start and end strings to add to a line to "comment"
 	 *         it out.
 	 */
-	public String[] getLineCommentStartAndEnd() {
+	public String[] getLineCommentStartAndEnd(int languageIndex) {
 		return new String[] { "--", null };
 	}
 
