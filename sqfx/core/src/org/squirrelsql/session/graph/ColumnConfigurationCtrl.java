b6fad13c6b964a582a739ecd26db0a3c007646bc
gerdwagner <gerdhwagner@t-online.de>
2016-11-07 22:11:21 +0100

    Query builder: Column configuration

--- sqfx/core/src/org/squirrelsql/session/graph/ColumnConfigurationCtrl.java
+++ sqfx/core/src/org/squirrelsql/session/graph/ColumnConfigurationCtrl.java
@@ -4,25 +4,21 @@ import javafx.scene.layout.HBox;
 
 public class ColumnConfigurationCtrl
 {
-   private final HBox _panel;
 
-   public ColumnConfigurationCtrl()
+   public HBox getPanel()
    {
-      _panel = new HBox();
-      _panel.setSpacing(3);
+      HBox panel;
+      panel = new HBox();
+      panel.setSpacing(3);
 
       AggregateFunctionPane aggregateFunctionPane = new AggregateFunctionPane(false);
 
       GraphListCheckBoxWA graphListCheckBox = new GraphListCheckBoxWA(b -> aggregateFunctionPane.setEnabled(b));
 
-      _panel.getChildren().add(graphListCheckBox);
-      _panel.getChildren().add(aggregateFunctionPane);
-      _panel.getChildren().add(new FilterPane());
-      _panel.getChildren().add(new OrderByPane());
-   }
-
-   public HBox getPanel()
-   {
-      return _panel;
+      panel.getChildren().add(graphListCheckBox);
+      panel.getChildren().add(aggregateFunctionPane);
+      panel.getChildren().add(new FilterPane());
+      panel.getChildren().add(new OrderByPane());
+      return panel;
    }
 }
