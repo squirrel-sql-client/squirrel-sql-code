8d163caf852473652c944b625fe44f8921a2d3e3
gerdwagner <gerdhwagner@t-online.de>
2017-01-02 22:00:39 +0100

    Query builder: Query configuration --> continue

--- sqfx/core/src/org/squirrelsql/session/graph/SelectConfigCtrl.java
+++ sqfx/core/src/org/squirrelsql/session/graph/SelectConfigCtrl.java
@@ -1,8 +1,14 @@
 package org.squirrelsql.session.graph;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import org.squirrelsql.Props;
+import org.squirrelsql.globalicons.GlobalIconNames;
+import org.squirrelsql.services.I18n;
 import org.squirrelsql.table.ColumnHandle;
 import org.squirrelsql.table.RowObjectTableLoader;
 
@@ -11,6 +17,11 @@ import java.util.ArrayList;
 public class SelectConfigCtrl
 {
    private final RowObjectTableLoader<SelectPositionRowObject> _tableLoader;
+
+   private I18n _i18n = new I18n(getClass());
+
+   private Button _btnUp = new Button(_i18n.t("SelectConfigCtrl.btnup"), new Props(getClass()).getImageView(GlobalIconNames.ARROW_UP));
+   private Button _btnDown = new Button(_i18n.t("SelectConfigCtrl.btndown"), new Props(getClass()).getImageView(GlobalIconNames.ARROW_DOWN));
 
    public SelectConfigCtrl(GraphPersistenceWrapper graphPersistenceWrapper)
    {
@@ -65,10 +76,21 @@ public class SelectConfigCtrl
 
    public Pane getPane()
    {
-      TableView ret = new TableView();
+      TableView tableView = new TableView();
 
-      _tableLoader.load(ret);
+      _tableLoader.load(tableView);
 
-      return new BorderPane(ret);
+      HBox hBox = new HBox();
+
+      HBox.setMargin(_btnUp, new Insets(5,10,5, 5));
+      HBox.setMargin(_btnDown, new Insets(5,0,5, 0));
+
+      hBox.getChildren().addAll(_btnUp, _btnDown);
+
+      BorderPane borderPane = new BorderPane(tableView);
+
+      borderPane.setBottom(hBox);
+
+      return borderPane;
    }
 }
