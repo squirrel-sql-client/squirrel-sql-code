942d5e45402498936d098e25cc1fd08900edb7c0
gerdwagner <gerdwagner@sourceforge.net>
2017-01-30 20:41:20 +0100

    Test commit

--- sql12/core/src/net/sourceforge/squirrel_sql/fw/datasetviewer/CellDataPopup.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/fw/datasetviewer/CellDataPopup.java
@@ -16,6 +16,8 @@ package net.sourceforge.squirrel_sql.fw.datasetviewer;
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Test
  */
 import java.awt.*;
 import java.awt.event.ActionEvent;
