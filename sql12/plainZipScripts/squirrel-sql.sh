7a78fc2fe2b563204946958735ad9ec3081448dc
gerdwagner <gerdwagner@sourceforge.net>
2017-11-06 21:54:35 +0100

    Updated plain zip scripts

--- sql12/plainZipScripts/squirrel-sql.sh
+++ sql12/plainZipScripts/squirrel-sql.sh
@@ -1,5 +1,27 @@
 #! /bin/sh
 
+# This function sets a global variable named "CP" to a command-path separated list of jars located beneath the
+# specified folder.  If the specified folder contains a lib directory, then jars beneath the lib folder are
+# @ added as well as the squirrel-sql.jar file located in the directory specified as the first argument to
+# this function.
+buildCPFromDir()
+{
+	CP=""
+	if [ -d "$1"/lib ]; then
+		# First entry in classpath is the Squirrel application.
+		CP="$1/squirrel-sql.jar"
+
+		# Then add all library jars to the classpath.
+		for a in "$1"/lib/*; do
+			CP="$CP":"$a"
+		done
+	else
+		for a in "$1"/*; do
+			CP="$CP":"$a"
+		done
+	fi
+}
+
 # IZPACK_JAVA_HOME is filtered in by the IzPack installer when this script is installed
 IZPACK_JAVA_HOME=%JAVA_HOME
 
@@ -34,8 +56,13 @@ esac
 # home as an absolute path, so that the command will work when exec'd from any location. So we attempt to 
 # detect the absolute path using dirname "$0", which should work in most cases.  
 if $macosx ; then
-        SQUIRREL_SQL_HOME=`dirname "$0"`/Contents/Resources/Java
-else 
+    SQUIRREL_SQL_HOME=`dirname "$0"`/Contents/Resources/Java
+    if [ ! -d "$SQUIRREL_SQL_HOME" ]; then
+        # We assume that this is the ZIP file extracted on MacOS,
+        # so, fall-back to the defult path
+        SQUIRREL_SQL_HOME=`dirname "$0"`
+    fi
+else
         SQUIRREL_SQL_HOME=`dirname "$0"`
 fi
 
@@ -53,21 +80,14 @@ cd "$UNIX_STYLE_HOME"
 # should be able to be run by that version or higher. The arguments to JavaVersionChecker below specify the 
 # minimum acceptable version (first arg) and any other acceptable subsequent versions.  <MAJOR>.<MINOR> should 
 # be all that is necessary for the version form. 
-$JAVACMD -cp "$UNIX_STYLE_HOME/lib/versioncheck.jar" JavaVersionChecker 1.6 1.7 1.8
+$JAVACMD -cp "$UNIX_STYLE_HOME/lib/versioncheck.jar" JavaVersionChecker 1.6 1.7 1.8 9
 if [ "$?" = "1" ]; then
   exit
 fi
 
-# First entry in classpath is the Squirrel application.
-TMP_CP="$UNIX_STYLE_HOME/squirrel-sql.jar"
-
-# Then add all library jars to the classpath.
-for a in "$UNIX_STYLE_HOME"/lib/*; do
-        TMP_CP="$TMP_CP":"$a"
-done
-
-
-
+# Build a command-path separated list of installed jars from the lib folder and squirrel-sql.jar
+buildCPFromDir "$UNIX_STYLE_HOME"
+TMP_CP=$CP
 
 # Now add the system classpath to the classpath. If running
 # Cygwin we also need to change the classpath to Windows format.
@@ -92,5 +112,8 @@ else
         SCRIPT_ARGS="$1 $2 $3 $4 $5 $6 $7 $8 $9"
 fi
 
+# Now, pickup all jars once again from the installation and lib directories. The variable "CP" is assigned this value.
+buildCPFromDir "$UNIX_STYLE_HOME"
+
 # Launch SQuirreL application
-$JAVACMD -Xmx256m -cp "$TMP_CP" $MACOSX_SQUIRREL_PROPS -splash:icons/splash.jpg net.sourceforge.squirrel_sql.client.Main --log-config-file "$UNIX_STYLE_HOME"/log4j.properties --squirrel-home "$UNIX_STYLE_HOME" $NATIVE_LAF_PROP $SCRIPT_ARGS
+$JAVACMD -Xmx256m -cp "$CP" $MACOSX_SQUIRREL_PROPS -splash:"$SQUIRREL_SQL_HOME/icons/splash.jpg" net.sourceforge.squirrel_sql.client.Main --log-config-file "$UNIX_STYLE_HOME"/log4j.properties --squirrel-home "$UNIX_STYLE_HOME" $NATIVE_LAF_PROP $SCRIPT_ARGS
