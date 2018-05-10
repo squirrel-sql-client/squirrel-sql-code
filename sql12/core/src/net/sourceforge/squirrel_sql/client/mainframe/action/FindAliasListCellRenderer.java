e6cbee2f76483789cfc4d3f43bb67ec3d0e47385
gerdwagner <gerdwagner@sourceforge.net>
2018-05-11 01:25:30 +0200

    Color Alias --> Last Step: Color Alias in Alias find dialog

--- sql12/core/src/net/sourceforge/squirrel_sql/client/mainframe/action/FindAliasListCellRenderer.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/client/mainframe/action/FindAliasListCellRenderer.java
@@ -1,12 +1,25 @@
 package net.sourceforge.squirrel_sql.client.mainframe.action;
 
+import net.sourceforge.squirrel_sql.client.gui.db.SQLAlias;
 import net.sourceforge.squirrel_sql.fw.sql.ISQLAlias;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.BorderFactory;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListCellRenderer;
+import java.awt.Color;
+import java.awt.Component;
 
 public class FindAliasListCellRenderer implements ListCellRenderer
 {
+   private final Color _defaultBackgroundColor;
+
+   public FindAliasListCellRenderer()
+   {
+      _defaultBackgroundColor = new JTextArea().getBackground();
+   }
+
    @Override
    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
    {
@@ -15,7 +28,7 @@ public class FindAliasListCellRenderer implements ListCellRenderer
          return null;
       }
 
-      ISQLAlias sqlAlias = (ISQLAlias) value;
+      SQLAlias sqlAlias = (SQLAlias) value;
 
       JTextArea comp = new JTextArea(sqlAlias.getName() + "\n  URL: " + sqlAlias.getUrl() + "\n  User: " + sqlAlias.getUserName() + "\n");
 
@@ -30,6 +43,22 @@ public class FindAliasListCellRenderer implements ListCellRenderer
       {
          comp.setBorder(BorderFactory.createLineBorder(Color.GRAY));
       }
+
+      
+      if(sqlAlias.getColorProperties().isOverrideAliasBackgroundColor())
+      {
+         Color bg = new Color(sqlAlias.getColorProperties().getAliasBackgroundColorRgbValue());
+
+         if (isSelected)
+         {
+            comp.setBackground(bg.darker());
+         }
+         else
+         {
+            comp.setBackground(bg);
+         }
+      }
+
 
       return comp;
 
