b5a373e0ece50b3d3664ca19aa273d143d1fcf34
gerdwagner <gerdhwagner@t-online.de>
2019-12-05 23:42:34 +0100

    ChangeTrack ... rendering changes in popup finished. --> Display of change tracking finished.

--- sql12/core/src/net/sourceforge/squirrel_sql/client/session/mainpanel/changetrack/matchpatch/ChangeRenderer.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/client/session/mainpanel/changetrack/matchpatch/ChangeRenderer.java
@@ -1,5 +1,7 @@
 package net.sourceforge.squirrel_sql.client.session.mainpanel.changetrack.matchpatch;
 
+import net.sourceforge.squirrel_sql.fw.util.Utilities;
+
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
@@ -9,6 +11,10 @@ import java.util.LinkedList;
 
 public class ChangeRenderer
 {
+
+   public static final Color BEFORE_INSERT_COLOR = new Color(204, 204, 255);
+   public static final Color AFTER_INSERT_COLOR = new Color(204, 255, 255);
+
    public static void renderChangeInTextPane(JTextPane txtFormerText, String sourceText, String targetText)
    {
       diff_match_patch dmp = new diff_match_patch();
@@ -23,35 +29,110 @@ public class ChangeRenderer
          switch (diff.operation)
          {
             case EQUAL:
-               print(txtFormerText, diff.text, txtFormerText.getForeground(), txtFormerText.getBackground(), false, false);
+               print(txtFormerText, diff.text, txtFormerText.getForeground(), txtFormerText.getBackground(), false, previousIsInsert(diffs, i), nextIsInsert(diffs, i));
                break;
 //            case INSERT:
 //               print(txtFormerText, diff.text, txtFormerText.getForeground(), txtFormerText.getBackground(), true);
 //               break;
             case DELETE:
-               print(txtFormerText, diff.text, txtFormerText.getForeground().darker().darker(), txtFormerText.getBackground(), true, false);
+               print(txtFormerText, diff.text, txtFormerText.getForeground().darker().darker(), txtFormerText.getBackground(), true, previousIsInsert(diffs, i), nextIsInsert(diffs, i));
                break;
          }
       }
 
    }
 
-   private static void print(JTextPane txtFormerText, String msg, Color foreground, Color background, boolean bold, boolean italic)
+   private static boolean nextIsInsert(LinkedList<diff_match_patch.Diff> diffs, int ix)
    {
-      System.out.println("msg = " + msg);
+      if (ix < diffs.size() - 1 && diffs.get(ix + 1).operation == diff_match_patch.Operation.INSERT)
+      {
+         return true;
+      }
+
+      return false;
+
+   }
+
+   private static boolean previousIsInsert(LinkedList<diff_match_patch.Diff> diffs, int ix)
+   {
+      if (ix > 0 && diffs.get(ix-1).operation == diff_match_patch.Operation.INSERT)
+      {
+         return true;
+      }
+
+      return false;
+   }
+
+   private static void print(JTextPane txtFormerText, String str, Color foreground, Color background, boolean bold, boolean previousIsInsert, boolean nextIsInsert)
+   {
 
       SimpleAttributeSet attributes = new SimpleAttributeSet(txtFormerText.getInputAttributes());
       StyleConstants.setForeground(attributes, foreground);
       StyleConstants.setBackground(attributes, background);
       StyleConstants.setBold(attributes, bold);
-      StyleConstants.setItalic(attributes, italic);
 
+      Color defaultBg = txtFormerText.getBackground();
+
+      if(previousIsInsert && nextIsInsert)
+      {
+         if (str.length() > 1)
+         {
+            StyleConstants.setBackground(attributes, AFTER_INSERT_COLOR);
+            insert(txtFormerText, str.substring(0, 1), attributes);
+
+            if (str.length() > 2)
+            {
+               StyleConstants.setBackground(attributes, defaultBg);
+               insert(txtFormerText, str.substring(1, str.length() - 1), attributes);
+            }
+
+            StyleConstants.setBackground(attributes, BEFORE_INSERT_COLOR);
+            insert(txtFormerText, str.substring(str.length() - 1), attributes);
+         }
+         else
+         {
+            insert(txtFormerText, str, attributes);
+         }
+      }
+      else if(false == previousIsInsert && nextIsInsert)
+      {
+         if (str.length() > 1)
+         {
+            StyleConstants.setBackground(attributes, defaultBg);
+            insert(txtFormerText, str.substring(0, str.length() - 1), attributes);
+         }
+
+         StyleConstants.setBackground(attributes, BEFORE_INSERT_COLOR);
+         insert(txtFormerText, str.substring(str.length() - 1), attributes);
+
+      }
+      else if(previousIsInsert && false == nextIsInsert)
+      {
+         StyleConstants.setBackground(attributes, AFTER_INSERT_COLOR);
+         insert(txtFormerText, str.substring(0, 1), attributes);
+
+         if (str.length() > 1)
+         {
+            StyleConstants.setBackground(attributes, defaultBg);
+            insert(txtFormerText, str.substring(1, str.length() - 1), attributes);
+         }
+      }
+      else
+      {
+         insert(txtFormerText, str, attributes);
+      }
+
+   }
+
+   private static void insert(JTextPane txtFormerText, String msg, SimpleAttributeSet attributes)
+   {
       try
       {
          txtFormerText.getStyledDocument().insertString(txtFormerText.getDocument().getLength(), msg, attributes);
       }
-      catch (BadLocationException ignored)
+      catch (BadLocationException e)
       {
+         throw Utilities.wrapRuntime(e);
       }
    }
 
