7a596a98073fa38dae413057606762c6c7b44892
gerdwagner <gerdhwagner@t-online.de>
2021-06-12 03:18:27 +0200

    Feature: Merge Request #76 Derby Plugin: Improved Derby shutdown handling for embedded DBs            Thanks to Stanimir Stamenkov for the merge Request.

--- sql12/plugins/derby/src/net/sourceforge/squirrel_sql/plugins/derby/DerbyPlugin.java
+++ sql12/plugins/derby/src/net/sourceforge/squirrel_sql/plugins/derby/DerbyPlugin.java
@@ -58,11 +58,17 @@ import net.sourceforge.squirrel_sql.plugins.derby.tab.ViewSourceTab;
 import net.sourceforge.squirrel_sql.plugins.derby.tokenizer.DerbyQueryTokenizer;
 import net.sourceforge.squirrel_sql.plugins.derby.types.DerbyClobDataTypeComponentFactory;
 
+import javax.swing.JOptionPane;
 import java.lang.ref.WeakReference;
-import java.net.MalformedURLException;
 import java.sql.Driver;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -71,6 +77,8 @@ import java.util.Properties;
  * @author manningr
  */
 public class DerbyPlugin extends DefaultSessionPlugin {
+
+   private static final String UNKNOWN_DBNAME = "<unknown>";
 
    private static final StringManager s_stringMgr = StringManagerFactory.getStringManager(DerbyPlugin.class);
 
@@ -92,12 +100,30 @@ public class DerbyPlugin extends DefaultSessionPlugin {
 
       //i18n[DerbyPlugin.prefsHint=Set preferences for Derby Plugin]
       String PREFS_HINT = s_stringMgr.getString("DerbyPlugin.prefsHint");
+
+      //i18n[DerbyPlugin.embeddedDerby.title=Embedded Derby]
+      String EMBEDDED_DERBY_TITLE = s_stringMgr.getString("DerbyPlugin.embeddedDerby.title");
+
+      //i18n[DerbyPlugin.embeddedDerby.inMemoryDropQuestion=Drop in-memory database: {0}?]
+      String DROP_DB_QUESTION = s_stringMgr.getString("DerbyPlugin.embeddedDerby.inMemoryDropQuestion");
+
+      //i18n[DerbyPlugin.embeddedDerby.inMemoryDropOption=Drop]
+      String DROP_OPTION = s_stringMgr.getString("DerbyPlugin.embeddedDerby.inMemoryDropOption");
+
+      //i18n[DerbyPlugin.embeddedDerby.inMemoryKeepOption=Keep]
+      String KEEP_OPTION = s_stringMgr.getString("DerbyPlugin.embeddedDerby.inMemoryKeepOption");
    }
 
    /** manages our query tokenizing preferences */
    private PluginQueryTokenizerPreferencesManager _prefsManager = null;
 
-   private ArrayList<WeakReference<ISession>> _embeddedSessions = new ArrayList<>();
+   private Map<String, List<WeakReference<ISession>>> _embeddedSessions = new HashMap<>();
+
+   // Reference to the JDBC driver so we could perform shutdown,
+   // even when session references have been cleared.
+   private volatile ISQLDriver _embeddedDriver;
+
+   private volatile boolean _appShuttingDown;
 
    /**
     * Return the internal name of this plugin.
@@ -170,8 +196,10 @@ public class DerbyPlugin extends DefaultSessionPlugin {
     *
     * @param	app	 Application API.
     */
-   public synchronized void load(IApplication app) throws PluginException {
+   public synchronized void load(IApplication app) throws PluginException
+   {
       super.load(app);
+      app.addApplicationListener(() -> _appShuttingDown = true);
    }
 
    /**
@@ -249,7 +277,10 @@ public class DerbyPlugin extends DefaultSessionPlugin {
 
       if (isEmbeddedDerby(session))
       {
-         _embeddedSessions.add(new WeakReference<>(session));
+         _embeddedSessions.computeIfAbsent(getDatabaseName(session), k -> new ArrayList<>())
+                          .add(new WeakReference<>(session));
+
+         _embeddedDriver = session.getDriver();
       }
 
       return new PluginSessionCallback()
@@ -273,8 +304,7 @@ public class DerbyPlugin extends DefaultSessionPlugin {
          @Override
          public void objectTreeInSQLTabOpened(ObjectTreePanel objectTreePanel)
          {
-            ObjectTreeInternalFrame objectTreeInternalFrame = null;
-            updateTreeApi(objectTreeInternalFrame.getObjectTreeAPI());
+            updateTreeApi(objectTreePanel);
          }
       };
    }
@@ -323,44 +353,126 @@ public class DerbyPlugin extends DefaultSessionPlugin {
       @Override
       public void sessionClosed(SessionEvent evt)
       {
-         ISession session = evt.getSession();
          if (flushEmbeddedSessions())
          {
-            shutdownEmbeddedDerby(session);
+            shutdownEmbeddedDerby();
          }
       }
    }
 
-   boolean flushEmbeddedSessions()
+   private boolean shouldDrop(String dbName)
    {
-      for (int i = _embeddedSessions.size() - 1; i >= 0; i--)
+      if (_appShuttingDown)
       {
-         ISession session = _embeddedSessions.get(i).get();
-         if (session == null || session.isClosed())
+         return true;
+      }
+
+      int option = JOptionPane.showOptionDialog(null, // Application-modal
+            MessageFormat.format(i18n.DROP_DB_QUESTION, dbName.replaceFirst("^memory:", "")),
+            i18n.EMBEDDED_DERBY_TITLE, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
+            null, new Object[] { i18n.DROP_OPTION, i18n.KEEP_OPTION }, i18n.KEEP_OPTION);
+      return option == JOptionPane.YES_OPTION;
+   }
+
+   private boolean flushEmbeddedSessions()
+   {
+      if (_embeddedSessions.isEmpty())
+      {
+         return false;
+      }
+
+      for (Iterator<Map.Entry<String, List<WeakReference<ISession>>>> iter = _embeddedSessions.entrySet().iterator(); iter.hasNext(); )
+      {
+         Map.Entry<String, List<WeakReference<ISession>>> entry = iter.next();
+         if (flushEmbeddedSessions(entry.getValue()))
          {
-            _embeddedSessions.remove(i);
+            String dbName = entry.getKey();
+            if (dbName.startsWith("memory:"))
+            {
+               if (shouldDrop(dbName))
+               {
+                  shutdownEmbeddedDerby(dbName, true);
+                  iter.remove();
+               }
+            }
+            else if (!dbName.equals(UNKNOWN_DBNAME))
+            {
+               shutdownEmbeddedDerby(dbName);
+               iter.remove();
+            }
          }
       }
       return _embeddedSessions.isEmpty();
    }
-   
+
+   private boolean flushEmbeddedSessions(List<WeakReference<ISession>> list)
+   {
+      if (list.isEmpty())
+      {
+         return false;
+      }
+
+      Iterator<WeakReference<ISession>> iter = list.iterator();
+      while (iter.hasNext())
+      {
+         ISession session = iter.next().get();
+         if (session == null || session.isClosed())
+         {
+            iter.remove();
+         }
+      }
+      return list.isEmpty();
+   }
+
    static boolean isEmbeddedDerby(final ISession session)
    {
       return session.getDriver().getDriverClassName().startsWith("org.apache.derby.jdbc.EmbeddedDriver");
    }
 
+   private static String getDatabaseName(ISession session)
+   {
+      try
+      {
+         String jdbcURL = session.getMetaData().getURL();
+         int semIdx = jdbcURL.indexOf(';');
+         return (semIdx > 0) ? jdbcURL.substring("jdbc:derby:".length(), semIdx)
+                             : jdbcURL.substring("jdbc:derby:".length());
+      }
+      catch (SQLException e)
+      {
+         s_log.warn("Could not determine Embedded Derby database name", e);
+         return UNKNOWN_DBNAME;
+      }
+   }
+
    /**
     * Shutdown Embedded Derby DB and reload JDBC Driver
     * 
-    * @param session
-    *           Current session.
-    * 
     * @author Alex Pivovarov
     */
-   private void shutdownEmbeddedDerby(final ISession session) {
-      try {
-         ISQLDriver iSqlDr = session.getDriver();
-         if (!isEmbeddedDerby(session)) {
+   private void shutdownEmbeddedDerby()
+   {
+      shutdownEmbeddedDerby("");
+   }
+
+   /**
+    * Shutdown a single Embedded Derby database.
+    *
+    * @param  dbName  the database name to shut down;
+    */
+   private void shutdownEmbeddedDerby(String dbName)
+   {
+      shutdownEmbeddedDerby(dbName, false);
+   }
+
+   private void shutdownEmbeddedDerby(String dbName, boolean drop)
+   {
+      try
+      {
+         ISQLDriver iSqlDr = _embeddedDriver;
+         if (_embeddedDriver == null)
+         {
+            s_log.warn("shutdownEmbeddedDerby: driver reference is null");
             return;
          }
          //the code bellow is only for Embedded Derby Driver
@@ -369,25 +481,34 @@ public class DerbyPlugin extends DefaultSessionPlugin {
          //Getting java.sql.Driver to run shutdown command
          Driver jdbcDr = sqlDrMan.getJDBCDriver(drId);
          //Shutdown Embedded Derby DB
-         try {
-            jdbcDr.connect("jdbc:derby:;shutdown=true", new Properties());
-         } catch (SQLException e) {
+         try
+         {
+            jdbcDr.connect("jdbc:derby:" + dbName + ";"
+                                 + (drop ? "drop" : "shutdown") + "=true", new Properties());
+         }
+         catch (SQLException e)
+         {
             //it is always thrown as said in Embedded Derby API.
             //So it is not error it is info
-            s_log.info(e.getMessage());
+            if (Arrays.asList("08006", "XJ015").contains(e.getSQLState()))
+            {
+               _app.getMessageHandler().showMessage(e.getMessage());
+            }
+            else
+            {
+               _app.getMessageHandler().showErrorMessage(e);
+            }
          }
          //Re-registering driver is necessary for Embedded Derby
-         sqlDrMan.registerSQLDriver(iSqlDr);
-      } catch (RuntimeException e) {
-         s_log.error(e.getMessage(),e);
-      } catch (MalformedURLException e) {
-         s_log.error(e.getMessage(),e);
-      } catch (IllegalAccessException e) {
-         s_log.error(e.getMessage(),e);
-      } catch (InstantiationException e) {
-         s_log.error(e.getMessage(),e);
-      } catch (ClassNotFoundException e) {
-         s_log.error(e.getMessage(),e);
+         if (dbName.isEmpty())
+         {
+            sqlDrMan.registerSQLDriver(iSqlDr);
+            _embeddedDriver = null;
+         }
+      }
+      catch (Exception e)
+      {
+         s_log.error(e.getMessage(), e);
       }
    }
 
