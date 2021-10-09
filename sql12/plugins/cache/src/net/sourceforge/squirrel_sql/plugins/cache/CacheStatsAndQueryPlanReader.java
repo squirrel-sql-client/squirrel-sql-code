b53fbe44b247216534cb930e6790acf4dc42e7a8
GWA <gwa@shd.de>
2021-10-09 02:27:58 +0200

    Intersystem Cache Plugin --> added background execution

---
+++ sql12/plugins/cache/src/net/sourceforge/squirrel_sql/plugins/cache/CacheStatsAndQueryPlanReader.java
@@ -0,0 +1,134 @@
+package net.sourceforge.squirrel_sql.plugins.cache;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+import net.sourceforge.squirrel_sql.client.Main;
+import net.sourceforge.squirrel_sql.fw.datasetviewer.textdataset.DataSetTextArea;
+import net.sourceforge.squirrel_sql.fw.gui.GUIUtils;
+import net.sourceforge.squirrel_sql.fw.util.StringManager;
+import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
+import net.sourceforge.squirrel_sql.fw.util.Utilities;
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
+
+public class CacheStatsAndQueryPlanReader implements Runnable
+{
+   private static final StringManager s_stringMgr = StringManagerFactory.getStringManager(CacheStatsAndQueryPlanReader.class);
+
+   private static final ILogger s_log = LoggerController.createLogger(CacheStatsAndQueryPlanReader.class);
+
+   private final String _cleanedSQL;
+   private final DataSetTextArea _dataSetTextArea;
+   private Connection _con;
+   private JScrollPane _scrollPane;
+   private volatile boolean _disposed;
+
+   public CacheStatsAndQueryPlanReader(String cleanedSQL, Connection con, DataSetTextArea dataSetTextArea, JScrollPane scrollPane)
+   {
+      _cleanedSQL = cleanedSQL;
+      _con = con;
+      _dataSetTextArea = dataSetTextArea;
+      _scrollPane = scrollPane;
+   }
+
+   @Override
+   public void run()
+   {
+      readCacheStatsAndQueryPlan();
+   }
+
+   private void readCacheStatsAndQueryPlan()
+   {
+      try(PreparedStatement pStat = _con.prepareStatement("SELECT DBUtilities.GetPlan('" + getNameSpaceName(_con) + "', ?)"))
+      {
+         pStat.setString(1, _cleanedSQL);
+         ResultSet res = pStat.executeQuery();
+
+         if(_disposed)
+         {
+            return;
+         }
+
+         if(false == res.next())
+         {
+            if(_disposed)
+            {
+               return;
+            }
+
+            String errMsg = s_stringMgr.getString("StatisticsAndQueryPlanAction.no.result");
+            Main.getApplication().getMessageHandler().showErrorMessage(errMsg);
+            s_log.error(new IllegalStateException(errMsg));
+
+            displayText(errMsg);
+         }
+         else
+         {
+            if(_disposed)
+            {
+               return;
+            }
+            String statisticAndQueryPlan = res.getString(1);
+
+            if(_disposed)
+            {
+               return;
+            }
+
+            displayText(statisticAndQueryPlan);
+         }
+
+      }
+      catch(Exception e)
+      {
+         if(_disposed)
+         {
+            return;
+         }
+
+         String errMsg = e.toString();
+         Main.getApplication().getMessageHandler().showErrorMessage(errMsg);
+         s_log.error(e);
+      }
+
+   }
+
+   private void displayText(String statisticAndQueryPlan)
+   {
+      SwingUtilities.invokeLater(() -> _dataSetTextArea.setText(statisticAndQueryPlan));
+      SwingUtilities.invokeLater(() -> GUIUtils.forceScrollToBegin(_scrollPane));
+   }
+
+   private String getNameSpaceName(Connection con)
+   {
+      try
+      {
+         String url = con.getMetaData().getURL();
+
+         String[] splits = url.split("/");
+
+         if(splits.length < 4)
+         {
+            throw new IllegalStateException("Expected 4th split(\"/\") of " + url + " to be the Intersystems Cache name space");
+         }
+
+         String nameSpace = splits[3];
+
+         return nameSpace;
+      }
+      catch(SQLException e)
+      {
+         throw Utilities.wrapRuntime(e);
+      }
+   }
+
+   public void setDisposed()
+   {
+      _disposed = true;
+   }
+}
--- sql12/plugins/cache/src/net/sourceforge/squirrel_sql/plugins/cache/StatisticsAndQueryPlanAction.java
+++ sql12/plugins/cache/src/net/sourceforge/squirrel_sql/plugins/cache/StatisticsAndQueryPlanAction.java
@@ -4,9 +4,8 @@ import java.awt.GridLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.swing.ImageIcon;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
@@ -22,19 +21,27 @@ import net.sourceforge.squirrel_sql.fw.util.StringManager;
 import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 import net.sourceforge.squirrel_sql.fw.util.StringUtilities;
 import net.sourceforge.squirrel_sql.fw.util.Utilities;
+import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
+import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
 
 
 public class StatisticsAndQueryPlanAction extends SquirrelAction implements ISQLPanelAction
 {
    private static final StringManager s_stringMgr = StringManagerFactory.getStringManager(StatisticsAndQueryPlanAction.class);
 
+   private static final ILogger s_log = LoggerController.createLogger(StatisticsAndQueryPlanAction.class);
+
+   private final ExecutorService _executorService = Executors.newCachedThreadPool();
+
    private ISQLPanelAPI _sqlPanelAPI;
    private CachePluginResources _resources;
+
 
    public StatisticsAndQueryPlanAction(CachePluginResources resources)
    {
       super(Main.getApplication(), resources);
       _resources = resources;
+
    }
 
    public void actionPerformed(ActionEvent e)
@@ -56,63 +63,29 @@ public class StatisticsAndQueryPlanAction extends SquirrelAction implements ISQL
          // Needed to remove comments
          IQueryTokenizer queryTokenizer = _sqlPanelAPI.getSession().getQueryTokenizer();
          queryTokenizer.setScriptToTokenize(sql);
-
-         Connection con = _sqlPanelAPI.getSession().getSQLConnection().getConnection();
-         PreparedStatement pStat = con.prepareStatement("SELECT DBUtilities.GetPlan('" + getNameSpaceName(con) + "', ?)");
-         pStat.setString(1, queryTokenizer.nextQuery().getQuery());
-
-         ResultSet res = pStat.executeQuery();
-         if(false == res.next())
-         {
-            Main.getApplication().getMessageHandler().showWarningMessage(s_stringMgr.getString("StatisticsAndQueryPlanAction.no.result"));
-            return;
-         }
-
-         String statisticAndQueryPlan = res.getString(1);
+         String cleanedSQL = queryTokenizer.nextQuery().getQuery();
 
          CustomResultPanel resultPanel = new CustomResultPanel(new GridLayout(1,1));
-         resultPanel.setDisposeListener(() -> onDispose());
-
-         DataSetTextArea dataSetTextArea = new DataSetTextArea(statisticAndQueryPlan);
-         resultPanel.add(new JScrollPane(dataSetTextArea));
-         SwingUtilities.invokeLater(() -> dataSetTextArea.scrollRectToVisible(new Rectangle(0, 0)));
-
 
          ImageIcon icon = _resources.getIcon(CachePluginResources.IKeys.INTERSYSTEMS_CAC);
          _sqlPanelAPI.getSQLResultExecuter().addCustomResult(resultPanel, s_stringMgr.getString("StatisticsAndQueryPlanAction.tab.title"), icon);
+
+         DataSetTextArea dataSetTextArea = new DataSetTextArea(s_stringMgr.getString("StatisticsAndQueryPlanAction.reading.stats.for.sql", cleanedSQL));
+
+         JScrollPane scrollPane = new JScrollPane(dataSetTextArea);
+         resultPanel.add(scrollPane);
+         SwingUtilities.invokeLater(() -> dataSetTextArea.scrollRectToVisible(new Rectangle(0, 0)));
+
+         Connection con = _sqlPanelAPI.getSession().getSQLConnection().getConnection();
+         CacheStatsAndQueryPlanReader readerTask = new CacheStatsAndQueryPlanReader(cleanedSQL, con, dataSetTextArea, scrollPane);
+         _executorService.submit(readerTask);
+
+         resultPanel.setDisposeListener(() -> readerTask.setDisposed());
       }
       catch(Exception ex)
       {
          Main.getApplication().getMessageHandler().showErrorMessage(s_stringMgr.getString("StatisticsAndQueryPlanAction.failed.to.query.statistics", ex));
          throw Utilities.wrapRuntime(ex);
-      }
-   }
-
-   private void onDispose()
-   {
-      System.out.println("StatisticsAndQueryPlanAction.onDispose");
-   }
-
-   private String getNameSpaceName(Connection con)
-   {
-      try
-      {
-         String url = con.getMetaData().getURL();
-
-         String[] splits = url.split("/");
-
-         if(splits.length < 4)
-         {
-            throw new IllegalStateException("Expected 4th split(\"/\") of " + url + " to be the Intersystems Cache name space");
-         }
-
-         String nameSpace = splits[3];
-
-         return nameSpace;
-      }
-      catch(SQLException e)
-      {
-         throw Utilities.wrapRuntime(e);
       }
    }
 
