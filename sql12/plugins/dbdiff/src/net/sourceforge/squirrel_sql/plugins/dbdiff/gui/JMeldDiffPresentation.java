804574cbdc832fa2201699e35592a14898995d50
gerdwagner <gerdhwagner@t-online.de>
2023-03-20 20:01:51 +0100

    Feature: Data compare:  UI-Tables offer a new right mouse menu "Compare selected cells".  Note that the function compares table cell selections. To compare complete tables use the Ctrl+a shortcut.  This feature includes improvements and bug fixes for the DBDiff Plugin.  --> Ignore white spaces checkbox

--- sql12/plugins/dbdiff/src/net/sourceforge/squirrel_sql/plugins/dbdiff/gui/JMeldDiffPresentation.java
+++ sql12/plugins/dbdiff/src/net/sourceforge/squirrel_sql/plugins/dbdiff/gui/JMeldDiffPresentation.java
@@ -19,17 +19,24 @@
 
 package net.sourceforge.squirrel_sql.plugins.dbdiff.gui;
 
-import java.awt.Font;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-
 import net.sourceforge.squirrel_sql.client.Main;
 import net.sourceforge.squirrel_sql.fw.gui.GUIUtils;
+import net.sourceforge.squirrel_sql.fw.props.Props;
 import net.sourceforge.squirrel_sql.fw.util.StringManager;
 import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 import org.jmeld.settings.EditorSettings;
 import org.jmeld.settings.JMeldSettings;
 import org.jmeld.ui.JMeldPanel;
+
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 
 /**
  * A DiffPresentation that uses components from the JMeld project to render a comparison of the content of two
@@ -37,7 +44,11 @@ import org.jmeld.ui.JMeldPanel;
  */
 public class JMeldDiffPresentation extends AbstractSideBySideDiffPresentation
 {
+	public static final String PREF_IGNORE_WHITE_SPACES = "JMeldDiffPresentation.PREF_IGNORE_WHITE_SPACES";
+
 	private static final StringManager s_stringMgr = StringManagerFactory.getStringManager(JMeldDiffPresentation.class);
+	private JCheckBox _chkIgnoreWhiteSpaces;
+	private JMeldPanel _meldPanel;
 
 	/**
 	 * @see net.sourceforge.squirrel_sql.plugins.dbdiff.gui.AbstractSideBySideDiffPresentation#
@@ -56,11 +67,14 @@ public class JMeldDiffPresentation extends AbstractSideBySideDiffPresentation
 
 		JDialog diffDialog = new JDialog(Main.getApplication().getMainFrame(), diffDialogTitle);
 
-		final JMeldPanel meldPanel = new NonExitingJMeldPanel(() -> close(diffDialog));
-		meldPanel.SHOW_TABBEDPANE_OPTION.disable();
-		meldPanel.SHOW_TOOLBAR_OPTION.disable();
+		JPanel pnl = createPanel(diffDialog);
+		diffDialog.getContentPane().setLayout(new GridLayout(1,1));
+		diffDialog.add(pnl);
 
-		diffDialog.add(meldPanel);
+		_chkIgnoreWhiteSpaces.setSelected(Props.getBoolean(PREF_IGNORE_WHITE_SPACES, false));
+		onIgnoreWhiteSpaces(_meldPanel);
+		_chkIgnoreWhiteSpaces.addActionListener(e -> onIgnoreWhiteSpaces(_meldPanel));
+
 
 		GUIUtils.enableCloseByEscape(diffDialog);
 		GUIUtils.initLocation(diffDialog, 500, 400, JMeldDiffPresentation.class.getName());
@@ -69,7 +83,41 @@ public class JMeldDiffPresentation extends AbstractSideBySideDiffPresentation
 
 		JMeldSettings.getInstance().getEditor().enableCustomFont(true);
 		JMeldSettings.getInstance().getEditor().setFont(new Font(Font.MONOSPACED, Font.PLAIN, new JLabel().getFont().getSize()));
-		meldPanel.openComparison(script1Filename, script2Filename);
+		_meldPanel.openComparison(script1Filename, script2Filename);
+	}
+
+	private JPanel createPanel(JDialog diffDialog)
+	{
+		JPanel ret = new JPanel(new GridBagLayout());
+
+		GridBagConstraints gbc;
+
+		gbc = new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5,5,5,5), 0,0);
+		_chkIgnoreWhiteSpaces = new JCheckBox(s_stringMgr.getString("JMeldDiffPresentation.ignore.white.spaces"));
+		ret.add(_chkIgnoreWhiteSpaces, gbc);
+
+		_meldPanel = new NonExitingJMeldPanel(() -> close(diffDialog));
+		_meldPanel.SHOW_TABBEDPANE_OPTION.disable();
+		_meldPanel.SHOW_TOOLBAR_OPTION.disable();
+		gbc = new GridBagConstraints(0,1,1,1,1,1,GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0,0);
+		ret.add(_meldPanel, gbc);
+
+		return ret;
+	}
+
+	private void onIgnoreWhiteSpaces(JMeldPanel meldPanel)
+	{
+		JMeldSettings.getInstance().getEditor().setIgnoreBlankLines(_chkIgnoreWhiteSpaces.isSelected());
+		JMeldSettings.getInstance().getEditor().setIgnoreWhitespaceAtBegin(_chkIgnoreWhiteSpaces.isSelected());
+		JMeldSettings.getInstance().getEditor().setIgnoreWhitespaceAtEnd(_chkIgnoreWhiteSpaces.isSelected());
+		JMeldSettings.getInstance().getEditor().setIgnoreWhitespaceInBetween(_chkIgnoreWhiteSpaces.isSelected());
+
+		//meldPanel.doRefresh(new ActionEvent("SQuirreL", 0, "onIgnoreWhiteSpaces"));
+		JMeldPanel.getContentPanelList(meldPanel.getTabbedPane()).forEach(p -> p.doRefresh());
+
+		meldPanel.revalidate();
+
+		Props.putBoolean(PREF_IGNORE_WHITE_SPACES, _chkIgnoreWhiteSpaces.isSelected());
 	}
 
 	private void close(JDialog diffDialog)
