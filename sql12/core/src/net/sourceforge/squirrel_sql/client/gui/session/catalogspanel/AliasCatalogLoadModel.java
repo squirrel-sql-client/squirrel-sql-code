3d1ba737f9df1873cb1d68f179ca818da28260b8
gerdwagner <gerdhwagner@t-online.de>
2023-08-26 02:21:50 +0200

    Multiple catalogs choice (GitHub issue 17): First version of AliasCatalogLoadModel which now is central to specifying the catalog for schema loading.

--- sql12/core/src/net/sourceforge/squirrel_sql/client/gui/session/catalogspanel/AliasCatalogLoadModel.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/client/gui/session/catalogspanel/AliasCatalogLoadModel.java
@@ -3,15 +3,18 @@ package net.sourceforge.squirrel_sql.client.gui.session.catalogspanel;
 import net.sourceforge.squirrel_sql.client.session.ISession;
 import net.sourceforge.squirrel_sql.client.session.schemainfo.SchemaInfo;
 import net.sourceforge.squirrel_sql.fw.dialects.DialectFactory;
+import net.sourceforge.squirrel_sql.fw.util.Utilities;
 import net.sourceforge.squirrel_sql.fw.util.log.ILogger;
 import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AliasCatalogLoadModel
 {
    private static final ILogger s_log = LoggerController.createLogger(AliasCatalogLoadModel.class);
+   private List<String> _catalogNamesToLoad;
 
    private AliasCatalogLoadModelJsonBean _modelJson;
 
@@ -22,31 +25,71 @@ public class AliasCatalogLoadModel
     */
    private String _catalogRepresentingDefaultLoad;
 
+   /**
+    * To be used by {@link #of(String)} only.
+    */
+   private AliasCatalogLoadModel()
+   {
+   }
+
    public AliasCatalogLoadModel(AliasCatalogLoadModelJsonBean modelJson, ISession session)
    {
-      _modelJson = modelJson;
-
-      _catalogRepresentingDefaultLoad = null;
-      if(DialectFactory.isMSSQLServer(session.getMetaData()))
+      try
       {
-         // This code is a workaround for bug #1508 (Sourceforge):
-         // After switching catalogs using the catalogs combobox (see CatalogsComboListener.actionPerformed())
-         // the MSSQL-Server JDBC driver (https://github.com/microsoft/mssql-jdbc)
-         // throws a "SQLServerException: The prepared statement handle 1 is not valid in this context ..."
-         // when SchemaInfo is reloaded.
-         // The problem doesn't occur, when the reloading is done with the current catalog specified.
-         //
-         // Note: Without switching catalog the MSSQL-Server JDBC driver loads SchemaInfo fine with catalog = null, too.
-         // It then loads the current catalog's objects. So setting the current catalog here makes no difference except from
-         // preventing the JDBC driver's bug.
-         try
+         if(false == session.getMetaData().supportsCatalogs())
          {
-            _catalogRepresentingDefaultLoad = session.getSQLConnection().getCatalog();
+            _catalogNamesToLoad = new ArrayList<>();
+            // Takes care null is passed to the catalog parameter of the java.sql.DatabaseMetaData methods.
+            _catalogNamesToLoad.add(null);
+            return;
          }
-         catch (Exception e)
+
+         _modelJson = modelJson;
+
+         _catalogRepresentingDefaultLoad = null;
+         if(DialectFactory.isMSSQLServer(session.getMetaData()))
          {
-            s_log.error("Error getting the current catalog from MSSQL-Server to workaround Sourceforge bug #1508.", e);
+            // This code is a workaround for bug #1508 (Sourceforge):
+            // After switching catalogs using the catalogs combobox (see CatalogsComboListener.actionPerformed())
+            // the MSSQL-Server JDBC driver (https://github.com/microsoft/mssql-jdbc)
+            // throws a "SQLServerException: The prepared statement handle 1 is not valid in this context ..."
+            // when SchemaInfo is reloaded.
+            // The problem doesn't occur, when the reloading is done with the current catalog specified.
+            //
+            // Note: Without switching catalog the MSSQL-Server JDBC driver loads SchemaInfo fine with catalog = null, too.
+            // It then loads the current catalog's objects. So setting the current catalog here makes no difference except from
+            // preventing the JDBC driver's bug.
+            try
+            {
+               _catalogRepresentingDefaultLoad = session.getSQLConnection().getCatalog();
+            }
+            catch (Exception e)
+            {
+               s_log.error("Error getting the current catalog from MSSQL-Server to workaround Sourceforge bug #1508.", e);
+            }
          }
+
+         _catalogNamesToLoad = new ArrayList<>();
+         _catalogNamesToLoad.add(_catalogRepresentingDefaultLoad);
+
+         for (String catalog : _modelJson.getAdditionalUserChosenCatalogs())
+         {
+            if (false == _catalogNamesToLoad.contains(catalog))
+            {
+               _catalogNamesToLoad.add(catalog);
+            }
+         }
+
+         // If null is contained we assume this will load the connection's current dialog.
+         // That's why we remove the connection's current dialog here.
+         if (_catalogNamesToLoad.contains(null) && null != session.getSQLConnection().getCatalog())
+         {
+            _catalogNamesToLoad.remove(session.getSQLConnection().getCatalog());
+         }
+      }
+      catch (SQLException e)
+      {
+         throw Utilities.wrapRuntime(e);
       }
    }
 
@@ -56,11 +99,7 @@ public class AliasCatalogLoadModel
     */
    public List<String> getCatalogStringsToLoad()
    {
-      ArrayList<String> ret = new ArrayList<>();
-      ret.add(_catalogRepresentingDefaultLoad);
-      ret.addAll(_modelJson.getAdditionalUserChosenCatalogs());
-
-      return ret;
+      return _catalogNamesToLoad;
    }
 
    /**
@@ -69,8 +108,8 @@ public class AliasCatalogLoadModel
     */
    public static AliasCatalogLoadModel of(String catalogName)
    {
-      AliasCatalogLoadModel ret = new AliasCatalogLoadModel(new AliasCatalogLoadModelJsonBean(), null);
-      ret._catalogRepresentingDefaultLoad = catalogName;
+      AliasCatalogLoadModel ret = new AliasCatalogLoadModel();
+      ret._catalogNamesToLoad = List.of(catalogName);
       return ret;
    }
 }
