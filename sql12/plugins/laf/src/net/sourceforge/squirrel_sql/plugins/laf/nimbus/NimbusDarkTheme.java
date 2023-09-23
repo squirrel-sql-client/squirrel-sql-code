340cc5a956006a6abf73c5f8d7d7943ee66ab605
gerdwagner <gerdhwagner@t-online.de>
2023-09-24 00:30:33 +0200

    Bug fix: Look and Feel Plugin / Nimbus look and feel:   Merged pull request https://github.com/squirrel-sql-client/squirrel-sql-code/pull/21   Fix for open/close icon's of trees for Nimbus dark mode.   Thanks to Wayne Zhang for the pull request.

--- sql12/plugins/laf/src/net/sourceforge/squirrel_sql/plugins/laf/nimbus/NimbusDarkTheme.java
+++ sql12/plugins/laf/src/net/sourceforge/squirrel_sql/plugins/laf/nimbus/NimbusDarkTheme.java
@@ -6,6 +6,7 @@ import net.sourceforge.squirrel_sql.fw.util.log.LoggerController;
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
+import java.net.URL;
 
 /**
  * Encapsulate Nimbus dark theme configuration logic
@@ -142,16 +143,17 @@ public class NimbusDarkTheme
       UIManager.getDefaults().remove("Tree.collapsedIcon");
       UIManager.getDefaults().remove("Tree.expandedIcon");
 
+      // open/close icon image files in the Java package are for dark mode only
       UIManager.put("Tree.collapsedIcon", loadImage("close.gif"));
       UIManager.put("Tree.expandedIcon", loadImage("open.gif"));
    }
 
    private static ImageIcon loadImage(String file)
    {
-      //URL fileUrl = NimbusDarkTheme.class.getResource(file);
-      //
-      //return new ImageIcon(fileUrl);
-
-      return new ImageIcon(file);
+      // load icon/image files that in the same package of this class,
+      // by Java classpath resource.
+      URL fileUrl = NimbusDarkTheme.class.getResource(file);
+      
+      return new ImageIcon(fileUrl);
    }
 }
