77321410a6562e66a96444692a8c8aa0a8b0a999
gerdwagner <gerdhwagner@t-online.de>
2024-05-09 01:07:41 +0200

    SavedSessionsGroup --> Group icon in SavedSessionMoreDialog

--- sql12/core/src/net/sourceforge/squirrel_sql/client/session/action/savedsession/SavedSessionGroupedListCellRenderer.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/client/session/action/savedsession/SavedSessionGroupedListCellRenderer.java
@@ -1,5 +1,7 @@
 package net.sourceforge.squirrel_sql.client.session.action.savedsession;
 
+import net.sourceforge.squirrel_sql.client.Main;
+import net.sourceforge.squirrel_sql.client.resources.SquirrelResources;
 import net.sourceforge.squirrel_sql.client.session.action.savedsession.savedsessionsgroup.SavedSessionGrouped;
 import net.sourceforge.squirrel_sql.fw.util.StringManager;
 import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
@@ -19,21 +21,30 @@ public class SavedSessionGroupedListCellRenderer implements ListCellRenderer<Sav
          return null;
       }
 
-      JTextArea comp = new JTextArea(SavedSessionUtil.getDisplayString(value));
-      comp.setEditable(false);
+      JTextField vanillaTextField = new JTextField();
 
-      if(isSelected)
+      JTextArea textArea = new JTextArea(SavedSessionUtil.getDisplayString(value));
+      textArea.setEditable(false);
+      textArea.setBackground(isSelected ? vanillaTextField.getSelectionColor() : vanillaTextField.getBackground());
+
+      JComponent ret = textArea;
+
+      if(value.isGroup())
       {
-         comp.setBackground(new JTextField().getSelectionColor());
+         ret = new JPanel(new BorderLayout());
+         ret.add(textArea, BorderLayout.CENTER);
+         JLabel lblGroupIcon = new JLabel(Main.getApplication().getResources().getIcon(SquirrelResources.IImageNames.SESSION_GROUP_SAVE));
+         lblGroupIcon.setBackground(isSelected ? vanillaTextField.getSelectionColor() : vanillaTextField.getBackground());
+         ret.setBackground(isSelected ? vanillaTextField.getSelectionColor() : vanillaTextField.getBackground());
+         ret.add(lblGroupIcon, BorderLayout.WEST);
       }
 
       if(cellHasFocus)
       {
-         comp.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+         ret.setBorder(BorderFactory.createLineBorder(Color.GRAY));
       }
 
-      return comp;
-
+      return ret;
    }
 
 }
