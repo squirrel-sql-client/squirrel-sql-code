c7f9d98526f73a8fb73a6e520d1b3d57c1ee1782
gerdwagner <gerdhwagner@t-online.de>
2024-06-28 00:31:28 +0200

    Image display (Github issue #34): Refactoring

--- sql12/core/src/net/sourceforge/squirrel_sql/fw/datasetviewer/celldatapopup/CellDataColumnDataPopupPanel.java
+++ sql12/core/src/net/sourceforge/squirrel_sql/fw/datasetviewer/celldatapopup/CellDataColumnDataPopupPanel.java
@@ -5,20 +5,14 @@ import net.sourceforge.squirrel_sql.fw.datasetviewer.cellcomponent.CellComponent
 import net.sourceforge.squirrel_sql.fw.util.StringManager;
 import net.sourceforge.squirrel_sql.fw.util.StringManagerFactory;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTable;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 
 class CellDataColumnDataPopupPanel extends JPanel
 {
    private static final StringManager s_stringMgr = StringManagerFactory.getStringManager(CellDataColumnDataPopupPanel.class);
 
-   private final PopupEditableIOPanel ioPanel;
+   private final PopupEditableIOPanel _ioPanel;
    private JDialog _parentFrame = null;
    private int _row;
    private int _col;
@@ -28,12 +22,11 @@ class CellDataColumnDataPopupPanel extends JPanel
    {
       super(new BorderLayout());
 
-      if (tableIsEditable &&
-            CellComponentFactory.isEditableInPopup(colDef, cellContents))
+      if (tableIsEditable && CellComponentFactory.isEditableInPopup(colDef, cellContents))
       {
 
          // data is editable in popup
-         ioPanel = new PopupEditableIOPanel(colDef, cellContents, true);
+         _ioPanel = new PopupEditableIOPanel(colDef, cellContents, true);
 
          // Since data is editable, we need to add control panel
          // to manage user requests for DB update, file IO, etc.
@@ -43,11 +36,9 @@ class CellDataColumnDataPopupPanel extends JPanel
       else
       {
          // data is not editable in popup
-         ioPanel = new PopupEditableIOPanel(colDef, cellContents, false);
+         _ioPanel = new PopupEditableIOPanel(colDef, cellContents, false);
       }
-
-      add(ioPanel, BorderLayout.CENTER);
-
+      add(_ioPanel, BorderLayout.CENTER);
    }
 
    /**
@@ -61,57 +52,13 @@ class CellDataColumnDataPopupPanel extends JPanel
       // create update/cancel controls using default layout
       JPanel updateControls = new JPanel();
 
-      // set up Update button
-      // i18n[cellDataPopUp.updateData=Update Data]
       JButton updateButton = new JButton(s_stringMgr.getString("cellDataPopUp.updateData"));
-      updateButton.addActionListener(new ActionListener()
-      {
-         public void actionPerformed(ActionEvent event)
-         {
-
-            // try to convert the text in the popup into a valid
-            // instance of type of data object being held in the table cell
-            StringBuffer messageBuffer = new StringBuffer();
-            Object newValue = CellDataColumnDataPopupPanel.this.ioPanel.getObject(messageBuffer);
-            if (messageBuffer.length() > 0)
-            {
-               // handle an error in conversion of text to object
-
-               // i18n[cellDataPopUp.cannnotBGeConverted=The given text cannot be converted into the internal object.\n
-               //Please change the data or cancel editing.\n
-               //The conversion error was:\n{0}]
-               String msg = s_stringMgr.getString("cellDataPopUp.cannnotBGeConverted", messageBuffer);
-
-               JOptionPane.showMessageDialog(
-                     CellDataColumnDataPopupPanel.this,
-                     msg,
-                     // i18n[cellDataPopUp.conversionError=Conversion Error]
-                     s_stringMgr.getString("cellDataPopUp.conversionError"),
-                     JOptionPane.ERROR_MESSAGE);
-
-               CellDataColumnDataPopupPanel.this.ioPanel.requestFocus();
-
-            }
-            else
-            {
-               _table.setValueAt(newValue, _row, _col);
-               CellDataColumnDataPopupPanel.this._parentFrame.setVisible(false);
-               CellDataColumnDataPopupPanel.this._parentFrame.dispose();
-            }
-         }
-      });
+      updateButton.addActionListener(event -> onUpdate());
 
       // set up Cancel button
       // i18n[cellDataPopup.cancel=Cancel]
       JButton cancelButton = new JButton(s_stringMgr.getString("cellDataPopup.cancel"));
-      cancelButton.addActionListener(new ActionListener()
-      {
-         public void actionPerformed(ActionEvent event)
-         {
-            CellDataColumnDataPopupPanel.this._parentFrame.setVisible(false);
-            CellDataColumnDataPopupPanel.this._parentFrame.dispose();
-         }
-      });
+      cancelButton.addActionListener(event -> onCancel());
 
       // add buttons to button panel
       updateControls.add(updateButton);
@@ -122,6 +69,44 @@ class CellDataColumnDataPopupPanel extends JPanel
 
       return panel;
    }
+
+   private void onCancel()
+   {
+      closeFrame();
+   }
+
+
+   private void onUpdate()
+   {
+      // try to convert the text in the popup into a valid
+      // instance of type of data object being held in the table cell
+      StringBuffer messageBuffer = new StringBuffer();
+      Object newValue = _ioPanel.getObject(messageBuffer);
+      if (messageBuffer.length() > 0)
+      {
+         // handle an error in conversion of text to object
+         JOptionPane.showMessageDialog(
+               CellDataColumnDataPopupPanel.this,
+               s_stringMgr.getString("cellDataPopUp.cannnotBGeConverted", messageBuffer),
+               // i18n[cellDataPopUp.conversionError=Conversion Error]
+               s_stringMgr.getString("cellDataPopUp.conversionError"),
+               JOptionPane.ERROR_MESSAGE);
+
+         _ioPanel.requestFocus();
+      }
+      else
+      {
+         _table.setValueAt(newValue, _row, _col);
+         closeFrame();
+      }
+   }
+
+   private void closeFrame()
+   {
+      _parentFrame.setVisible(false);
+      _parentFrame.dispose();
+   }
+
 
    /*
     * Save various information which is needed to do Update & Cancel.
